d3c5ab38ea19d9276c3a48f80123fd0487b13697
Pedro Gonnet <pedro.gonnet@durham.ac.uk>
2013-05-27 23:07:03 +0000

    missed atomic_inc in space_addtask.
    Former-commit-id: 6113c0ebcb030d3db5b06c5f04751f98251d272e

--- src/space.c
+++ src/space.c
@@ -827,8 +827,7 @@ struct task *space_addtask ( struct space *s , int type , int subtype , int flag
     
     /* Add an index for it. */
     // lock_lock( &s->lock );
-    s->tasks_ind[ s->nr_tasks ] = ind;
-    s->nr_tasks += 1;
+    s->tasks_ind[ atomic_inc( s->nr_tasks ) ] = ind;
     // lock_unlock_blind( &s->lock );
     
     /* Return a pointer to the new task. */
