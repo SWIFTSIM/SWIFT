1508d5dffa8052b256d4aa0770f171577a68b7a0
Pedro Gonnet <pedro.gonnet@durham.ac.uk>
2013-09-17 13:24:27 +0000

    more specific error message.
    Former-commit-id: c9ba870826abe29d61892c61bb05ff7f8d63f675

--- src/task.c
+++ src/task.c
@@ -101,7 +101,7 @@ int task_lock ( struct task *t ) {
                 char buff[ MPI_MAX_ERROR_STRING ];
                 int len;
                 MPI_Error_string( err , buff , &len );
-                error( "Failed to test request on send/recv task (%s)." , buff );
+                error( "Failed to test request on send/recv task (tag=%i, %s)." , t->flags , buff );
                 }
             return res;
         #else
