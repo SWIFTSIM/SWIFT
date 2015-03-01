78801287d7cdcb590af1897935364e01cb501f6a
Pedro Gonnet <gonnet@google.com>
2015-03-01 22:10:17 +0100

    change the parameters for the test runs so that they actually make sense.
    Former-commit-id: efeaa6d162b72b5889560a11247cd478d196926b

--- examples/runs.sh
+++ examples/runs.sh
@@ -8,33 +8,33 @@ for cpu in {1..32}
 do
 
     # Sod-Shock runs
-    if [ ! -e SodShock_${cpu}.dump ]
+    if [ ! -e SodShock_mindt_${cpu}.dump ]
     then
-        ./test -c 1.0 -t $cpu -f sodShock.hdf5 -m 0.1 -w 5000 -d 1.0 > SodShock_${cpu}.dump
+        ./test_mindt -c 1.0 -t $cpu -f SodShock/sodShock.hdf5 -m 0.1 -w 5000 -d 1.0 > SodShock_${cpu}.dump
     fi
     if [ ! -e SodShock_fixed_${cpu}.dump ]
     then
-        ./test_fixdt -r 1000 -t $cpu -f sodShock.hdf5 -m 0.1 -w 5000 -d 1e-3 > SodShock_fixed_${cpu}.dump
+        ./test_fixdt -r 1000 -t $cpu -f SodShock/sodShock.hdf5 -m 0.0075 -w 5000 -d 1e-4 > SodShock_fixed_${cpu}.dump
     fi
     
     # Sedov blast
-    if [ ! -e SedovBlast_${cpu}.dump ]
+    if [ ! -e SedovBlast_mindt_${cpu}.dump ]
     then
-        ./test -c 0.2 -t $cpu -f sedov.hdf5 -m 1.0 -w 5000 -d 0.2 > SedovBlast_${cpu}.dump
+        ./test_mindt -c 0.2 -t $cpu -f SedovBlast/sedov.hdf5 -m 0.1 -w 5000 -d 1e-10 > SedovBlast_${cpu}.dump
     fi
     if [ ! -e SedovBlast_fixed_${cpu}.dump ]
     then
-        ./test_fixdt -r 4096 -t $cpu -f sedov.hdf5 -m 1.0 -w 5000 -d 5e-5 > SedovBlast_fixed_${cpu}.dump
+        ./test_fixdt -r 4096 -t $cpu -f SedovBlast/sedov.hdf5 -m 0.1 -w 5000 -d 5e-5 > SedovBlast_fixed_${cpu}.dump
     fi
     
     # Cosmological volume
-    if [ ! -e CosmoVolume_${cpu}.dump ]
+    if [ ! -e CosmoVolume_mindt_${cpu}.dump ]
     then
-        ./test -c 0.01 -t $cpu -f cosmoVolume.hdf5 -m 0.6 -w 5000 -d 0.01 > CosmoVolume_${cpu}.dump
+        ./test_mindt -c 0.01 -t $cpu -f CosmoVolume/cosmoVolume.hdf5 -m 0.6 -w 5000 -d 0.01 > CosmoVolume_${cpu}.dump
     fi
     if [ ! -e CosmoVolume_fixed_${cpu}.dump ]
     then
-        ./test_fixdt -r 256 -t $cpu -f cosmoVolume.hdf5 -m 0.6 -w 5000 -d 1e-8 > CosmoVolume_fixed_${cpu}.dump
+        ./test_fixdt -r 256 -t $cpu -f CosmoVolume/cosmoVolume.hdf5 -m 0.6 -w 5000 -d 1e-8 > CosmoVolume_fixed_${cpu}.dump
     fi
 
 done
