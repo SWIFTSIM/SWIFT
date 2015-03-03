2f28ad21cdcaf79ad37fd728f52cbf33b839e0a5
Peter W. Draper <p.w.draper@durham.ac.uk>
2015-03-03 15:44:42 +0000

    Remove generated libtool support files (these are recovered by automake). Make GPL V3 licensing explicit. Update autogen.sh script to use autoreconf, that is all that is necessary.
    Former-commit-id: f67db1f4c85f1943d4e7190a8e65c7e6b86084ac

--- autogen.sh
+++ autogen.sh
@@ -1,34 +1,9 @@
 #! /bin/sh
-# autogen.sh
 
-# This file is part of GadgetSMP.
-# Coypright (c) 2012 pedro.gonnet@durham.ac.uk.
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  Update generated configuration files, i.e. do work so that a
+#  developer checkout can be configured.
 
-# make libtool and stuff
-libtoolize --force --copy
+autoreconf --install --symlink 
 
-# run aclocal
-aclocal -I m4/
-
-# run autoconf
-autoconf -I m4
-
-# run autoheader to generate config.h.in
-autoheader -I m4
-
-# run automake
-automake --add-missing
+exit
 
