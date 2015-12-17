27a98593572dbecee86a5916000947e567e37564
Peter W. Draper <p.w.draper@durham.ac.uk>
2015-12-17 15:20:15 +0000

    Don't plot all ranks at once, provide command-line options to pick one

--- examples/plot_tasks_MPI.py
+++ examples/plot_tasks_MPI.py
@@ -23,64 +23,73 @@
 
 
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 import pylab as pl
 import numpy as np
 import sys
 
+#  Handle command-line.
+if len( sys.argv ) != 4:
+    print "Usage: ", sys.argv[0], "input.dat rank output.png"
+    sys.exit(1)
+
+infile = sys.argv[1]
+rank = int(sys.argv[2])
+outpng = sys.argv[3]
+
+#  CPU ticks per second.
 CPU_CLOCK = 2.7e9
 
-params = {'axes.labelsize': 10,
-          'axes.titlesize': 10,
-          'font.size': 12,
-          'legend.fontsize': 12,
-          'xtick.labelsize': 10,
-          'ytick.labelsize': 10,
-          'text.usetex': False,
-          'figure.figsize' : (12., 4.),
-          'figure.subplot.left' : 0.03,
-          'figure.subplot.right' : 0.995,
-          'figure.subplot.bottom' : 0.1,
-          'figure.subplot.top' : 0.99,
-          'figure.subplot.wspace' : 0.,
-          'figure.subplot.hspace' : 0.,
-          'lines.markersize' : 6,
-          'lines.linewidth' : 3.,
-          'text.latex.unicode': True
+params = {"axes.labelsize": 10,
+          "axes.titlesize": 10,
+          "font.size": 12,
+          "legend.fontsize": 12,
+          "xtick.labelsize": 10,
+          "ytick.labelsize": 10,
+          "figure.figsize" : (12., 4.),
+          "figure.subplot.left" : 0.03,
+          "figure.subplot.right" : 0.995,
+          "figure.subplot.bottom" : 0.1,
+          "figure.subplot.top" : 0.99,
+          "figure.subplot.wspace" : 0.,
+          "figure.subplot.hspace" : 0.,
+          "lines.markersize" : 6,
+          "lines.linewidth" : 3.
 }
 pl.rcParams.update(params)
-#pl.rc('font',**{'family':'sans-serif','sans-serif':['Times']})
 
-types = {"0": "task_none",
-         "1": "task_sort",
-         "2": "task_self",
-         "3": "task_pair",
-         "4": "task_sub",
-         "5": "task_ghost",
-         "6": "task_kick1",
-         "7": "task_kick2",
-         "8": "task_send",
-         "9": "task_recv",
-         "10": "task_grav_pp",
-         "11": "task_grav_mm",
-         "12": "task_grav_up",
-         "13": "task_grav_down",
-         "14": "task_psort",
-         "15": "task_split_cell",
-         "16": "task_rewait",
-         "17": "task_count"}
+types = {"0": "none",
+         "1": "sort",
+         "2": "self",
+         "3": "pair",
+         "4": "sub",
+         "5": "ghost",
+         "6": "kick1",
+         "7": "kick2",
+         "8": "send",
+         "9": "recv",
+         "10": "grav_pp",
+         "11": "grav_mm",
+         "12": "grav_up",
+         "13": "grav_down",
+         "14": "psort",
+         "15": "split_cell",
+         "16": "rewait",
+         "17": "count"}
 
-subtypes = {"0": "subtask_none",
-            "1": "subtask_density",
-            "2": "subtask_force",
-            "3": "subtask_grav",
-            "4": "subtask_count"}
+subtypes = {"0": "none",
+            "1": "density",
+            "2": "force",
+            "3": "grav",
+            "4": "count"}
+
 
 # Assign colours for all types.
-colours = ["red","blue","green","yellow","cyan","magenta","black"]
-colours = colours + list(matplotlib.colors.cnames)
-index = 0
+#colours = ["blue","green","red","cyan","magenta","yellow","black"]
+#colours = colours + list(matplotlib.colors.cnames)
+colours = list(matplotlib.colors.cnames)
 
+index = 0
 subtypecolours = {}
 for key in subtypes:
     subtypecolours[subtypes[key]] = colours[index]
@@ -93,87 +102,94 @@ for key in types:
     print types[key], " = ", colours[index]
     index = index + 1
 
-
-data = pl.loadtxt("thread_info_MPI.dat")
+#  Read input.
+data = pl.loadtxt( infile )
 
 nranks = int(max(data[:,0])) + 1
 print "Number of ranks:", nranks
+if rank < 0 or rank >= nranks:
+    print "Cannot read rank: ", rank, ", out of range"
+    sys.exit(1)
 
 nthread = int(max(data[:,1])) + 1
 print "Number of threads:", nthread
 
 tasks = {}
 tasks[-1] = []
-for i in range(nthread*nranks):
+for i in range(nthread):
     tasks[i] = []
 
-# Start and end times, avoiding embarrassing zeros.
-tics = data[:,5]
-tics = tics[tics != 0]
-start_t = tics.min()
-end_t = max(data[:,6])
-data[:,5] -= start_t
-data[:,6] -= start_t
+# Pick data for our rank, and avoid start and end times of zero.
+sdata = data[data[:,5] != 0]
+sdata = sdata[sdata[:,6] != 0]
+sdata = sdata[sdata[:,0] == rank]
 
-num_lines = pl.size(data) / 10
+start_t = min(sdata[:,5])
+end_t = max(sdata[:,6])
+sdata[:,5] -= start_t
+sdata[:,6] -= start_t
+
+#np.savetxt("dump.txt", sdata, fmt="%d")
+
+num_lines = pl.size(sdata) / 10
 for line in range(num_lines):
-    tic = int(data[line,5]) / CPU_CLOCK * 1000
-    toc = int(data[line,6]) / CPU_CLOCK * 1000
-    if tic > 0 and toc > 0:
-        rank = int(data[line,0])
-        thread = int(data[line,1])
-        index = rank*nthread + thread
-        tasks[index].append({})
-        tasks[index][-1]["type"] = types[ str(int(data[line,2])) ]
-        tasks[index][-1]["subtype"] = subtypes[str(int(data[line,3]))]
-        tasks[index][-1]["tic"] = tic
-        tasks[index][-1]["toc"] = toc
-        tasks[index][-1]["t"] = (toc + tic)/ 2
+    thread = int(sdata[line,1])
+    tasks[thread].append({})
+    tasks[thread][-1]["type"] = types[str(int(sdata[line,2]))]
+    tasks[thread][-1]["subtype"] = subtypes[str(int(sdata[line,3]))]
+    tic = int(sdata[line,5]) / CPU_CLOCK * 1000
+    toc = int(sdata[line,6]) / CPU_CLOCK * 1000
+    tasks[thread][-1]["tic"] = tic
+    tasks[thread][-1]["toc"] = toc
+    tasks[thread][-1]["t"] = (toc + tic)/ 2
 
 combtasks = {}
 combtasks[-1] = []
-for i in range(nthread*nranks):
+for i in range(nthread):
     combtasks[i] = []
 
-for index in range(nthread*nranks):
-    tasks[index] = sorted(tasks[index], key=lambda l: l["t"])
+for thread in range(nthread):
+    tasks[thread] = sorted(tasks[thread], key=lambda l: l["t"])
     lasttype = ""
     types = []
-    for task in tasks[index]:
+    for task in tasks[thread]:
         if task["type"] not in types:
             types.append(task["type"])
         if lasttype == "" or not lasttype == task["type"]:
-            combtasks[index].append({})
-            combtasks[index][-1]["type"] = task["type"]
-            combtasks[index][-1]["subtype"] = task["subtype"]
-            combtasks[index][-1]["tic"] = task["tic"]
-            combtasks[index][-1]["toc"] = task["toc"]
-            if task["type"] == 'task_self' or task["type"] == 'task_pair' or task["type"] == 'task_sub':
-                combtasks[index][-1]["colour"] = subtypecolours[task["subtype"]]
+            combtasks[thread].append({})
+            combtasks[thread][-1]["type"] = task["type"]
+            combtasks[thread][-1]["subtype"] = task["subtype"]
+            combtasks[thread][-1]["tic"] = task["tic"]
+            combtasks[thread][-1]["toc"] = task["toc"]
+            if task["type"] == "self" or task["type"] == "pair" or task["type"] == "sub":
+                combtasks[thread][-1]["colour"] = subtypecolours[task["subtype"]]
             else:
-                combtasks[index][-1]["colour"] = taskcolours[task["type"]]
+                combtasks[thread][-1]["colour"] = taskcolours[task["type"]]
             lasttype = task["type"]
         else:
-            combtasks[index][-1]["toc"] = task["toc"]
+            combtasks[thread][-1]["toc"] = task["toc"]
 
 typesseen = []
-for i in range(nthread*nranks):
+for i in range(nthread):
     for task in combtasks[i]:
         pl.fill_between([task["tic"], task["toc"]], i+0.05, i+0.95,
-                        facecolor=task["colour"])
-        qtask = task["type"] + "/" + task["subtype"]
+                        facecolor=task["colour"], linewidths=0)
+        if task["subtype"] != "none":
+            qtask = task["type"] + "/" + task["subtype"]
+        else:
+            qtask = task["type"]
         if qtask not in typesseen:
-            pl.plot([], [], color=task["colour"], label=qtask)
-            typesseen.append(qtask)
+             pl.plot([], [], color=task["colour"], label=qtask)
+             typesseen.append(qtask)
 
 #  Legend and room for it.
-pl.fill_between([0, 0], nthread*nranks+0.05, nthread*nranks+0.95, facecolor="white")
+pl.fill_between([0, 0], nthread, nthread+len(typesseen)/3, facecolor="white")
 pl.legend(loc=1, shadow=True, mode="expand", ncol=3)
 
 pl.xlabel("Wall clock time [ms]")
 pl.xlim(0, (end_t - start_t)*1.03 * 1000 / CPU_CLOCK)
-pl.ylabel("Thread ID*MPI Rank")
-pl.yticks(pl.array(range(nthread*nranks)) + 0.5, pl.array(range(nthread*nranks)))
+pl.ylabel("Thread ID for MPI Rank " + str(rank) )
+pl.yticks(pl.array(range(nthread)) + 0.5, pl.array(range(nthread)))
 
 pl.show()
-pl.savefig("task_graph_MPI.png")
+pl.savefig(outpng)
