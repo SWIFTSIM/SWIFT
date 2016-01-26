b56f617965ded0182ff6a02e236557110860f3f6
Peter W. Draper <p.w.draper@durham.ac.uk>
2016-01-26 11:22:18 +0000

    Use single swift command instead of the old _fixdt and _mindt

--- examples/runs.sh
+++ examples/runs.sh
@@ -32,33 +32,21 @@ for cpu in {1..32}
 do
 
     # Sod-Shock runs
-    if [ ! -e SodShock_mindt_${cpu}.dump ]
+    if [ ! -e SodShock_${cpu}.dump ]
     then
-        ./swift_mindt -c 1.0 -t $cpu -f SodShock/sodShock.hdf5 -m 0.01 -w 5000 -d 1.0 > SodShock_${cpu}.dump
-    fi
-    if [ ! -e SodShock_fixed_${cpu}.dump ]
-    then
-        ./swift_fixdt -r 1000 -t $cpu -f SodShock/sodShock.hdf5 -m 0.01 -w 5000 -d 1e-4 > SodShock_fixed_${cpu}.dump
+        ./swift -r 1000 -t $cpu -f SodShock/sodShock.hdf5 -m 0.01 -w 5000 -d 1e-4 > SodShock_fixed_${cpu}.dump
     fi
     
     # Sedov blast
-    if [ ! -e SedovBlast_mindt_${cpu}.dump ]
+    if [ ! -e SedovBlast_${cpu}.dump ]
     then
-        ./swift_mindt -c 0.2 -t $cpu -f SedovBlast/sedov.hdf5 -m 0.02 -w 5000 -d 1e-10 > SedovBlast_${cpu}.dump
-    fi
-    if [ ! -e SedovBlast_fixed_${cpu}.dump ]
-    then
-        ./swift_fixdt -r 4096 -t $cpu -f SedovBlast/sedov.hdf5 -m 0.02 -w 5000 -d 5e-5 > SedovBlast_fixed_${cpu}.dump
+        ./swift -r 4096 -t $cpu -f SedovBlast/sedov.hdf5 -m 0.02 -w 5000 -d 5e-5 > SedovBlast_fixed_${cpu}.dump
     fi
     
     # Cosmological volume
-    if [ ! -e CosmoVolume_mindt_${cpu}.dump ]
+    if [ ! -e CosmoVolume_${cpu}.dump ]
     then
-        ./swift_mindt -c 0.01 -t $cpu -f CosmoVolume/cosmoVolume.hdf5 -m 0.6 -w 5000 -d 0.01 > CosmoVolume_${cpu}.dump
-    fi
-    if [ ! -e CosmoVolume_fixed_${cpu}.dump ]
-    then
-        ./swift_fixdt -r 256 -t $cpu -f CosmoVolume/cosmoVolume.hdf5 -m 0.6 -w 5000 -d 1e-8 > CosmoVolume_fixed_${cpu}.dump
+        ./swift -r 256 -t $cpu -f CosmoVolume/cosmoVolume.hdf5 -m 0.6 -w 5000 -d 1e-8 > CosmoVolume_fixed_${cpu}.dump
     fi
 
 done
