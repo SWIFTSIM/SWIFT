ee114579fd50b24c60262ae993a1bf41a4d91460
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-01-29 00:54:57 +0000

    Same treatment for the gravity core routines

--- src/gravity.h
+++ src/gravity.h
@@ -24,6 +24,6 @@
 /* So far only one model here */
 /* Straight-forward import */
 #include "./gravity/Default/gravity.h"
-#include "runner_iact_grav.h"
+#include "./gravity/Default/runner_iact_grav.h"
 
 #endif
