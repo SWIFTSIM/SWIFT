a88726d21eafce231abb1c223cef37f9477ae672
James Willis <james.s.willis@durham.ac.uk>
2016-02-04 11:37:46 +0000

    Can parse basic file, store results to struct as strings and retrieve parameters by parsing strings held with struct.

--- src/testParser.c
+++ src/testParser.c
@@ -6,8 +6,20 @@ int main(int argc, char *argv[]) {
 
   const char * input_file = argv[1];
   struct swift_params param_file;
- 
-  parseFile(&param_file,input_file);
+  int no_of_threads = 0;
+  int no_of_time_steps = 0;
+  float max_h = 0.0f;
+  char ic_file [MAX_LINE_SIZE];
+
+  parseFile(input_file,&param_file);
 
   printParameters(&param_file);
+  
+  getParamInt(&param_file,"no_of_threads",&no_of_threads);
+  getParamInt(&param_file,"no_of_time_steps",&no_of_time_steps);
+  getParamFloat(&param_file,"max_h",&max_h);
+  getParamString(&param_file,"ic_file",ic_file);
+  
+  printf("no_of_threads: %d, no_of_time_steps: %d, max_h: %f, ic_file: %s\n",no_of_threads, no_of_time_steps, max_h, ic_file);
+
 }
