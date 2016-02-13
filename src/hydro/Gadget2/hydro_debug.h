c0c4a315570ce1bb77cbfbf8c74979eab804ce84
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-02-13 13:46:35 +0000

    More particle information printed when debugging

--- src/hydro/Gadget2/hydro_debug.h
+++ src/hydro/Gadget2/hydro_debug.h
@@ -26,10 +26,11 @@ __attribute__((always_inline))
       "wcount=%d, m=%.3e, dh_drho=%.3e, rho=%.3e, P=%.3e, S=%.3e, "
       "dS/dt=%.3e,\n"
       "divV=%.3e, curlV=%.3e, rotV=[%.3e,%.3e,%.3e]  \n "
-      "v_sig=%e t_begin=%.3e, t_end=%.3e\n",
+      "v_sig=%e dh/dt=%.3e t_begin=%.3e, t_end=%.3e\n",
       p->x[0], p->x[1], p->x[2], p->v[0], p->v[1], p->v[2], xp->v_full[0],
       xp->v_full[1], xp->v_full[2], p->a_hydro[0], p->a_hydro[1], p->a_hydro[2],
       p->h, (int)p->density.wcount, p->mass, p->rho_dh, p->rho, p->force.pressure,
       p->entropy, p->force.entropy_dt, p->div_v, p->force.curl_v, p->density.rot_v[0],
-      p->density.rot_v[1], p->density.rot_v[2], p->force.v_sig, p->t_begin, p->t_end);
+      p->density.rot_v[1], p->density.rot_v[2], p->force.v_sig, p->force.h_dt,
+      p->t_begin, p->t_end);
 }
