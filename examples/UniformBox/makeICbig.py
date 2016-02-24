7c7a0aee8c308eef462e7868393219f1bc57d16e
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-02-24 17:37:52 +0000

    New script to generate really really large uniform boxes

--- examples/UniformBox/makeICbig.py
+++ examples/UniformBox/makeICbig.py
@@ -1,7 +1,6 @@
 ###############################################################################
  # This file is part of SWIFT.
- # Copyright (c) 2013 Pedro Gonnet (pedro.gonnet@durham.ac.uk),
- #                    Matthieu Schaller (matthieu.schaller@durham.ac.uk)
+ # Copyright (c) 2016 Matthieu Schaller (matthieu.schaller@durham.ac.uk)
  # 
  # This program is free software: you can redistribute it and/or modify
  # it under the terms of the GNU Lesser General Public License as published
@@ -29,17 +28,25 @@ from numpy import *
 periodic= 1           # 1 For periodic box
 boxSize = 1.
 L = int(sys.argv[1])  # Number of particles along one axis
+N = int(sys.argv[2])  # Write N particles at a time to avoid requiring a lot of RAM
 rho = 2.              # Density
 P = 1.                # Pressure
 gamma = 5./3.         # Gas adiabatic index
-fileName = "uniformBox.hdf5" 
+fileName = "uniformBox_%d.hdf5"%L
 
 #---------------------------------------------------
 numPart = L**3
 mass = boxSize**3 * rho / numPart
 internalEnergy = P / ((gamma - 1.)*rho)
 
-#--------------------------------------------------
+#---------------------------------------------------
+
+n_iterations = numPart / N
+remainder = numPart % N
+
+print "Writing", numPart, "in", n_iterations, "iterations of size", N, "and a remainder of", remainder
+
+#---------------------------------------------------
 
 #File
 file = h5py.File(fileName, 'w')
@@ -63,38 +70,79 @@ grp.attrs["PeriodicBoundariesOn"] = periodic
 #Particle group
 grp = file.create_group("/PartType0")
 
-v  = zeros((numPart, 3))
-ds = grp.create_dataset('Velocities', (numPart, 3), 'f')
-ds[()] = v
+# First create the arrays in the file
+ds_v = grp.create_dataset('Velocities', (numPart, 3), 'f', chunks=True, compression="gzip")
+ds_m = grp.create_dataset('Masses', (numPart,1), 'f', chunks=True, compression="gzip")
+ds_h = grp.create_dataset('SmoothingLength', (numPart,1), 'f', chunks=True, compression="gzip")
+ds_u = grp.create_dataset('InternalEnergy', (numPart,1), 'f', chunks=True, compression="gzip")
+ds_id = grp.create_dataset('ParticleIDs', (numPart, 1), 'L', chunks=True, compression="gzip")
+ds_x = grp.create_dataset('Coordinates', (numPart, 3), 'd', chunks=True, compression="gzip")
+
+# Now loop and create parts of the dataset
+offset = 0
+for n in range(n_iterations):
+    v  = zeros((N, 3))
+    ds_v[offset:offset+N,:] = v
+    v = zeros(1)
+
+    m = full((N, 1), mass)
+    ds_m[offset:offset+N] = m
+    m = zeros(1)
+
+    h = full((N, 1), 1.1255 * boxSize / L)
+    ds_h[offset:offset+N] = h
+    h = zeros(1)
+
+    u = full((N, 1), internalEnergy)
+    ds_u[offset:offset+N] = u
+    u = zeros(1)
+
+    ids = linspace(offset, offset+N, N, endpoint=False).reshape((N,1))
+    ds_id[offset:offset+N] = ids
+    x      = ids % L;
+    y      = ((ids - x) / L) % L;
+    z      = (ids - x - L * y) / L**2;
+    coords = zeros((N, 3))
+    coords[:,0] = z[:,0] * boxSize / L + boxSize / (2*L)
+    coords[:,1] = y[:,0] * boxSize / L + boxSize / (2*L)
+    coords[:,2] = x[:,0] * boxSize / L + boxSize / (2*L)
+    ds_x[offset:offset+N,:] = coords
+
+    offset += N
+    print "Done", offset,"/", numPart, "(%.1f %%)"%(100*(float)(offset)/numPart)
+
+# And now, the remainder
+v  = zeros((remainder, 3))
+ds_v[offset:offset+remainder,:] = v
 v = zeros(1)
 
-m = full((numPart, 1), mass)
-ds = grp.create_dataset('Masses', (numPart,1), 'f')
-ds[()] = m
+m = full((remainder, 1), mass)
+ds_m[offset:offset+remainder] = m
 m = zeros(1)
 
-h = full((numPart, 1), 1.1255 * boxSize / L)
-ds = grp.create_dataset('SmoothingLength', (numPart,1), 'f')
-ds[()] = h
+h = full((remainder, 1), 1.1255 * boxSize / L)
+ds_h[offset:offset+remainder] = h
 h = zeros(1)
 
-u = full((numPart, 1), internalEnergy)
-ds = grp.create_dataset('InternalEnergy', (numPart,1), 'f')
-ds[()] = u
+u = full((remainder, 1), internalEnergy)
+ds_u[offset:offset+remainder] = u
 u = zeros(1)
 
+print "Done", offset+remainder,"/", numPart
 
-ids = linspace(0, numPart, numPart, endpoint=False).reshape((numPart,1))
-ds = grp.create_dataset('ParticleIDs', (numPart, 1), 'L')
-ds[()] = ids
+ids = linspace(offset, offset+remainder, remainder, endpoint=False).reshape((remainder,1))
+ds_id[offset:offset+remainder] = ids
 x      = ids % L;
 y      = ((ids - x) / L) % L;
 z      = (ids - x - L * y) / L**2;
-coords = zeros((numPart, 3))
+coords = zeros((remainder, 3))
 coords[:,0] = z[:,0] * boxSize / L + boxSize / (2*L)
 coords[:,1] = y[:,0] * boxSize / L + boxSize / (2*L)
 coords[:,2] = x[:,0] * boxSize / L + boxSize / (2*L)
-ds = grp.create_dataset('Coordinates', (numPart, 3), 'd')
-ds[()] = coords
+ds_x[offset:offset+remainder,:] = coords
+
+
+
+
 
 file.close()
