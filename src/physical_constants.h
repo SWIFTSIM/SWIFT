50566270c45a48affd2d00822c287511471fb955
Tom Theuns <tom.theuns@durham.ac.uk>
2016-03-08 10:58:35 +0000

    added physical_constants

---
+++ src/physical_constants.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (c) 2012 Pedro Gonnet (ptcedro.gonnet@durham.ac.uk)
+ *                    Matthieu Schaller (matthieu.schaller@durham.ac.uk)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+/* physical constants in cgs */
+#define NEWTON_GRAVITY_CGS  6.672e-8f
+#define SOLAR_MASS_IN_CGS   1.989e33f
+#define PARSEC_IN_CGS       3.086e18f
+#define PROTON_MASS_IN_CGS  1.6726231e24f
+#define YEAR_IN_CGS         3.154e+7f
+
+/* Hydrodynamical constants. */
+#define const_hydro_gamma (5.0f / 3.0f)
