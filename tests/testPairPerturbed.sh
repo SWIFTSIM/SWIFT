43dc5a1b6801789b0f258dce0463c8d0c0b3cd2a
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-03-23 18:16:23 +0000

    Unified both pair tests into one single file (but still two tests)

--- tests/testPairPerturbed.sh
+++ tests/testPairPerturbed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm brute_force_perturbed.dat swift_dopair_perturbed.dat
 
-./testPairPerturbed -p 6 -r 1 -d 0.1
+./testPair -p 6 -r 1 -d 0.1 -f perturbed
 
 python difffloat.py brute_force_perturbed.dat swift_dopair_perturbed.dat 1e-5 5e-6
 
