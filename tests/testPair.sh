15daafd0ae88f9a4c85e99a8a8d55090ed0131a1
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-03-23 17:48:51 +0000

    Changed tolerance in the cell pair case

--- tests/testPair.sh
+++ tests/testPair.sh
@@ -3,6 +3,6 @@ rm brute_force.dat swift_dopair.dat
 
 ./testPair -p 6 -r 1
 
-python difffloat.py brute_force.dat swift_dopair.dat 1e-5 2e-6
+python difffloat.py brute_force.dat swift_dopair.dat 1e-5 5e-6
 
 exit $?
