7ff9b72d7f4afe51348bc362cdf78891ffa9e9b9
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-03-25 19:11:57 +0000

    Typo

--- src/space.c
+++ src/space.c
@@ -480,7 +480,7 @@ void space_rebuild(struct space *s, double cell_max, int verbose) {
     const struct gpart *const p = &s->gparts[k];
     gind[k] =
         cell_getid(cdim, p->x[0] * ih[0], p->x[1] * ih[1], p->x[2] * ih[2]);
-    cells[gind[k]].count += 1;
+    cells[gind[k]].gcount += 1;
     /* if ( cells[ ind[k] ].nodeID != nodeID )
         error( "Received part that does not belong to me (nodeID=%i)." , cells[
        ind[k] ].nodeID ); */
