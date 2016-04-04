a25314336093af078bac68db8448346a75fbbc25
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-04-04 14:13:45 +0100

    Added unit system definition to the IC generation scripts. Will be useful later on.

--- examples/SedovBlast/makeIC_fcc.py
+++ examples/SedovBlast/makeIC_fcc.py
@@ -101,6 +101,14 @@ grp.attrs["Flag_Entropy_ICs"] = 0
 grp = file.create_group("/RuntimePars")
 grp.attrs["PeriodicBoundariesOn"] = periodic
 
+#Units
+grp = file.create_group("/Units")
+grp.attrs["Unit length in cgs (U_L)"] = 1.
+grp.attrs["Unit mass in cgs (U_M)"] = 1.
+grp.attrs["Unit time in cgs (U_t)"] = 1.
+grp.attrs["Unit current in cgs (U_I)"] = 1.
+grp.attrs["Unit temperature in cgs (U_T)"] = 1.
+
 #Particle group
 grp = file.create_group("/PartType0")
 grp.create_dataset('Coordinates', data=coords, dtype='d')
