382838e3325b57b38879e858fa8a6ac31db13b1a
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-04-06 14:31:30 +0100

    Updated the run scripts to generate the ICs if they are not present. Removed the outdated global 'run.sh' script.

--- examples/SodShock/run.sh
+++ examples/SodShock/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Generate the initial conditions if they are not present.
+if [ ! -e sodShock.hdf5 ]
+then
+    echo "Generating initial conditions for the SodShock example..."
+    python makeIC.py
+fi
+
 ../swift -s sodShock.yml
