a1cc8509bec4390346479535456c3b938287b4c4
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-04-07 17:57:48 +0100

    Renamed test-case to be more descriptive

---
+++ examples/ExternalPointMass/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Generate the initial conditions if they are not present.
+if [ ! -e uniformBox.hdf5 ]
+then
+    echo "Generating initial conditions for the point mass potential box example..."
+    python makeIC.py 10000
+fi
+
+../swift -g externalPointMass.yml
