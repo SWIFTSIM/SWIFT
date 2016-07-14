214ef8a8d71cf47f2ca64402353c10dbefc7229f
Peter W. Draper <p.w.draper@durham.ac.uk>
2016-07-14 17:33:16 +0100

    Fix to use new entropy flag

--- tests/testReading.c
+++ tests/testReading.c
@@ -27,6 +27,7 @@ int main() {
 
   size_t Ngas = 0, Ngpart = 0;
   int periodic = -1;
+  int entropy = 1;
   int i, j, k, n;
   double dim[3];
   struct part *parts = NULL;
@@ -39,7 +40,7 @@ int main() {
 
   /* Read data */
   read_ic_single("input.hdf5", dim, &parts, &gparts, &Ngas, &Ngpart, &periodic,
-                 0);
+                 &entropy, 0);
 
   /* Check global properties read are correct */
   assert(dim[0] == boxSize);
