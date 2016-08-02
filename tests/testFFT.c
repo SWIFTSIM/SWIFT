8a7620ec6d35cd19c700b14167e8359278464126
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-08-02 15:01:03 +0100

    Only run the FFT test when we have the FFTW3 library

--- tests/testFFT.c
+++ tests/testFFT.c
@@ -21,6 +21,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef HAVE_FFTW
+
+int main() {
+
+  return 0;
+}
+
+#else
+
 #include <fftw3.h>
 
 /* Includes. */
@@ -187,3 +196,5 @@ int main() {
   free(gparts);
   return 0;
 }
+
+#endif
