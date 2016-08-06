beea6ec92ad0b3a8ab6c3a38b528ff0a03dbe6d2
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-08-06 12:10:34 +0100

    Updated the task plotting script to include the new gravity tasks

--- src/task.c
+++ src/task.c
@@ -47,14 +47,14 @@
 
 /* Task type names. */
 const char *taskID_names[task_type_count] = {
-    "none",    "sort",          "self",          "pair",       "sub",
-    "init",    "ghost",         "drift",         "kick",       "kick_fixdt",
-    "send",    "recv",          "grav_gather_m", "grav_fft",   "grav_mm",
-    "grav_up", "grav_external", "part_sort",     "gpart_sort", "split_cell",
-    "rewait"};
+    "none",       "sort",    "self",          "pair",          "sub_self",
+    "sub_pair",   "init",    "ghost",         "drift",         "kick",
+    "kick_fixdt", "send",    "recv",          "grav_gather_m", "grav_fft",
+    "grav_mm",    "grav_up", "grav_external", "part_sort",     "gpart_sort",
+    "split_cell", "rewait"};
 
-const char *subtaskID_names[task_type_count] = {"none", "density", "force",
-                                                "grav"};
+const char *subtaskID_names[task_subtype_count] = {"none", "density", "force",
+                                                   "grav", "tend"};
 
 /**
  * @brief Computes the overlap between the parts array of two given cells.
