a3ba56c914313f7f7c1f52d5418ff2efe62c1a7b
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-08-22 19:58:41 +0100

    Updated task plotting script for the new gradient loop

--- examples/plot_tasks_MPI.py
+++ examples/plot_tasks_MPI.py
@@ -62,9 +62,8 @@ pl.rcParams.update(PLOT_PARAMS)
 
 #  Tasks and subtypes. Indexed as in tasks.h.
 TASKTYPES = ["none", "sort", "self", "pair", "sub_self", "sub_pair", "init", "ghost",
-             "drift", "kick", "kick_fixdt", "send", "recv", "grav_gather_m", "grav_fft",
-             "grav_mm", "grav_up", "grav_external", "part_sort", "gpart_sort",
-             "split_cell", "rewait", "count"]
+             "kick", "kick_fixdt", "send", "recv", "grav_gather_m", "grav_fft",
+             "grav_mm", "grav_up", "grav_external", "count"]
 
 TASKCOLOURS = {"none": "black",
                "sort": "lightblue",
@@ -74,7 +73,6 @@ TASKCOLOURS = {"none": "black",
                "sub_pair": "navy",
                "init": "indigo",
                "ghost": "cyan",
-               "drift": "maroon",
                "kick": "green",
                "kick_fixdt": "green",
                "send": "yellow",
@@ -84,20 +82,17 @@ TASKCOLOURS = {"none": "black",
                "grav_mm": "mediumturquoise",
                "grav_up": "mediumvioletred",
                "grav_external": "darkred",
-               "part_sort": "steelblue",
-               "gpart_sort": "teal" ,
-               "split_cell": "seagreen",
-               "rewait": "olive",
                "count": "powerblue"}
 
-SUBTYPES = ["none", "density", "force", "grav", "tend", "count"]
+SUBTYPES = ["none", "density", "gradient", "force", "grav", "tend", "count"]
 
 SUBCOLOURS = {"none": "black",
               "density": "red",
+              "gradient": "powerblue",
               "force": "blue",
               "grav": "indigo",
               "tend": "grey",
-              "count": "purple"}
+              "count": "black"}
 
 #  Show docs if help is requested.
 if len( sys.argv ) == 2 and ( sys.argv[1][0:2] == "-h" or sys.argv[1][0:3] == "--h" ):
