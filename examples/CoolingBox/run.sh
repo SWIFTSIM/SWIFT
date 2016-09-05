512d266f0312b228dfcb122f2dfba3bbeef11287
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-09-05 15:33:35 +0100

    Made the cooling box run script actually run the code with cooling

--- examples/CoolingBox/run.sh
+++ examples/CoolingBox/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Generate the initial conditions if they are not present.
-echo "Generating initial conditions for the uniform box example..."
+echo "Generating initial conditions for the cooling box example..."
 
 python makeIC.py 10
 
-../swift -s -t 16 uniformBox.yml
+../swift -s -t 1 coolingBox.yml -C
 
 python energy_plot.py 0
 
