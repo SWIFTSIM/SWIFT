c0c0ddae75fc26e6baed62b203636f6cb6a31602
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2016-09-08 14:18:31 +0100

    Renamed 'E_rad' to 'E_radcool' in the energy stastistics file.

--- src/engine.c
+++ src/engine.c
@@ -3365,7 +3365,7 @@ void engine_init(struct engine *e, struct space *s,
     fprintf(e->file_stats,
             "#%14s %14s %14s %14s %14s %14s %14s %14s %14s %14s %14s %14s %14s "
             "%14s\n",
-            "Time", "Mass", "E_tot", "E_kin", "E_int", "E_pot", "E_rad",
+            "Time", "Mass", "E_tot", "E_kin", "E_int", "E_pot", "E_radcool",
             "Entropy", "p_x", "p_y", "p_z", "ang_x", "ang_y", "ang_z");
     fflush(e->file_stats);
 
