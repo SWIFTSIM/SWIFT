9d79c7f9cb7c39f044786c869c214a65d5b5c3ee
Stefan Arridge <stefan.arridge@durham.ac.uk>
2016-09-15 14:35:05 +0100

    Added python script to plot the radial density profiles for each snapshot in HydrostaticHalo

--- examples/HydrostaticHalo/run.sh
+++ examples/HydrostaticHalo/run.sh
@@ -2,6 +2,8 @@
 
 # Generate the initial conditions if they are not present.
 echo "Generating initial conditions for the isothermal potential box example..."
-python makeIC.py 1000 
+python makeIC.py 3000 
 
 ../swift -g -s -t 16 hydrostatic.yml 2>&1 | tee output.log
+
+python radial_profile.py 10
