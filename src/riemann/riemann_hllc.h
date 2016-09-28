5362afd6172ff35e74135cf67833930151b0f4a6
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2016-09-28 12:19:40 +0100

    Added an exact isothermal Riemann solver.

--- src/riemann/riemann_hllc.h
+++ src/riemann/riemann_hllc.h
@@ -24,6 +24,11 @@
 #include "minmax.h"
 #include "riemann_vacuum.h"
 
+#ifndef EOS_IDEAL_GAS
+#error \
+    "The HLLC Riemann solver currently only supports and ideal gas equation of state. Either select this equation of state, or try using another Riemann solver!"
+#endif
+
 __attribute__((always_inline)) INLINE static void riemann_solve_for_flux(
     float *WL, float *WR, float *n, float *vij, float *totflux) {
 
