cc2dfa53ae9a3ce4fd7e20f0ab54ee484a6e0f31
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-02-07 12:30:28 +0000

    Removed unnecessary lines in script to generate ICs for external point mass example.

--- examples/ExternalPointMass/run.sh
+++ examples/ExternalPointMass/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate the initial conditions if they are not present.
-if [ ! -e Sphere.hdf5 ]
+if [ ! -e PointMass.hdf5 ]
 then
     echo "Generating initial conditions for the point mass potential box example..."
     python makeIC.py 10000
