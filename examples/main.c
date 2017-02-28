3046b2953651a00e96b58e1a96d1d04aa476dd31
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-02-28 10:40:46 +0000

    Updated README and help message to account for latest changes.

--- examples/main.c
+++ examples/main.c
@@ -57,9 +57,9 @@ void print_help_message() {
   printf("       swift_mpi [OPTION]... PARAMFILE\n\n");
 
   printf("Valid options are:\n");
-  printf("  %2s %8s %s\n", "-a", "", "Pin runners using processor affinity");
-  printf("  %2s %8s %s\n", "-c", "", "Run with cosmological time integration");
-  printf("  %2s %8s %s\n", "-C", "", "Run with cooling");
+  printf("  %2s %8s %s\n", "-a", "", "Pin runners using processor affinity.");
+  printf("  %2s %8s %s\n", "-c", "", "Run with cosmological time integration.");
+  printf("  %2s %8s %s\n", "-C", "", "Run with cooling.");
   printf(
       "  %2s %8s %s\n", "-d", "",
       "Dry run. Read the parameter file, allocate memory but does not read ");
@@ -70,29 +70,32 @@ void print_help_message() {
          "Allows user to check validy of parameter and IC files as well as "
          "memory limits.");
   printf("  %2s %8s %s\n", "-D", "",
-         "Always drift all particles even the ones far from active particles.");
+         "Always drift all particles even the ones far from active particles. "
+         "This emulates");
+  printf("  %2s %8s %s\n", "", "",
+         "Gadget-[23] and GIZMO's default behaviours.");
   printf("  %2s %8s %s\n", "-e", "",
-         "Enable floating-point exceptions (debugging mode)");
+         "Enable floating-point exceptions (debugging mode).");
   printf("  %2s %8s %s\n", "-f", "{int}",
-         "Overwrite the CPU frequency (Hz) to be used for time measurements");
+         "Overwrite the CPU frequency (Hz) to be used for time measurements.");
   printf("  %2s %8s %s\n", "-g", "",
-         "Run with an external gravitational potential");
-  printf("  %2s %8s %s\n", "-F", "", "Run with feedback ");
-  printf("  %2s %8s %s\n", "-G", "", "Run with self-gravity");
+         "Run with an external gravitational potential.");
+  printf("  %2s %8s %s\n", "-F", "", "Run with feedback.");
+  printf("  %2s %8s %s\n", "-G", "", "Run with self-gravity.");
   printf("  %2s %8s %s\n", "-n", "{int}",
          "Execute a fixed number of time steps. When unset use the time_end "
          "parameter to stop.");
-  printf("  %2s %8s %s\n", "-s", "", "Run with SPH");
-  printf("  %2s %8s %s\n", "-S", "", "Run with stars");
+  printf("  %2s %8s %s\n", "-s", "", "Run with hydrodynamics.");
+  printf("  %2s %8s %s\n", "-S", "", "Run with stars.");
   printf("  %2s %8s %s\n", "-t", "{int}",
          "The number of threads to use on each MPI rank. Defaults to 1 if not "
          "specified.");
-  printf("  %2s %8s %s\n", "-v", "[12]", "Increase the level of verbosity");
+  printf("  %2s %8s %s\n", "-v", "[12]", "Increase the level of verbosity.");
   printf("  %2s %8s %s\n", "", "", "1: MPI-rank 0 writes ");
   printf("  %2s %8s %s\n", "", "", "2: All MPI-ranks write");
   printf("  %2s %8s %s\n", "-y", "{int}",
-         "Time-step frequency at which task graphs are dumped");
-  printf("  %2s %8s %s\n", "-h", "", "Print this help message and exit");
+         "Time-step frequency at which task graphs are dumped.");
+  printf("  %2s %8s %s\n", "-h", "", "Print this help message and exit.");
   printf(
       "\nSee the file parameter_example.yml for an example of "
       "parameter file.\n");
