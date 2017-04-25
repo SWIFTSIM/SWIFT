df642d3fde99a4482d557d1669db90145b700585
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-04-25 21:40:24 +0100

    Added more documentation of the softened FMM scheme

--- theory/Multipoles/potential.py
+++ theory/Multipoles/potential.py
@@ -20,7 +20,6 @@ import matplotlib
 matplotlib.use("Agg")
 from pylab import *
 from scipy import integrate
-import distinct_colours as colours
 from scipy.optimize import curve_fit
 from scipy.optimize import fsolve
 from matplotlib.font_manager import FontProperties
@@ -29,15 +28,15 @@ import math
 
 params = {'axes.labelsize': 9,
 'axes.titlesize': 10,
-'font.size': 12,
+'font.size': 10,
 'legend.fontsize': 10,
-'xtick.labelsize': 9,
-'ytick.labelsize': 9,
+'xtick.labelsize': 8,
+'ytick.labelsize': 8,
 'text.usetex': True,
 'figure.figsize' : (3.15,3.15),
 'figure.subplot.left'    : 0.115,
 'figure.subplot.right'   : 0.99  ,
-'figure.subplot.bottom'  : 0.08  ,
+'figure.subplot.bottom'  : 0.065  ,
 'figure.subplot.top'     : 0.99  ,
 'figure.subplot.wspace'  : 0.  ,
 'figure.subplot.hspace'  : 0.  ,
@@ -112,52 +111,59 @@ for i in range(np.size(r)):
         F_gadget2[i] = u[i] * (10.666667 + u[i]**2 * (32. * u[i] - 38.4)) / epsilon_gadget**2
 
 figure()
+colors=['#4477AA', '#CC6677', '#DDCC77', '#117733']
+
+# Density
+subplot(311)
+plot(r, W_newton, '--', lw=1.4, label="${\\rm Newtonian}$", color=colors[0])
+plot(r, W_plummer, ':', lw=1.4, label="${\\rm Plummer}$", color=colors[1])
+plot(r, W_gadget2, '-', lw=1.4, label="${\\rm Gadget}$", color=colors[2])
+plot(r, W, '-', lw=1.4, label="${\\rm SWIFT}$", color=colors[3])
+plot([epsilon, epsilon], [0, 10], 'k-', alpha=0.5, lw=0.5)
+plot([epsilon/plummer_equivalent_factor, epsilon/plummer_equivalent_factor], [0, 10], 'k-', alpha=0.5, lw=0.5)
+
+legend(loc="upper right", frameon=True, handletextpad=0.1, handlelength=3.2, fontsize=8)
+
+xlim(0,r_max_plot)
+xticks([0., 0.5, 1., 1.5, 2., 2.5], ["", "", "", "", "", ""])
+
+ylim(0., 0.84)
+yticks([0, 0.2, 0.4, 0.6, 0.8], ["$0$", "$0.2$", "$0.4$", "$0.6$", "$0.8$"])
+ylabel("$\\rho(r)$", labelpad=2)
 
 # Potential
-subplot(311)
-plot(r, phi_newton, 'g--', lw=0.8, label="${\\rm Newtonian}$")
-plot(r, phi_plummer, 'b:', lw=0.8, label="${\\rm Plummer}$")
-plot(r, phi_gadget2, 'm-.', lw=0.8, label="${\\rm Gadget-2}$")
-plot(r, phi, 'r-', lw=0.8, label="${\\rm SWIFT}$")
+subplot(312)
+plot(r, phi_newton, '--', lw=1.4, label="${\\rm Newtonian}$", color=colors[0])
+plot(r, phi_plummer, ':', lw=1.4, label="${\\rm Plummer}$", color=colors[1])
+plot(r, phi_gadget2, '-', lw=1.4, label="${\\rm Gadget}$", color=colors[2])
+plot(r, phi, '-', lw=1.4, label="${\\rm SWIFT}$", color=colors[3])
 plot([epsilon, epsilon], [-10, 10], 'k-', alpha=0.5, lw=0.5)
-#text(epsilon+0.05, 2., "$\\epsilon$", color='k', alpha=0.5, rotation=90, va="top", ha="left", fontsize=8) 
-text(1.2, 1.6, "$\\epsilon_{\\rm{Plummer}} = \\frac{\\epsilon}{%d}$"%plummer_equivalent_factor, color='k', fontsize=8, backgroundcolor='w')
-ylim(0, 2.1)
-ylabel("$|\\phi(r)|$", labelpad=0)
+plot([epsilon/plummer_equivalent_factor, epsilon/plummer_equivalent_factor], [0, 10], 'k-', alpha=0.5, lw=0.5)
 
+ylim(0, 2.3)
+ylabel("$|\\phi(r)|$", labelpad=1)
+#yticks([0., 0.5, 1., 1.5, 2., 2.5], ["$%.1f$"%(0.*epsilon), "$%.1f$"%(0.5*epsilon), "$%.1f$"%(1.*epsilon), "$%.1f$"%(1.5*epsilon), "$%.1f$"%(2.*epsilon)])
 
 xlim(0,r_max_plot)
 xticks([0., 0.5, 1., 1.5, 2., 2.5], ["", "", "", "", "", ""])
 
 # Force
-subplot(312)
-plot(r, F_newton, 'g--', lw=0.8)
-plot(r, F_plummer, 'b:', lw=0.8)
-plot(r, F_gadget2, 'm-.', lw=0.8)
-plot(r, F, 'r-', lw=0.8)
+subplot(313)
+plot(r, F_newton, '--', lw=1.4, color=colors[0])
+plot(r, F_plummer, ':', lw=1.4, color=colors[1])
+plot(r, F_gadget2, '-', lw=1.4, color=colors[2])
+plot(r, F, '-', lw=1.4, color=colors[3])
 plot([epsilon, epsilon], [0, 10], 'k-', alpha=0.5, lw=0.5)
+plot([epsilon/plummer_equivalent_factor, epsilon/plummer_equivalent_factor], [0, 10], 'k-', alpha=0.5, lw=0.5)
+text(epsilon+0.03, 0.05, "$\\epsilon$", color='k', alpha=0.5, rotation=90, va="bottom", ha="left", fontsize=8)
+text(epsilon/plummer_equivalent_factor+0.03, 0.05, "$\\epsilon_{\\rm Plummer}$", color='k', alpha=0.5, rotation=90, va="bottom", ha="left", fontsize=8) 
 
 xlim(0,r_max_plot)
-xticks([0., 0.5, 1., 1.5, 2., 2.5], ["", "", "", "", "", ""])
+xticks([0., 0.5, 1., 1.5, 2., 2.5], ["$%.1f$"%(0./epsilon), "", "$%.1f$"%(1./epsilon), "", "$%.1f$"%(2./epsilon)])
+xlabel("$r/H$", labelpad=-7)
 
 ylim(0, 0.95)
 ylabel("$|\\overrightarrow{\\nabla}\\phi(r)|$", labelpad=0)
-
-# Density
-subplot(313)
-plot(r, W_newton, 'g--', lw=0.8, label="${\\rm Newtonian}$")
-plot(r, W_plummer, 'b:', lw=0.8, label="${\\rm Plummer}$")
-plot(r, W_gadget2, 'm-.', lw=0.8, label="${\\rm Gadget\\textendash 2}$")
-plot(r, W, 'r-', lw=0.8, label="${\\rm SWIFT}$")
-plot([epsilon, epsilon], [0, 10], 'k-', alpha=0.5, lw=0.5)
-xlim(0,r_max_plot)
-xticks([0., 0.5, 1., 1.5, 2., 2.5], ["$%.1f$"%(0./epsilon), "", "$%.1f$"%(1./epsilon), "", "$%.1f$"%(2./epsilon)])
-xlabel("$r/\\epsilon$", labelpad=-4)
-legend(loc="upper right", frameon=True, handletextpad=0.1, handlelength=3.2, fontsize=8)
-
-ylim(0., 0.92)
-yticks([0, 0.2, 0.4, 0.6, 0.8], ["$0$", "$0.2$", "$0.4$", "$0.6$", "$0.8$"])
-ylabel("$\\rho(r)$", labelpad=0)
 
 savefig("potential.pdf")
 
