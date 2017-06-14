66e309a0aeaa33191bc0b8f9eeb233458e93965d
Aidan Chalk <aidan.chalk@stfc.ac.uk>
2017-06-14 11:03:22 +0100

    Added a force kernel function and moved the cuda_kernel_deval code to a separate call

--- src/CUDA/runner_cuda_main.cu
+++ src/CUDA/runner_cuda_main.cu
@@ -1,7 +1,8 @@
 
 #include "queue_cuda.h"
 #include "task_cuda.h"
-#include "../kernel_hydro.h":
+#include "../kernel_hydro.h"
+#include "../dimension.h"
 
 /*Definition of particle structures (SoA on GPU) */
 struct particle_arrays{
@@ -74,6 +75,20 @@ __constant__ cuda_kernel_coeffs[(kernel_degree+1)*(kernel_ivals+1)];
 
 __device__ int cuda_cell_is_active( struct cell_cuda *c ) {
   return (c->ti_end_min == e->ti_current);
+}
+
+__device__ float cuda_pow_dimension_plus_one( float x ) {
+#if defined(HYDRO_DIMENSION_3D)
+  const float x2 = x*x;
+  return x2*x2;
+#elif defined(HYDRO_DIMENSION_2D)
+  return x*x*x;
+#elif defined(HYDRO_DIMENSION_3D)
+  return x*x;
+#else
+  printf("The dimension is not defined!");
+  return 0.f;
+#endif
 }
 
 
@@ -280,7 +295,7 @@ __device__ void dopair_density( struct cell_cuda *ci, struct cell_cuda *cj ) {
 
     /* Loop over the parts in cj. */
     /* TODO May be possible to optimize this loop ordering.*/
-    for( int pjd = 0; pjd < count_j; pjd++ ) {
+    for( int pjd = part_j; pjd < part_j + count_j; pjd++ ) {
 
       /* Compute the pairwise distance. */
       float r2 = 0.0f;
@@ -308,26 +323,7 @@ __device__ void dopair_density( struct cell_cuda *ci, struct cell_cuda *cj ) {
         const float hi_inv = 1.0f / hi;
         const float ui = r * hi_inv;
         
-        /* Go to the range [0,1] from [0,H] */
-        const float x = ui * kernel_gamma_inv;
-
-        /* Pick the correct branch of the kernel */
-        const int temp = (int)(x* kernel_ivals_f);
-        const int ind = temp > kernel_ivals ? kernel_ivals : temp;
-        *float coeffs = &kernel_coeffs[ind * (kernel_degree + 1)];
-
-        /* First two terms of the polynomial */
-        float w = coeffs[0] * x + coeffs[1];
-        float dw_dx = coeffs[0];
-
-        /* Compute the rest of the polynomial */
-        for( int k = 2; k < kernel_degree; k++) {
-          dw_dx = dw_dx * x + w;
-          w = x * w + coeffs[k];
-        }
-        /* Complete kernel computation. */
-        w = w * kernel_constant * kernel_gamma_inv_dim;
-        dw_dx = dw_dx * kernel_constant * kernel_gamma_inv_dim_plus_one;
+        cuda_kernel_deval(ui, &w, &dw_dx);
 
         /* Compute contribution to the density. */
         rho += mj * w;
@@ -372,6 +368,183 @@ __device__ void dopair_density( struct cell_cuda *ci, struct cell_cuda *cj ) {
     atomic_add( &cuda_parts.rot_v[pid].z, rot_v.z);
 
   }//Loop over ci.
+}
+
+__device__ __inline__ void cuda_kernel_deval( float u, float *restrict W, float *restrict dW_dx) {
+  /* Go to the range [0,1] from [0,H] */
+  const float x = u * kernel_gamma_inv;
+
+  /* Pick the correct branch of the kernel */
+  const int temp = (int)(x*kernel_ivals_f);
+  const int ind = temp > kernel_ivals ? kernel_ivals : temp;
+  const float *coeffs = &kernel_coeffs[ind*(kernel_degree+1)];
+
+  /* First two terms of the polynomial*/
+  float w = coeffs[0] * x + coeffs[1];
+  float dw_dx = coeffs[0];
+
+  /* and the rest of them*/
+  for(int k = 2; k <= kernel_degree; k++)
+  {
+    dw_dx = dw_dx * x + w;
+    w = x * w + coeffs[k];
+  }
+
+  /* Return the results */
+  *W = w * kernel_constant * kernel_gamma_inv_dim;
+  *dW_dx = dw_dx * kernel_constant * kernel_gamma_inv_dim_plus_one;
+}
+
+/* Task function to execute a force task. Uses naive n^2 algorithm without symmetry*/
+/* To do force between Cell i and cell j this needs to be called twice. */
+__device__ void dopair_force( struct cell_cuda *ci, struct cell_cuda *cj) {
+
+ /* Are these cells active? */
+  if( !cell_is_active(ci) && !cell_is_active(cj) ) return;
+
+  const int count_i = ci->count;
+  const int count_j = cj->count;
+  int part_i = ci->first_part;
+  int part_j = cj->first_part;
+
+  float3 a_hydro;
+  float h_dt, v_sig_stor, entropy_dt;
+
+  /* Deal with periodicity concerns. */
+  for(int k = 0; k < 3; k++ ) {
+    if( cj->loc[k] - ci->loc[k] < dim[k] / 2)
+      shift[k] = dim[k];
+    else if( cj->loc[k] - ci->loc[k] > dim[k] / 2 )
+      shift[k] = dim[k]
+  }
+
+  /* Loop over the parts in cell ci */
+  for( int pid = part_i + threadidx.x; pid < part_i+count_i; pid += blockDim.x ){
+
+    const float hi = cuda_parts.h[pid];
+
+    /* Reset the values. */
+    a_hydro.x = 0.0f; a_hydro.y = 0.0f; a_hydro.z = 0.0f;
+    h_dt = 0.0f; v_sig_stor = cuda_parts.v_sig[pid]; entropy_dt = 0.0f;
+
+    double pix[3];
+    pix[0] = cuda_parts.x_x[pid] - shift[0];
+    pix[1] = cuda_parts.x_y[pid] - shift[1];
+    pix[2] = cuda_parts.x_z[pid] - shift[2];
+
+    const float hig2 = hi * hi * kernel_gamma2;
+
+    /* Loop over the particles in cj. */
+    for( int pjd = part_j; pjd < part_j + count_j; pjd++ ) {
+      
+      /* Compute the pairwise distance. */
+      float r2 = 0.0f;
+      float dx[3];
+      dx[0] = pix[0] - cuda_parts.x_x[pjd];
+      r2 += dx[0] * dx[0];
+      dx[1] = pix[1] - cuda_parts.x_y[pjd];
+      r2 += dx[1] * dx[1];
+      dx[2] = pix[2] - cuda_parts.x_z[pjd];
+      r2 += dx[2] * dx[2];
+
+      const float hj = cuda_parts.h[pjd];
+      if(r2 < hig2 || r2 < hj*hj*kernel_gamma2) {
+        float wi, wj, wi_dx, wj_dx;
+        const float fac_mu = 1.f; 
+
+        const float r = sqrtf(r2);
+        const float r_inv = 1.0f / r;
+
+        /* Load some data.*/
+        const float mj = cuda_parts.mass[pjd];
+        const float rhoi = cuda_parts.rho[pid];
+        const float rhoj = cuda_parts.rho[pjd];
+
+        /* Get the kernel for hi. */
+        const float hi_inv = 1.0f/hi;
+        const float hid_inv = cuda_pow_dimension_plus_one(hi_inv);
+        const float ui = r * hi_inv;
+        cuda_kernel_deval(ui, &wi, &wi_dx)
+        const float wi_dr = hid_inv * wi_dx;
+
+        /* Get the kernel for hj. */
+        const float hj_inv = 1.0f/hj;
+        const float hjd_inv = cuda_pow_dimension_plus_one(hj_inv);
+        const float xj = r * hj_inv;
+        cuda_kernel_deval(xj, &wj, &wj_dx);
+        const float wj_dr = hjd_inv * wj_dx;
+
+        /* Compute h-gradient terms */
+        const float f_i = cuda_parts.f[pid];
+        const float f_j = cuda_parts.f[pjd];
+
+        /* Compute pressure terms */
+        const float P_over_rho2_i = cuda_parts.P_over_rho2[pid];
+        const float P_over_rho2_j = cuda_parts.P_over_rho2[pjd];
+
+        /* Compute sound speeds*/
+        const float ci = cuda_parts.soundspeed[pid];
+        const float cj = cuda_parts.soundspeed[pjd];
+
+        /* Compute dv dot r. */
+        const float dvdr = (cuda_parts.v[pid].x - cuda_parts.v[pjd].x) * dx[0] + 
+                            cuda_parts.v[pid].y - cuda_parts.v[pjd].y) * dx[1] + 
+                            cuda_parts.v[pid].z - cuda_parts.v[pjd].z) * dx[2];
+
+        /* Balsara term */
+        const float balsara_i = cuda_parts.balsara[pid];
+        const float balsara_j = cuda_parts.balsara[pjd];
+
+        /* Are the particles moving towards each other? */
+        const float omega_ij = (dvdr < 0.f) ? dvdr : 0.f;
+        const float mu_ij = fac_mu * r_inv * omega_id;
+
+        /* Signal velocity */
+        const float v_sig = ci + cj - 3.f * mu_ij;
+
+        /* Now construct the full viscosity term */
+        const float rho_ij = 0.5f* (rhoi+rhoj);
+        const float visc = -0.25 * const_viscosity_alpha * v_sig * mu_id * 
+                           (balsara_i + balsara_j) / rho_ij;
+
+        /* Now convolce with the kernel */
+        const float visc_term = 0.5f* visc * (wi_dr + wj_dr) * r_inv;
+        const float sph_term = 
+          (f_i * P_over_rho2_i * wi_dr + f_j * P_over_rho2_j * wj_dr) * r_inv;
+
+        /* Compute the acceleration */
+        const float acc = visc_term + sph_term;
+
+        /* Compute the force */
+        a_hydro.x -= mj * acc * dx[0];
+        a_hydro.y -= mj * acc * dx[1];
+        a_hydro.z -= mj * acc * dx[2];
+
+        /* Get the time derivative for h. */
+        h_dt -= mj * dvdr * r_inv / rhoj * wi_dr;
+
+        /* Update the signal velocity. */
+        v_sig_stor = (v_sig_stor > v_sig ) ? v_sig_stor : v_sig;
+
+        /* Change in entropy */
+        entropy_dt += mj * visc_term * dvdr;
+        //INTERACT
+      }
+
+    } //Loop over cell cj.
+
+    /* Flush to global stores.*/
+    atomicAdd(&cuda_parts.a_hydro[pid].x, a_hydro.x);
+    atomicAdd(&cuda_parts.a_hydro[pid].y, a_hydro.y);
+    atomicAdd(&cuda_parts.a_hydro[pid].z, a_hydro.z);
+    atomicAdd(&cuda_parts.h_dt[pid], h_dt);
+    atomicAdd(&cuda_parts.entropy_dt[pid], entropy_dt);
+
+    /* Update the signal velocity */
+    float global_vsig = cuda_parts.v_sig[pid];
+    
+
+  }//Loop over cell ci.
 }
 
 /* Runner function to retrieve a task index from a queue. */
