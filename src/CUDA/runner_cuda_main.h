820af7ddb6ac1f292914dd7f937d2dddaa276ae9
Aidan Chalk <aidan.chalk@stfc.ac.uk>
2017-06-14 16:29:16 +0100

    Missed the header file needed later

---
+++ src/CUDA/runner_cuda_main.h
@@ -0,0 +1,11 @@
+#ifndef SWIFT_CUDA_RUNNER_MAIN_H
+#define SWIFT_CUDA_RUNNER_MAIN_H
+
+__host__ void create_tasks(struct engine *e);
+__host__ void create_cells_and_data_tasks(struct engine *e);
+
+
+
+
+
+#endif
