baf8a27a92f0cd9807142f9fe11b31054969871b
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-06-16 12:47:16 +0200

    Small overall updates to the self-gravity scheme tex file.

--- theory/Multipoles/run.sh
+++ theory/Multipoles/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-python potential.py
+echo "Generating figures..."
+python plot_potential.py
+echo "Generating PDF..."
 pdflatex -jobname=fmm fmm_standalone.tex
 bibtex fmm.aux
 pdflatex -jobname=fmm fmm_standalone.tex
