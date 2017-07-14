135d6034d099c02e3fb83f8503aec733e8ed2f06
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-07-14 11:15:33 +0100

    Removed incorrect merge and diff lines in space.h

--- src/space.h
+++ src/space.h
@@ -43,14 +43,8 @@ struct cell;
 #define space_cellallocchunk 1000
 #define space_splitsize_default 400
 #define space_maxsize_default 8000000
-<<<<<<< HEAD
 #define space_subsize_pair_default 256000000
 #define space_subsize_self_default 32000
-#define space_maxcount_default 10000
-=======
-#define space_subsize_pair_default 64000000
-#define space_subsize_self_default 16000
->>>>>>> 88356221ae330b1818217a6f4c5ac7890ad02c11
 #define space_max_top_level_cells_default 12
 #define space_stretch 1.10f
 #define space_maxreldx 0.1f
