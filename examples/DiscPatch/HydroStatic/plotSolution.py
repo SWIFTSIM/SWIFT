e240f03bca4fbd374363a46de0c5214e70b3f2a7
Bert Vandenbroucke <bert.vandenbroucke@ugent.be>
2017-07-25 09:56:47 +0100

    Switched disc patch potential direction from z to x to be able to run it in 1D and 2D.

--- examples/DiscPatch/HydroStatic/plotSolution.py
+++ examples/DiscPatch/HydroStatic/plotSolution.py
@@ -1,6 +1,7 @@
 ################################################################################
 # This file is part of SWIFT.
 # Copyright (c) 2017 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+#                    Matthieu Schaller (matthieu.schaller@durham.ac.uk)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
@@ -34,9 +35,9 @@ import sys
 # Parameters
 surface_density = 10.
 scale_height = 100.
-z_disc = 400.
-z_trunc = 300.
-z_max = 350.
+x_disc = 400.
+x_trunc = 300.
+x_max = 350.
 utherm = 20.2678457288
 gamma = 5. / 3.
 
@@ -50,14 +51,14 @@ if len(sys.argv) > 2:
 # Get the analytic solution for the density
 def get_analytic_density(x):
   return 0.5 * surface_density / scale_height / \
-           np.cosh( (x - z_disc) / scale_height )**2
+           np.cosh( (x - x_disc) / scale_height )**2
 
 # Get the analytic solution for the (isothermal) pressure
 def get_analytic_pressure(x):
   return (gamma - 1.) * utherm * get_analytic_density(x)
 
 # Get the data fields to plot from the snapshot file with the given name:
-#  snapshot time, z-coord, density, pressure, velocity norm
+#  snapshot time, x-coord, density, pressure, velocity norm
 def get_data(name):
   file = h5py.File(name, "r")
   coords = np.array(file["/PartType0/Coordinates"])
@@ -69,7 +70,7 @@ def get_data(name):
 
   vtot = np.sqrt( v[:,0]**2 + v[:,1]**2 + v[:,2]**2 )
 
-  return float(file["/Header"].attrs["Time"]), coords[:,2], rho, P, vtot
+  return float(file["/Header"].attrs["Time"]), coords[:,0], rho, P, vtot
 
 # scan the folder for snapshot files and plot all of them (within the requested
 # range)
@@ -80,38 +81,38 @@ for f in sorted(glob.glob("Disc-Patch_*.hdf5")):
 
   print "processing", f, "..."
 
-  zrange = np.linspace(0., 2. * z_disc, 1000)
-  time, z, rho, P, v = get_data(f)
+  xrange = np.linspace(0., 2. * x_disc, 1000)
+  time, x, rho, P, v = get_data(f)
 
   fig, ax = pl.subplots(3, 1, sharex = True)
 
-  ax[0].plot(z, rho, "r.")
-  ax[0].plot(zrange, get_analytic_density(zrange), "k-")
-  ax[0].plot([z_disc - z_max, z_disc - z_max], [0, 10], "k--", alpha=0.5)
-  ax[0].plot([z_disc + z_max, z_disc + z_max], [0, 10], "k--", alpha=0.5)
-  ax[0].plot([z_disc - z_trunc, z_disc - z_trunc], [0, 10], "k--", alpha=0.5)
-  ax[0].plot([z_disc + z_trunc, z_disc + z_trunc], [0, 10], "k--", alpha=0.5)
-  ax[0].set_ylim(0., 1.2 * get_analytic_density(z_disc))
+  ax[0].plot(x, rho, "r.")
+  ax[0].plot(xrange, get_analytic_density(xrange), "k-")
+  ax[0].plot([x_disc - x_max, x_disc - x_max], [0, 10], "k--", alpha=0.5)
+  ax[0].plot([x_disc + x_max, x_disc + x_max], [0, 10], "k--", alpha=0.5)
+  ax[0].plot([x_disc - x_trunc, x_disc - x_trunc], [0, 10], "k--", alpha=0.5)
+  ax[0].plot([x_disc + x_trunc, x_disc + x_trunc], [0, 10], "k--", alpha=0.5)
+  ax[0].set_ylim(0., 1.2 * get_analytic_density(x_disc))
   ax[0].set_ylabel("density")
 
-  ax[1].plot(z, v, "r.")
-  ax[1].plot(zrange, np.zeros(len(zrange)), "k-")
-  ax[1].plot([z_disc - z_max, z_disc - z_max], [0, 10], "k--", alpha=0.5)
-  ax[1].plot([z_disc + z_max, z_disc + z_max], [0, 10], "k--", alpha=0.5)
-  ax[1].plot([z_disc - z_trunc, z_disc - z_trunc], [0, 10], "k--", alpha=0.5)
-  ax[1].plot([z_disc + z_trunc, z_disc + z_trunc], [0, 10], "k--", alpha=0.5)
+  ax[1].plot(x, v, "r.")
+  ax[1].plot(xrange, np.zeros(len(xrange)), "k-")
+  ax[1].plot([x_disc - x_max, x_disc - x_max], [0, 10], "k--", alpha=0.5)
+  ax[1].plot([x_disc + x_max, x_disc + x_max], [0, 10], "k--", alpha=0.5)
+  ax[1].plot([x_disc - x_trunc, x_disc - x_trunc], [0, 10], "k--", alpha=0.5)
+  ax[1].plot([x_disc + x_trunc, x_disc + x_trunc], [0, 10], "k--", alpha=0.5)
   ax[1].set_ylim(-0.5, 10.)
   ax[1].set_ylabel("velocity norm")
 
-  ax[2].plot(z, P, "r.")
-  ax[2].plot(zrange, get_analytic_pressure(zrange), "k-")
-  ax[2].plot([z_disc - z_max, z_disc - z_max], [0, 10], "k--", alpha=0.5)
-  ax[2].plot([z_disc + z_max, z_disc + z_max], [0, 10], "k--", alpha=0.5)
-  ax[2].plot([z_disc - z_trunc, z_disc - z_trunc], [0, 10], "k--", alpha=0.5)
-  ax[2].plot([z_disc + z_trunc, z_disc + z_trunc], [0, 10], "k--", alpha=0.5)
-  ax[2].set_xlim(0., 2. * z_disc)
-  ax[2].set_ylim(0., 1.2 * get_analytic_pressure(z_disc))
-  ax[2].set_xlabel("z")
+  ax[2].plot(x, P, "r.")
+  ax[2].plot(xrange, get_analytic_pressure(xrange), "k-")
+  ax[2].plot([x_disc - x_max, x_disc - x_max], [0, 10], "k--", alpha=0.5)
+  ax[2].plot([x_disc + x_max, x_disc + x_max], [0, 10], "k--", alpha=0.5)
+  ax[2].plot([x_disc - x_trunc, x_disc - x_trunc], [0, 10], "k--", alpha=0.5)
+  ax[2].plot([x_disc + x_trunc, x_disc + x_trunc], [0, 10], "k--", alpha=0.5)
+  ax[2].set_xlim(0., 2. * x_disc)
+  ax[2].set_ylim(0., 1.2 * get_analytic_pressure(x_disc))
+  ax[2].set_xlabel("x")
   ax[2].set_ylabel("pressure")
 
   pl.suptitle("t = {0:.2f}".format(time))
