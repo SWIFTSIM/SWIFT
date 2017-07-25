7a885c3b7b73f880346fb78f03aba0eda5acaf59
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-07-25 15:04:07 +0100

    Updated task plotting script for the new gravity tasks.

--- examples/analyse_tasks.py
+++ examples/analyse_tasks.py
@@ -50,8 +50,8 @@ infile = args.input
 TASKTYPES = ["none", "sort", "self", "pair", "sub_self", "sub_pair",
              "init_grav", "ghost", "extra_ghost", "drift_part",
              "drift_gpart", "kick1", "kick2", "timestep", "send", "recv",
-             "grav_top_level", "grav_long_range", "grav_mm", "grav_down",
-             "cooling", "sourceterms", "count"]
+             "grav_top_level", "grav_long_range", "grav_ghost", "grav_mm",
+             "grav_down", "cooling", "sourceterms", "count"]
 
 SUBTYPES = ["none", "density", "gradient", "force", "grav", "external_grav",
             "tend", "xv", "rho", "gpart", "multipole", "spart", "count"]
