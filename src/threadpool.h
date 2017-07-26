b32c270b3f007912af378b3cfd8abba3d9686976
Pedro Gonnet <gonnet@google.com>
2017-07-26 16:58:30 +0200

    increase the default chunk ratio.

--- src/threadpool.h
+++ src/threadpool.h
@@ -30,7 +30,7 @@
 
 /* Local defines. */
 #define threadpool_log_initial_size 1000
-#define threadpool_default_chunk_ratio 5
+#define threadpool_default_chunk_ratio 11
 
 /* Function type for mappings. */
 typedef void (*threadpool_map_function)(void *map_data, int num_elements,
