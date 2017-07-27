44bacc50794125d0e0eb26e38d412059a47bb6fc
Aidan Chalk <aidan.chalk@stfc.ac.uk>
2017-07-27 16:17:52 +0100

    Fix to make test27cells still give 48 neighbours

--- tests/test27cells.c
+++ tests/test27cells.c
@@ -204,6 +204,8 @@ void zero_particle_fields(struct cell *c) {
 void end_calculation(struct cell *c) {
   for (int pid = 0; pid < c->count; pid++) {
     hydro_end_density(&c->parts[pid]);
+    c->parts[pid].density.wcount *= pow_dimension(c->parts[pid].h);
+    c->parts[pid].density.wcount *= kernel_norm;
   }
 }
 
