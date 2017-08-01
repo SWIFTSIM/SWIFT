770d37e9cf344fc4b5d7dd2e147f579490916efe
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2017-08-01 14:01:09 +0100

    Split the gravity pp tasks into truncated and full Newtonian potential. Use the full potential when the particles are close enough.

--- src/runner_doiact_grav.h
+++ src/runner_doiact_grav.h
@@ -150,87 +150,26 @@ void runner_dopair_grav_mm(const struct runner *r, struct cell *restrict ci,
 }
 
 /**
- * @brief Computes the interaction of all the particles in a cell with the
- * multipole of another cell.
- *
- * @param r The #runner.
- * @param ci The #cell with particles to interct.
- * @param cj The #cell with the multipole.
- */
-void runner_dopair_grav_pm(const struct runner *r,
-                           const struct cell *restrict ci,
-                           const struct cell *restrict cj) {
-
-  error("Function should not be called");
-}
-
-/**
  * @brief Computes the interaction of all the particles in a cell with all the
- * particles of another cell.
+ * particles of another cell using the full Newtonian potential
  *
  * @param r The #runner.
  * @param ci The first #cell.
  * @param cj The other #cell.
- *
- * @todo Use a local cache for the particles.
+ * @param shift The distance vector (periodically wrapped) between the cell
+ * centres.
  */
-void runner_dopair_grav_pp(struct runner *r, struct cell *ci, struct cell *cj) {
+void runner_dopair_grav_pp_full(struct runner *r, struct cell *ci,
+                                struct cell *cj, double shift[3]) {
 
   /* Some constants */
-  const struct engine *e = r->e;
-  const struct space *s = e->s;
-  const int periodic = s->periodic;
-  const double cell_width = s->width[0];
-  const double dim[3] = {s->dim[0], s->dim[1], s->dim[2]};
-  const float a_smooth = e->gravity_properties->a_smooth;
-  const float rlr_inv = 1. / (a_smooth * cell_width);
+  const struct engine *const e = r->e;
 
   /* Cell properties */
   const int gcount_i = ci->gcount;
   const int gcount_j = cj->gcount;
   struct gpart *restrict gparts_i = ci->gparts;
   struct gpart *restrict gparts_j = cj->gparts;
-
-  TIMER_TIC;
-
-  /* Anything to do here? */
-  if (!cell_is_active(ci, e) && !cell_is_active(cj, e)) return;
-
-  /* Let's start by drifting things */
-  if (!cell_are_gpart_drifted(ci, e)) cell_drift_gpart(ci, e);
-  if (!cell_are_gpart_drifted(cj, e)) cell_drift_gpart(cj, e);
-
-#if ICHECK > 0
-  for (int pid = 0; pid < gcount_i; pid++) {
-
-    /* Get a hold of the ith part in ci. */
-    struct gpart *restrict gp = &gparts_i[pid];
-
-    if (gp->id_or_neg_offset == ICHECK)
-      message("id=%lld loc=[ %f %f %f ] size= %f count= %d",
-              gp->id_or_neg_offset, cj->loc[0], cj->loc[1], cj->loc[2],
-              cj->width[0], cj->gcount);
-  }
-
-  for (int pid = 0; pid < gcount_j; pid++) {
-
-    /* Get a hold of the ith part in ci. */
-    struct gpart *restrict gp = &gparts_j[pid];
-
-    if (gp->id_or_neg_offset == ICHECK)
-      message("id=%lld loc=[ %f %f %f ] size= %f count=%d",
-              gp->id_or_neg_offset, ci->loc[0], ci->loc[1], ci->loc[2],
-              ci->width[0], ci->gcount);
-  }
-#endif
-
-  /* Get the relative distance between the pairs, wrapping. */
-  double shift[3] = {0.0, 0.0, 0.0};
-  if (periodic) {
-    shift[0] = nearest(cj->loc[0] - ci->loc[0], dim[0]);
-    shift[1] = nearest(cj->loc[1] - ci->loc[1], dim[1]);
-    shift[2] = nearest(cj->loc[2] - ci->loc[2], dim[2]);
-  }
 
   /* MATTHIEU: Should we use local DP accumulators ? */
 
@@ -268,7 +207,7 @@ void runner_dopair_grav_pp(struct runner *r, struct cell *ci, struct cell *cj) {
 #endif
 
         /* Interact ! */
-        runner_iact_grav_pp_nonsym(rlr_inv, r2, dx, gpi, gpj, periodic);
+        runner_iact_grav_pp_nonsym(r2, dx, gpi, gpj);
 
 #ifdef SWIFT_DEBUG_CHECKS
         gpi->num_interacted++;
@@ -311,7 +250,7 @@ void runner_dopair_grav_pp(struct runner *r, struct cell *ci, struct cell *cj) {
 #endif
 
         /* Interact ! */
-        runner_iact_grav_pp_nonsym(rlr_inv, r2, dx, gpj, gpi, periodic);
+        runner_iact_grav_pp_nonsym(r2, dx, gpj, gpi);
 
 #ifdef SWIFT_DEBUG_CHECKS
         gpj->num_interacted++;
@@ -319,56 +258,196 @@ void runner_dopair_grav_pp(struct runner *r, struct cell *ci, struct cell *cj) {
       }
     }
   }
+}
+
+/**
+ * @brief Computes the interaction of all the particles in a cell with all the
+ * particles of another cell using the truncated Newtonian potential
+ *
+ * @param r The #runner.
+ * @param ci The first #cell.
+ * @param cj The other #cell.
+ * @param shift The distance vector (periodically wrapped) between the cell
+ * centres.
+ */
+void runner_dopair_grav_pp_truncated(struct runner *r, struct cell *ci,
+                                     struct cell *cj, double shift[3]) {
+
+  /* Some constants */
+  const struct engine *const e = r->e;
+  const struct space *s = e->s;
+  const double cell_width = s->width[0];
+  const double a_smooth = e->gravity_properties->a_smooth;
+  const double rlr = cell_width * a_smooth;
+  const float rlr_inv = 1. / rlr;
+
+  /* Cell properties */
+  const int gcount_i = ci->gcount;
+  const int gcount_j = cj->gcount;
+  struct gpart *restrict gparts_i = ci->gparts;
+  struct gpart *restrict gparts_j = cj->gparts;
+
+  /* MATTHIEU: Should we use local DP accumulators ? */
+
+  /* Loop over all particles in ci... */
+  if (cell_is_active(ci, e)) {
+    for (int pid = 0; pid < gcount_i; pid++) {
+
+      /* Get a hold of the ith part in ci. */
+      struct gpart *restrict gpi = &gparts_i[pid];
+
+      if (!gpart_is_active(gpi, e)) continue;
+
+      /* Apply boundary condition */
+      const double pix[3] = {gpi->x[0] - shift[0], gpi->x[1] - shift[1],
+                             gpi->x[2] - shift[2]};
+
+      /* Loop over every particle in the other cell. */
+      for (int pjd = 0; pjd < gcount_j; pjd++) {
+
+        /* Get a hold of the jth part in cj. */
+        const struct gpart *restrict gpj = &gparts_j[pjd];
+
+        /* Compute the pairwise distance. */
+        const float dx[3] = {pix[0] - gpj->x[0],   // x
+                             pix[1] - gpj->x[1],   // y
+                             pix[2] - gpj->x[2]};  // z
+        const float r2 = dx[0] * dx[0] + dx[1] * dx[1] + dx[2] * dx[2];
+
+#ifdef SWIFT_DEBUG_CHECKS
+        /* Check that particles have been drifted to the current time */
+        if (gpi->ti_drift != e->ti_current)
+          error("gpi not drifted to current time");
+        if (gpj->ti_drift != e->ti_current)
+          error("gpj not drifted to current time");
+#endif
+
+        /* Interact ! */
+        runner_iact_grav_pp_truncated_nonsym(r2, dx, gpi, gpj, rlr_inv);
+
+#ifdef SWIFT_DEBUG_CHECKS
+        gpi->num_interacted++;
+#endif
+      }
+    }
+  }
+
+  /* Loop over all particles in cj... */
+  if (cell_is_active(cj, e)) {
+    for (int pjd = 0; pjd < gcount_j; pjd++) {
+
+      /* Get a hold of the ith part in ci. */
+      struct gpart *restrict gpj = &gparts_j[pjd];
+
+      if (!gpart_is_active(gpj, e)) continue;
+
+      /* Apply boundary condition */
+      const double pjx[3] = {gpj->x[0] + shift[0], gpj->x[1] + shift[1],
+                             gpj->x[2] + shift[2]};
+
+      /* Loop over every particle in the other cell. */
+      for (int pid = 0; pid < gcount_i; pid++) {
+
+        /* Get a hold of the ith part in ci. */
+        const struct gpart *restrict gpi = &gparts_i[pid];
+
+        /* Compute the pairwise distance. */
+        const float dx[3] = {pjx[0] - gpi->x[0],   // x
+                             pjx[1] - gpi->x[1],   // y
+                             pjx[2] - gpi->x[2]};  // z
+        const float r2 = dx[0] * dx[0] + dx[1] * dx[1] + dx[2] * dx[2];
+
+#ifdef SWIFT_DEBUG_CHECKS
+        /* Check that particles have been drifted to the current time */
+        if (gpi->ti_drift != e->ti_current)
+          error("gpi not drifted to current time");
+        if (gpj->ti_drift != e->ti_current)
+          error("gpj not drifted to current time");
+#endif
+
+        /* Interact ! */
+        runner_iact_grav_pp_truncated_nonsym(r2, dx, gpj, gpi, rlr_inv);
+
+#ifdef SWIFT_DEBUG_CHECKS
+        gpj->num_interacted++;
+#endif
+      }
+    }
+  }
+}
+
+/**
+ * @brief Computes the interaction of all the particles in a cell with all the
+ * particles of another cell (switching function between full and truncated).
+ *
+ * @param r The #runner.
+ * @param ci The first #cell.
+ * @param cj The other #cell.
+ */
+void runner_dopair_grav_pp(struct runner *r, struct cell *ci, struct cell *cj) {
+
+  /* Some properties of the space */
+  const struct engine *e = r->e;
+  const struct space *s = e->s;
+  const int periodic = s->periodic;
+  const double cell_width = s->width[0];
+  const double dim[3] = {s->dim[0], s->dim[1], s->dim[2]};
+  const double a_smooth = e->gravity_properties->a_smooth;
+  const double r_cut_min = e->gravity_properties->r_cut_min;
+  const double min_trunc = cell_width * r_cut_min * a_smooth;
+  double shift[3] = {0.0, 0.0, 0.0};
+
+  TIMER_TIC;
+
+  /* Anything to do here? */
+  if (!cell_is_active(ci, e) && !cell_is_active(cj, e)) return;
+
+  /* Let's start by drifting things */
+  if (!cell_are_gpart_drifted(ci, e)) cell_drift_gpart(ci, e);
+  if (!cell_are_gpart_drifted(cj, e)) cell_drift_gpart(cj, e);
+
+  /* Can we use the Newtonian version or do we need the truncated one ? */
+  if (!periodic) {
+    runner_dopair_grav_pp_full(r, ci, cj, shift);
+  } else {
+
+    /* Get the relative distance between the pairs, wrapping. */
+    shift[0] = nearest(cj->loc[0] - ci->loc[0], dim[0]);
+    shift[1] = nearest(cj->loc[1] - ci->loc[1], dim[1]);
+    shift[2] = nearest(cj->loc[2] - ci->loc[2], dim[2]);
+    const double r2 =
+        shift[0] * shift[0] + shift[1] * shift[1] + shift[2] * shift[2];
+
+    /* Get the maximal distance between any two particles */
+    const double max_r = sqrt(r2) + ci->multipole->r_max + cj->multipole->r_max;
+
+    /* Do we need to use the truncated interactions ? */
+    if (max_r > min_trunc)
+      runner_dopair_grav_pp_truncated(r, ci, cj, shift);
+    else
+      runner_dopair_grav_pp_full(r, ci, cj, shift);
+  }
 
   TIMER_TOC(timer_dopair_grav_pp);
 }
 
 /**
- * @brief Computes the interaction of all the particles in a cell directly
+ * @brief Computes the interaction of all the particles in a cell using the
+ * full Newtonian potential.
  *
  * @param r The #runner.
  * @param c The #cell.
  *
  * @todo Use a local cache for the particles.
  */
-void runner_doself_grav_pp(struct runner *r, struct cell *c) {
+void runner_doself_grav_pp_full(struct runner *r, struct cell *c) {
 
   /* Some constants */
-  const struct engine *e = r->e;
-  const struct space *s = e->s;
-  const int periodic = s->periodic;
-  const double cell_width = s->width[0];
-  const float a_smooth = e->gravity_properties->a_smooth;
-  const float rlr_inv = 1. / (a_smooth * cell_width);
+  const struct engine *const e = r->e;
 
   /* Cell properties */
   const int gcount = c->gcount;
   struct gpart *restrict gparts = c->gparts;
-
-  TIMER_TIC;
-
-#ifdef SWIFT_DEBUG_CHECKS
-  if (c->gcount == 0) error("Doing self gravity on an empty cell !");
-#endif
-
-  /* Anything to do here? */
-  if (!cell_is_active(c, e)) return;
-
-  /* Do we need to start by drifting things ? */
-  if (!cell_are_gpart_drifted(c, e)) cell_drift_gpart(c, e);
-
-#if ICHECK > 0
-  for (int pid = 0; pid < gcount; pid++) {
-
-    /* Get a hold of the ith part in ci. */
-    struct gpart *restrict gp = &gparts[pid];
-
-    if (gp->id_or_neg_offset == ICHECK)
-      message("id=%lld loc=[ %f %f %f ] size= %f count= %d",
-              gp->id_or_neg_offset, c->loc[0], c->loc[1], c->loc[2],
-              c->width[0], c->gcount);
-  }
-#endif
 
   /* MATTHIEU: Should we use local DP accumulators ? */
 
@@ -401,7 +480,7 @@ void runner_doself_grav_pp(struct runner *r, struct cell *c) {
       /* Interact ! */
       if (gpart_is_active(gpi, e) && gpart_is_active(gpj, e)) {
 
-        runner_iact_grav_pp(rlr_inv, r2, dx, gpi, gpj, periodic);
+        runner_iact_grav_pp(r2, dx, gpi, gpj);
 
 #ifdef SWIFT_DEBUG_CHECKS
         gpi->num_interacted++;
@@ -412,7 +491,7 @@ void runner_doself_grav_pp(struct runner *r, struct cell *c) {
 
         if (gpart_is_active(gpi, e)) {
 
-          runner_iact_grav_pp_nonsym(rlr_inv, r2, dx, gpi, gpj, periodic);
+          runner_iact_grav_pp_nonsym(r2, dx, gpi, gpj);
 
 #ifdef SWIFT_DEBUG_CHECKS
           gpi->num_interacted++;
@@ -423,7 +502,7 @@ void runner_doself_grav_pp(struct runner *r, struct cell *c) {
           dx[0] = -dx[0];
           dx[1] = -dx[1];
           dx[2] = -dx[2];
-          runner_iact_grav_pp_nonsym(rlr_inv, r2, dx, gpj, gpi, periodic);
+          runner_iact_grav_pp_nonsym(r2, dx, gpj, gpi);
 
 #ifdef SWIFT_DEBUG_CHECKS
           gpj->num_interacted++;
@@ -431,6 +510,139 @@ void runner_doself_grav_pp(struct runner *r, struct cell *c) {
         }
       }
     }
+  }
+}
+
+/**
+ * @brief Computes the interaction of all the particles in a cell using the
+ * truncated Newtonian potential.
+ *
+ * @param r The #runner.
+ * @param c The #cell.
+ *
+ * @todo Use a local cache for the particles.
+ */
+void runner_doself_grav_pp_truncated(struct runner *r, struct cell *c) {
+
+  /* Some constants */
+  const struct engine *const e = r->e;
+  const struct space *s = e->s;
+  const double cell_width = s->width[0];
+  const double a_smooth = e->gravity_properties->a_smooth;
+  const double rlr = cell_width * a_smooth;
+  const float rlr_inv = 1. / rlr;
+
+  /* Cell properties */
+  const int gcount = c->gcount;
+  struct gpart *restrict gparts = c->gparts;
+
+  /* MATTHIEU: Should we use local DP accumulators ? */
+
+  /* Loop over all particles in ci... */
+  for (int pid = 0; pid < gcount; pid++) {
+
+    /* Get a hold of the ith part in ci. */
+    struct gpart *restrict gpi = &gparts[pid];
+
+    /* Loop over every particle in the other cell. */
+    for (int pjd = pid + 1; pjd < gcount; pjd++) {
+
+      /* Get a hold of the jth part in ci. */
+      struct gpart *restrict gpj = &gparts[pjd];
+
+      /* Compute the pairwise distance. */
+      float dx[3] = {gpi->x[0] - gpj->x[0],   // x
+                     gpi->x[1] - gpj->x[1],   // y
+                     gpi->x[2] - gpj->x[2]};  // z
+      const float r2 = dx[0] * dx[0] + dx[1] * dx[1] + dx[2] * dx[2];
+
+#ifdef SWIFT_DEBUG_CHECKS
+      /* Check that particles have been drifted to the current time */
+      if (gpi->ti_drift != e->ti_current)
+        error("gpi not drifted to current time");
+      if (gpj->ti_drift != e->ti_current)
+        error("gpj not drifted to current time");
+#endif
+
+      /* Interact ! */
+      if (gpart_is_active(gpi, e) && gpart_is_active(gpj, e)) {
+
+        runner_iact_grav_pp_truncated(r2, dx, gpi, gpj, rlr_inv);
+
+#ifdef SWIFT_DEBUG_CHECKS
+        gpi->num_interacted++;
+        gpj->num_interacted++;
+#endif
+
+      } else {
+
+        if (gpart_is_active(gpi, e)) {
+
+          runner_iact_grav_pp_truncated_nonsym(r2, dx, gpi, gpj, rlr_inv);
+
+#ifdef SWIFT_DEBUG_CHECKS
+          gpi->num_interacted++;
+#endif
+
+        } else if (gpart_is_active(gpj, e)) {
+
+          dx[0] = -dx[0];
+          dx[1] = -dx[1];
+          dx[2] = -dx[2];
+          runner_iact_grav_pp_truncated_nonsym(r2, dx, gpj, gpi, rlr_inv);
+
+#ifdef SWIFT_DEBUG_CHECKS
+          gpj->num_interacted++;
+#endif
+        }
+      }
+    }
+  }
+}
+
+/**
+ * @brief Computes the interaction of all the particles in a cell directly
+ * (Switching function between truncated and full)
+ *
+ * @param r The #runner.
+ * @param c The #cell.
+ */
+void runner_doself_grav_pp(struct runner *r, struct cell *c) {
+
+  /* Some properties of the space */
+  const struct engine *e = r->e;
+  const struct space *s = e->s;
+  const int periodic = s->periodic;
+  const double cell_width = s->width[0];
+  const double a_smooth = e->gravity_properties->a_smooth;
+  const double r_cut_min = e->gravity_properties->r_cut_min;
+  const double min_trunc = cell_width * r_cut_min * a_smooth;
+
+  TIMER_TIC;
+
+#ifdef SWIFT_DEBUG_CHECKS
+  if (c->gcount == 0) error("Doing self gravity on an empty cell !");
+#endif
+
+  /* Anything to do here? */
+  if (!cell_is_active(c, e)) return;
+
+  /* Do we need to start by drifting things ? */
+  if (!cell_are_gpart_drifted(c, e)) cell_drift_gpart(c, e);
+
+  /* Can we use the Newtonian version or do we need the truncated one ? */
+  if (!periodic) {
+    runner_doself_grav_pp_full(r, c);
+  } else {
+
+    /* Get the maximal distance between any two particles */
+    const double max_r = 2 * c->multipole->r_max;
+
+    /* Do we need to use the truncated interactions ? */
+    if (max_r > min_trunc)
+      runner_doself_grav_pp_truncated(r, c);
+    else
+      runner_doself_grav_pp_full(r, c);
   }
 
   TIMER_TOC(timer_doself_grav_pp);
