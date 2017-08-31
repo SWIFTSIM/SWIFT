73cef4f8826f8caa87df6a9115a3d856cb26bcbc
lhausamm <loic_hausammann@hotmail.com>
2017-08-31 11:07:57 +0200

    Remove bug in dependency, but now we have tasks for inactive cells

--- src/scheduler.h
+++ src/scheduler.h
@@ -113,11 +113,11 @@ struct scheduler {
  */
 __attribute__((always_inline)) INLINE static void scheduler_activate(
     struct scheduler *s, struct task *t) {
-#ifdef WITH_CUDA
-  if (!t->gpu && atomic_cas(&t->skip, 1, 0)) {
-#else
+  //#ifdef WITH_CUDA
+  //  if (!t->gpu && atomic_cas(&t->skip, 1, 0)) {
+  //#else
   if (atomic_cas(&t->skip, 1, 0)){ 
-#endif
+    //#endif
     t->wait = 0;
     int ind = atomic_inc(&s->active_count);
     s->tid_active[ind] = t - s->tasks;
