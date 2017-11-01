5680f7a67d364ba2cc5e3087752fdc2a8a1e09df
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2017-11-01 11:39:23 +0000

    Added 3D Evrard collapse test.

---
+++ examples/EvrardCollapse_3D/plot_density_profile.py
@@ -0,0 +1,71 @@
+################################################################################
+# This file is part of SWIFT.
+# Copyright (c) 2017 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+# plots the density, velocity and pressure as a function of radius for all
+# snapshots in the current directory
+
+import numpy as np
+import h5py
+import matplotlib
+matplotlib.use("Agg")
+import pylab as pl
+import glob
+import os
+
+pl.rcParams["figure.figsize"] = (12, 6)
+pl.rcParams["text.usetex"] = True
+
+M = 1./6.67408e-8 # total mass of the sphere
+R = 1. # radius of the sphere
+
+for filename in sorted(glob.glob("evrard_????.hdf5")):
+  print "processing", filename, "..."
+  imagefile = "{0}.png".format(filename[:-5])
+  if os.path.exists(imagefile) and \
+     os.path.getmtime(imagefile) > os.path.getmtime(filename):
+    print "already processed, skipping..."
+    continue
+  file = h5py.File(filename, 'r')
+  coords = np.array(file["/PartType0/Coordinates"])
+  box = np.array(file["/Header"].attrs["BoxSize"])
+  rho = np.array(file["/PartType0/Density"])
+  v = np.array(file["/PartType0/Velocities"])
+  P = np.array(file["/PartType0/Pressure"])
+
+  coords -= 0.5 * box
+
+  r = np.sqrt(coords[:,0]**2 + coords[:,1]**2 + coords[:,2]**2)
+  vr = (coords[:,0] * v[:,0] + coords[:,1] * v[:,1] + coords[:,2] * v[:,2]) / r
+
+  fig, ax = pl.subplots(1, 3, sharex = True)
+
+  ax[0].semilogy(r, rho, "k.")
+  ax[1].plot(r, vr, "k.")
+  ax[2].semilogy(r, P, "k.")
+
+  for i in range(3):
+    ax[i].set_xlabel("radius")
+  ax[0].set_ylabel("density")
+  ax[1].set_ylabel("velocity")
+  ax[2].set_ylabel("pressure")
+  ax[1].set_title("time = {0:.2f}".format(
+                    	float(file["/Header"].attrs["Time"])))	
+
+  pl.tight_layout()
+  pl.savefig(imagefile)
---
+++ examples/EvrardCollapse_3D/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ # Generate the initial conditions if they are not present.
+if [ ! -e evrard.hdf5 ]
+then
+    echo "Generating initial conditions for the Evrard collapse example..."
+    python makeIC.py
+fi
+
+# Run SWIFT
+../swift -s -G -t 4 evrard.yml 2>&1 | tee output.log
+
+# Plot the solution
+python plot_density_profile.py
