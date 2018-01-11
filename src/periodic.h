73d4e5db66fe4f387fb9448dc875b3e98e3baf9c
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2018-01-11 18:03:35 +0100

    Sort particles consistently between edges.

--- src/periodic.h
+++ src/periodic.h
@@ -36,7 +36,7 @@
     const __typeof__(x) _x = (x);                       \
     const __typeof__(a) _a = (a);                       \
     const __typeof__(b) _b = (b);                       \
-    _x < _a ? (_x + _b) : ((_x > _b) ? (_x - _b) : _x); \
+    _x < _a ? (_x + _b) : ((_x >= _b) ? (_x - _b) : _x);\
   })
 
 /**
