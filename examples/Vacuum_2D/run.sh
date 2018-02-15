7d4dce9b35e7c1dcd24b1e7f99b96a621afd0f49
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2018-02-15 15:27:26 +0000

    Added 3D vacuum expansion test.

--- examples/Vacuum_2D/run.sh
+++ examples/Vacuum_2D/run.sh
@@ -3,12 +3,12 @@
 # Generate the initial conditions if they are not present.
 if [ ! -e glassPlane_128.hdf5 ]
 then
-    echo "Fetching initial glass file for the 1D vacuum expansion example..."
+    echo "Fetching initial glass file for the 2D vacuum expansion example..."
     ./getGlass.sh
 fi
 if [ ! -e vacuum.hdf5 ]
 then
-    echo "Generating initial conditions for the 1D vacuum expansion example..."
+    echo "Generating initial conditions for the 2D vacuum expansion example..."
     python makeIC.py
 fi
 
