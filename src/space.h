868b77e5f75b26722d1b4bd7a4ffd7fa57248824
lhausamm <loic_hausammann@hotmail.com>
2018-04-18 16:28:29 +0200

    Parallel first initialization

--- src/space.h
+++ src/space.h
@@ -222,11 +222,10 @@ void space_do_parts_sort();
 void space_do_gparts_sort();
 void space_do_sparts_sort();
 void space_first_init_parts(struct space *s,
-                            const struct chemistry_global_data *chemistry,
-                            const struct cooling_function_data *cool_func);
+                            int verbose);
 void space_first_init_gparts(struct space *s,
-                             const struct gravity_props *grav_props);
-void space_first_init_sparts(struct space *s);
+			     int verbose);
+void space_first_init_sparts(struct space *s, int verbose);
 void space_init_parts(struct space *s, int verbose);
 void space_init_gparts(struct space *s, int verbose);
 void space_convert_quantities(struct space *s, int verbose);
