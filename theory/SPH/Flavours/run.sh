d7a1830c01a4edc9c8d6d2e64354202a3308b71a
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-05-21 17:05:19 +0100

    Added new information to sph_flavours about P-U SPH

--- theory/SPH/Flavours/run.sh
+++ theory/SPH/Flavours/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+python plotSoundspeed.py
+
 pdflatex -jobname=sph_flavours sph_flavours_standalone.tex
 bibtex sph_flavours.aux 
 pdflatex -jobname=sph_flavours sph_flavours_standalone.tex
