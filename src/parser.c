5a3b2fcc3f96a0df107ca218cf30d9504459dead
lhausamm <loic_hausammann@hotmail.com>
2018-06-01 13:58:33 +0200

    Add optional parameter in params

--- src/parser.c
+++ src/parser.c
@@ -634,6 +634,18 @@ int parser_get_opt_param_int(const struct swift_params *params,
     }
   }
 
+  /* Generate string for new parameter */
+  strcpy(str, name);
+  char str_tmp[PARSER_MAX_LINE_SIZE];
+  sprintf(str_tmp, ": %i", def);
+  strcat(str, str_tmp);
+
+  /* Add it to params */
+  parser_set_param(tmp, str);
+
+  /* Set parameter as used */
+  tmp->data[params->paramCount-1].used = 1;
+  
   return def;
 }
 
@@ -668,6 +680,18 @@ char parser_get_opt_param_char(const struct swift_params *params,
       return retParam;
     }
   }
+
+  /* Generate string for new parameter */
+  strcpy(str, name);
+  char str_tmp[PARSER_MAX_LINE_SIZE];
+  sprintf(str_tmp, ": %c", def);
+  strcat(str, str_tmp);
+
+  /* Add it to params */
+  parser_set_param(tmp, str);
+
+  /* Set parameter as used */
+  tmp->data[params->paramCount-1].used = 1;
 
   return def;
 }
@@ -704,6 +728,18 @@ float parser_get_opt_param_float(const struct swift_params *params,
     }
   }
 
+  /* Generate string for new parameter */
+  strcpy(str, name);
+  char str_tmp[PARSER_MAX_LINE_SIZE];
+  sprintf(str_tmp, ": %f", def);
+  strcat(str, str_tmp);
+
+  /* Add it to params */
+  parser_set_param(tmp, str);
+
+  /* Set parameter as used */
+  tmp->data[params->paramCount-1].used = 1;
+
   return def;
 }
 
@@ -739,6 +775,18 @@ double parser_get_opt_param_double(const struct swift_params *params,
     }
   }
 
+  /* Generate string for new parameter */
+  strcpy(str, name);
+  char str_tmp[PARSER_MAX_LINE_SIZE];
+  sprintf(str_tmp, ": %lf", def);
+  strcat(str, str_tmp);
+
+  /* Add it to params */
+  parser_set_param(tmp, str);
+
+  /* Set parameter as used */
+  tmp->data[params->paramCount-1].used = 1;
+
   return def;
 }
 
@@ -764,6 +812,19 @@ void parser_get_opt_param_string(const struct swift_params *params,
       return;
     }
   }
+
+  /* Generate string for new parameter */
+  char str[PARSER_MAX_LINE_SIZE];
+  strcpy(str, name);
+  char str_tmp[PARSER_MAX_LINE_SIZE];
+  sprintf(str_tmp, ": %s", def);
+  strcat(str, str_tmp);
+
+  /* Add it to params */
+  parser_set_param(tmp, str);
+
+  /* Set parameter as used */
+  tmp->data[params->paramCount-1].used = 1;
 
   strcpy(retParam, def);
 }
