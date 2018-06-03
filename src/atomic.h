28127ee8b341df143cb1e168dcb266560a045023
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2018-06-03 19:33:18 +0200

    Added some 'const' decoration and better documentation to the atomics to make them easier to follow.

--- src/atomic.h
+++ src/atomic.h
@@ -39,13 +39,16 @@
  *
  * This is a text-book implementation based on an atomic CAS.
  *
+ * We create a temporary union to cope with the int-only atomic CAS
+ * and the floating-point min that we want.
+ *
  * @param address The address to update.
  * @param y The value to update the address with.
  */
 __attribute__((always_inline)) INLINE static void atomic_min_f(
-    volatile float* address, float y) {
+    volatile float* const address, const float y) {
 
-  int* int_ptr = (int*)address;
+  int* const int_ptr = (int* const)address;
 
   typedef union {
     float as_float;
@@ -67,13 +70,16 @@ __attribute__((always_inline)) INLINE static void atomic_min_f(
  *
  * This is a text-book implementation based on an atomic CAS.
  *
+ * We create a temporary union to cope with the int-only atomic CAS
+ * and the floating-point max that we want.
+ *
  * @param address The address to update.
  * @param y The value to update the address with.
  */
 __attribute__((always_inline)) INLINE static void atomic_max_f(
-    volatile float* address, float y) {
+    volatile float* const address, const float y) {
 
-  int* int_ptr = (int*)address;
+  int* const int_ptr = (int* const)address;
 
   typedef union {
     float as_float;
@@ -95,13 +101,16 @@ __attribute__((always_inline)) INLINE static void atomic_max_f(
  *
  * This is a text-book implementation based on an atomic CAS.
  *
+ * We create a temporary union to cope with the int-only atomic CAS
+ * and the floating-point add that we want.
+ *
  * @param address The address to update.
  * @param y The value to update the address with.
  */
 __attribute__((always_inline)) INLINE static void atomic_add_f(
-    volatile float* address, float y) {
+    volatile float* const address, const float y) {
 
-  int* int_ptr = (int*)address;
+  int* const int_ptr = (int* const)address;
 
   typedef union {
     float as_float;
