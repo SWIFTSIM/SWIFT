92b113f4049207916f2c6494fd0798e3caa1f415
lhausamm <loic_hausammann@hotmail.com>
2018-06-21 12:51:45 +0200

    Add Gear snapshots

--- examples/AgoraDisk/run.sh
+++ examples/AgoraDisk/run.sh
@@ -20,5 +20,15 @@ cp $1.hdf5 agora_disk.hdf5
 # Run SWIFT
 ../swift -s -G -t 8 agora_disk.yml 2>&1 | tee output.log
 
-# Plot the solution
-# python plotSolution.py 5
+echo "Changing smoothing length to be Gadget compatible"
+./change_h.py agora_disk_0000.hdf5
+./change_h.py agora_disk_0050.hdf5
+
+if [ ! -e snapshot_0000 ] || [ ! -e snapshot_0500 ]
+then
+    echo "Fetching Gear solution..."
+    ./getSolution.sh
+fi
+
+echo "Plotting..."
+./plot_solution.py
