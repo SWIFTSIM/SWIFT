2b765b45a943fa944f4711cb18bacdc79f3671f0
Peter W. Draper <p.w.draper@durham.ac.uk>
2018-07-05 11:08:32 +0100

    Dump top-level as well as super cells, we need top-level for comparison plots

--- src/debug.c
+++ src/debug.c
@@ -324,9 +324,9 @@ static void dumpCells_map(struct cell *c, void *data) {
     else
       active = 1;
 
-    /* So output local super cells that are active and have MPI
+    /* So output local super cells or top-level cells that are active and have MPI
      * tasks as requested. */
-    if (c->nodeID == e->nodeID && (!super ||(super && c->super == c)) && active && mpiactive) {
+    if (c->nodeID == e->nodeID && (!super ||((super && c->super == c) || (c->parent == NULL))) && active && mpiactive) {
 
       /* If requested we work out how many particles are active in this cell. */
       int pactcount = 0;
