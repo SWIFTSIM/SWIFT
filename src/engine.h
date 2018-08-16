59eb8882e985e1b394ea12a1568c1ffb54eb7ea9
Alexei Borissov <dc-bori1@cosma-a.cosma>
2018-08-16 12:54:25 +0100

    update to automated cooling box test, general tidy

--- src/engine.h
+++ src/engine.h
@@ -50,12 +50,6 @@
 #include "task.h"
 #include "units.h"
 
-// cooling counters
-int n_eagle_cooling_rate_calls_1;
-int n_eagle_cooling_rate_calls_2;
-int n_eagle_cooling_rate_calls_3;
-int n_eagle_cooling_rate_calls_4;
-
 /**
  * @brief The different policies the #engine can follow.
  */
