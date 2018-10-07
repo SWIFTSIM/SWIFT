d8c492ecacde505377c6c9a949ec82f1b3b7b379
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2018-10-07 12:47:24 +0100

    Set the default cosmology viscosity parameters before merging

--- src/const.h
+++ src/const.h
@@ -22,8 +22,8 @@
 
 /* SPH Viscosity constants. */
 /* Cosmology defaults: a=0.8, b=1.5a. Planetary defaults: a=1.5, b=2.0a */ 
-#define const_viscosity_alpha 1.5f
-#define const_viscosity_beta_over_alpha 2.0f
+#define const_viscosity_alpha 0.8f
+#define const_viscosity_beta_over_alpha 1.5f
 
 #define const_viscosity_alpha_min \
   0.1f /* Values taken from (Price,2004), not used in legacy gadget mode */
