0ac817abddffcc042d9fbfc7b2490f15e65b00fc
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2018-10-24 16:03:38 +0200

    Also adapt the unit tests to the new i/o function signatures.

--- tests/testSelectOutput.c
+++ tests/testSelectOutput.c
@@ -85,8 +85,8 @@ int main(int argc, char *argv[]) {
 
   char *base_name = "testSelectOutput";
   size_t Ngas = 0, Ngpart = 0, Nspart = 0;
-  int periodic = -1;
   int flag_entropy_ICs = -1;
+  int periodic = 1;
   double dim[3];
   struct part *parts = NULL;
   struct gpart *gparts = NULL;
@@ -111,8 +111,8 @@ int main(int argc, char *argv[]) {
   /* Read data */
   message("Reading initial conditions.");
   read_ic_single("input.hdf5", &us, dim, &parts, &gparts, &sparts, &Ngas,
-                 &Ngpart, &Nspart, &periodic, &flag_entropy_ICs, 1, 0, 0, 0, 0,
-                 1., 1., 1, 0);
+                 &Ngpart, &Nspart, &flag_entropy_ICs, 1, 0, 0, 0, 0, 1., 1., 1,
+                 0);
 
   /* pseudo initialization of the space */
   message("Initialization of the space.");
