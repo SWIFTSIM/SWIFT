324ee0ec8bf09677ff0793f091e67fd316446739
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2018-11-04 16:04:23 +0000

    More post-merge fixes.

--- src/engine_maketasks.c
+++ src/engine_maketasks.c
@@ -80,8 +80,8 @@ void engine_addtasks_send_gravity(struct engine *e, struct cell *ci,
     /* Create the tasks and their dependencies? */
     if (t_grav == NULL) {
 
-      /* Create a tag for this cell. */
-      if (ci->mpi.tag < 0) cell_tag(ci);
+      /* Make sure this cell is tagged. */
+      cell_ensure_tagged(ci);
 
       t_grav = scheduler_addtask(s, task_type_send, task_subtype_gpart,
                                  ci->mpi.tag, 0, ci, cj);
@@ -139,8 +139,8 @@ void engine_addtasks_send_hydro(struct engine *e, struct cell *ci,
     /* Create the tasks and their dependencies? */
     if (t_xv == NULL) {
 
-      /* Create a tag for this cell. */
-      if (ci->mpi.tag < 0) cell_tag(ci);
+      /* Make sure this cell is tagged. */
+      cell_ensure_tagged(ci);
 
       t_xv = scheduler_addtask(s, task_type_send, task_subtype_xv, ci->mpi.tag,
                                0, ci, cj);
@@ -238,8 +238,8 @@ void engine_addtasks_send_timestep(struct engine *e, struct cell *ci,
     /* Create the tasks and their dependencies? */
     if (t_ti == NULL) {
 
-      /* Create a tag for this cell. */
-      if (ci->mpi.tag < 0) cell_tag(ci);
+      /* Make sure this cell is tagged. */
+      cell_ensure_tagged(ci);
 
       t_ti = scheduler_addtask(s, task_type_send, task_subtype_tend,
                                ci->mpi.tag, 0, ci, cj);
