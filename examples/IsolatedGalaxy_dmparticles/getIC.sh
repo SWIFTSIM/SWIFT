6d567f90f4f4ebb911c783e0ef990dbcbca50b07
Folkert Nobels <nobels@strw.leidenuniv.nl>
2018-11-07 16:28:18 +0100

    Improve the DM halo example

---
+++ examples/IsolatedGalaxy_dmparticles/getIC.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget http://www.strw.leidenuniv.nl/~nobels/data/reddeathgalaxywithDM.hdf5
--- examples/IsolatedGalaxy_dmparticles/run.sh
+++ examples/IsolatedGalaxy_dmparticles/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ ! -e reddeathgalaxywithDM.hdf5 ]
+then 
+    echo "Fetch the isolated galaxy initial conditions with a DM halo"
+    ./getIC.sh
+fi
+
 ../swift -G -S -t 64 isolated_galaxy.yml 2>&1 | tee output.log
 
