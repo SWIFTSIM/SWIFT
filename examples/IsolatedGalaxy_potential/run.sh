87189d7fdc9e943834a1fd78b4f2f5e570f9141a
Folkert Nobels <nobels@strw.leidenuniv.nl>
2018-11-21 10:48:13 +0100

    Update the run.sh file to also execute the python scripts

--- examples/IsolatedGalaxy_potential/run.sh
+++ examples/IsolatedGalaxy_potential/run.sh
@@ -8,3 +8,17 @@ fi
 
 ../swift -g -G -S -t 16 isolated_galaxy.yml 2>&1 | tee output.log
 
+
+echo "Make plots of conservation of total angular momentum" 
+if command -v python3 &>/dev/null; then
+    python3 angularmomentum.py 
+else
+    python angularmomentum.py 
+fi
+
+echo "Make plots of change of vertical and radial profile"
+if command -v python3 &>/dev/null; then
+    python3 profilefit.py 
+else
+    python profilefit.py 
+fi
