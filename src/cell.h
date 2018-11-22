5a87a4b4a8a26d84a25b5d12a0f8c10e078d10f0
James Willis <james.s.willis@durham.ac.uk>
2018-11-22 16:18:25 +0000

    Perform FOF tasks splitting using the gravity task strategy.

--- src/cell.h
+++ src/cell.h
@@ -841,7 +841,8 @@ __attribute__((always_inline)) INLINE static int cell_can_split_self_fof_task(
     const struct cell *c) {
 
   /* Is the cell split ? */
-  return c->split && c->grav.count > 400 && c->depth < 4;
+  //return c->split && c->grav.count > 400 && c->depth < 4;
+  return c->split && ((c->maxdepth - c->depth) > space_subdepth_diff_grav);
 }
 
 /**
