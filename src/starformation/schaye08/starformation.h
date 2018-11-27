01251d5968d52b5b5097f9c4b4a88a812bc0f290
Folkert Nobels <nobels@strw.leidenuniv.nl>
2018-11-27 17:19:10 +0100

    Start of the starformation gas to star function pair and add example syntax to the parameter_example file

--- src/starformation/schaye08/starformation.h
+++ src/starformation/schaye08/starformation.h
@@ -79,6 +79,7 @@ struct star_formation {
  * @brief Calculate if the gas has the potential of becoming
  * a star.
  *
+<<<<<<< HEAD
  * @param starform the star formation law properties to use.
  * @param p the gas particles
  * @param xp the additional properties of the gas particles
@@ -132,11 +133,18 @@ static int starformation_potential_to_become_star(
   } else {
     return 0;
   }
+=======
+ *
+ * */
+static int starformation_potential_to_become_star(
+    ){
+  return 0;
 }
 
 /*
  * @brief Calculate if the gas particle is converted 
  *
+<<<<<<< HEAD
  * @param starform the star formation struct
  * @param p the gas particles with their properties
  * @param xp the additional gas particle properties
