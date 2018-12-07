fa961048b3688c5b14e33e3ca6f85ae773a4e4fb
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2018-12-07 15:31:44 +0100

    Code formatting

--- tests/testCooling.c
+++ tests/testCooling.c
@@ -19,12 +19,12 @@
 
 #include <unistd.h>
 #include "cooling.h"
+#include "cooling/EAGLE/interpolate.h"
 #include "cooling_struct.h"
 #include "hydro.h"
 #include "physical_constants.h"
 #include "swift.h"
 #include "units.h"
-#include "cooling/EAGLE/interpolate.h"
 
 /*
  * @brief Assign particle density and entropy corresponding to the
@@ -40,29 +40,27 @@
  * @param u Internal energy (cgs units)
  * @param ti_current integertime to set cosmo quantities
  */
-void set_quantities(struct part *restrict p,
-		    struct xpart *restrict xp,
+void set_quantities(struct part *restrict p, struct xpart *restrict xp,
                     const struct unit_system *restrict us,
                     const struct cooling_function_data *restrict cooling,
                     struct cosmology *restrict cosmo,
                     const struct phys_const *restrict phys_const, float nh_cgs,
-                    double u,
-		    integertime_t ti_current) {
+                    double u, integertime_t ti_current) {
 
   /* Update cosmology quantities */
-  cosmology_update(cosmo,phys_const,ti_current);
+  cosmology_update(cosmo, phys_const, ti_current);
 
   /* calculate density */
   double hydrogen_number_density = nh_cgs / cooling->number_density_scale;
   p->rho = hydrogen_number_density * phys_const->const_proton_mass /
-           p->chemistry_data.metal_mass_fraction[chemistry_element_H] * (cosmo->a * cosmo->a * cosmo->a);
+           p->chemistry_data.metal_mass_fraction[chemistry_element_H] *
+           (cosmo->a * cosmo->a * cosmo->a);
 
   /* update entropy based on internal energy */
   float pressure = (u * cosmo->a * cosmo->a) / cooling->internal_energy_scale *
                    p->rho * (hydro_gamma_minus_one);
   p->entropy = pressure * (pow(p->rho, -hydro_gamma));
   xp->entropy_full = p->entropy;
-
 }
 
 /*
@@ -83,17 +81,17 @@ int main(int argc, char **argv) {
   struct cosmology cosmo;
   char *parametersFileName = "./testCooling.yml";
 
-  float nh;              // hydrogen number density
-  double u;              // internal energy
+  float nh;  // hydrogen number density
+  double u;  // internal energy
 
-  /* Number of values to test for in redshift, 
+  /* Number of values to test for in redshift,
    * hydrogen number density and internal energy */
   const int n_z = 50;
   const int n_nh = 50;
-  const int n_u = 50; 
+  const int n_u = 50;
 
   /* Number of subcycles and tolerance used to compare
-   * subcycled and implicit solution. Note, high value 
+   * subcycled and implicit solution. Note, high value
    * of tolerance due to mismatch between explicit and
    * implicit solution for large timesteps */
   const int n_subcycle = 1000;
@@ -143,39 +141,56 @@ int main(int argc, char **argv) {
   /* Cooling function needs to know the minimal energy. Set it to the lowest
    * internal energy in the cooling table. */
   struct hydro_props hydro_properties;
-  hydro_properties.minimal_internal_energy = exp(M_LN10 * cooling.Therm[0]) / cooling.internal_energy_scale;
-  
+  hydro_properties.minimal_internal_energy =
+      exp(M_LN10 * cooling.Therm[0]) / cooling.internal_energy_scale;
+
   /* calculate spacing in nh and u */
-  const float delta_nh = (cooling.nH[cooling.N_nH - 1] - cooling.nH[0])/n_nh;
-  const float delta_u = (cooling.Therm[cooling.N_Temp - 1] - cooling.Therm[0])/n_u;
+  const float delta_nh = (cooling.nH[cooling.N_nH - 1] - cooling.nH[0]) / n_nh;
+  const float delta_u =
+      (cooling.Therm[cooling.N_Temp - 1] - cooling.Therm[0]) / n_u;
 
-  for(int z_i = 0; z_i < n_z; z_i++) {
-    integertime_t ti_current = max_nr_timesteps/n_z*z_i;
-    for(int nh_i = 0; nh_i < n_nh; nh_i++) {
-      nh = exp(M_LN10 * cooling.nH[0] + delta_nh*nh_i);
+  for (int z_i = 0; z_i < n_z; z_i++) {
+    integertime_t ti_current = max_nr_timesteps / n_z * z_i;
+    for (int nh_i = 0; nh_i < n_nh; nh_i++) {
+      nh = exp(M_LN10 * cooling.nH[0] + delta_nh * nh_i);
       for (int u_i = 0; u_i < n_u; u_i++) {
-	u = exp(M_LN10 * cooling.Therm[0] + delta_u*u_i);
+        u = exp(M_LN10 * cooling.Therm[0] + delta_u * u_i);
 
-	/* update nh, u, z */
-        set_quantities(&p,&xp,&us,&cooling,&cosmo,&phys_const, nh, u, ti_current);
-        
-	/* calculate subcycled solution */
-	for (int t_subcycle = 0; t_subcycle < n_subcycle; t_subcycle++) {
-	  p.entropy_dt = 0;
-          cooling_cool_part(&phys_const,&us,&cosmo,&hydro_properties,&cooling,&p,&xp,dt_cool/n_subcycle,dt_therm/n_subcycle);
-          xp.entropy_full += p.entropy_dt*dt_therm/n_subcycle;
-	}
-        double u_subcycled = hydro_get_physical_internal_energy(&p,&xp,&cosmo)*cooling.internal_energy_scale;
+        /* update nh, u, z */
+        set_quantities(&p, &xp, &us, &cooling, &cosmo, &phys_const, nh, u,
+                       ti_current);
 
-	/* reset quantities to nh, u, and z that we want to test */
-        set_quantities(&p,&xp,&us,&cooling,&cosmo,&phys_const, nh, u, ti_current);
+        /* calculate subcycled solution */
+        for (int t_subcycle = 0; t_subcycle < n_subcycle; t_subcycle++) {
+          p.entropy_dt = 0;
+          cooling_cool_part(&phys_const, &us, &cosmo, &hydro_properties,
+                            &cooling, &p, &xp, dt_cool / n_subcycle,
+                            dt_therm / n_subcycle);
+          xp.entropy_full += p.entropy_dt * dt_therm / n_subcycle;
+        }
+        double u_subcycled =
+            hydro_get_physical_internal_energy(&p, &xp, &cosmo) *
+            cooling.internal_energy_scale;
 
-	/* compute implicit solution */
-	cooling_cool_part(&phys_const,&us,&cosmo,&hydro_properties,&cooling,&p,&xp,dt_cool,dt_therm);
-	double u_implicit = hydro_get_physical_internal_energy(&p,&xp,&cosmo)*cooling.internal_energy_scale;
-	
-	/* check if the two solutions are consistent */
-	if (fabs((u_implicit-u_subcycled)/u_subcycled) > integration_tolerance) message("implicit and subcycled solutions do not match. z_i %d nh_i %d u_i %d implicit %.5e subcycled %.5e error %.5e", z_i, nh_i, u_i, u_implicit, u_subcycled, fabs((u_implicit-u_subcycled)/u_subcycled));
+        /* reset quantities to nh, u, and z that we want to test */
+        set_quantities(&p, &xp, &us, &cooling, &cosmo, &phys_const, nh, u,
+                       ti_current);
+
+        /* compute implicit solution */
+        cooling_cool_part(&phys_const, &us, &cosmo, &hydro_properties, &cooling,
+                          &p, &xp, dt_cool, dt_therm);
+        double u_implicit =
+            hydro_get_physical_internal_energy(&p, &xp, &cosmo) *
+            cooling.internal_energy_scale;
+
+        /* check if the two solutions are consistent */
+        if (fabs((u_implicit - u_subcycled) / u_subcycled) >
+            integration_tolerance)
+          message(
+              "implicit and subcycled solutions do not match. z_i %d nh_i %d "
+              "u_i %d implicit %.5e subcycled %.5e error %.5e",
+              z_i, nh_i, u_i, u_implicit, u_subcycled,
+              fabs((u_implicit - u_subcycled) / u_subcycled));
       }
     }
   }
@@ -184,4 +199,3 @@ int main(int argc, char **argv) {
   free(params);
   return 0;
 }
-
