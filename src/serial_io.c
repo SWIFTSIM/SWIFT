8586ddbb866a00bb32d9d2852bb3a62c889bd983
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2018-12-10 16:10:31 +0000

    Add the Temperature to the output of the particles in the EAGLE cooling model.

--- src/serial_io.c
+++ src/serial_io.c
@@ -1071,8 +1071,8 @@ void write_output_serial(struct engine* e, const char* baseName,
               Nparticles = Ngas;
               hydro_write_particles(parts, xparts, list, &num_fields);
               num_fields += chemistry_write_particles(parts, list + num_fields);
-              num_fields += cooling_write_particles(xparts, list + num_fields,
-                                                    e->cooling_func);
+              num_fields += cooling_write_particles(
+                  parts, xparts, list + num_fields, e->cooling_func);
             } else {
 
               /* Ok, we need to fish out the particles we want */
@@ -1095,8 +1095,9 @@ void write_output_serial(struct engine* e, const char* baseName,
                                     &num_fields);
               num_fields +=
                   chemistry_write_particles(parts_written, list + num_fields);
-              num_fields += cooling_write_particles(
-                  xparts_written, list + num_fields, e->cooling_func);
+              num_fields +=
+                  cooling_write_particles(parts_written, xparts_written,
+                                          list + num_fields, e->cooling_func);
             }
           } break;
 
--- src/single_io.c
+++ src/single_io.c
@@ -890,8 +890,8 @@ void write_output_single(struct engine* e, const char* baseName,
           N = Ngas;
           hydro_write_particles(parts, xparts, list, &num_fields);
           num_fields += chemistry_write_particles(parts, list + num_fields);
-          num_fields += cooling_write_particles(xparts, list + num_fields,
-                                                e->cooling_func);
+          num_fields += cooling_write_particles(
+              parts, xparts, list + num_fields, e->cooling_func);
         } else {
 
           /* Ok, we need to fish out the particles we want */
@@ -914,8 +914,9 @@ void write_output_single(struct engine* e, const char* baseName,
                                 &num_fields);
           num_fields +=
               chemistry_write_particles(parts_written, list + num_fields);
-          num_fields += cooling_write_particles(
-              xparts_written, list + num_fields, e->cooling_func);
+          num_fields +=
+              cooling_write_particles(parts_written, xparts_written,
+                                      list + num_fields, e->cooling_func);
         }
       } break;
 
