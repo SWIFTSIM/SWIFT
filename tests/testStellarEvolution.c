f6e3261c9318958d1af527c4a3880dfce9dd837e
Alexei Borissov <dc-bori1@cosma-b.pri.cosma7.alces.network>
2018-12-11 10:11:29 +0000

    fixed bugs in stellar evolution test

---
+++ tests/testStellarEvolution.c
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (C) 2015 Matthieu Schaller (matthieu.schaller@durham.ac.uk).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#include <unistd.h>
+#include "hydro.h"
+#include "physical_constants.h"
+#include "swift.h"
+#include "units.h"
+#include "stars.h"
+
+/*
+ * @brief Produces contributions to cooling rates for different
+ * hydrogen number densities, from different metals,
+ * tests 1d and 4d table interpolations produce
+ * same results for cooling rate, dlambda/du and temperature.
+ */
+int main(int argc, char **argv) {
+  /* Declare relevant structs */
+  struct swift_params *params = malloc(sizeof(struct swift_params));
+  struct unit_system us;
+  struct chemistry_global_data chem_data;
+  struct part p;
+  struct spart sp;
+  struct xpart xp;
+  struct phys_const phys_const;
+  struct cosmology cosmo;
+  struct hydro_props hp;
+  struct stars_props stars;
+  char *parametersFileName = "./testStellarEvolution.yml";
+
+  /* Read the parameter file */
+  if (params == NULL) error("Error allocating memory for the parameter file.");
+  message("Reading runtime parameters from file '%s'", parametersFileName);
+  parser_read_file(parametersFileName, params);
+
+  /* Init units */
+  units_init_from_params(&us, params, "InternalUnitSystem");
+  phys_const_init(&us, params, &phys_const);
+
+  /* Init chemistry */
+  chemistry_init(params, &us, &phys_const, &chem_data);
+  chemistry_first_init_part(&phys_const, &us, &cosmo, &chem_data, &p, &xp);
+  chemistry_print(&chem_data);
+
+  /* Init cosmology */
+  cosmology_init(params, &us, &phys_const, &cosmo);
+  cosmology_print(&cosmo);
+
+  /* Init hydro_props  */
+  hydro_props_init(&hp, &phys_const, &us, params);
+
+  /* Init stars_props  */
+  stars_props_init(&stars, &phys_const, &us, params, &hp);
+  // To do: Read in IMF model from yaml file.
+  strcpy(stars.IMF_Model, "Chabrier");
+
+  /* Read yield tables  */
+  stars_evolve_init(params,&stars);
+
+  /* Create spart */
+  stars_first_init_spart(&sp);
+  stars_init_spart(&sp);
+  // Should these assignments be done in stars_init_part? Check with Matthieu.
+  sp.chemistry_data = p.chemistry_data;
+  sp.age = 0.f;
+
+  /* Evolve spart */
+  stars_evolve_spart(&sp, &stars, &cosmo);
+
+  free(params);
+
+  message("done stellar evolution test");
+  return 0;
+}
+
