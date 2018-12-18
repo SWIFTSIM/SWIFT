0192652f53aa7118ea73e0829135f66cf0c4d31c
Aidan Chalk <aidan.chalk@stfc.ac.uk>
2018-12-18 18:52:14 +0000

    Add initial implementations of the C11 atomic versions of current GNU99 atomics, as discussed in #129

--- src/atomic.h
+++ src/atomic.h
@@ -19,6 +19,13 @@
 #ifndef SWIFT_ATOMIC_H
 #define SWIFT_ATOMIC_H
 
+#if STDC_VERSION >= 201112L
+  #if !defined(__GNUC__) ||( __GNUC__ > 4 || \
+    (__GNUC__ == 4 && (__GNUC_MINOR >= 9 )))
+    #define SWIFT_MODERN_ATOMICS
+  #endif
+#endif
+
 /* Config parameters. */
 #include "../config.h"
 
@@ -26,6 +33,229 @@
 #include "inline.h"
 #include "minmax.h"
 
+//#if STDC_VERSION >= 201112L
+
+#ifdef NOT_DEFINED
+#include <stdatomic.h>
+
+#define atomic_add(v, i) atomic_fetch_add(v, i)
+#define atomic_sub(v, i) atomic_fetch_sub(v, i)
+#define atomic_or(v, i) atomic_fetch_or(v, i)
+#define atomic_inv(v) atomic_fetch_add(v, 1)
+#define atomic_dec(v) atomic_fetch_sub(v, 1)
+#ifdef SWIFT_MODERN_ATOMICS
+typedef _Atomic(float) atomic_float;
+typedef _Atomic(double) atomic_double;
+#define atomic_cas(obj,expected,desired) _Generic((desired), \
+  int: val_atomic_compare_and_swap_i, \
+  long long: val_atomic_compare_and_swap_ll, \
+  float: val_atomic_compare_and_swap_f, \
+  default: val_atomic_compare_and_swap_ll \
+  )(obj,expected,desired)
+#else
+typedef float atomic_float;
+typedef double atomic_double;
+#define atomic_cas(obj,expected,desired) _Generic((desired), \
+  int: val_atomic_compare_and_swap_i, \
+  long long: val_atomic_compare_and_swap_ll, \
+  default: val_atomic_compare_and_swap_ll \
+  )(obj,expected,desired)
+#endif
+#define atomic_swap(v, n) atomic_exchange(v,n)
+
+int val_atomic_compare_and_swap_i( atomic_int *obj, int *expected, int desired){
+  int preexpected = *expected;
+  atomic_compare_exchange_strong(obj, expected, desired);
+  int retval = *expected;
+  *expected = preexpected;
+  return retval;
+}
+
+long long val_atomic_compare_and_swap_ll( atomic_llong *obj, long long *expected, long long desired){
+  long long preexpected = *expected;
+  atomic_compare_exchange_strong(obj, expected, desired);
+  int retval = *expected;
+  *expected = preexpected;
+  return retval;
+}
+
+#ifdef SWIFT_MODERN_ATOMICS
+float val_atomic_compare_and_swap_f( atomic_float *obj, float *expected, float desired){
+  float preexpected = *expected;
+  atomic_compare_exchange_strong( obj, expected, desired);
+  int retval = *expected;
+  *expected = preexpected;
+  return retval;
+
+}
+
+#endif
+/**
+ * @brief Atomic min operation on floats.
+ *
+ * This is a text-book implementation based on an atomic CAS.
+ *
+ * We create a temporary union to cope with the int-only atomic CAS
+ * and the floating-point min that we want.
+ *
+ * @param address The address to update.
+ * @param y The value to update the address with.
+ */
+#ifdef SWIFT_MODERN_ATOMICS
+static void atomic_min_f( atomic_float *obj, float const y){
+
+  float test_val, old_val, new_val;
+  old_val = *obj;
+
+  do{
+    test_val = old_val;
+    new_val = fmin(old_val, y);
+    old_val = atomic_cas( obj, &test_val, new_val);
+  }while(test_val != old_val);
+
+}
+#else
+static void atomic_min_f( volatile float *const address, float const y) {
+
+  atomic_int *const int_ptr = (atomic_int *) address;
+
+  typedef union{
+    float as_float;
+    int as_int;
+  } cast_type;
+
+  cast_type test_val, old_val, new_val;
+  old_val.as_float = *address;
+
+  do {
+    test_val.as_int = old_val.as_int;
+    new_val.as_float = fmin(old_val.as_float, y);
+    old_val.as_int = atomic_cas(int_ptr, &test_val.as_int, new_val.as_int);
+  } while (test_val.as_int != old_val.as_int);
+
+}
+
+#endif
+
+/**
+ * @brief Atomic max operation on floats.
+ *
+ * This is a text-book implementation based on an atomic CAS.
+ *
+ * We create a temporary union to cope with the int-only atomic CAS
+ * and the floating-point max that we want.
+ *
+ * @param address The address to update.
+ * @param y The value to update the address with.
+ */
+#ifdef SWIFT_MODERN_ATOMICS
+static void atomic_max_f( atomic_float *obj, float const y){
+
+  float test_val, old_val, new_val;
+  old_val = *obj;
+
+  do{
+    test_val = old_val;
+    new_val = fmax(old_val, y);
+    old_val = atomic_cas( obj, &test_val, new_val);
+  }while(test_val != old_val);
+
+}
+#else
+static void atomic_max_f( volatile float *const address, float const y) {
+
+  atomic_int *const int_ptr = (atomic_int *) address;
+
+  typedef union{
+    float as_float;
+    int as_int;
+  } cast_type;
+
+  cast_type test_val, old_val, new_val;
+  old_val.as_float = *address;
+
+  do {
+    test_val.as_int = old_val.as_int;
+    new_val.as_float = fmax(old_val.as_float, y);
+    old_val.as_int = atomic_cas(int_ptr, &test_val.as_int, new_val.as_int);
+  } while (test_val.as_int != old_val.as_int);
+
+}
+#endif
+
+#ifdef SWIFT_MODERN_ATOMICS
+#define atomic_add_f(v, i) atomic_fetch_add(v, i)
+#else
+/**
+ * @brief Atomic add operation on floats.
+ *
+ * This is a text-book implementation based on an atomic CAS.
+ *
+ * We create a temporary union to cope with the int-only atomic CAS
+ * and the floating-point add that we want.
+ *
+ * @param address The address to update.
+ * @param y The value to update the address with.
+ */
+__attribute__((always_inline)) INLINE static void atomic_add_f(
+    volatile float *const address, const float y) {
+
+  atomic_int *const int_ptr = (int *)address;
+
+  typedef union {
+    float as_float;
+    int as_int;
+  } cast_type;
+
+  cast_type test_val, old_val, new_val;
+  old_val.as_float = *address;
+
+  do {
+    test_val.as_int = old_val.as_int;
+    new_val.as_float = old_val.as_float + y;
+    old_val.as_int = atomic_cas(int_ptr, test_val.as_int, new_val.as_int);
+  } while (test_val.as_int != old_val.as_int);
+}
+#endif
+
+/**
+ * @brief Atomic add operation on doubles.
+ *
+ * This is a text-book implementation based on an atomic CAS.
+ *
+ * We create a temporary union to cope with the int-only atomic CAS
+ * and the floating-point add that we want.
+ *
+ * @param address The address to update.
+ * @param y The value to update the address with.
+ */
+#ifdef SWIFT_MODERN_ATOMICS
+#define atomic_add_d(v, i) atomic_fetch_add(v, i)
+#else
+__attribute__((always_inline)) INLINE static void atomic_add_d(
+    volatile double *const address, const double y) {
+
+  atomic_llong *const long_long_ptr = (long long *)address;
+
+  typedef union {
+    double as_double;
+    long long as_long_long;
+  } cast_type;
+
+  cast_type test_val, old_val, new_val;
+  old_val.as_double = *address;
+
+  do {
+    test_val.as_long_long = old_val.as_long_long;
+    new_val.as_double = old_val.as_double + y;
+    old_val.as_long_long =
+        atomic_cas(long_long_ptr, test_val.as_long_long, new_val.as_long_long);
+  } while (test_val.as_long_long != old_val.as_long_long);
+}
+#endif
+
+#else
+//Old GNU99 implementation
 #define atomic_add(v, i) __sync_fetch_and_add(v, i)
 #define atomic_sub(v, i) __sync_fetch_and_sub(v, i)
 #define atomic_or(v, i) __sync_fetch_and_or(v, i)
@@ -314,5 +544,6 @@ atomic_add_d(volatile double *const address, const double y) {
         atomic_cas(long_long_ptr, test_val.as_long_long, new_val.as_long_long);
   } while (test_val.as_long_long != old_val.as_long_long);
 }
+#endif
 
 #endif /* SWIFT_ATOMIC_H */
