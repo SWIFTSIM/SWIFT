d79bcbc3ce9ca0df06a9f23aff078193cca22b16
Folkert Nobels <nobels@strw.leidenuniv.nl>
2019-01-14 15:45:15 +0100

    Fix a typo

--- src/tracers/none/tracers_io.h
+++ src/tracers/none/tracers_io.h
@@ -16,8 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-#ifndef SWIFT_TRACERS_EAGLE_IO_H
-#define SWIFT_TRACERS_EAGLE_IO_H
+#ifndef SWIFT_TRACERS_NONE_IO_H
+#define SWIFT_TRACERS_NONE_IO_H
 
 /* Config parameters. */
 #include "../config.h"
@@ -57,4 +57,4 @@ __attribute__((always_inline)) INLINE static int tracers_write_particles(
   return 0;
 }
 
-#endif /* SWIFT_TRACERS_EAGLE_IO_H */
+#endif /* SWIFT_TRACERS_NONE_IO_H */
