f661ea77f3968aa3257ad23db36c82370d1d4372
loikki <loic.hausammann@protonmail.ch>
2019-01-21 10:40:39 +0100

    Change function name

--- src/stars/Default/stars.h
+++ src/stars/Default/stars.h
@@ -75,11 +75,11 @@ __attribute__((always_inline)) INLINE static void stars_reset_predicted_values(
     struct spart* restrict sp) {}
 
 /**
- * @brief Finishes the calculation of (non-gravity) forces acting on stars
+ * @brief Finishes the calculation of the feedback
  *
  * @param sp The particle to act upon
  */
-__attribute__((always_inline)) INLINE static void stars_end_force(
+__attribute__((always_inline)) INLINE static void stars_end_feedback(
     struct spart* sp) {}
 
 /**
