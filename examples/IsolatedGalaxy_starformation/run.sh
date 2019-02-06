f5e13aee694c81f0388fcf60f6cf8c2beb84b1cf
Folkert Nobels <nobels@strw.leidenuniv.nl>
2019-02-06 10:27:41 +0100

    Add script to plot the SFH both from the snapshots and from the last snapshot only

--- examples/IsolatedGalaxy_starformation/run.sh
+++ examples/IsolatedGalaxy_starformation/run.sh
@@ -8,4 +8,8 @@ fi
 
 ../swift --threads=32 --external-gravity --self-gravity --stars --star-formation --cooling --temperature --hydro isolated_galaxy.yml 2>&1 | tee output.log
 
+# Kennicutt-Schmidt law plot
 python3 plotSolution.py
+
+# Plot that the random star formation matches the expected SFH
+python3 SFH.py
