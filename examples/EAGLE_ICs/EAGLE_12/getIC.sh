33b77e5a0e1a1b4fd086e0a45ef1ced17ad92a92
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-02-13 10:48:55 +0100

    Change the name of the EAGLE-12 ICs file.

--- examples/EAGLE_ICs/EAGLE_12/getIC.sh
+++ examples/EAGLE_ICs/EAGLE_12/getIC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/EAGLE_ICs/EAGLE_ICs_12.hdf5
+wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/EAGLE_ICs/EAGLE_L0012N0188_ICs.hdf5
