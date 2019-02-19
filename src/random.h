7755f7edd8be98b79b03e7850352af6a61cec9aa
Folkert Nobels <nobels@strw.leidenuniv.nl>
2019-02-19 11:33:34 +0100

    Update the random number generator to have MWC -> Xorshift -> Quadratic congruential generator

--- src/random.h
+++ src/random.h
@@ -33,10 +33,10 @@
  * doing!
  */
 enum random_number_type {
-  random_number_star_formation = 7,
-  random_number_stellar_feedback = 53,
-  random_number_stellar_enrichment = 197,
-  random_number_BH_feedback = 491
+  random_number_star_formation = 4294957665,
+  random_number_stellar_feedback = 3947008974,
+  random_number_stellar_enrichment = 2936881968,
+  random_number_BH_feedback = 1640531364
 };
 
 /**
@@ -59,16 +59,33 @@ INLINE static double random_unit_interval(const long long int id,
   /* Range used for the seeds. Best if prime */
   static const long long seed_range = RAND_MAX;
   static const double RAND_MAX_inv = 1. / ((double)RAND_MAX);
+  static const long long mwc_number =(int)pow(2,32) - 1;
 
   /* Calculate the seed */
   /* WARNING: Only change the math if you really know what you are doing!
-     The numbers are carefully chosen prime numbers that prevent correlation
-     with either the current integer time or the particle IDs.
-     The calculation overflows on purpose.  */
-  unsigned int seed = ((937LL * id + 1109LL) % 2147987LL +
-                       (ti_current - 1LL) % 1514917LL + (long long)type) %
-                      seed_range;
-
+   * The numbers are carefully chosen prime numbers that prevent correlation
+   * with either the current integer time or the particle IDs. The current 
+   * method also prevents any correlation between different random number 
+   * types.
+   * The calculation overflows on purpose.  
+   * 1. The first step is calculating the seed by using a multiply with carry
+   * (MWC) method, this method depends on the type of random number and 
+   * this therefore also prevents that there is any correlation between 
+   * the different types of random numbers.
+   * 2. After this we use the 64 bit Xorshift method to randomize the seeds
+   * even more. 
+   * 3. We calculate a prime multiplication for the id with a quadratic
+   * term.
+   * 4. We calculate the seed by using a Quadratic congruential generator,
+   * in which we use the id part and the current time step bin.
+   */
+  unsigned long long number = ti_current;
+  number = type * ( number & (mwc_number)) + (number >> 32);
+  number ^= number << 21;
+  number ^= number >> 35;
+  number ^= number << 4;
+  const unsigned long long idpart = 3457LL * id + 593LL * id;
+  unsigned int seed = (937LL * number + 5171LL * number * number + idpart + 1109LL)%9996361LL % seed_range;
   /* Generate a random number between 0 and 1. */
   return rand_r(&seed) * RAND_MAX_inv;
 }
