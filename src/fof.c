12864a6b7b166c64773716eb853ba423c1f64a04
John Helly <j.c.helly@durham.ac.uk>
2019-02-28 14:13:03 +0000

    Correction to qsort comparison function in fof.c

--- src/fof.c
+++ src/fof.c
@@ -173,7 +173,12 @@ void fof_init(struct space *s) {
 int cmp_func(const void *a, const void *b) {
   struct group_length *a_group_size = (struct group_length *)a;
   struct group_length *b_group_size = (struct group_length *)b;
-  return (b_group_size->size > a_group_size->size);
+  if(b_group_size->size > a_group_size->size)
+    return 1;
+  else if(b_group_size->size < a_group_size->size)
+    return -1;
+  else
+    return 0;
 }
 
 /* Finds the global root ID of the group a particle exists in. */
