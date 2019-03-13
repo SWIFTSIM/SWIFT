b2aa326b16093a4799404cda1f019cf133255b3f
Peter W. Draper <p.w.draper@durham.ac.uk>
2019-03-13 16:26:31 +0000

    Update to process the memory log dumps

--- examples/process_memuse.py
+++ examples/process_memuse.py
@@ -3,11 +3,12 @@
 Usage:
     process_memuse.py output.dat
 
-Parse the output of a run of SWIFT to convert the memuse output strings
-into memory use per rank per step.
+Parse the output of a run of SWIFT to convert the memuse output dumps
+into a timeseries of memory use. Also outputs use in memory per labelled
+type.
 
 This file is part of SWIFT.
-Copyright (c) 2018 Peter W. Draper (p.w.draper@durham.ac.uk)
+Copyright (c) 2019 Peter W. Draper (p.w.draper@durham.ac.uk)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published
@@ -28,28 +29,60 @@ from collections import OrderedDict
 
 #  Command-line arguments.
 if len(sys.argv) != 2:
-    print "usage: ", sys.argv[0], " output.dat"
+    print "usage: ", sys.argv[0], " memuse_report<>.dat"
     sys.exit(1)
 
 memuse = OrderedDict()
+labels = {}
+totalmem = 0
 
 with open(sys.argv[1]) as infile:
     for line in infile:
-        if ":memuse:" in line:
-            words = line.split()
-            if len(words) > 5:
-                key = words[3] + ":" + words[4]
-                if key not in memuse:
-                    memuse[key] = 0
-                try:
-                    memuse[key] = memuse[key] + int(words[-1])
-                except ValueError:
-                    pass
+        if line[0] == "#":
+            if "# Current use:" in line:
+                print line
+        else:
+            tic , adr, rank, step, allocated, label, size = line.split()
+            rank = int(rank)
+            step = int(step)
+            allocated = int(allocated)
+            size = int(size)
 
-print "# step label function memuse"
-for key, value in memuse.items():
-    words = key.split(":", 2)
-    if len(words) == 3:
-        print words[0], words[1], words[2], value
+            doprint = True
+            if allocated == 1:
+                #  Allocation.
+                totalmem = totalmem + size
+                if not adr in memuse:
+                    memuse[adr] = [size]
+                    labels[adr] = label
+                else:
+                    memuse[adr] = memuse[adr].append(size)
+            else:
+                #  Free, locate allocation.
+                if adr in memuse:
+                    allocs = memuse[adr]
+                    totalmem = totalmem - allocs[0]
+                    if len(allocs) > 1:
+                        memuse[adr] = allocs[1:]
+                    else:
+                        del memuse[adr]
+                else:
+                    #  Unmatched free, skip for now.
+                    doprint = False
+            if doprint:
+                print tic, label, totalmem/(1048576.0)
+
+totals = {}
+for adr in labels:
+    #  If any remaining allocations.
+    if adr in memuse:
+        if labels[adr] in totals:
+            totals[labels[adr]] = totals[labels[adr]] + memuse[adr][0]
+        else:
+            totals[labels[adr]] = memuse[adr][0]
+
+print "# Memory use by label"
+for label in sorted(totals):
+    print "## ", label, totals[label]/(1048576.0)
 
 sys.exit(0)
