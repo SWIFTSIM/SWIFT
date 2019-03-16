bc81a1f8a065d46994c8ba1761a91c6ab02b8d12
james.s.willis@durham.ac.uk <james.s.willis@durham.ac.uk>
2019-03-16 13:13:32 +0000

    Removed unnecessary array and allocate memory in FOF fcuntion instead of in the space.

--- src/space.h
+++ src/space.h
@@ -243,7 +243,6 @@ struct space {
 
   /*! List of cell indices. */
   int *cell_index;
-  size_t *gpart_index;
 
   /*! The group information returned by VELOCIraptor for each #gpart. */
   struct velociraptor_gpart_data *gpart_group_data;
