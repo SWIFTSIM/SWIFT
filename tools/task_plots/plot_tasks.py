3ccf3454be66270d14a5e83cd6aeda93c98a299c
James Willis <james.s.willis@durham.ac.uk>
2019-03-19 15:37:29 +0000

    Fix typo.

--- tools/task_plots/plot_tasks.py
+++ tools/task_plots/plot_tasks.py
@@ -442,10 +442,9 @@ for rank in ranks:
             toc = int(data[line, toccol]) / CPU_CLOCK
             tasks[thread][-1]["tic"] = tic
             tasks[thread][-1]["toc"] = toc
-            if (                "fof" in tasktype):
+            if ("fof" in tasktype):
                 tasks[thread][-1]["colour"] = TASKCOLOURS[tasktype]
-
-            else if(
+            elif(
                     "self" in tasktype
                     or "pair" in tasktype
                     or "recv" in tasktype
