d1824f6097d070611dc18fd2cac6cdd1a965e6c8
Alexei Borissov <dc-bori1@cosma-b.pri.cosma7.alces.network>
2019-04-08 11:00:32 +0100

    add isolated galaxy feedback example

---
+++ examples/IsolatedGalaxy/IsolatedGalaxy_feedback/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -e lowres8.hdf5 ] 
+then     
+    echo "Fetching initial conditions for the isolated galaxy example..."
+    ./getIC.sh
+fi
+
+if [ ! -e cooling_tables/ ] 
+then     
+    echo "Fetching EAGLE cooling tables for the isolated galaxy example..."
+    ./getEagleCoolingTable.sh
+fi
+
+../../swift --threads=32 --feedback --external-gravity --self-gravity --stars --star-formation --cooling --temperature --hydro isolated_galaxy.yml 2>&1 | tee output.log
+
+# Kennicutt-Schmidt law plot
+python3 plotSolution.py
+
+# Plot that the random star formation matches the expected SFH
+python3 SFH.py
