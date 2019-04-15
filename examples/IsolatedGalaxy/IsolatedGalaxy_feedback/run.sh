319853a7ce7011635a20fc97862bcf9780679136
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-04-15 21:37:32 +0200

    Make the run script download the Yield tables.

--- examples/IsolatedGalaxy/IsolatedGalaxy_feedback/run.sh
+++ examples/IsolatedGalaxy/IsolatedGalaxy_feedback/run.sh
@@ -6,10 +6,16 @@ then
     ./getIC.sh
 fi
 
-if [ ! -e cooling_tables ] 
+if [ ! -e coolingtables ] 
 then     
     echo "Fetching EAGLE cooling tables for the isolated galaxy example..."
     ./getEagleCoolingTable.sh
+fi
+
+if [ ! -e yieldtables ] 
+then     
+    echo "Fetching EAGLE stellar yield tables for the isolated galaxy example..."
+    ./getYieldTable.sh
 fi
 
 ../../swift --threads=16 --feedback --external-gravity --self-gravity --stars --star-formation --cooling --hydro isolated_galaxy.yml 2>&1 | tee output.log
