944900c48bfb2488d443a621d4a34e1ca3ad842d
Pedro Gonnet <gonnet@google.com>
2019-04-26 11:28:57 +0100

    Don't pass enums in the `cell_*_flag` functions, but rather use the type of the `flags` value itself. Using the enum causes problems when passing the union of more than one flag.

--- src/cell.h
+++ src/cell.h
@@ -1292,19 +1292,19 @@ __attribute__((always_inline)) INLINE static void cell_free_stars_sorts(
 
 /** Set the given flag for the given cell. */
 __attribute__((always_inline)) INLINE static void cell_set_flag(
-    struct cell *c, enum cell_flags flag) {
+    struct cell *c, uint16_t cell_flags flag) {
   c->flags |= flag;
 }
 
 /** Clear the given flag for the given cell. */
 __attribute__((always_inline)) INLINE static void cell_clear_flag(
-    struct cell *c, enum cell_flags flag) {
+    struct cell *c, uint16_t cell_flags flag) {
   c->flags &= ~flag;
 }
 
 /** Get the given flag for the given cell. */
 __attribute__((always_inline)) INLINE static int cell_get_flag(
-    const struct cell *c, enum cell_flags flag) {
+    const struct cell *c, uint16_t cell_flags flag) {
   return (c->flags & flag) > 0;
 }
 
