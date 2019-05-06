5ee42c0f882f252b6f075bdbbc2a306640b7abf9
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2019-05-06 09:48:06 +0100

    Added a function to check whether a given cell is starting its BH time integration on this step.

--- src/active.h
+++ src/active.h
@@ -497,6 +497,28 @@ __attribute__((always_inline)) INLINE static int cell_is_starting_stars(
 }
 
 /**
+ * @brief Does a cell contain any b-particle starting their time-step now ?
+ *
+ * @param c The #cell.
+ * @param e The #engine containing information about the current time.
+ * @return 1 if the #cell contains at least an active particle, 0 otherwise.
+ */
+__attribute__((always_inline)) INLINE static int cell_is_starting_black_holes(
+    const struct cell *c, const struct engine *e) {
+
+#ifdef SWIFT_DEBUG_CHECKS
+  if (c->black_holes.ti_beg_max > e->ti_current)
+    error(
+        "cell in an impossible time-zone! c->ti_beg_max=%lld (t=%e) and "
+        "e->ti_current=%lld (t=%e, a=%e)",
+        c->black_holes.ti_beg_max, c->black_holes.ti_beg_max * e->time_base, e->ti_current,
+        e->ti_current * e->time_base, e->cosmology->a);
+#endif
+
+  return (c->black_holes.ti_beg_max == e->ti_current);
+}
+
+/**
  * @brief Is this particle starting its time-step now ?
  *
  * @param p The #part.
