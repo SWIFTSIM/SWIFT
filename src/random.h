dcf25c9480ec4bcb9928c990d0b2805707fe00f6
Folkert Nobels <nobels@strw.leidenuniv.nl>
2019-07-11 10:11:10 +0200

    Add an improved RNG that does not have problems with low probabilities

--- src/random.h
+++ src/random.h
@@ -23,94 +23,65 @@
 /* COde configuration */
 #include "../config.h"
 
+/* Random 123 headers */
+#include "Random123/threefry.h"
+
 /* Standard header */
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * @brief The categories of random number generated.
  *
  * The values of the fields are carefully chose numbers
- * the numbers are very large primes such that the IDs
- * will not have a prime factorization with this coefficient
- * this results in a very high period for the random number
- * generator.
- * Only change when you know what you are doing, changing
- * the numbers to bad values will break the random number
- * generator.
- * In case new numbers need to be added other possible
- * numbers could be:
- * 5947309451, 6977309513
+ * but it is not super important what the values are, 
+ * as long as for the different types the numbers are 
+ * integers and not the same
  */
 enum random_number_type {
-  random_number_star_formation = 0LL,
-  random_number_stellar_feedback = 3947008991LL,
-  random_number_stellar_enrichment = 2936881973LL,
-  random_number_BH_feedback = 1640531371LL,
-  random_number_BH_swallow = 4947009007LL
+  random_number_star_formation = 0,
+  random_number_stellar_feedback = 1, 
+  random_number_stellar_enrichment = 2,
+  random_number_BH_feedback = 3,
+  random_number_BH_swallow = 4,
+  random_number_stellar_winds = 5,
+  random_number_SNIa_feedback = 6,
+  random_number_HII_regions = 7
 };
 
 /**
- * @brief Returns a pseudo-random number in the range [0, 1[.
+ * @brief Returns a pseudo-random number in the range (0, 1).
  *
  * We generate numbers that are always reproducible for a given particle ID and
  * simulation time (on the integer time-line). If more than one number per
  * time-step per particle is needed, additional randomness can be obtained by
- * using the type argument.
+ * using the type argument. This random number generator has a very small spacing
+ * of 2^-64, and therefore can be used for processes with a very low propability
  *
  * @param id The ID of the particle for which to generate a number.
  * @param ti_current The time (on the time-line) for which to generate a number.
  * @param type The #random_number_type to generate.
- * @return a random number in the interval [0, 1.[.
+ * @return a random number in the open interval (0, 1.).
  */
 INLINE static double random_unit_interval(const long long int id,
                                           const integertime_t ti_current,
                                           const enum random_number_type type) {
 
-  /* Range used for the seeds. Best if prime */
-  static const long long seed_range = RAND_MAX;
-  static const double RAND_MAX_inv = 1. / ((double)RAND_MAX);
-  static const long long mwc_number = (1LL << 32) - 1LL;
+  /* Default used limits */
+  static const unsigned long long int max_long = ULLONG_MAX;
+  static const double max_long_inv = 1. / max_long;
+  static const int number_of_rounds = 12;
+  
+  /* Prepare the state, the initial state depends on the type */
+  threefry4x64_ctr_t ctr = {{type,0}};
+  threefry4x64_key_t key = {{id, ti_current}};
+    
+  /* Generate the random number */
+  threefry4x64_ctr_t rand = threefry4x64_R(number_of_rounds,ctr, key);
 
-  /* Calculate the seed */
-  /* WARNING: Only change the math if you really know what you are doing!
-   * The numbers are carefully chosen prime numbers that prevent correlation
-   * with either the current integer time or the particle IDs. The current
-   * method also prevents any correlation between different random number
-   * types.
-   * The calculation overflows on purpose.
-   * 1. The first step is calculating the seed by using a multiply with carry
-   * (MWC) method, this method depends on the type of random number and
-   * this therefore also prevents that there is any correlation between
-   * the different types of random numbers.
-   * 2. After this we use the 64 bit Xorshift method to randomize the seeds
-   * even more.
-   * 3. We calculate a prime multiplication for the id with a quadratic
-   * term.
-   * 4. We calculate the seed by using a Quadratic congruential generator,
-   * in which we use the id part and the current time step bin.
-   */
-  unsigned long long number = ti_current;
-
-  /* Multiply with carry (MWC), (adviced variables by NR) */
-  number = 4294957665LL * (number & (mwc_number)) + (number >> 32);
-
-  /* 64-bit Xorshift (adviced variables by NR) */
-  number ^= number << 21;
-  number ^= number >> 35;
-  number ^= number << 4;
-
-  /* Add constant to ID */
-  const unsigned long long idt = id + type;
-
-  /* Nonlinear congruential generator */
-  const unsigned long long idpart =
-      3457LL * idt + 593LL * idt * ti_current + 5417LL * idt * idt;
-  unsigned int seed =
-      (937LL * number + 5171LL * number * number + idpart + 1109LL) %
-      9996361LL % seed_range;
-
-  /* Generate a random number between 0 and 1. */
-  return rand_r(&seed) * RAND_MAX_inv;
+  return rand.v[0] * max_long_inv;
+  
 }
+
 
 #endif /* SWIFT_RANDOM_H */
