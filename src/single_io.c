9181e0c0379e3499aba94f1f0cfe39c8eac8d21f
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-07-17 16:10:54 +0100

    Fixes a bunch of warnings triggered by GCC 9.1

--- src/single_io.c
+++ src/single_io.c
@@ -1151,7 +1151,7 @@ void write_output_single(struct engine* e, const char* baseName,
 
       /* Did the user cancel this field? */
       char field[PARSER_MAX_LINE_SIZE];
-      sprintf(field, "SelectOutput:%s_%s", list[i].name,
+      sprintf(field, "SelectOutput:%.*s_%s", FIELD_BUFFER_SIZE, list[i].name,
               part_type_names[ptype]);
       int should_write = parser_get_opt_param_int(params, field, 1);
 
