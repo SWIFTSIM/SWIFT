b2cd1290bd409d7164d0ea7d63a8ba7ac9b7bbe0
Pedro Gonnet <gonnet@google.com>
2019-07-18 23:01:36 +0200

    clean up the implementation.

--- src/random.h
+++ src/random.h
@@ -55,27 +55,29 @@ enum random_number_type {
 #include <limits.h>
 #include <sys/types.h>
 
-/* Inline the default RNG functions to avoid costly function calls. */
+/* Inline the default RNG functions to avoid costly function calls. These
+   functions are minor modifications, but functional equivalents, of their glibc
+   counterparts. */
 
-INLINE static int inl_rand_r(unsigned int *seed) {
-  unsigned int next = *seed;
+INLINE static int inl_rand_r(uint32_t *seed) {
+  uint32_t next = *seed;
   int result;
   next *= 1103515245;
   next += 12345;
-  result = (unsigned int)(next / 65536) % 2048;
+  result = (uint32_t)(next / 65536) % 2048;
   next *= 1103515245;
   next += 12345;
   result <<= 10;
-  result ^= (unsigned int)(next / 65536) % 1024;
+  result ^= (uint32_t)(next / 65536) % 1024;
   next *= 1103515245;
   next += 12345;
   result <<= 10;
-  result ^= (unsigned int)(next / 65536) % 1024;
+  result ^= (uint32_t)(next / 65536) % 1024;
   *seed = next;
   return result;
 }
 
-INLINE void inl_drand48_iterate(unsigned short int xsubi[3]) {
+INLINE void inl_drand48_iterate(uint16_t xsubi[3]) {
   uint64_t X;
   uint64_t result;
   const uint64_t __a = 0x5deece66dull;
@@ -91,7 +93,7 @@ INLINE void inl_drand48_iterate(unsigned short int xsubi[3]) {
   xsubi[2] = (result >> 32) & 0xffff;
 }
 
-INLINE double inl_erand48(unsigned short int xsubi[3]) {
+INLINE double inl_erand48(uint16_t xsubi[3]) {
   union ieee754_double temp;
 
   /* Compute next state.  */
@@ -121,7 +123,7 @@ INLINE double inl_erand48(unsigned short int xsubi[3]) {
  * @param type The #random_number_type to generate.
  * @return a random number in the interval [0, 1.[.
  */
-INLINE static double random_unit_interval(long long int id,
+INLINE static double random_unit_interval(int64_t id,
                                           const integertime_t ti_current,
                                           const enum random_number_type type) {
   /* Start by packing the state into a sequence of 16-bit seeds for rand_r. */
@@ -129,6 +131,10 @@ INLINE static double random_unit_interval(long long int id,
   id += type;
   memcpy(&buff[0], &id, 8);
   memcpy(&buff[4], &ti_current, 8);
+
+  /* The inputs give 16 bytes of state, but we need a multiple of 6 for the
+     calls to erand48(), so we add an additional aribrary constant two-byte
+     value to get 18 bytes of state. */
   buff[8] = 6178;
 
   /* Shuffle the buffer values, this will be our source of entropy for
@@ -149,56 +155,6 @@ INLINE static double random_unit_interval(long long int id,
 
   /* Generate one final value, this is our output. */
   return inl_erand48(seed48);
-}
-
-INLINE static double random_unit_interval_previous(
-    const long long int id, const integertime_t ti_current,
-    const enum random_number_type type) {
-  /* Range used for the seeds. Best if prime */
-  static const long long seed_range = RAND_MAX;
-  static const double RAND_MAX_inv = 1. / ((double)RAND_MAX);
-  static const long long mwc_number = (1LL << 32) - 1LL;
-
-  /* Calculate the seed */
-  /* WARNING: Only change the math if you really know what you are doing!
-   * The numbers are carefully chosen prime numbers that prevent correlation
-   * with either the current integer time or the particle IDs. The current
-   * method also prevents any correlation between different random number
-   * types.
-   * The calculation overflows on purpose.
-   * 1. The first step is calculating the seed by using a multiply with carry
-   * (MWC) method, this method depends on the type of random number and
-   * this therefore also prevents that there is any correlation between
-   * the different types of random numbers.
-   * 2. After this we use the 64 bit Xorshift method to randomize the seeds
-   * even more.
-   * 3. We calculate a prime multiplication for the id with a quadratic
-   * term.
-   * 4. We calculate the seed by using a Quadratic congruential generator,
-   * in which we use the id part and the current time step bin.
-   */
-  unsigned long long number = ti_current;
-
-  /* Multiply with carry (MWC), (adviced variables by NR) */
-  number = 4294957665LL * (number & (mwc_number)) + (number >> 32);
-
-  /* 64-bit Xorshift (adviced variables by NR) */
-  number ^= number << 21;
-  number ^= number >> 35;
-  number ^= number << 4;
-
-  /* Add constant to ID */
-  const unsigned long long idt = id + type;
-
-  /* Nonlinear congruential generator */
-  const unsigned long long idpart =
-      3457LL * idt + 593LL * idt * ti_current + 5417LL * idt * idt;
-  unsigned int seed =
-      (937LL * number + 5171LL * number * number + idpart + 1109LL) %
-      9996361LL % seed_range;
-
-  /* Generate a random number between 0 and 1. */
-  return rand_r(&seed) * RAND_MAX_inv;
 }
 
 #endif /* SWIFT_RANDOM_H */
