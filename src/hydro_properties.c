d81dcbeedcc7f490c6ffcfb0fa3d1efbd3cd79f2
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-07-18 11:37:18 +0200

    Use the correct softening length to set the minimal SPH smoothing length.

--- src/hydro_properties.c
+++ src/hydro_properties.c
@@ -338,8 +338,7 @@ void hydro_props_update(struct hydro_props *p, const struct gravity_props *gp,
    * is a fixed fraction of the radius at which the softened forces
    * recover a Newtonian behaviour (i.e. 2.8 * Plummer equivalent softening
    * in the case of a cubic spline kernel). */
-  // MATTHIEU
-  p->h_min = p->h_min_ratio * 1. /*gp->epsilon_cur*/ / kernel_gamma;
+  p->h_min = p->h_min_ratio * gp->epsilon_cur / kernel_gamma;
 }
 
 /**
