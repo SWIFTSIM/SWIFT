6052ad4aaa79d3c57f7c2322c24620bcb53db887
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-07-22 20:35:27 +0100

    Applied the code formatting tool.

--- src/serial_io.c
+++ src/serial_io.c
@@ -1350,7 +1350,7 @@ void write_output_serial(struct engine* e, const char* baseName,
               black_holes_write_particles(bparts_written, list, &num_fields);
               num_fields +=
                   chemistry_write_bparticles(bparts, list + num_fields);
-	      num_fields += fof_write_bparts(bparts_written, list + num_fields);
+              num_fields += fof_write_bparts(bparts_written, list + num_fields);
               if (with_stf) {
                 num_fields += velociraptor_write_bparts(bparts_written,
                                                         list + num_fields);
