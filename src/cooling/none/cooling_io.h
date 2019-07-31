cd1618f7a292de067e81665af8487669a96a8689
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2019-07-31 14:56:17 +0100

    Update the i/o of the basic cooling models to use the new function signature.

--- src/cooling/none/cooling_io.h
+++ src/cooling/none/cooling_io.h
@@ -63,7 +63,7 @@ __attribute__((always_inline)) INLINE static int cooling_write_particles(
     const struct cooling_function_data* cooling) {
 
   list[0] = io_make_output_field_convert_part(
-      "Temperature", FLOAT, 1, UNIT_CONV_TEMPERATURE, 0.f, parts, xparts,
+      "Temperatures", FLOAT, 1, UNIT_CONV_TEMPERATURE, 0.f, parts, xparts,
       convert_part_T, "Temperature of the particles");
   return 1;
 }
