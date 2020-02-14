8828b585a888cad10e6e021ca0903713198f5657
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-02-14 11:10:58 +0100

    Applied code formatting tool

--- src/stars/EAGLE/stars_io.h
+++ src/stars/EAGLE/stars_io.h
@@ -132,8 +132,9 @@ INLINE static void stars_write_particles(const struct spart *sparts,
                                  "Masses of the particles at the current point "
                                  "in time (i.e. after stellar losses");
 
-  list[3] = io_make_output_field("ParticleIDs", ULONGLONG, 1, UNIT_CONV_NO_UNITS,
-                                 0.f, sparts, id, "Unique ID of the particles");
+  list[3] =
+      io_make_output_field("ParticleIDs", ULONGLONG, 1, UNIT_CONV_NO_UNITS, 0.f,
+                           sparts, id, "Unique ID of the particles");
 
   list[4] = io_make_output_field(
       "SmoothingLengths", FLOAT, 1, UNIT_CONV_LENGTH, 1.f, sparts, h,
