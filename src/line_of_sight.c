ea151874b1f86a50af2179942d294ed2171ad772
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-03-30 16:59:25 +0200

    Applied code formatting tool

--- src/line_of_sight.c
+++ src/line_of_sight.c
@@ -34,17 +34,19 @@ double los_periodic(double x, double dim) {
  * @param params Sightline parameters.
  */
 void generate_line_of_sights(struct line_of_sight *Los,
-        struct line_of_sight_params *params) {
+                             struct line_of_sight_params *params) {
 
   /* Keep track of number of sightlines. */
   int count = 0;
 
   /* Sightlines in XY plane. */
   for (int i = 0; i < params->num_along_xy; i++) {
-    Los[count].Xpos = ((float)rand()/(float)(RAND_MAX) *\
-            (params->xmax - params->xmin)) + params->xmin;
-    Los[count].Ypos = ((float)rand()/(float)(RAND_MAX) *\
-            (params->ymax - params->ymin)) + params->ymin;
+    Los[count].Xpos =
+        ((float)rand() / (float)(RAND_MAX) * (params->xmax - params->xmin)) +
+        params->xmin;
+    Los[count].Ypos =
+        ((float)rand() / (float)(RAND_MAX) * (params->ymax - params->ymin)) +
+        params->ymin;
     Los[count].particles_in_los_local = 0;
     Los[count].particles_in_los_total = 0;
     Los[count].xaxis = 0;
@@ -55,10 +57,12 @@ void generate_line_of_sights(struct line_of_sight *Los,
 
   /* Sightlines in YZ plane. */
   for (int i = 0; i < params->num_along_yz; i++) {
-    Los[count].Xpos = ((float)rand()/(float)(RAND_MAX) *\
-            (params->ymax - params->ymin)) + params->ymin;
-    Los[count].Ypos = ((float)rand()/(float)(RAND_MAX) *\
-            (params->zmax - params->zmin)) + params->zmin;
+    Los[count].Xpos =
+        ((float)rand() / (float)(RAND_MAX) * (params->ymax - params->ymin)) +
+        params->ymin;
+    Los[count].Ypos =
+        ((float)rand() / (float)(RAND_MAX) * (params->zmax - params->zmin)) +
+        params->zmin;
     Los[count].particles_in_los_local = 0;
     Los[count].particles_in_los_total = 0;
     Los[count].xaxis = 1;
@@ -69,10 +73,12 @@ void generate_line_of_sights(struct line_of_sight *Los,
 
   /* Sightlines in XZ plane. */
   for (int i = 0; i < params->num_along_xz; i++) {
-    Los[count].Xpos = ((float)rand()/(float)(RAND_MAX) *\
-            (params->xmax - params->xmin)) + params->xmin;
-    Los[count].Ypos = ((float)rand()/(float)(RAND_MAX) *\
-            (params->zmax - params->zmin)) + params->zmin;
+    Los[count].Xpos =
+        ((float)rand() / (float)(RAND_MAX) * (params->xmax - params->xmin)) +
+        params->xmin;
+    Los[count].Ypos =
+        ((float)rand() / (float)(RAND_MAX) * (params->zmax - params->zmin)) +
+        params->zmin;
     Los[count].particles_in_los_local = 0;
     Los[count].particles_in_los_total = 0;
     Los[count].xaxis = 0;
@@ -89,12 +95,12 @@ void generate_line_of_sights(struct line_of_sight *Los,
  * @param params Sightline parameters.
  */
 void print_los_info(struct line_of_sight *Los,
-        struct line_of_sight_params *params) {
+                    struct line_of_sight_params *params) {
 
   printf("\nPrinting LOS information...\n");
   for (int i = 0; i < params->num_tot; i++) {
-    printf("[LOS %i] Xpos:%g Ypos:%g particles_in_los_total:%lld\n",
-      i,Los[i].Xpos,Los[i].Ypos,Los[i].particles_in_los_total);
+    printf("[LOS %i] Xpos:%g Ypos:%g particles_in_los_total:%lld\n", i,
+           Los[i].Xpos, Los[i].Ypos, Los[i].particles_in_los_total);
   }
 }
 
@@ -104,19 +110,29 @@ void do_line_of_sight(struct engine *e) {
   const size_t nr_parts = s->nr_parts;
 
   /* LOS params, dummy until included in parameter file. */
-  struct line_of_sight_params LOS_params = {.num_along_xy = 1, .num_along_yz = 2, .num_along_xz = 3,
-      .xmin = 4, .xmax = 6, .ymin = 4, .ymax = 6, .zmin = 4, .zmax = 6};
-  LOS_params.num_tot = LOS_params.num_along_xy + LOS_params.num_along_yz + LOS_params.num_along_xz;
+  struct line_of_sight_params LOS_params = {.num_along_xy = 1,
+                                            .num_along_yz = 2,
+                                            .num_along_xz = 3,
+                                            .xmin = 4,
+                                            .xmax = 6,
+                                            .ymin = 4,
+                                            .ymax = 6,
+                                            .zmin = 4,
+                                            .zmax = 6};
+  LOS_params.num_tot = LOS_params.num_along_xy + LOS_params.num_along_yz +
+                       LOS_params.num_along_xz;
 
   /* Start by generating the random sightline positions. */
-  struct line_of_sight *LOS_list = (struct line_of_sight*)malloc(LOS_params.num_tot * sizeof(struct line_of_sight));
+  struct line_of_sight *LOS_list = (struct line_of_sight *)malloc(
+      LOS_params.num_tot * sizeof(struct line_of_sight));
   if (e->nodeID == 0) generate_line_of_sights(LOS_list, &LOS_params);
 #ifdef WITH_MPI
-  MPI_Bcast(LOS_list, LOS_params.num_tot * sizeof(struct line_of_sight), MPI_BYTE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(LOS_list, LOS_params.num_tot * sizeof(struct line_of_sight),
+            MPI_BYTE, 0, MPI_COMM_WORLD);
 #endif
 
   double dx, dy, r2, hsml;
-  //size_t LOS_nmax;
+  // size_t LOS_nmax;
 
   FILE *f;
 
@@ -133,26 +149,28 @@ void do_line_of_sight(struct engine *e) {
   /* Loop over each random LOS. */
   for (int j = 0; j < LOS_params.num_tot; j++) {
 
-    //LOS_nmax = 0;
+    // LOS_nmax = 0;
 
     /* Loop over each gas particle to find those in LOS. */
     for (size_t i = 0; i < nr_parts; i++) {
-      if (s->parts[i].gpart->type == swift_type_gas) { 
-        dx = los_periodic(s->parts[i].x[LOS_list[j].xaxis] - LOS_list[j].Xpos, s->dim[LOS_list[j].xaxis]);
+      if (s->parts[i].gpart->type == swift_type_gas) {
+        dx = los_periodic(s->parts[i].x[LOS_list[j].xaxis] - LOS_list[j].Xpos,
+                          s->dim[LOS_list[j].xaxis]);
         hsml = s->parts[i].h;
 
         /* Does this particle fall into our LOS? */
         if (dx <= hsml) {
-          dy = los_periodic(s->parts[i].x[LOS_list[j].yaxis] - LOS_list[j].Ypos, s->dim[LOS_list[j].yaxis]);
+          dy = los_periodic(s->parts[i].x[LOS_list[j].yaxis] - LOS_list[j].Ypos,
+                            s->dim[LOS_list[j].yaxis]);
 
           if (dy <= hsml) {
             r2 = dx * dx + dy * dy;
 
-              if (r2 <= hsml * hsml) {
+            if (r2 <= hsml * hsml) {
 
-                /* We've found one. */
-                LOS_list[j].particles_in_los_local += 1;
-              }
+              /* We've found one. */
+              LOS_list[j].particles_in_los_local += 1;
+            }
           }
         }
       }
@@ -163,36 +181,46 @@ void do_line_of_sight(struct engine *e) {
     int LOS_disps[e->nr_nodes];
 
     /* How many particles does each rank have for this LOS? */
-    MPI_Allgather(&LOS_list[j].particles_in_los_local, 1, MPI_LONG_LONG_INT, 
-            &LOS_counts, 1, MPI_LONG_LONG_INT, MPI_COMM_WORLD);
-    
+    MPI_Allgather(&LOS_list[j].particles_in_los_local, 1, MPI_LONG_LONG_INT,
+                  &LOS_counts, 1, MPI_LONG_LONG_INT, MPI_COMM_WORLD);
+
     for (int k = 0, disp_count = 0; k < e->nr_nodes; k++) {
-        if (e->nodeID == 0) message("k=%i disp_count=%i LOS_counts=%i", k, disp_count, LOS_counts[k]);
-        /* Total particles in this LOS. */
-        LOS_list[j].particles_in_los_total += LOS_counts[k];
+      if (e->nodeID == 0)
+        message("k=%i disp_count=%i LOS_counts=%i", k, disp_count,
+                LOS_counts[k]);
+      /* Total particles in this LOS. */
+      LOS_list[j].particles_in_los_total += LOS_counts[k];
 
-        LOS_disps[k] = disp_count;
+      LOS_disps[k] = disp_count;
 
-        disp_count += LOS_counts[k];
+      disp_count += LOS_counts[k];
 
-        //message("k=%i disp_count=%i LOS_counts=%lld", k, disp_count, LOS_counts[k]);
-        /* Max number of LOS particles on one node. */
-        //if (LOS_counts[k] > LOS_nmax) LOS_nmax = LOS_counts[k];
+      // message("k=%i disp_count=%i LOS_counts=%lld", k, disp_count,
+      // LOS_counts[k]);
+      /* Max number of LOS particles on one node. */
+      // if (LOS_counts[k] > LOS_nmax) LOS_nmax = LOS_counts[k];
     }
 #else
-    //LOS_nmax = LOS_list[j].particles_in_los_local;
+    // LOS_nmax = LOS_list[j].particles_in_los_local;
     LOS_list[j].particles_in_los_total = LOS_list[j].particles_in_los_local;
-#endif 
+#endif
 
     /* Setup los particles structure. */
     struct line_of_sight_particles *LOS_particles;
     struct line_of_sight_particles *LOS_particles_total;
     if (e->nodeID == 0) {
-      //LOS_particles = (struct line_of_sight_particles*)malloc(LOS_nmax * sizeof(struct line_of_sight_particles));
-      LOS_particles_total = (struct line_of_sight_particles*)malloc(LOS_list[j].particles_in_los_total * sizeof(struct line_of_sight_particles));
-      LOS_particles = (struct line_of_sight_particles*)malloc(LOS_list[j].particles_in_los_local * sizeof(struct line_of_sight_particles));
+      // LOS_particles = (struct line_of_sight_particles*)malloc(LOS_nmax *
+      // sizeof(struct line_of_sight_particles));
+      LOS_particles_total = (struct line_of_sight_particles *)malloc(
+          LOS_list[j].particles_in_los_total *
+          sizeof(struct line_of_sight_particles));
+      LOS_particles = (struct line_of_sight_particles *)malloc(
+          LOS_list[j].particles_in_los_local *
+          sizeof(struct line_of_sight_particles));
     } else {
-      LOS_particles = (struct line_of_sight_particles*)malloc(LOS_list[j].particles_in_los_local * sizeof(struct line_of_sight_particles));
+      LOS_particles = (struct line_of_sight_particles *)malloc(
+          LOS_list[j].particles_in_los_local *
+          sizeof(struct line_of_sight_particles));
     }
 
     /* Loop over each gas particle again to store properties. */
@@ -200,13 +228,15 @@ void do_line_of_sight(struct engine *e) {
 
     for (size_t i = 0; i < nr_parts; i++) {
 
-      if (s->parts[i].gpart->type == swift_type_gas) { 
-        dx = los_periodic(s->parts[i].x[LOS_list[j].xaxis] - LOS_list[j].Xpos, s->dim[LOS_list[j].xaxis]);
+      if (s->parts[i].gpart->type == swift_type_gas) {
+        dx = los_periodic(s->parts[i].x[LOS_list[j].xaxis] - LOS_list[j].Xpos,
+                          s->dim[LOS_list[j].xaxis]);
         hsml = s->parts[i].h;
 
         /* Does this particle fall into our LOS? */
         if (dx <= hsml) {
-          dy = los_periodic(s->parts[i].x[LOS_list[j].yaxis] - LOS_list[j].Ypos, s->dim[LOS_list[j].yaxis]);
+          dy = los_periodic(s->parts[i].x[LOS_list[j].yaxis] - LOS_list[j].Ypos,
+                            s->dim[LOS_list[j].yaxis]);
 
           if (dy <= hsml) {
             r2 = dx * dx + dy * dy;
@@ -229,15 +259,17 @@ void do_line_of_sight(struct engine *e) {
 
 #ifdef WITH_MPI
     message("rank %i about to gather for LOS %i", e->nodeID, j);
-    MPI_Gatherv(&LOS_particles, LOS_list[j].particles_in_los_local, lospart_mpi_type,
-         &LOS_particles_total, LOS_counts, LOS_disps, lospart_mpi_type, 0,
-         MPI_COMM_WORLD);
+    MPI_Gatherv(&LOS_particles, LOS_list[j].particles_in_los_local,
+                lospart_mpi_type, &LOS_particles_total, LOS_counts, LOS_disps,
+                lospart_mpi_type, 0, MPI_COMM_WORLD);
 //
 //    if (e->nodeID == 0) {
 //       for (size_t kk = 0; kk < LOS_list[j].particles_in_los_total; kk++) {
-//          fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles_total[kk].pos[0], LOS_particles_total[kk].pos[1], LOS_particles_total[kk].pos[2], LOS_particles_total[kk].h); 
+//          fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles_total[kk].pos[0],
+//          LOS_particles_total[kk].pos[1], LOS_particles_total[kk].pos[2],
+//          LOS_particles_total[kk].h);
 //       }
-//    }  
+//    }
 //    /* Loop over each rank and rank 0 saves LOS data. */
 //    for (int k = 0; k < e->nr_nodes; k++) {
 //      /* First dump any particles on node 0. */
@@ -246,36 +278,44 @@ void do_line_of_sight(struct engine *e) {
 //          if (LOS_counts[k] > 0) {
 //            message("node 0 saving %lld", LOS_counts[k]);
 //            for (size_t kk = 0; kk < LOS_counts[k]; kk++) {
-//              fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles[kk].pos[0], LOS_particles[kk].pos[1], LOS_particles[kk].pos[2], LOS_particles[kk].h);
+//              fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles[kk].pos[0],
+//              LOS_particles[kk].pos[1], LOS_particles[kk].pos[2],
+//              LOS_particles[kk].h);
 //            }
 //          }
 //        } else {
 //          if (LOS_counts[k] > 0) {
 //            message("node 0 recieving %lld particles", LOS_counts[k]);
-//            MPI_Recv(LOS_particles, LOS_counts[k], lospart_mpi_type, k, 12, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//            MPI_Recv(LOS_particles, LOS_counts[k], lospart_mpi_type, k, 12,
+//            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 //
 //            for (size_t kk = 0; kk < LOS_counts[k]; kk++) {
-//              fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles[kk].pos[0], LOS_particles[kk].pos[1], LOS_particles[kk].pos[2], LOS_particles[kk].h);
+//              fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles[kk].pos[0],
+//              LOS_particles[kk].pos[1], LOS_particles[kk].pos[2],
+//              LOS_particles[kk].h);
 //            }
 //          }
 //        }
 //      } else {
 //          if (k > 0 && LOS_counts[k] > 0) {
 //            message("node %i sending %lld particles to 0", k, LOS_counts[k]);
-//            MPI_Send(LOS_particles, LOS_counts[k], lospart_mpi_type, 0, 12, MPI_COMM_WORLD);
+//            MPI_Send(LOS_particles, LOS_counts[k], lospart_mpi_type, 0, 12,
+//            MPI_COMM_WORLD);
 //          }
 //      }
 //    } /* End of loop over each node. */
 #else
     for (size_t kk = 0; kk < LOS_list[j].particles_in_los_local; kk++) {
-      fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles[kk].pos[0], LOS_particles[kk].pos[1], LOS_particles[kk].pos[2], LOS_particles[kk].h);
+      fprintf(f, "%i,%g,%g,%g,%g\n", j, LOS_particles[kk].pos[0],
+              LOS_particles[kk].pos[1], LOS_particles[kk].pos[2],
+              LOS_particles[kk].h);
     }
 #endif
   } /* End of loop over each LOS */
 
   if (e->nodeID == 0) fclose(f);
 
-//#ifdef SWIFT_DEBUG_CHECKS
+  //#ifdef SWIFT_DEBUG_CHECKS
   if (e->nodeID == 0) print_los_info(LOS_list, &LOS_params);
-//#endif
+  //#endif
 }
--- src/line_of_sight.h
+++ src/line_of_sight.h
@@ -11,7 +11,7 @@ struct line_of_sight {
 struct line_of_sight_particles {
   double pos[3];
   float h;
-};  
+};
 
 struct line_of_sight_params {
   int num_along_xy;
@@ -28,10 +28,9 @@ struct line_of_sight_params {
 double los_periodic(double x, double dim);
 
 void generate_line_of_sights(struct line_of_sight *Los,
-        struct line_of_sight_params *params);
+                             struct line_of_sight_params *params);
 
 void print_los_info(struct line_of_sight *Los,
-        struct line_of_sight_params *params);
+                    struct line_of_sight_params *params);
 
 void do_line_of_sight(struct engine *e);
-
