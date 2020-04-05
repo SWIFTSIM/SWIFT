073a1b78c4c31a3304dbb965c51c6ffe06ceb374
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-04-05 21:23:45 +0200

    Add the new locking mechanism to the SPHENIX hydro scheme

--- src/hydro/SPHENIX/hydro_part.h
+++ src/hydro/SPHENIX/hydro_part.h
@@ -30,6 +30,7 @@
 #include "chemistry_struct.h"
 #include "cooling_struct.h"
 #include "feedback_struct.h"
+#include "part_lock.h"
 #include "pressure_floor_struct.h"
 #include "star_formation_struct.h"
 #include "timestep_limiter_struct.h"
@@ -96,6 +97,9 @@ struct part {
 
   /*! Particle acceleration. */
   float a_hydro[3];
+
+  /*! Lock for the particle in parallel task sections */
+  swift_particle_lock_t lock;
 
   /*! Particle mass. */
   float mass;
