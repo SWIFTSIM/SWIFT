b668559da218505edae2f1b94d2d7bd0f8c15c0a
loikki <loic.hausammann@protonmail.ch>
2020-04-09 16:06:22 +0200

    GEAR: fix define

--- src/star_formation/GEAR/star_formation.h
+++ src/star_formation/GEAR/star_formation.h
@@ -304,7 +304,7 @@ __attribute__((always_inline)) INLINE static void star_formation_end_density(
   /* Copy the velocity divergence */
   xp->sf_data.div_v = p->viscosity.div_v;
   /* SPHENIX is already including the Hubble flow */
-#elif GADGET_SPH
+#elif GADGET2_SPH
   /* Copy the velocity divergence */
   xp->sf_data.div_v = p->density.div_v;
   xp->sf_data.div_v += hydro_dimension * cosmo->H;
