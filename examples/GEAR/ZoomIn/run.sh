80f7bdf660ede88fe88cc1d9c474262ae1c9a8e9
loikki <loic.hausammann@protonmail.ch>
2020-04-15 09:13:04 +0200

    Update run

--- examples/GEAR/ZoomIn/run.sh
+++ examples/GEAR/ZoomIn/run.sh
@@ -4,13 +4,3 @@ echo "Fetching initial conditions for the zoom in example..."
 ./getIC.sh
 
 ../../swift --cooling --feedback --cosmology  --limiter --sync --self-gravity --hydro --stars --star-formation --threads=8 zoom_in.yml 2>&1 | tee output.log
-
-echo "Fetching the solution from GEAR..."
-./getSolution.sh
-
-# Convert SWIFT snapshot to GEAR
-python3 cleanupSwift.py snapshot_1010.hdf5 swift_final.hdf5
-
-
-# Make the plots
-./make_image.py
