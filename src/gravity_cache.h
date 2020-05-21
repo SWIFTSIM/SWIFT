d32a8b8c94ab430e8b4f57530af152bd990ebff0
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-05-21 16:00:33 +0200

    Also force the vectorization of the cache write-back loop

--- src/gravity_cache.h
+++ src/gravity_cache.h
@@ -485,6 +485,9 @@ INLINE static void gravity_cache_write_back(const struct gravity_cache *c,
   swift_declare_aligned_ptr(int, active, c->active, SWIFT_CACHE_ALIGNMENT);
 
   /* Write stuff back to the particles */
+#ifndef SWIFT_DEBUG_CHECKS
+#pragma omp simd
+#endif
   for (int i = 0; i < gcount; ++i) {
     if (active[i]) {
       accumulate_add_f(&gparts[i].a_grav[0], a_x[i]);
