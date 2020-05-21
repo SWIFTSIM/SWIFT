c5e8da53fcf9c5fbe26d009ddc24441820477ef3
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-05-21 15:32:06 +0200

    In the no-cache functions, try first to use the M2P kernel

--- src/runner_doiact_grav.c
+++ src/runner_doiact_grav.c
@@ -194,7 +194,7 @@ static INLINE void runner_dopair_grav_pp_full_no_cache(
 
     /* Can we use the Mulipole here? */
     if (gcount_j > 1 && gravity_M2P_accept(grav_props, gpi, multi_j, r2_multi,
-					   /*periodic=*/1)) {
+                                           /*periodic=*/1)) {
 
       const float h_inv_i = 1.f / h_i;
 
@@ -364,7 +364,7 @@ static INLINE void runner_dopair_grav_pp_truncated_no_cache(
 
     /* Can we use the Mulipole here? */
     if (gcount_j > 1 && gravity_M2P_accept(grav_props, gpi, multi_j, r2_multi,
-					   /*periodic=*/1)) {
+                                           /*periodic=*/1)) {
 
       const float h_inv_i = 1.f / h_i;
 
