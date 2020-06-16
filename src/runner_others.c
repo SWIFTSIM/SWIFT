599e66c67f0e16bd853c65ee8e6125b78072e72e
Loic Hausammann <loic.hausammann@protonmail.ch>
2020-06-16 07:46:13 +0100

    Update comment

--- src/runner_others.c
+++ src/runner_others.c
@@ -379,9 +379,8 @@ void runner_do_star_formation(struct runner *r, struct cell *c, int timer) {
                 const float dx_part = sqrtf(dx2_part);
                 const float dx_sort = sqrtf(dx2_sort);
 
-                /* No need to climb up the tree as the star formation starts
-                   higher in the hierarchy than the hydro and goes to the bottom
-                 */
+                /* Note: no need to update quantities further up the tree as
+                   this task is always called at the top-level */
                 c->hydro.dx_max_part = max(c->hydro.dx_max_part, dx_part);
                 c->hydro.dx_max_sort = max(c->hydro.dx_max_sort, dx_sort);
               }
