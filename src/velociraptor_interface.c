d4dd7c954f41d5dab8da31712b351eee262d6038
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-06-23 13:25:03 +0200

    Applied code formatting script

--- src/velociraptor_interface.c
+++ src/velociraptor_interface.c
@@ -661,7 +661,8 @@ void velociraptor_invoke(struct engine *e, const int linked_with_snap) {
 
   /* Then create output-specific subdirectory if necessary */
   char subDirName[FILENAME_BUFFER_SIZE] = "";
-  if (strcmp(e->stf_subdir_per_output, engine_default_stf_subdir_per_output) != 0) {
+  if (strcmp(e->stf_subdir_per_output, engine_default_stf_subdir_per_output) !=
+      0) {
     if (snprintf(subDirName, FILENAME_BUFFER_SIZE, "%s%s_%04i/", outputDirName,
                  e->stf_subdir_per_output,
                  e->stf_output_count) >= FILENAME_BUFFER_SIZE) {
@@ -719,9 +720,9 @@ void velociraptor_invoke(struct engine *e, const int linked_with_snap) {
 
   /* Call VELOCIraptor. */
   group_info = (struct groupinfo *)InvokeVelociraptor(
-      e->stf_output_count, outputFileName, cosmo_info, sim_info, nr_gparts, nr_parts,
-      nr_sparts, swift_parts, cell_node_ids, e->nr_threads, linked_with_snap,
-      &num_gparts_in_groups);
+      e->stf_output_count, outputFileName, cosmo_info, sim_info, nr_gparts,
+      nr_parts, nr_sparts, swift_parts, cell_node_ids, e->nr_threads,
+      linked_with_snap, &num_gparts_in_groups);
 
   /* Report that the memory was freed */
   memuse_log_allocation("VR.cell_loc", sim_info.cell_loc, 0, 0);
