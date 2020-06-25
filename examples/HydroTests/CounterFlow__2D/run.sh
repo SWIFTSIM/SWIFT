77e58ca7840da52f12ed15f9a4224cf6abc389ed
Richard Bower <r.g.bower@durham.ac.uk>
2020-06-25 15:59:47 +0100

    added counterFlow example to test weakly compressible SPH

---
+++ examples/HydroTests/CounterFlow__2D/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ # Generate the initial conditions if they are not present.
+if [ ! -e counterFlow.hdf5 ]
+then
+    echo "Generating initial conditions for the Kelvin-Helmholtz example..."
+    python3 makeIC.py
+fi
+
+# Run SWIFT
+../../swift --hydro --threads=4 counterFlow.yml 2>&1 | tee output.log
+
+
+# Plot the solution
+python3 makeMovieSwiftsimIO.py
