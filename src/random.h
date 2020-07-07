059fbe63eab2a96ee17638485d52ef7a12213757
Camila Correa <correa@strw.leidenuniv.nl>
2020-07-07 17:35:54 +0200

    Inserting random numbers to compete with SIDM probability

--- src/random.h
+++ src/random.h
@@ -50,7 +50,8 @@ enum random_number_type {
   random_number_stellar_feedback_2 = 6977309513LL,
   random_number_stellar_enrichment = 2936881973LL,
   random_number_BH_feedback = 1640531371LL,
-  random_number_BH_swallow = 4947009007LL
+  random_number_BH_swallow = 4947009007LL,
+  random_number_SIDM = 2189989727LL
 };
 
 #ifndef __APPLE__
