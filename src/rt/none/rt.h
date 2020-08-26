71ab62e541a10e6837747eceb45d53e29c937d32
Mladen Ivkovic <mladen.ivkovic@epfl.ch>
2020-08-26 20:11:46 +0100

    created rt header files

---
+++ src/rt/none/rt.h
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (c) 2020 Mladen Ivkovic (mladen.ivkovic@hotmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#ifndef SWIFT_RT_NONE_H
+#define SWIFT_RT_NONE_H
+
+/**
+ * @file src/rt/debug/rt.h
+ * @brief Main header file for no radiative transfer scheme.
+ */
+
+/**
+ * @brief Dummy function to test whether inclusions work properly.
+ */
+void rt_dummy_function(void) { message("Called no RT scheme."); }
+
+#endif /* SWIFT_RT_NONE_H */
