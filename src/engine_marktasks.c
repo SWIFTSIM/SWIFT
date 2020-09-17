714663ea85d1e51eb786735c009c9267660d4d30
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-09-17 13:18:26 +0200

    Do not rebuild for now

--- src/engine_marktasks.c
+++ src/engine_marktasks.c
@@ -737,8 +737,8 @@ void engine_marktasks_mapper(void *map_data, int num_elements,
       else if (t->subtype == task_subtype_stars_density) {
 
         /* Too much particle movement? */
-        if (cell_need_rebuild_for_stars_pair(ci, cj)) *rebuild_space = 1;
-        if (cell_need_rebuild_for_stars_pair(cj, ci)) *rebuild_space = 1;
+        // if (cell_need_rebuild_for_stars_pair(ci, cj)) *rebuild_space = 1;
+        // if (cell_need_rebuild_for_stars_pair(cj, ci)) *rebuild_space = 1;
 
 #ifdef WITH_MPI
         /* Activate the send/recv tasks. */
