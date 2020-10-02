2737a743129762fb86dea15950c5065a83068330
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-10-02 22:03:16 +0200

    Fix typo in the blob test run script

--- examples/HydroTests/BlobTest_3D/run.sh
+++ examples/HydroTests/BlobTest_3D/run.sh
@@ -3,8 +3,8 @@
 # Generate the initial conditions if they are not present.
 if [ ! -e blob.hdf5 ]
 then
-    echo "Generating initial conditions for the Sod shock example..."
-    python makeIC.py
+    echo "Generating initial conditions for the 'blob test'..."
+    python3 makeIC.py
 fi
 
 # Run SWIFT
