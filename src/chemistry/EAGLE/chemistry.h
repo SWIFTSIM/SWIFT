39fb15a2b3a95205b16cbbefa35fc44f9b685b3c
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-10-07 14:29:54 +0100

    Update the collection of statistics to store the masses of the different types as well as the total metal content

--- src/chemistry/EAGLE/chemistry.h
+++ src/chemistry/EAGLE/chemistry.h
@@ -587,4 +587,40 @@ chemistry_get_metal_mass_fraction_for_star_formation(
   return p->chemistry_data.smoothed_metal_mass_fraction;
 }
 
+/**
+ * @brief Returns the total metallicity (metal mass fraction) of the
+ * gas particle to be used in the stats related routines.
+ *
+ * @param p Pointer to the particle data.
+ */
+__attribute__((always_inline)) INLINE static float
+chemistry_get_total_metal_mass_for_stats(const struct part* restrict p) {
+
+  return p->chemistry_data.metal_mass_fraction_total * hydro_get_mass(p);
+}
+
+/**
+ * @brief Returns the total metallicity (metal mass fraction) of the
+ * star particle to be used in the stats related routines.
+ *
+ * @param p Pointer to the particle data.
+ */
+__attribute__((always_inline)) INLINE static float
+chemistry_get_star_total_metal_mass_for_stats(const struct spart* restrict sp) {
+
+  return sp->chemistry_data.metal_mass_fraction_total * sp->mass;
+}
+
+/**
+ * @brief Returns the total metallicity (metal mass fraction) of the
+ * black hole particle to be used in the stats related routines.
+ *
+ * @param p Pointer to the particle data.
+ */
+__attribute__((always_inline)) INLINE static float
+chemistry_get_bh_total_metal_mass_for_stats(const struct bpart* restrict bp) {
+
+  return bp->chemistry_data.metal_mass_total;
+}
+
 #endif /* SWIFT_CHEMISTRY_EAGLE_H */
