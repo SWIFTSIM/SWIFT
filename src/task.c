ef52a8e348a6a2acd2d95b09c1822130be5e5d36
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-11-10 10:34:15 +0100

    Lock the limiter loop tasks

--- src/task.c
+++ src/task.c
@@ -536,9 +536,7 @@ void task_unlock(struct task *t) {
       } else if (subtype == task_subtype_do_bh_swallow) {
         cell_bunlocktree(ci);
       } else if (subtype == task_subtype_limiter) {
-#ifdef SWIFT_TASKS_WITHOUT_ATOMICS
         cell_unlocktree(ci);
-#endif
       } else { /* hydro */
         cell_unlocktree(ci);
       }
@@ -571,10 +569,8 @@ void task_unlock(struct task *t) {
         cell_bunlocktree(ci);
         cell_bunlocktree(cj);
       } else if (subtype == task_subtype_limiter) {
-#ifdef SWIFT_TASKS_WITHOUT_ATOMICS
         cell_unlocktree(ci);
         cell_unlocktree(cj);
-#endif
       } else { /* hydro */
         cell_unlocktree(ci);
         cell_unlocktree(cj);
@@ -733,10 +729,8 @@ int task_lock(struct task *t) {
         if (ci->black_holes.hold) return 0;
         if (cell_blocktree(ci) != 0) return 0;
       } else if (subtype == task_subtype_limiter) {
-#ifdef SWIFT_TASKS_WITHOUT_ATOMICS
         if (ci->hydro.hold) return 0;
         if (cell_locktree(ci) != 0) return 0;
-#endif
       } else { /* subtype == hydro */
         if (ci->hydro.hold) return 0;
         if (cell_locktree(ci) != 0) return 0;
@@ -816,14 +810,12 @@ int task_lock(struct task *t) {
           return 0;
         }
       } else if (subtype == task_subtype_limiter) {
-#ifdef SWIFT_TASKS_WITHOUT_ATOMICS
         if (ci->hydro.hold || cj->hydro.hold) return 0;
         if (cell_locktree(ci) != 0) return 0;
         if (cell_locktree(cj) != 0) {
           cell_unlocktree(ci);
           return 0;
         }
-#endif
       } else { /* subtype == hydro */
         /* Lock the parts in both cells */
         if (ci->hydro.hold || cj->hydro.hold) return 0;
