275a4bce5fc78129d17a57daf198a03c8237476d
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-11-22 21:49:52 +0100

    Early abort in DOPAIR_SUBSET() works correctly for cells not periodically wrapped

--- src/runner_doiact_functions_hydro.h
+++ src/runner_doiact_functions_hydro.h
@@ -748,9 +748,21 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
 
   const struct sort_entry *restrict sort_j = cell_get_hydro_sorts(cj, sid);
 
+  int is_ci = 0;
+  if (ci->loc[0] > 0.3 && ci->loc[0] < 0.4 &&
+      ci->loc[1] > 0.3 && ci->loc[1] < 0.4 &&
+      ci->loc[2] > 0.3 && ci->loc[2] < 0.4)
+    is_ci = 1;
+  
   /* Parts are on the left? */
   if (!flipped) {
 
+    if(is_ci)
+      message("NOT flipped! ci->loc=[%e %e %e] width=%f sid=%d", ci->loc[0], ci->loc[1], ci->loc[2], ci->dmin, sid);
+
+    if(is_ci)
+      message("    other:   cj->loc=[%e %e %e] shift=[%e %e %e]", cj->loc[0], cj->loc[1], cj->loc[2], shift[0], shift[1], shift[2]);
+    
     /* Loop over the parts_i. */
     for (int pid = 0; pid < count; pid++) {
 
@@ -781,26 +793,37 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
       // MATTHIEU: todo: early abort here
 
       /* Is the particle overlapping with the other cell? */
-      /* const double di = /\*hi * kernel_gamma + *\/ pix * runner_shift[sid][0]
-       * + */
-      /*                   piy * runner_shift[sid][1] + piz *
-       * runner_shift[sid][2]; */
+      const double di = /*hi * kernel_gamma + */ pix * runner_shift[sid][0]
+	+
+                        piy * runner_shift[sid][1] + piz *
+      runner_shift[sid][2];
 
-      /* const double dj_cell = sort_get_cell_min_dist(sid, cj->loc, cj->width);
-       */
+      const double dj_cell = sort_get_cell_min_dist(sid, cj->loc, cj->width);
 
+      if(is_ci && pid == 0)
+	message("     dj_cell= %e", dj_cell);
+      
       /* Most negative position on the axis of a particle in j */
-      // const double dj_min = sort_j[0].d - cj->hydro.dx_max_sort;
+      const double dj_min = sort_j[0].d - cj->hydro.dx_max_sort;
 
-      /* if (di > dj_cell + ci->hydro.dx_max_part) error("aa"); */
-      // if (dj_min  < dj_cell - cj->hydro.dx_max_part) message("bb");
+      if (di > dj_cell + ci->hydro.dx_max_part) error("aa");
+      if (dj_min  < dj_cell - cj->hydro.dx_max_part) message("bb");
 
+      /* No particle in range */
+      if(is_ci)
+      if (di + hi * kernel_gamma < dj_cell - cj->hydro.dx_max_part) {
+	//message("Abort!");
+	continue;
+      }
+
+      //if(is_ci) message("hello");
+      
       /* Loop over the parts in cj. */
       for (int pjd = 0; pjd < count_j /*&& sort_j[pjd].d < di*/; pjd++) {
 
         /* Get a pointer to the jth particle. */
-        // struct part *restrict pj = &parts_j[sort_j[pjd].i];
-        struct part *restrict pj = &parts_j[pjd];
+	struct part *restrict pj = &parts_j[sort_j[pjd].i];
+        //struct part *restrict pj = &parts_j[pjd];
 
         /* Skip inhibited particles. */
         if (part_is_inhibited(pj, e)) continue;
@@ -810,17 +833,17 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
         const double pjy = pj->x[1];
         const double pjz = pj->x[2];
 
-        /* const float dj = /\*hi * kernel_gamma + *\/ pjx *
-         * runner_shift[sid][0] + */
-        /*                  pjy * runner_shift[sid][1] + */
-        /*                  pjz * runner_shift[sid][2]; */
+        const float dj = /*hi * kernel_gamma + */ pjx *
+        runner_shift[sid][0] +
+                         pjy * runner_shift[sid][1] +
+                         pjz * runner_shift[sid][2];
 
-        // const float dj_sort = sort_j[pjd].d;
+        const float dj_sort = sort_j[pjd].d;
 
-        // if (dj < dj_sort - cj->hydro.dx_max_sort) error("cc rshift=%f",
-        // rshift);
+        if (dj < dj_sort - cj->hydro.dx_max_sort) error("cc rshift=%f",
+							rshift);
 
-        /* if (dj < dj_cell - cj->hydro.dx_max_part) error("dd"); */
+        if (dj < dj_cell - cj->hydro.dx_max_part) error("dd");
 
         /* Compute the pairwise distance. */
         float dx[3] = {(float)(pix - pjx), (float)(piy - pjy),
@@ -841,10 +864,10 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
         /* Hit or miss? */
         if (r2 < hig2) {
 
-          /* if (di + hi * kernel_gamma < dj_cell - cj->hydro.dx_max_part) */
-          /*   error("ee"); */
-          // if (di + hi * kernel_gamma > dj_cell - cj->hydro.dx_max_part)
-          // message("yay !");
+          if (di + hi * kernel_gamma < dj_cell - cj->hydro.dx_max_part)
+            error("ee");
+          /* if (di + hi * kernel_gamma > dj_cell - cj->hydro.dx_max_part) */
+          /* message("yay !"); */
 
           IACT_NONSYM(r2, dx, hi, hj, pi, pj, a, H);
 #if (FUNCTION_TASK_LOOP == TASK_LOOP_DENSITY)
@@ -861,13 +884,22 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
       } /* loop over the parts in cj. */
     }   /* loop over the parts in ci. */
   }
-
+  
   /* Parts are on the right. */
   else {
+
+    if(is_ci)
+      message("flipped! ci->loc=[%e %e %e] width=%f sid=%d", ci->loc[0], ci->loc[1], ci->loc[2], ci->dmin, sid);
+    if(is_ci)
+      message("  other: cj->loc=[%e %e %e] shift=[%e %e %e]", cj->loc[0], cj->loc[1], cj->loc[2], shift[0], shift[1], shift[2]);
 
     /* Loop over the parts_i. */
     for (int pid = 0; pid < count; pid++) {
 
+      /* Get the cutoff shift. */
+      double rshift = 0.0;
+      for (int k = 0; k < 3; k++) rshift += shift[k] * runner_shift[sid][k];
+      
       /* Get a hold of the ith part in ci. */
       struct part *restrict pi = &parts_i[ind[pid]];
       const double pix = pi->x[0] - (shift[0]);
@@ -891,17 +923,26 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
       // MATTHIEU: todo: early abort here
 
       /* Is the particle overlapping with the other cell? */
-      /* const double di = /\*-hi * kernel_gamma  + *\/ pix *
-       * runner_shift[sid][0] + */
-      /*                   piy * runner_shift[sid][1] + piz *
-       * runner_shift[sid][2]; */
-      /* if (di > 0.) continue; */
+      const double di = /*-hi * kernel_gamma  + */ pix * runner_shift[sid][0] +
+	piy * runner_shift[sid][1] + piz *  runner_shift[sid][2];
 
-      /* const double dj_cell = sort_get_cell_min_dist(sid, cj->loc, cj->width);
-       */
+      const double di_cell = sort_get_cell_min_dist(sid, ci->loc, ci->width);
 
-      /* if (di < dj_cell - ci->hydro.dx_max_part) error("aa"); */
+      if(is_ci && pid == 0)
+	message("     di_cell= %e", di_cell);
+      
+      if(is_ci)
+	if (di < di_cell - ci->hydro.dx_max_part) error("aa");
 
+      /* No particle in range */
+      if (is_ci)
+      if( di - hi * kernel_gamma > di_cell + cj->hydro.dx_max_part) {
+	//message("Abort!");
+	continue;
+      }
+     
+      //if(is_ci) message("hello");
+      
       /* Loop over the parts in cj. */
       for (int pjd = count_j - 1; pjd >= 0 /* && di < sort_j[pjd].d */; pjd--) {
 
@@ -917,14 +958,17 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
         const double pjy = pj->x[1];
         const double pjz = pj->x[2];
 
-        /* const float dj = /\*hi * kernel_gamma + *\/pjx * runner_shift[sid][0]
-         * + */
-        /* pjy * runner_shift[sid][1] + pjz * */
-        /* runner_shift[sid][2];  */
+        const float dj = pjx * runner_shift[sid][0] + pjy * runner_shift[sid][1] + pjz * runner_shift[sid][2];
 
-        // if(di < dj) error("oO");
+        const float dj_sort = sort_j[pjd].d;
+        if (dj < dj_sort - cj->hydro.dx_max_sort) error("cc rshift=%f",
+							rshift);
 
-        // if (dj > dj_cell + cj->hydro.dx_max_part) error("dd");
+	
+        //if(di < dj) error("oO");
+
+	if(is_ci)
+	  if (dj > di_cell + cj->hydro.dx_max_part) error("dd rshift=%f", rshift);
 
         /* Compute the pairwise distance. */
         float dx[3] = {(float)(pix - pjx), (float)(piy - pjy),
@@ -963,6 +1007,10 @@ void DOPAIR_SUBSET(struct runner *r, const struct cell *restrict ci,
     }   /* loop over the parts in ci. */
   }
 
+  if(is_ci)
+    message("");
+
+  
   TIMER_TOC(timer_dopair_subset);
 }
 
