602b41345bd4bb28464219c1f39bb4936e2902c9
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2020-11-25 17:21:32 +0100

    Small improvements to the gas neighbour number bisection scheme

--- src/runner_ghost.c
+++ src/runner_ghost.c
@@ -1050,7 +1050,7 @@ void runner_do_ghost(struct runner *r, struct cell *c, int timer) {
       if (part_is_active(&parts[k], e)) {
         pid[count] = k;
         h_0[count] = parts[k].h;
-        left[count] = 0.f;
+        left[count] = hydro_h_min;
         right[count] = hydro_h_max;
         ++count;
       }
@@ -1090,6 +1090,9 @@ void runner_do_ghost(struct runner *r, struct cell *c, int timer) {
 
           /* Double h and try again */
           h_new = 2.f * h_old;
+
+          /* Improve the bisection bound as well */
+          left[i] = max(left[i], h_old);
 
         } else {
 
