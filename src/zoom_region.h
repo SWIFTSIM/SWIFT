3d12a275fc65e928f2f0a6d68fd2dded119fd1fd
rttw52 <stuart.mcalpine@helsink.fi>
2021-03-13 14:31:36 +0000

    Fixed typo

--- src/zoom_region.h
+++ src/zoom_region.h
@@ -7,7 +7,7 @@
 void zoom_region_init(struct swift_params *params, struct space *s);
 void construct_zoom_region(struct space *s, int verbose);
 void construct_tl_cells_with_zoom_region(struct space *s, const int *cdim, const float dmin, 
-        const integertime_t ti_current, cont int verbose);
+        const integertime_t ti_current, const int verbose);
 void find_neighbouring_cells(struct space *s, const int verbose);
 
 #endif /* SWIFT_ZOOM_H */
