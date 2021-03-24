2fbc41d58fbef6461efc6e737f69f74bed0b66f5
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-03-24 23:41:41 +0100

    Update the stand-alone FOF executable to use the new space_init() signature

--- examples/main_fof.c
+++ examples/main_fof.c
@@ -556,8 +556,9 @@ int main(int argc, char *argv[]) {
              sinks, sparts, bparts, Ngas, Ngpart, Nsink, Nspart, Nbpart,
              Nnupart, periodic, replicate, /*remap_ids=*/0,
              /*generate_gas_in_ics=*/0, /*hydro=*/N_total[0] > 0, /*gravity=*/1,
-             /*with_star_formation=*/0, with_DM_background_particles,
-             with_neutrinos, talking, /*dry_run=*/0, nr_nodes);
+             /*with_star_formation=*/0, /*sink=*/N_total[swift_type_sink],
+             with_DM_background_particles, with_neutrinos, talking,
+             /*dry_run=*/0, nr_nodes);
 
   if (myrank == 0) {
     clocks_gettime(&toc);
