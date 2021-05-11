79a6183f6016e3b2e1c9f45d36953226b6c97398
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-05-11 14:27:37 +0200

    Add counters also to the limiter loops to test them against the correct answer

--- src/timestep_limiter_struct.h
+++ src/timestep_limiter_struct.h
@@ -43,6 +43,20 @@ struct timestep_limiter_data {
 
   /* Do we want this particle to be synched back on the time-line? */
   char to_be_synchronized;
+
+#ifdef SWIFT_HYDRO_DENSITY_CHECKS
+  /* Weighted number of neighbours in the limiter loop */
+  float n_limiter;
+
+  /* Exact weighted number of neighbours in the limiter loop */
+  float n_limiter_exact;
+
+  /* Integer number of neighbours in the limiter loop */
+  int N_limiter;
+
+  /* Exact integer number of neighbours in the limiter loop */
+  int N_limiter_exact;
+#endif
 };
 
 #endif /* SWIFT_TIMESTEP_LIMITER_STRUCT_H */
