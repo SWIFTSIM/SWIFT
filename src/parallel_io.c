2f4e1c4ee99769de967a33bd992499e4279968f8
Camila Correa <cami.acorrea@gmail.com>
2021-06-01 22:31:08 +0200

    Minor fix

--- src/parallel_io.c
+++ src/parallel_io.c
@@ -762,7 +762,7 @@ void read_ic_parallel(char* fileName, const struct unit_system* internal_units,
                       size_t* Ngparts_background, size_t* Nnuparts,
                       size_t* Nsinks, size_t* Nstars, size_t* Nblackholes,
                       int* flag_entropy, int with_hydro, int with_gravity,
-                      int with_sink, int with_stars, int with_black_holes, int with_sidm
+                      int with_sink, int with_stars, int with_black_holes, int with_sidm,
                       int with_cosmology, int cleanup_h, int cleanup_sqrt_a,
                       double h, double a, int mpi_rank, int mpi_size,
                       MPI_Comm comm, MPI_Info info, int n_threads, int dry_run,
