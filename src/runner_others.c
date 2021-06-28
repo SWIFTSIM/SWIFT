454ead32795ec5fe9d840047f733cb80196ea58e
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-06-28 12:56:28 +0200

    Add a check to verify that FOF tasks are run on purely local pairs

--- src/runner_others.c
+++ src/runner_others.c
@@ -988,6 +988,10 @@ void runner_do_fof_pair(struct runner *r, struct cell *ci, struct cell *cj,
   const struct gpart *const gparts = s->gparts;
   const double search_r2 = e->fof_properties->l_x2;
 
+#ifdef SWIFT_DEBUG_CHECKS
+  if (ci->nodeID != cj->nodeID) error("Searching foreign cells!");
+#endif
+
   rec_fof_search_pair(e->fof_properties, dim, search_r2, periodic, gparts, ci,
                       cj);
 
