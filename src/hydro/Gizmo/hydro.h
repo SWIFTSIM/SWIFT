4fe763bb50c445a68701da85c51c0ad0eb0b0074
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2021-08-24 15:47:35 +0200

    Fixed flux asymmetry problem during initial time step(s).

--- src/hydro/Gizmo/hydro.h
+++ src/hydro/Gizmo/hydro.h
@@ -698,6 +698,16 @@ __attribute__((always_inline)) INLINE static void hydro_kick_extra(
     /* invalidate the particle time step. It is considered to be inactive until
        dt is set again in hydro_prepare_force() */
     p->flux.dt = -1.0f;
+  } else if (p->flux.dt == 0.0f) {
+    /* something tricky happens at the beginning of the simulation: the flux
+       exchange is done for all particles, but using a time step of 0. This
+       in itself is not a problem. However, it causes some issues with the
+       initialisation of flux.dt for inactive particles, since this value will
+       remain 0 until the particle is active again, and its flux.dt is set to
+       the actual time step in hydro_prepare_force(). We have to make sure it
+       is properly set to -1 here, so that inactive particles are indeed found
+       to be inactive during the flux loop. */
+    p->flux.dt = -1.0f;
   }
 
 #ifndef HYDRO_GAMMA_5_3
