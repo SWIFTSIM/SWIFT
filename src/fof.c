aad7800f16ef8b1f679c7f91acc42898eb466464
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2021-09-02 15:07:54 +0200

    Fix a mismatch in the logging of memory usage in fof_finalise_group_data()

--- src/fof.c
+++ src/fof.c
@@ -2057,10 +2057,12 @@ void fof_finalise_group_data(struct fof_props *props,
                              const struct gpart *gparts, const int periodic,
                              const double dim[3], const int num_groups) {
 
-  size_t *group_size = (size_t *)malloc(num_groups * sizeof(size_t));
-  size_t *group_index = (size_t *)malloc(num_groups * sizeof(size_t));
-  double *group_centre_of_mass =
-      (double *)malloc(3 * num_groups * sizeof(double));
+  size_t *group_size =
+      (size_t *)swift_malloc("fof_group_size", num_groups * sizeof(size_t));
+  size_t *group_index =
+      (size_t *)swift_malloc("fof_group_index", num_groups * sizeof(size_t));
+  double *group_centre_of_mass = (double *)swift_malloc(
+      "fof_group_centre_of_mass", 3 * num_groups * sizeof(double));
 
   for (int i = 0; i < num_groups; i++) {
 
