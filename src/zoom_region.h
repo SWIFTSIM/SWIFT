83d37e977b6c4718d400415ad3929002af15a05f
wjr21 <wjr21@sussex.ac.uk>
2022-01-17 12:53:13 +0000

    Define neighbour cells by gravity task distance including theta crit

--- src/zoom_region.h
+++ src/zoom_region.h
@@ -11,7 +11,7 @@ int cell_getid_zoom(const int cdim[3], const double x, const double y,
 void construct_zoom_region(struct space *s, int verbose);
 void construct_tl_cells_with_zoom_region(struct space *s, const int *cdim, const float dmin, 
         const integertime_t ti_current, int verbose);
-void find_neighbouring_cells(struct space *s, const int verbose);
+void find_neighbouring_cells(struct space *s, struct engine *engine, const int verbose);
 double cell_min_dist2_diff_size(const struct cell *restrict ci,
                                 const struct cell *restrict cj,
                                 const int periodic, const double dim[3]);
