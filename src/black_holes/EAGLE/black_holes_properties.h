1b98dc3f3ac9b3e82bc7e6411a99f0f3236df465
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2022-01-31 21:39:22 +0100

    Rename the minimal mass for nibbling parameter to 'min_gas_mass_for_nibbling_Msun' to indicate the units

--- src/black_holes/EAGLE/black_holes_properties.h
+++ src/black_holes/EAGLE/black_holes_properties.h
@@ -381,8 +381,8 @@ INLINE static void black_holes_props_init(struct black_holes_props *bp,
 
   bp->use_nibbling = parser_get_param_int(params, "EAGLEAGN:use_nibbling");
   if (bp->use_nibbling) {
-    bp->min_gas_mass_for_nibbling =
-        parser_get_param_float(params, "EAGLEAGN:min_gas_mass_for_nibbling");
+    bp->min_gas_mass_for_nibbling = parser_get_param_float(
+        params, "EAGLEAGN:min_gas_mass_for_nibbling_Msun");
     bp->min_gas_mass_for_nibbling *= phys_const->const_solar_mass;
   }
 
