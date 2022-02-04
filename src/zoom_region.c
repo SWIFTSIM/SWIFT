562cd8a3778118fdd84ea99cb309ccd4d4908ccd
wjr21 <wjr21@sussex.ac.uk>
2022-02-04 19:01:40 +0000

    testing symmetry condition for task creation

--- src/zoom_region.c
+++ src/zoom_region.c
@@ -1839,8 +1839,6 @@ void engine_make_self_gravity_tasks_mapper_with_zoom_diffsize(void *map_data,
 	/* Handle on the cells and proxies */
 	struct cell *cells = s->cells_top;
 
-	message("%d %d", s->zoom_props->tl_cell_offset, s->nr_cells);
-
 	/* Some info about the zoom domain */
 	const int bkg_cell_offset = s->zoom_props->tl_cell_offset;
 
@@ -1867,9 +1865,9 @@ void engine_make_self_gravity_tasks_mapper_with_zoom_diffsize(void *map_data,
 		/* Get the cell */
 		struct cell *ci = &cells[cid];
 
-    if (ci->nodeID != nodeID) {
-			continue;
-		}
+//    if (ci->nodeID != nodeID) {
+//			continue;
+//		}
 
 		/* Skip cells without gravity particles */
 		if (ci->grav.count == 0) continue;
@@ -1893,10 +1891,10 @@ void engine_make_self_gravity_tasks_mapper_with_zoom_diffsize(void *map_data,
 			/* Get the cell */
 			struct cell *cj = &cells[cjd];
 
-			/* Skip non-neighbour natural cells. */
-			if (cj->tl_cell_type != tl_cell_neighbour){
-				continue;
-			}
+//			/* Skip non-neighbour natural cells. */
+//			if (cj->tl_cell_type != tl_cell_neighbour){
+//				continue;
+//			}
 
 //			/* Explictly avoid duplicates */
 //			if (((ci->nodeID == nodeID && ci->tl_cell_type <= 2) && (cj->nodeID == nodeID && cj->tl_cell_type == zoom_tl_cell)) ||
@@ -1926,9 +1924,6 @@ void engine_make_self_gravity_tasks_mapper_with_zoom_diffsize(void *map_data,
 			/* Are the cells too close for a MM interaction ? */
 			if (!cell_can_use_pair_mm(ci, cj, e, s, /*use_rebuild_data=*/1,
 					/*is_tree_walk=*/0)) {
-
-				message("cid=%d cjd=%d, ci->tl_cell_type=%d cj->tl_cell_type=%d, ci->nodeID=%d cj->nodeID=%d",
-						cid, cjd, ci->tl_cell_type, cj->tl_cell_type, ci->nodeID, cj->nodeID);
 
 				/* Ok, we need to add a direct pair calculation */
 				scheduler_addtask(sched, task_type_pair, task_subtype_grav, 0, 0,
