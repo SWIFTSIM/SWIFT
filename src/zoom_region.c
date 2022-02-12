d2ee3eb286164444e5960bdc7b0199d8d1f29515
wjr21 <wjr21@sussex.ac.uk>
2022-02-12 01:04:10 +0000

    debug previous commit

--- src/zoom_region.c
+++ src/zoom_region.c
@@ -243,8 +243,9 @@ void construct_zoom_region(struct space *s, int verbose) {
 	
   	/* Find the bounding cells (including zoom boost factor) */
   	const double width = (new_zoom_boundary[(ijk * 2) + 1] - new_zoom_boundary[ijk * 2]) * zoom_boost_factor;
-		const int ijk_low_bound = (s->zoom_props->com[ijk] - (width / 2)) * s->iwidth[ijk];
-		const int ijk_up_bound = (s->zoom_props->com[ijk] + (width / 2)) * s->iwidth[ijk];
+  	message("%f %d %d %d", width, (ijk * 2) + 1, ijk * 2, ijk);
+		const int ijk_low_bound = (com[ijk] - (width / 2)) * s->iwidth[ijk];
+		const int ijk_up_bound = (com[ijk] + (width / 2)) * s->iwidth[ijk];
 		message("%d %d", ijk_low_bound, ijk_up_bound);
 		/* Find the length of the region along this axis and assign it to array */
 		widths[ijk] = (ijk_up_bound + 1 - ijk_low_bound) * s->width[ijk];
