c1f4c8a884356eb253580f99b531a12af6e1368e
wjr21 <wjr21@sussex.ac.uk>
2022-02-23 23:36:13 +0000

    testing regrid width and h_max calculation

--- src/zoom_regrid.c
+++ src/zoom_regrid.c
@@ -168,7 +168,7 @@ void space_regrid_zoom(struct space *s, struct gravity_props *gravity_properties
 		}
 	}
 
-	mesage("h_max=%f,  kernel_gamma=%f, space_stretch=%f, h_max * kernel_gamma * space_stretch=%f, zoom_cell_min=%f, max=%f", h_max, kernel_gamma, space_stretch, h_max * kernel_gamma * space_stretch, zoom_cell_min, fmax(h_max * kernel_gamma * space_stretch, zoom_cell_min));
+	message("h_max=%f,  kernel_gamma=%f, space_stretch=%f, h_max * kernel_gamma * space_stretch=%f, zoom_cell_min=%f, max=%f", h_max, kernel_gamma, space_stretch, h_max * kernel_gamma * space_stretch, zoom_cell_min, fmax(h_max * kernel_gamma * space_stretch, zoom_cell_min));
 
 	/* Get the new putative zoom cell dimensions. We can initially use the
 	 * input from s->zoom_props->nr_zoom_per_bkg_cells
