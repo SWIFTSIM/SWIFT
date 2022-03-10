6cdc73fa38aca4d563534b86b4035b108c16a0df
wjr21 <wjr21@sussex.ac.uk>
2022-03-10 14:32:38 +0000

    moved legend

--- tools/task_plots/plot_threadpool.py
+++ tools/task_plots/plot_threadpool.py
@@ -318,10 +318,10 @@ if not args.nolegend:
     ax.fill_between([0, 0], nthread + 0.5, nthread + nrow + 0.5, facecolor="white")
     ax.set_ylim(0, nthread + 0.5)
     ax.legend(
-        loc=1, shadow=True, bbox_to_anchor=(0.0, 1.05, 1.0, 0.2), mode="expand", ncol=5
+        loc=1, shadow=True, bbox_to_anchor=(0.0, 1.15, 1.0, 0.2), mode="expand", ncol=5
     )
     box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width, box.height * 0.6])
+    ax.set_position([box.x0, box.y0, box.width, box.height * 0.8])
 
 # Start and end of time-step
 real_start_t = (mintic_step - tic_step) / CPU_CLOCK
