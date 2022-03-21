bf487956b9d973edf184b2713851d1bb845906c5
Yves Revaz <yves.revaz@epfl.ch>
2022-03-21 19:25:56 +0000

    GEAR model improvements plus bug fixed

--- src/feedback/GEAR/stellar_evolution.h
+++ src/feedback/GEAR/stellar_evolution.h
@@ -43,8 +43,8 @@ void stellar_evolution_compute_continuous_feedback_properties(
 void stellar_evolution_compute_discrete_feedback_properties(
     struct spart* restrict sp, const struct stellar_model* sm,
     const struct phys_const* phys_const, const float log_m_beg_step,
-    const float log_m_end_step, const float m_beg_step, const float m_end_step,
-    const float m_init, const int number_snia, const int number_snii);
+    const float m_end_step, const float m_init, const int number_snia,
+    const int number_snii);
 
 void stellar_evolution_evolve_spart(
     struct spart* restrict sp, const struct stellar_model* sm,
@@ -54,6 +54,9 @@ void stellar_evolution_evolve_spart(
 
 const char* stellar_evolution_get_element_name(const struct stellar_model* sm,
                                                int i);
+int stellar_evolution_get_element_index(const struct stellar_model* sm,
+                                        const char* element_name);
+
 void stellar_evolution_read_elements(struct stellar_model* sm,
                                      struct swift_params* params);
 void stellar_evolution_props_init(struct stellar_model* sm,
