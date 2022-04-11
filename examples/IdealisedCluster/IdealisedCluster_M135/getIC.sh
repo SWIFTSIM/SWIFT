e1f1e6fa72bb87a82ab5bdd15baa6209f97fb93b
Folkert Nobels <nobels@strw.leidenuniv.nl>
2022-04-11 09:29:10 +0000

    Idealised cluster example

---
+++ examples/IdealisedCluster/IdealisedCluster_M135/getIC.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# The fiducial IC (T_0 = 10^6.5 K)
+wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H135_Tmin65.hdf5
+
+# Different central temperatures T_0
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H135_Tmin60.hdf5
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H135_Tmin675.hdf5
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H135_Tmin70.hdf5
---
+++ examples/IdealisedCluster/IdealisedCluster_M14/getIC.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# fiducial IC (T_0 = 10^7 K)
+wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H14_fid.hdf5
+
+# Different central temperatures (T_0)
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H14_Tmin65.hdf5
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H14_Tmin675.hdf5
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_H14_Tmin725.hdf5
+
+# Fiducial IC at different resolutions
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_lowres8_Tmin7.hdf5
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_lowres64_Tmin7.hdf5
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_lowres512_Tmin7.hdf5
+#wget http://virgodb.cosma.dur.ac.uk/swift-webstorage/ICs/IdealisedCluster/M5_lowres4096_Tmin7.hdf5
