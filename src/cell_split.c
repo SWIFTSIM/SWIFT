837567cb0ec4453bc7722bffd034476f884251f5
wjr21 <wjr21@sussex.ac.uk>
2022-06-14 11:44:43 +0100

    Preallocating the progeny of depth 0 to mitigate waiting on locks

--- src/cell_split.c
+++ src/cell_split.c
@@ -267,7 +267,8 @@ void cell_split_recursive(struct space *s, struct cell *c, const int maxdepth,
                                c->width[2] / 2};
 
     /* Create the cell's progeny. */
-    space_getcells(s, 8, c->progeny);
+    /* NOTE: Depth 0 progeny are already allocated. */
+    if (c->depth > 0) space_getcells(s, 8, c->progeny);
     for (int k = 0; k < 8; k++) {
 
       /* Lets work out whese this progeny should go based on the hilbert key
