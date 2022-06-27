0de2ff0784990d43ba7ed5731b5deeed787f1ced
wjr21 <wjr21@sussex.ac.uk>
2022-06-27 17:26:58 +0100

    Saity check that task order does not matter

--- src/engine_maketasks.c
+++ src/engine_maketasks.c
@@ -4383,14 +4383,14 @@ void engine_maketasks(struct engine *e) {
 #ifdef WITH_ZOOM_REGION
     if (s->with_zoom_region) {
       threadpool_map(
-          &e->threadpool, engine_make_self_gravity_tasks_mapper_zoom_cells,
-          NULL, s->zoom_props->nr_zoom_cells, 1, threadpool_auto_chunk_size, e);
-      threadpool_map(
           &e->threadpool, engine_make_self_gravity_tasks_mapper_natural_cells,
           NULL, s->zoom_props->nr_bkg_cells, 1, threadpool_auto_chunk_size, e);
       threadpool_map(&e->threadpool,
                      engine_make_self_gravity_tasks_mapper_with_zoom_diffsize,
                      NULL, s->nr_cells, 1, threadpool_auto_chunk_size, e);
+      threadpool_map(
+          &e->threadpool, engine_make_self_gravity_tasks_mapper_zoom_cells,
+          NULL, s->zoom_props->nr_zoom_cells, 1, threadpool_auto_chunk_size, e);
     } else {
       threadpool_map(&e->threadpool, engine_make_self_gravity_tasks_mapper,
                      NULL, s->nr_cells, 1, threadpool_auto_chunk_size, e);
