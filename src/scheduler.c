14fb2151a11e870ba5fad3eaa2c2550f3a997562
wjr21 <wjr21@sussex.ac.uk>
2022-06-30 14:29:31 +0100

    Randomising queue IDs

--- src/scheduler.c
+++ src/scheduler.c
@@ -2461,7 +2461,8 @@ void scheduler_enqueue(struct scheduler *s, struct task *t) {
 
     /* If no previous owner, pick a random queue. */
     /* Note that getticks() is random enough */
-    if (qid < 0) qid = getticks() % s->nr_queues;
+    /* if (qid < 0)  */
+    qid = getticks() % s->nr_queues;
     
     /* Increase the waiting counter. */
     atomic_inc(&s->waiting);
