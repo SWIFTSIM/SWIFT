cbfbf54007e9b351f3ce9d9b26e98726f61a5ba9
wjr21 <wjr21@sussex.ac.uk>
2022-07-05 18:31:50 +0100

    Fixing duplicated line from merge

--- src/space.h
+++ src/space.h
@@ -477,7 +477,6 @@ void space_map_cells_post(struct space *s, int full,
                           void (*fun)(struct cell *c, void *data), void *data);
 void space_rebuild(struct space *s, int repartitioned,
                    struct gravity_props *gravity_properties, int verbose);
-void space_recycle(struct space *s, struct cell *c);
 void space_recycle(struct space *s, struct cell *c, const int lock);
 void space_recycle_list(struct space *s, struct cell *cell_list_begin,
                         struct cell *cell_list_end,
