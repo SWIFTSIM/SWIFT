d0999769d2f2cbf36c1f8977d273cdb81833072b
Peter W. Draper <p.w.draper@durham.ac.uk>
2022-07-11 14:25:44 +0000

    Move executable out of examples

--- examples/Cooling/CoolingHaloWithSpin/run.sh
+++ examples/Cooling/CoolingHaloWithSpin/run.sh
@@ -5,7 +5,7 @@ echo "Generating initial conditions for the isothermal potential box example..."
 python3 makeIC.py 10000 
 
 # Run SWIFT with external potential, SPH and cooling
-../../swift --external-gravity --hydro --cooling --threads=1 cooling_halo.yml 2>&1 | tee output.log
+../../../swift --external-gravity --hydro --cooling --threads=1 cooling_halo.yml 2>&1 | tee output.log
 
 # python3 radial_profile.py 10
 
--- examples/SantaBarbara/SantaBarbara-128/run.sh
+++ examples/SantaBarbara/SantaBarbara-128/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run SWIFT
-../../swift --cosmology --hydro --self-gravity --threads=28 santa_barbara.yml
+../../../swift --cosmology --hydro --self-gravity --threads=28 santa_barbara.yml
 
