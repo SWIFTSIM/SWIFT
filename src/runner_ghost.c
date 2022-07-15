492c3d5ee5d5eed3074d9b8f674fd3256aa1aedd
Doug Rennehan <rennehan@uvic.ca>
2022-07-15 15:02:16 +0200

    It's a pointer.

--- src/runner_ghost.c
+++ src/runner_ghost.c
@@ -1125,7 +1125,7 @@ void runner_do_ghost(struct runner *r, struct cell *c, int timer) {
     for (int k = 0; k < c->hydro.count; k++)
       if (part_is_active(&parts[k], e)) {
         /* Do not recompute h for wind particles */
-        if (parts[k]->feedback_data.decoupling_delay_time > 0.f) continue;
+        if (&parts[k]->feedback_data.decoupling_delay_time > 0.f) continue;
         pid[count] = k;
         h_0[count] = parts[k].h;
         left[count] = 0.f;
