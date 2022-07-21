ddfc71b375061328664a839c5a64c0be1e323a1d
Doug Rennehan <rennehan@uvic.ca>
2022-07-21 16:34:30 +0200

    Be more explicit in which timestep is zero.

--- src/timestep.h
+++ src/timestep.h
@@ -206,6 +206,11 @@ __attribute__((always_inline)) INLINE static integertime_t get_part_timestep(
   new_dt = min(new_dt, e->dt_max);
 
   if (new_dt < e->dt_min)
+    message("dt_h_change=%g new_dt_mhd=%g new_dt_chemistry=%g"
+            "new_dt_radiation=%g new_dt_grav=%g new_dt_cooling=%g"
+            "new_dt_hydro=%g",
+            dt_h_change, new_dt_mhd, new_dt_chemistry, new_dt_radiation,
+            new_dt_grav, new_dt_cooling, new_dt_hydro);
     error("part (id=%lld) wants a time-step (%e) below dt_min (%e)", p->id,
           new_dt, e->dt_min);
 
