e243a389cbf961b5d0c5c7832c33763f5ecb7153
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2022-08-10 07:44:09 +0000

    GEARRT thermochemistry fixes

--- src/const.h
+++ src/const.h
@@ -102,4 +102,8 @@
 #define SOURCETERMS_NONE
 //#define SOURCETERMS_SN_FEEDBACK
 
+/* GRACKLE doesn't really like exact zeroes, so use something
+ * comparatively small instead. */
+#define RT_GEAR_TINY_MASS_FRACTION 1.e-20
+
 #endif /* SWIFT_CONST_H */
