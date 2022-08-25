8d609c8d7f9c9194263dc50345aba3ae5c8b7fcf
Doug Rennehan <rennehan@uvic.ca>
2022-08-25 14:50:34 -0400

    It has to be xpart.

--- src/black_holes/SIMBA/black_holes_iact.h
+++ src/black_holes/SIMBA/black_holes_iact.h
@@ -877,9 +877,9 @@ runner_iact_nonsym_bh_gas_feedback(
         const float r = sqrtf(r2);
         const float dv_comoving = dv_phys * cosmo->a;
         const float prefactor = dv_comoving / r;
-        pj->gpart->v_full[0] += prefactor * dx[0];
-        pj->gpart->v_full[1] += prefactor * dx[1];
-        pj->gpart->v_full[2] += prefactor * dx[2];
+        xpj->v_full[0] += prefactor * dx[0];
+        xpj->v_full[1] += prefactor * dx[1];
+        xpj->v_full[2] += prefactor * dx[2];
 #endif
 
         du_xray_phys *= (1. - bh_props->xray_kinetic_fraction);
@@ -944,9 +944,9 @@ runner_iact_nonsym_bh_gas_feedback(
         random_unit_interval(bi->id, ti_current, random_number_BH_feedback);
     const float dirsign = (random_number > 0.5) ? 1.f : -1.f;
     const float prefactor = bi->v_kick * cosmo->a * dirsign / norm;
-    pj->gpart->v_full[0] += prefactor * bi->angular_momentum_gas[0];
-    pj->gpart->v_full[1] += prefactor * bi->angular_momentum_gas[1];
-    pj->gpart->v_full[2] += prefactor * bi->angular_momentum_gas[2];
+    xpj->v_full[0] += prefactor * bi->angular_momentum_gas[0];
+    xpj->v_full[1] += prefactor * bi->angular_momentum_gas[1];
+    xpj->v_full[2] += prefactor * bi->angular_momentum_gas[2];
 #endif
 
 #ifdef SIMBA_DEBUG_CHECKS
