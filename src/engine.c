d75f98bf58badd946c0195c7d6a5e8ddda66c370
wjr21 <wjr21@sussex.ac.uk>
2022-10-10 17:09:47 +0100

    Extra debugging information

--- src/engine.c
+++ src/engine.c
@@ -512,11 +512,11 @@ void engine_exchange_top_multipoles(struct engine *e) {
     if (e->s->cells_top[i].nodeID == engine_rank) {
       if (m->m_pole.M_000 > 0.) {
         if (m->CoM[0] < 0. || m->CoM[0] > e->s->dim[0])
-          error("Invalid multipole position in X");
+          error("Invalid multipole position in X (cid=%d, m->CoM[0]=%.2f)", i, m->CoM[0]);
         if (m->CoM[1] < 0. || m->CoM[1] > e->s->dim[1])
-          error("Invalid multipole position in Y");
+          error("Invalid multipole position in Y (cid=%d)", i);
         if (m->CoM[2] < 0. || m->CoM[2] > e->s->dim[2])
-          error("Invalid multipole position in Z");
+          error("Invalid multipole position in Z (cid=%d)", i);
       }
     } else {
       if (m->m_pole.M_000 != 0.) error("Non-zero mass for foreign m-pole");
