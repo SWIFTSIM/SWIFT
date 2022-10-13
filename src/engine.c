e8497167286d13f1e2aeeac340c1c06f0dd8faf7
wjr21 <wjr21@sussex.ac.uk>
2022-10-13 18:10:03 +0100

    Typo

--- src/engine.c
+++ src/engine.c
@@ -1922,9 +1922,10 @@ void engine_init_particles(struct engine *e, int flag_entropy_ICs,
   /* Check that we have the correct total mass in the top-level multipoles */
   long long num_gpart_mpole = 0;
   if (e->policy & engine_policy_self_gravity) {
-    for (int i = 0; i < e->s->nr_cells; ++i)
+    for (int i = 0; i < e->s->nr_cells; ++i) {
       if (e->s->cells_top[i].tl_cell_type == void_tl_cell) continue;
-      num_gpart_mpole += e->s->cells_top[i].grav.multipole->m_pole.num_gpart;
+      num_gpart_mpole += e->s->cells_top[i].grav.multipole->m_pole.num_gpart; 
+    }
     if (num_gpart_mpole != e->total_nr_gparts)
       error(
           "Top-level multipoles don't contain the total number of gpart "
