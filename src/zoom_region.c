ee8d624119db023af79677710a3e904fe7b18a05
wjr21 <wjr21@sussex.ac.uk>
2022-10-15 16:46:43 +0100

    Removing neighbours from pool again and removing pool tasks from MPI

--- src/zoom_region.c
+++ src/zoom_region.c
@@ -1708,14 +1708,18 @@ void engine_make_self_gravity_tasks_mapper_natural_cells(void *map_data,
                         NULL);
     }
 
+#ifndef WITH_MPI
+
     /* Create the background specific pair task for non-neighbour bkg cells. */
-    if (ci->tl_cell_type == tl_cell || ci->tl_cell_type == tl_cell_neighbour) {
+    if (ci->tl_cell_type == tl_cell) {
       /* Ok, we need to add a direct pair calculation */
       scheduler_addtask(sched, task_type_pair, task_subtype_grav_bkg_pool,
                         0, 0, ci, NULL);
       continue;
       
     }
+
+#endif
 
     /* Loop over every other cell within (Manhattan) range delta */
     for (int ii = i - delta_m; ii <= i + delta_p; ii++) {
