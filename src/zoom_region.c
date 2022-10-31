2ca41e96515509ecce8db9503262c4e6c39bfce8
wjr21 <wjr21@sussex.ac.uk>
2022-10-31 18:54:03 +0000

    Unused variable

--- src/zoom_region.c
+++ src/zoom_region.c
@@ -846,9 +846,9 @@ void find_vertex_edges(struct space *s, const int verbose) {
           for (int jj = void_j - 1; jj <= void_j + 1; jj++) {
             for (int kk = void_k - 1; kk <= void_k + 1; kk++) {
 
-              /* Get this cell. */
-              const size_t cjd =
-                  cell_getid(cdim, ii, jj, kk) + bkg_cell_offset;
+              /* /\* Get this cell. *\/ */
+              /* const size_t cjd = */
+              /*     cell_getid(cdim, ii, jj, kk) + bkg_cell_offset; */
 
               /* /\* Handle the void cell. *\/ */
               /* if (cjd == s->zoom_props->void_cell_index) continue; */
