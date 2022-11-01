9840cc0ccfbd8b0ca14c6405b7ad6d76ec7835c8
wjr21 <wjr21@sussex.ac.uk>
2022-11-01 16:22:52 +0000

    Fixing missed edges

--- src/partition.c
+++ src/partition.c
@@ -447,7 +447,8 @@ static void graph_init(struct space *s, int periodic, idx_t *weights_e,
                 cj = &s->cells_top[cjd];
 
                 /* Include the zoom cells if the neighbour is the void cell. */
-                if ((cid == s->zoom_props->void_cell_index) && (cid == cjd)) {
+                if (((cid == s->zoom_props->void_cell_index) && (cid == cjd)) ||
+                    (cj->tl_cell_type == void_tl_cell)) {
 
                   /* Loop over all zoom cells recording the neighbours. */
                   for (int zoom_cjd = 0; zoom_cjd < nr_zoom_cells; zoom_cjd++) {
