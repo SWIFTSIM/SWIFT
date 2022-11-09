8803e02038da4367cb27a216161535c07744959b
wjr21 <wjr21@sussex.ac.uk>
2022-11-09 15:32:14 +0100

    Typo

--- src/zoom_region.c
+++ src/zoom_region.c
@@ -1911,7 +1911,7 @@ void engine_make_self_gravity_tasks_mapper_natural_cells(void *map_data,
               cj->tl_cell_type == tl_cell) continue;
 
           /* Avoid duplicates, empty cells and completely foreign pairs */
-          if (cid >= cjd) || cj->grav.count == 0 ||
+          if (cid >= cjd || cj->grav.count == 0 ||
               (ci->nodeID != nodeID && cj->nodeID != nodeID))
             continue;
 
