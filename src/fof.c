ce00af4a2ccd779b0c6fa0e3c29121c58c3b781f
wjr21 <wjr21@sussex.ac.uk>
2022-11-24 13:19:46 +0000

    Missing ;

--- src/fof.c
+++ src/fof.c
@@ -2261,7 +2261,7 @@ void fof_calc_group_mass(struct fof_props *props, const struct space *s,
     first_position = props->subhalo_first_position;
   }
 
-  message("got prop arrays.")
+  message("got prop arrays.");
 
   /* Loop over particles, compute CoM and find the densest particle in each
    * group. */
