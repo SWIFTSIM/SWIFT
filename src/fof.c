da683503a22ce564c263117d92fc87ecc13f0460
wjr21 <wjr21@sussex.ac.uk>
2022-11-25 14:07:06 +0000

    Added debug print

--- src/fof.c
+++ src/fof.c
@@ -5202,6 +5202,8 @@ void halo_finder_search_self_cell_gpart(const struct fof_props *props,
       /* r2 /= l_x2; */
       v2 /= l_v2;
 
+      message("v2=%.2f", v2)
+
       /* /\* Hit or miss? *\/ */
       /* if ((r2 + v2) < 2) { */
 
