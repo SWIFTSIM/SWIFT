45117d24fd0d2fe2696d51c295a7f0e7ead6ddfa
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2022-12-07 08:32:31 +0100

    Set default nr of extra parts back to 0 (does not work yet when number of top level cells decreases)

--- src/space.h
+++ src/space.h
@@ -53,7 +53,7 @@ struct hydro_props;
 #endif
 #define space_maxsize_default 8000000
 #define space_grid_split_threshold_default 400
-#define space_extra_parts_default 40
+#define space_extra_parts_default 0
 #define space_extra_gparts_default 0
 #define space_extra_sparts_default 100
 #define space_extra_bparts_default 0
