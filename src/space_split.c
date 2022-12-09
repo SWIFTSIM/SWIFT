23cf574ee0f6bcede9b5a3d1c8dc20ee7a67387d
wjr21 <wjr21@sussex.ac.uk>
2022-12-09 16:38:29 +0000

    Going too deep in the void cell tree

--- src/space_split.c
+++ src/space_split.c
@@ -837,7 +837,7 @@ void space_split(struct space *s, int verbose) {
 void void_tree_recursive(struct space *s, struct cell *c, const int thread_id) {
 
   /* Check we aren't at the depth of the zoom cells. */
-  if (c->width[0] != s->zoom_props->width[0]) {
+  if (c->width[0] / 2 != s->zoom_props->width[0]) {
     message("c->depth=%d", c->depth);
 
 #ifdef SWIFT_DEBUG_CHECKS
