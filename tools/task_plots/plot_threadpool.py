117d5061ac6490ddeda5424a645c9f07617c8d2a
Peter W. Draper <p.w.draper@durham.ac.uk>
2022-12-19 10:08:56 +0000

    Squash some matplotlib warnings

--- tools/task_plots/plot_threadpool.py
+++ tools/task_plots/plot_threadpool.py
@@ -334,13 +334,12 @@ if expand == 1:
     ax.set_ylabel("Thread ID", labelpad=0)
 else:
     ax.set_ylabel("Thread ID * " + str(expand), labelpad=0)
-ax.set_yticks(pl.array(list(range(nthread))), True)
+ax.set_yticks(pl.array(list(range(nthread))), minor=True)
 
 loc = plticker.MultipleLocator(base=expand)
 ax.yaxis.set_major_locator(loc)
 ax.grid(True, which="major", axis="y", linestyle="-")
 
-pl.show()
 pl.savefig(outpng)
 print("Graphics done, output written to", outpng)
 
