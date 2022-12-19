8bbd919641baa97dbaa2007c300d0263b5b52664
Peter W. Draper <p.w.draper@durham.ac.uk>
2022-12-19 11:44:31 +0000

    Add threadpool processing script for MPI runs

--- tools/task_plots/analyse_threadpool_tasks.py
+++ tools/task_plots/analyse_threadpool_tasks.py
@@ -28,11 +28,6 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import matplotlib
-
-matplotlib.use("Agg")
-import matplotlib.collections as collections
-import matplotlib.ticker as plticker
 import pylab as pl
 import sys
 import argparse
