3128cf9c1a5b4ed5da5e1a27949570eb419a6917
wjr21 <wjr21@sussex.ac.uk>
2022-12-29 16:48:16 +0000

    Including conditions to ensure we have particles in particular cell types

--- src/mesh_gravity.c
+++ src/mesh_gravity.c
@@ -983,16 +983,18 @@ void compute_potential_global(struct pm_mesh* mesh, const struct space* s,
                      sizeof(int), threadpool_auto_chunk_size, (void*)&data);
 
       /* Do the buffer cells. */
-      threadpool_map(tp, bkg_cell_gpart_to_mesh_CIC_mapper,
-                     (void*)s->zoom_props->local_buffer_cells_with_particles_top,
-                     s->zoom_props->nr_local_buffer_cells_with_particles,
-                     sizeof(int), threadpool_auto_chunk_size, (void*)&data);
+      if (s->zoom_props->nr_local_buffer_cells_with_particles > 0)
+        threadpool_map(tp, bkg_cell_gpart_to_mesh_CIC_mapper,
+                       (void*)s->zoom_props->local_buffer_cells_with_particles_top,
+                       s->zoom_props->nr_local_buffer_cells_with_particles,
+                       sizeof(int), threadpool_auto_chunk_size, (void*)&data);
 
       /* Do the background cells. */
-      threadpool_map(tp, bkg_cell_gpart_to_mesh_CIC_mapper,
-                     (void*)s->zoom_props->local_bkg_cells_with_particles_top,
-                     s->zoom_props->nr_local_bkg_cells_with_particles,
-                     sizeof(int), threadpool_auto_chunk_size, (void*)&data);
+      if (s->zoom_props->nr_local_bkg_cells_with_particles > 0)
+        threadpool_map(tp, bkg_cell_gpart_to_mesh_CIC_mapper,
+                       (void*)s->zoom_props->local_bkg_cells_with_particles_top,
+                       s->zoom_props->nr_local_bkg_cells_with_particles,
+                       sizeof(int), threadpool_auto_chunk_size, (void*)&data);
     } else {
       threadpool_map(tp, cell_gpart_to_mesh_CIC_mapper, (void*)local_cells,
                      nr_local_cells, sizeof(int), threadpool_auto_chunk_size,
@@ -1113,16 +1115,18 @@ void compute_potential_global(struct pm_mesh* mesh, const struct space* s,
                      sizeof(int), threadpool_auto_chunk_size, (void*)&data);
       
       /* Do buffer cells. */
-      threadpool_map(tp, bkg_cell_mesh_to_gpart_CIC_mapper,
-                     (void*)s->zoom_props->local_buffer_cells_with_particles_top,
-                     s->zoom_props->nr_local_buffer_cells_with_particles,
-                     sizeof(int), threadpool_auto_chunk_size, (void*)&data);
+      if (s->zoom_props->nr_local_buffer_cells_with_particles > 0)
+        threadpool_map(tp, bkg_cell_mesh_to_gpart_CIC_mapper,
+                       (void*)s->zoom_props->local_buffer_cells_with_particles_top,
+                       s->zoom_props->nr_local_buffer_cells_with_particles,
+                       sizeof(int), threadpool_auto_chunk_size, (void*)&data);
 
       /* Do background cells. */
-      threadpool_map(tp, bkg_cell_mesh_to_gpart_CIC_mapper,
-                     (void*)s->zoom_props->local_bkg_cells_with_particles_top,
-                     s->zoom_props->nr_local_bkg_cells_with_particles,
-                     sizeof(int), threadpool_auto_chunk_size, (void*)&data);
+      if (s->zoom_props->nr_local_bkg_cells_with_particles > 0)
+        threadpool_map(tp, bkg_cell_mesh_to_gpart_CIC_mapper,
+                       (void*)s->zoom_props->local_bkg_cells_with_particles_top,
+                       s->zoom_props->nr_local_bkg_cells_with_particles,
+                       sizeof(int), threadpool_auto_chunk_size, (void*)&data);
     } else {
       threadpool_map(tp, cell_mesh_to_gpart_CIC_mapper, (void*)local_cells,
                      nr_local_cells, sizeof(int), threadpool_auto_chunk_size,
