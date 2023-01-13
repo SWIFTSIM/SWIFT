78a3f87213b5e4aba1c4d7582b9328adaba0ad56
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-01-13 14:29:00 +0100

    extra safety check

--- src/scheduler.c
+++ src/scheduler.c
@@ -1642,6 +1642,8 @@ static void scheduler_splittask_grid(struct task *t, struct scheduler *s) {
             break;
           }
         }
+        if (first_pair_id == csp->count)
+          error("No suitable pairs found while attempting to split this task!");
         cell_set_flag(t->ci, cell_flag_has_tasks);
         cell_set_flag(t->cj, cell_flag_has_tasks);
         t->flags = csp->pairs[first_pair_id].sid;
