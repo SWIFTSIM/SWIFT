92eb654c445baa8a46814ae352430e1ae5ec3cc4
wjr21 <wjr21@sussex.ac.uk>
2023-02-22 21:29:05 +0000

    Removing tests

--- src/partition.c
+++ src/partition.c
@@ -2134,9 +2134,8 @@ static void repart_edge_metis_zoom(int vweights, int eweights, int timebins,
     pick_metis(nodeID, s, nr_nodes, weights_v, weights_e,
                repartition->celllist);
   } else {
-    message("refine %d", refine);
     pick_parmetis(nodeID, s, nr_nodes, weights_v, weights_e, refine,
-                  0, 0.0f,
+                  repartition->adaptive, repartition->itr,
                   repartition->celllist);
   }
 #else
