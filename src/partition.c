4cd3fa3a0292d6c5a52926dac7bae61df1f38792
wjr21 <wjr21@sussex.ac.uk>
2023-03-03 21:06:12 +0000

    Typo

--- src/partition.c
+++ src/partition.c
@@ -2799,7 +2799,7 @@ void partition_initial_partition(struct partition *initial_partition,
           wedge_ind = theta_ind * phi_nslices + phi_ind;
         }
 
-        message("wedge_ind=%d", wedge_ind)
+        message("wedge_ind=%d", wedge_ind);
 
         /* Add this weight. */
         weights_v[s->zoom_props->nr_zoom_cells + wedge_ind] +=
