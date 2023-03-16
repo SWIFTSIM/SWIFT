fc2d31536c0217d0aa4e16cf255a36b54d05b0e0
Tom Sandnes <tdsandnes@gmail.com>
2023-03-16 16:34:06 +0000

    Refactor planetary plus

--- src/hydro/PlanetaryPlus/hydro_parameters.h
+++ src/hydro/PlanetaryPlus/hydro_parameters.h
@@ -191,15 +191,16 @@ static INLINE void diffusion_print_snapshot(
 
 #ifdef PLANETARY_IMBALANCE
 /* Define alpha depending on kernel and eta */  // nosqrt variation
-#ifdef CUBIC_SPLINE_KERNEL
-#define planetary_imbalance_alpha 7.5f
-#endif
-#ifdef WENDLAND_C6_KERNEL
-// // eta=1.2348
-// #define alpha 7.1f
-// eta=2.2
-#define planetary_imbalance_alpha 5.1f
-#endif
+    #ifdef CUBIC_SPLINE_KERNEL
+        #define planetary_imbalance_alpha 7.5f
+    #elif WENDLAND_C6_KERNEL
+    // // eta=1.2348
+    // #define alpha 7.1f
+    // eta=2.2
+        #define planetary_imbalance_alpha 5.1f
+    #else
+        #error "Planetary imbalance mode not calibrated for this kernel"
+    #endif
 #endif /* PLANETARY_IMBALANCE */
 
 #endif /* SWIFT_PLANETARY_HYDRO_PARAMETERS_H */
