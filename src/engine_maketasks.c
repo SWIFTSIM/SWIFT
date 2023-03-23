e66d93f817b32981dfe834657ed9c63b09d244f3
wjr21 <wjr21@sussex.ac.uk>
2023-03-23 13:55:31 +0000

    Typo

--- src/engine_maketasks.c
+++ src/engine_maketasks.c
@@ -1805,7 +1805,7 @@ void engine_make_hierarchical_tasks_mapper(void *map_data, int num_elements,
     /* Make the common tasks (time integration) */
     engine_make_hierarchical_tasks_common(e, c);
     /* Add the hydro stuff */
-    if (with_hydro && (!e->s->with_zoom_region || c->tl_cell_type->zoom_tl_cell))
+    if (with_hydro && (!e->s->with_zoom_region || c->tl_cell_type == zoom_tl_cell))
       engine_make_hierarchical_tasks_hydro(e, c, /*star_resort_cell=*/NULL);
     /* And the gravity stuff */
     if (with_self_gravity || with_ext_gravity)
