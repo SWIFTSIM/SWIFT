529d72fb53cce1d36d810f3082cca077a07b013d
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2023-03-28 12:11:28 +0200

    Fixed typo reported by the automated suite in the minimal hydro header

--- src/hydro/Minimal/hydro.h
+++ src/hydro/Minimal/hydro.h
@@ -362,7 +362,7 @@ hydro_set_physical_internal_energy(struct part *p, struct xpart *xp,
 __attribute__((always_inline)) INLINE static void
 hydro_set_drifted_physical_internal_energy(
     struct part *p, const struct cosmology *cosmo,
-    const struct pressure_floor_props *pressure_floor const float u) {
+    const struct pressure_floor_props *pressure_floor, const float u) {
 
   p->u = u / cosmo->a_factor_internal_energy;
 
