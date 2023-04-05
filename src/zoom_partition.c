1866ed6276401f88df0010c3b925e06c426faaec
wjr21 <wjr21@sussex.ac.uk>
2023-04-05 13:34:37 +0100

    Using correct cell distance function

--- src/zoom_partition.c
+++ src/zoom_partition.c
@@ -563,7 +563,7 @@ void engine_makeproxies_with_zoom_region(struct engine *e) {
 
           /* Minimal distance between any two points in the cells */
           const double min_dist_CoM2 =
-            cell_min_dist2_same_size(&cells[cid], &cells[cjd], periodic, dim);
+            cell_min_dist2(&cells[cid], &cells[cjd], periodic, dim);
 
           /* Are we beyond the distance where the truncated forces are 0
            * but not too far such that M2L can be used? */
