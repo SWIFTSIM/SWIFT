03457580eb66c9b86dc257d4eb3ed592379b00c1
wjr21 <wjr21@sussex.ac.uk>
2023-07-31 18:06:40 +0100

    Fixing broken variable due to indexing from end

--- tools/task_plots/plot_tasks.py
+++ tools/task_plots/plot_tasks.py
@@ -330,7 +330,7 @@ else:
     toccol = 5
 
 #  Get CPU_CLOCK to convert ticks into milliseconds.
-CPU_CLOCK = float(full_step[-1]) / 1000.0
+CPU_CLOCK = float(full_step[-5]) / 1000.0
 if args.verbose:
     print("# CPU frequency:", CPU_CLOCK * 1000.0)
 
