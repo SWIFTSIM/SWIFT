114eee02300bd5697f33f8f5f6a95892cad15afb
wjr21 <wjr21@sussex.ac.uk>
2023-08-09 14:39:05 +0100

    Using the correct distances for cell interactions

--- src/runner_doiact_grav.c
+++ src/runner_doiact_grav.c
@@ -2759,7 +2759,7 @@ void runner_do_grav_long_range(struct runner *r, struct cell *ci,
 
     /* Maximal distance any interaction can take place
      * before the mesh kicks in, rounded up to the next integer */
-    int d = ceil(max_distance * max3(s->zoom_props->buffer_iwidth[0],
+    int d = ceil(hr_max_distance * max3(s->zoom_props->buffer_iwidth[0],
                                      s->zoom_props->buffer_iwidth[1],
                                      s->zoom_props->buffer_iwidth[2])) + 1;
 
@@ -2842,9 +2842,9 @@ void runner_do_grav_long_range(struct runner *r, struct cell *ci,
 
     /* Maximal distance any interaction can take place
      * before the mesh kicks in, rounded up to the next integer */
-    d = ceil(max_distance * max3(s->iwidth[0],
-                                 s->iwidth[1],
-                                 s->iwidth[2])) + 1;
+    d = ceil(global_max_distance * max3(s->iwidth[0],
+                                        s->iwidth[1],
+                                        s->iwidth[2])) + 1;
 
     /* Loop over plausibly useful cells */
     for (int ii = top_i - d; ii <= top_i + d; ++ii) {
@@ -2914,9 +2914,9 @@ void runner_do_grav_long_range(struct runner *r, struct cell *ci,
 
     /* Maximal distance any interaction can take place
      * before the mesh kicks in, rounded up to the next integer */
-    int d = ceil(max_distance * max3(s->iwidth[0],
-                                     s->iwidth[1],
-                                     s->iwidth[2])) + 1;
+    int d = ceil(global_max_distance * max3(s->iwidth[0],
+                                            s->iwidth[1],
+                                            s->iwidth[2])) + 1;
 
     /* Loop over plausibly useful cells */
     for (int ii = top_i - d; ii <= top_i + d; ++ii) {
