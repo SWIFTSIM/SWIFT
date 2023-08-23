cafb0f87ec29737c6b947887c9e11bfff5fc05b4
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2023-08-23 15:06:29 +0200

    Also implemented the Gasoline2-like terms

--- src/hydro/MAGMA/hydro_iact.h
+++ src/hydro/MAGMA/hydro_iact.h
@@ -275,10 +275,6 @@ __attribute__((always_inline)) INLINE static void runner_iact_nonsym_force(
       1.f * rhoj *
       (-const_viscosity_alpha * cj * mu_j + const_viscosity_beta * mu_j * mu_j);
 
-  /* Compute pressure terms */
-  const float P_over_rho2_i = (pressurei + Qi) / (rhoi * rhoi);
-  const float P_over_rho2_j = (pressurej + Qj) / (rhoj * rhoj);
-
   /* Construct the gradient functions (eq. 4 and 5) */
   float G_i[3], G_j[3];
   sym_matrix_multiply_by_vector(G_i, &pi->force.c_matrix, dx);
@@ -303,16 +299,45 @@ __attribute__((always_inline)) INLINE static void runner_iact_nonsym_force(
   G_j[2] = wj_dr * r_inv * dx[2];
 #endif
 
+#ifdef I_LOVE_GASOLINE
+
+  const float G_ij[3] = {0.5f * (G_i[0] + G_j[0]), 0.5f * (G_i[1] + G_j[1]),
+                         0.5f * (G_i[2] + G_j[2])};
+
+  const float acc_term = (pressurei + pressurej + Qi + Qj) / (rhoi * rhoj);
+  const float du_term = (pressurei + Qi) / (rhoi * rhoj);
+
+  /* Raw fluid acceleration (eq. 10) */
+  pi->a_hydro[0] -= mj * acc_term * G_ij[0];
+  pi->a_hydro[1] -= mj * acc_term * G_ij[1];
+  pi->a_hydro[2] -= mj * acc_term * G_ij[2];
+
+  /* Equivalent of div v */
+  const float v_ij_dot_G_ij =
+      v_ij[0] * G_ij[0] + v_ij[1] * G_ij[1] + v_ij[2] * G_ij[2];
+
+  /* Raw change in internal energy (eq. 11) */
+  pi->u_dt += du_term * mj * v_ij_dot_G_ij;
+
+#else
+
+  /* Compute pressure terms */
+  const float P_over_rho2_i = (pressurei + Qi) / (rhoi * rhoi);
+  const float P_over_rho2_j = (pressurej + Qj) / (rhoj * rhoj);
+
   /* Raw fluid acceleration (eq. 2) */
   pi->a_hydro[0] -= mj * (P_over_rho2_i * G_i[0] + P_over_rho2_j * G_j[0]);
   pi->a_hydro[1] -= mj * (P_over_rho2_i * G_i[1] + P_over_rho2_j * G_j[1]);
   pi->a_hydro[2] -= mj * (P_over_rho2_i * G_i[2] + P_over_rho2_j * G_j[2]);
 
+  /* Equivalent of div v */
   const float v_ij_dot_G_i =
       v_ij[0] * G_i[0] + v_ij[1] * G_i[1] + v_ij[2] * G_i[2];
 
   /* Raw change in internal energy (eq. 3) */
   pi->u_dt += P_over_rho2_i * mj * v_ij_dot_G_i;
+
+#endif
 
   /* Difference in internal energy */
   const float delta_u = pi->u - pj->u;
