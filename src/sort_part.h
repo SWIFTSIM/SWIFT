a1301fb8359f7d77d937bad296c9abee4ace7d19
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-09-06 15:17:22 +0200

    make sort_get_cell_min_dist work for all sid (not only sid < 13)

--- src/sort_part.h
+++ src/sort_part.h
@@ -154,7 +154,7 @@ __attribute__((always_inline, const)) INLINE static int sort_is_face(
  * @param cell_loc The location of cj.
  * @param cell_width The width of the cells.
  */
-INLINE static double sort_get_cell_min_dist(const int sid,
+INLINE static double sort_get_cell_min_dist(int sid,
                                             const double cell_loc[3],
                                             const double cell_width[3]) {
 
@@ -303,6 +303,9 @@ INLINE static double sort_get_cell_min_dist(const int sid,
       pos[1] = 0.;
       pos[2] = 0.;
   }
+
+  if (sid > 13)
+    sid = 26 - sid;
 
   return pos[0] * runner_shift[sid][0] + pos[1] * runner_shift[sid][1] +
          pos[2] * runner_shift[sid][2];
