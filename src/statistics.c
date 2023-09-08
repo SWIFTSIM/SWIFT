a94f10feaebfbde7aa7abbf33548fc2f51275e5c
Orestis Karapiperis <karapiperis@quasar.strw.leidenuniv.nl>
2023-09-08 11:14:34 +0200

    Fixed Bms in stats files

--- src/statistics.c
+++ src/statistics.c
@@ -348,6 +348,7 @@ void stats_collect_spart_mapper(void *map_data, int nr_sparts,
     if (gp != NULL && with_ext_grav)
       stats.E_pot_ext += m * external_gravity_get_potential_energy(
                                  time, potential, phys_const, gp);
+  
   }
 
   /* Now write back to memory */
@@ -690,8 +691,6 @@ void stats_finalize(struct statistics *stats) {
     stats->centre_of_mass[0] /= stats->total_mass;
     stats->centre_of_mass[1] /= stats->total_mass;
     stats->centre_of_mass[2] /= stats->total_mass;
-
-    stats->Bms /= stats->total_mass;
   }
 }
 
