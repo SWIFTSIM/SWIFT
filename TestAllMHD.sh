f472811e76b395e7b1df2e09cfe984cb1cde8e7c
Federico Stasyszyn <fstasyszyn@unc.edu.ar>
2023-09-08 15:40:29 +0200

    new compact version

--- TestAllMHD.sh
+++ TestAllMHD.sh
@@ -2,47 +2,59 @@
 
 echo "Testing MHD schemes"
 
-rm MHD_Pass.log
+LOG_FILE="MHD_Pass.log"
+rm $LOG_FILE
+
+SCHEME=("vector-potential" "direct-induction" "direct-induction-fede")
+SCHEME_ID=("VeP" "ODI" "FDI")
+SCHEME_NAME=("Vector Potential" "Direct Induction (Orestis)" "Direct Induction (Fede)")
+# VP ~ Vector Potential
+# DO ~ Direct Induction Orestis
+# DF ~ Direct Induction Fede
 
 case $1 in
-   [01])
-	rm sw_vp*  MHD_VP.log
-   	echo "Compiling Vector Potential version" 
-
-   	./configure --with-spmhd=vector-potential $2 > MHD_VP.log
-	make -j 32 >> MHD_VP.log
-	mv swift sw_vp
-	mv swift_mpi sw_vp_mpi
-	
-	echo "MHD: VP OK!" >> MHD_Pass.log
-   ;;&
-   [02])
-	rm sw_di*  MHD_DI.log
-        echo "Compiling Direct Induction *Orestis* version" 
-
-	./configure --with-spmhd=direct-induction $2 > MHD_DI.log
-	make -j 32 >> MHD_DI.log
-	mv swift sw_di
-	mv swift_mpi sw_di_mpi
-	
-	echo "MHD: DI Orestis OK!" >> MHD_Pass.log
-   ;;&
-   [03])
-	rm sw_diF*  MHD_DIF.log
-	echo "Compiling Direct Induction *Fede* version" 
-	
-	./configure --with-spmhd=direct-induction-fede $2 > MHD_DIF.log
-	make -j 32 >> MHD_DIF.log
-	mv swift sw_diF
-	mv swift_mpi sw_diF_mpi
-	
-	echo "MHD: DI Fede OK!" >> MHD_Pass.log
+   all)
+	varN=( 0 1 2 )
+	echo ${varN[@]}
+	echo "compiling all schemes: "${SCHEME_NAME[*]}
+   ;;
+   vp)
+	varN=0
+	echo "compiling scheme: "${SCHEME_NAME[$varN]}
+   ;;
+   odi)
+	varN=1
+	echo "compiling scheme: "${SCHEME_NAME[$varN]}
+   ;;
+   fdi)
+	varN=2
+	echo "compiling scheme: "${SCHEME_NAME[$varN]}
    ;;
    *)
-	echo "Usage $0 [0123] [configure extra parameters]"
-	echo "0 all schemes"
-	echo "1 vector potenial schemes"
-	echo "2 Direct Induction Orestis schemes"
-	echo "3 Direct Induction Federico schemes"
+	echo "Usage $0 [what] [configure extra parameters]"
+	echo "[what] \n all: compile all schemes"
+	echo " vp: vector potenial scheme"
+	echo "odi: Direct Induction Orestis scheme"
+	echo "fdi: Direct Induction Federico scheme"
+	exit
    ;;
 esac
+
+git status -bs > $LOG_FILE
+echo ${varN[*]}
+for J in ${varN[@]}
+do
+   echo $J
+   ID=${SCHEME_ID[$J]}
+   rm sw_$ID*  MHD_$ID.log
+   echo "Compiling "${SCHEME_NAME[$J]}" version" 
+
+   ./configure --with-spmhd=${SCHEME[$J]} $2 > MHD_$ID.log
+   make -j 32 >> MHD_$ID.log
+   mv swift sw_$ID
+   mv swift_mpi sw_$ID"_mpi"
+	
+   echo "MHD: "$ID" OK!" >> $LOG_FILE
+	
+done
+exit
