512af08a0853f29b48251d527e5749cb81871dd7
wjr21 <wjr21@sussex.ac.uk>
2023-09-08 19:55:54 +0100

    Function still causes segfault

--- src/zoom_proxy.c
+++ src/zoom_proxy.c
@@ -593,36 +593,36 @@ void engine_makeproxies_with_zoom_region(struct engine *e) {
                 /* Handle buffer->void proxies. */
                 if (ci->subtype == void_cell) {
 
-                  /* int nr_zoom_cells = s->zoom_props->nr_zoom_cells; */
+                  int nr_zoom_cells = s->zoom_props->nr_zoom_cells;
 
-                  /* /\* Loop over zoom cells. *\/ */
-                  /* for (int zoom_cid = 0; zoom_cid < nr_zoom_cells; zoom_cid++) { */
+                  /* Loop over zoom cells. */
+                  for (int zoom_cid = 0; zoom_cid < nr_zoom_cells; zoom_cid++) {
 
-                  /*   /\* Get the cell. *\/ */
-                  /*   struct cell *zoom_ci = &cells[zoom_cid]; */
+                    /* Get the cell. */
+                    struct cell *zoom_ci = &cells[zoom_cid];
 
-                  /*   /\* Avoid completely local and foreign pairs *\/ */
-                  /*   if ((zoom_ci->nodeID == nodeID && cj->nodeID == nodeID) || */
-                  /*       (zoom_ci->nodeID != nodeID && cj->nodeID != nodeID)) */
-                  /*     continue; */
+                    /* Avoid completely local and foreign pairs */
+                    if ((zoom_ci->nodeID == nodeID && cj->nodeID == nodeID) ||
+                        (zoom_ci->nodeID != nodeID && cj->nodeID != nodeID))
+                      continue;
 
-                  /*   /\* What type of proxy do we need? */
-                  /*    * (proxy_cell_type_none if no proxy needed). *\/ */
-                  /*   int proxy_type  = find_proxy_type(zoom_ci, ci, e, */
-                  /*                                     0 /\*is_adjacent*\/, */
-                  /*                                     r_max_zoom + r_max_buff, */
-                  /*                                     dim, periodic); */
+                    /* What type of proxy do we need?
+                     * (proxy_cell_type_none if no proxy needed). */
+                    int proxy_type  = find_proxy_type(zoom_ci, ci, e,
+                                                      0 /*is_adjacent*/,
+                                                      r_max_zoom + r_max_buff,
+                                                      dim, periodic);
 
-                  /*   /\* Abort if not in range at all *\/ */
-                  /*   if (proxy_type == proxy_cell_type_none) continue; */
+                    /* Abort if not in range at all */
+                    if (proxy_type == proxy_cell_type_none) continue;
 
-                  /*   /\* Make the proxies. *\/ */
-                  /*   add_proxy(zoom_ci, ci, e, proxies, nodeID, proxy_type); */
-                  /* } */
+                    /* Make the proxies. */
+                    add_proxy(zoom_ci, ci, e, proxies, nodeID, proxy_type);
+                  }
 
 
-                  get_void_proxy(cj, ci, s, proxies, nodeID,
-                                 r_max_buff, r_max_zoom);
+                  /* get_void_proxy(cj, ci, s, proxies, nodeID, */
+                  /*                r_max_buff, r_max_zoom); */
                 }
 
                 /* Handle buffer->buffer proxies */
