48d2207ab2941c7e41e0ef8b06d44661f44287d6
wjr21 <wjr21@sussex.ac.uk>
2023-09-14 21:51:33 +0100

    Debugging

--- src/zoom_region.c
+++ src/zoom_region.c
@@ -3080,7 +3080,7 @@ void engine_addtasks_send_void(struct engine *e) {
 
       /* If there are no valid zoom progeny: skip. */
       if (zoom_c == NULL) continue;
-      
+      message("Got zoom cell in send");
       /* Make the send, link it and add unlocks. */
       engine_addtasks_send_zoom_gravity(e, void_c, zoom_c, /*tgrav*/NULL,
                                         /*tag*/-1);
