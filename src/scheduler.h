539772c892cd87d8558afd84168026a79ce075b0
wjr21 <wjr21@sussex.ac.uk>
2023-09-15 12:55:43 +0100

    More information in error

--- src/scheduler.h
+++ src/scheduler.h
@@ -181,8 +181,8 @@ scheduler_activate_send(struct scheduler *s, struct link *link,
        l = l->next)
     ;
   if (l == NULL) {
-    error("Missing link to send task. (task_subtype=%s, c->type=%d)",
-          subtaskID_names[subtype], link->t->ci->type);
+    error("Missing link to send task. (task_subtype=%s, c->type=%d, c->depth=%d)",
+          subtaskID_names[subtype], link->t->ci->type, c->depth);
   }
   scheduler_activate(s, l->t);
   return l;
