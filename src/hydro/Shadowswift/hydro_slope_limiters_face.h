03fda6e2781bcdae7498f346c176a044468ea11d
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-09-26 09:00:39 +0200

    avoid unnecessary divisions

--- src/hydro/Shadowswift/hydro_slope_limiters_face.h
+++ src/hydro/Shadowswift/hydro_slope_limiters_face.h
@@ -11,15 +11,15 @@
  * @param phi_i Value of the quantity at the particle position.
  * @param phi_j Value of the quantity at the neighbouring particle position.
  * @param phi_mid0 Extrapolated value of the quantity at the interface position.
- * @param xij_norm Distance between the particle position and the interface
- * position.
- * @param r Distance between the particle and its neighbour.
+ * @param xij_norm_over_r Distance between the particle position and the
+ * interface position divided by the distance between the particle and its
+ * neighbour.
  * @return The slope limited difference between the quantity at the particle
  * position and the quantity at the interface position.
  */
 __attribute__((always_inline)) INLINE static float
 hydro_slope_limit_face_quantity(float phi_i, float phi_j, float phi_mid0,
-                                float xij_norm, float r) {
+                                float xij_norm_over_r) {
 
   float phi_mid, delta1, delta2, phimin, phimax, phibar, phiplus, phiminus;
   const float psi1 = 0.5f;
@@ -35,7 +35,7 @@ hydro_slope_limit_face_quantity(float phi_i, float phi_j, float phi_mid0,
   phimin = fminf(phi_i, phi_j);
   phimax = fmaxf(phi_i, phi_j);
 
-  phibar = phi_i + xij_norm / r * (phi_j - phi_i);
+  phibar = phi_i + xij_norm_over_r * (phi_j - phi_i);
 
   /* if sign(phimax+delta1) == sign(phimax) */
   if ((phimax + delta1) * phimax >= 0.0f) {
@@ -77,37 +77,40 @@ __attribute__((always_inline)) INLINE static void hydro_slope_limit_face(
     float *Wi, float *Wj, float *dWi, float *dWj, const float *xij_i,
     const float *xij_j, float r) {
 
-  float xij_i_norm, xij_j_norm;
-  xij_i_norm =
-      sqrtf(xij_i[0] * xij_i[0] + xij_i[1] * xij_i[1] + xij_i[2] * xij_i[2]);
-  xij_j_norm =
-      sqrtf(xij_j[0] * xij_j[0] + xij_j[1] * xij_j[1] + xij_j[2] * xij_j[2]);
+  float xij_i_norm_over_r, xij_j_norm_over_r;
+  const float r_inv = 1.f / r;
+  xij_i_norm_over_r =
+      sqrtf(xij_i[0] * xij_i[0] + xij_i[1] * xij_i[1] + xij_i[2] * xij_i[2]) *
+      r_inv;
+  xij_j_norm_over_r =
+      sqrtf(xij_j[0] * xij_j[0] + xij_j[1] * xij_j[1] + xij_j[2] * xij_j[2]) *
+      r_inv;
 
   dWi[0] = hydro_slope_limit_face_quantity(Wi[0], Wj[0], Wi[0] + dWi[0],
-                                           xij_i_norm, r);
+                                           xij_i_norm_over_r);
   dWi[1] = hydro_slope_limit_face_quantity(Wi[1], Wj[1], Wi[1] + dWi[1],
-                                           xij_i_norm, r);
+                                           xij_i_norm_over_r);
   dWi[2] = hydro_slope_limit_face_quantity(Wi[2], Wj[2], Wi[2] + dWi[2],
-                                           xij_i_norm, r);
+                                           xij_i_norm_over_r);
   dWi[3] = hydro_slope_limit_face_quantity(Wi[3], Wj[3], Wi[3] + dWi[3],
-                                           xij_i_norm, r);
+                                           xij_i_norm_over_r);
   dWi[4] = hydro_slope_limit_face_quantity(Wi[4], Wj[4], Wi[4] + dWi[4],
-                                           xij_i_norm, r);
+                                           xij_i_norm_over_r);
   dWi[5] = hydro_slope_limit_face_quantity(Wi[5], Wj[5], Wi[5] + dWi[5],
-                                           xij_i_norm, r);
+                                           xij_i_norm_over_r);
 
   dWj[0] = hydro_slope_limit_face_quantity(Wj[0], Wi[0], Wj[0] + dWj[0],
-                                           xij_j_norm, r);
+                                           xij_j_norm_over_r);
   dWj[1] = hydro_slope_limit_face_quantity(Wj[1], Wi[1], Wj[1] + dWj[1],
-                                           xij_j_norm, r);
+                                           xij_j_norm_over_r);
   dWj[2] = hydro_slope_limit_face_quantity(Wj[2], Wi[2], Wj[2] + dWj[2],
-                                           xij_j_norm, r);
+                                           xij_j_norm_over_r);
   dWj[3] = hydro_slope_limit_face_quantity(Wj[3], Wi[3], Wj[3] + dWj[3],
-                                           xij_j_norm, r);
+                                           xij_j_norm_over_r);
   dWj[4] = hydro_slope_limit_face_quantity(Wj[4], Wi[4], Wj[4] + dWj[4],
-                                           xij_j_norm, r);
+                                           xij_j_norm_over_r);
   dWj[5] = hydro_slope_limit_face_quantity(Wj[5], Wi[5], Wj[5] + dWj[5],
-                                           xij_j_norm, r);
+                                           xij_j_norm_over_r);
 }
 
 #endif  // SWIFTSIM_HYDRO_SLOPE_LIMITERS_PER_FACE_H
