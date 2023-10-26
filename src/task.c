1510d85c78db0cffc43941aecf5f2673973bbc6c
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2023-10-26 13:31:51 +0200

    Code formatting

--- src/task.c
+++ src/task.c
@@ -683,12 +683,11 @@ void task_unlock(struct task *t) {
       cell_gunlocktree(ci);
       break;
 
-     case task_type_fof_pair:
+    case task_type_fof_pair:
       cell_gunlocktree(ci);
       cell_gunlocktree(cj);
       break;
 
-      
     case task_type_star_formation:
       cell_unlocktree(ci);
       cell_sunlocktree(ci);
@@ -1085,16 +1084,16 @@ int task_lock(struct task *t) {
     case task_type_fof_self:
       /* Lock the gpart as this this what we act on */
       if (ci->grav.phold) return 0;
-      if (cell_glocktree(ci) != 0) return 0;      
+      if (cell_glocktree(ci) != 0) return 0;
       break;
 
-     case task_type_fof_pair:
+    case task_type_fof_pair:
       /* Lock the gpart as this this what we act on */
       if (ci->grav.phold || cj->grav.phold) return 0;
       if (cell_glocktree(ci) != 0) return 0;
       if (cell_glocktree(cj) != 0) {
-	cell_gunlocktree(ci);
-	return 0;
+        cell_gunlocktree(ci);
+        return 0;
       }
       break;
 
@@ -1112,7 +1111,7 @@ int task_lock(struct task *t) {
         return 0;
       }
       break;
-      
+
     case task_type_star_formation_sink:
       /* Lock the gas, gravity and star particles */
       if (ci->sinks.hold || ci->stars.hold || ci->grav.phold) return 0;
