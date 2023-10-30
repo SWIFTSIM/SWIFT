b80cc7e6c850ea9455b4708b64c5733ba1ffbe08
Tom Sandnes <tdsandnes@gmail.com>
2023-10-30 11:37:55 +0000

    Temporary kernel fix

--- src/kernel_hydro.h
+++ src/kernel_hydro.h
@@ -258,7 +258,7 @@ __attribute__((always_inline)) INLINE static void kernel_deval(
     float u, float *restrict W, float *restrict dW_dx) {
 
   /* Go to the range [0,1[ from [0,H[ */
-  const float x = u * kernel_gamma_inv;
+  const long double x = u * kernel_gamma_inv;
 
   /* Pick the correct branch of the kernel */
   const int temp = (int)(x * kernel_ivals_f);
@@ -266,20 +266,57 @@ __attribute__((always_inline)) INLINE static void kernel_deval(
   const float *const coeffs = &kernel_coeffs[ind * (kernel_degree + 1)];
 
   /* First two terms of the polynomial ... */
-  float w = coeffs[0] * x + coeffs[1];
-  float dw_dx = coeffs[0];
+  long double w = coeffs[0] * x + coeffs[1];
+  long double dw_dx = coeffs[0];
 
   /* ... and the rest of them */
   for (int k = 2; k <= kernel_degree; k++) {
     dw_dx = dw_dx * x + w;
     w = x * w + coeffs[k];
   }
-
+    /*
+    if (x < 0.98){
+        if (w <= 0){
+            printf("here1");
+            printf("\n");
+            printf("%.20Lf", x);
+            printf("\n");
+            printf("%.20Lf", w);
+            printf("\n");
+            exit(0);
+        }
+        
+        if (dw_dx >= 0){
+            printf("here2");
+            printf("\n");
+            printf("%.20Lf", x);
+            printf("\n");
+            printf("%.20Lf", dw_dx);
+            printf("\n");
+            exit(0);
+        }
+    }
+    */
   w = max(w, 0.f);
   dw_dx = min(dw_dx, 0.f);
 
   /* Return everything */
   *W = w * kernel_constant * kernel_gamma_inv_dim;
+    /*
+    if (x < 0.98){
+        if (*W <= 0){
+            printf("here1");
+            printf("\n");
+            printf("%.20Lf", x);
+            printf("\n");
+            printf("%.20Lf", w);
+            printf("\n");
+            printf("%.20f", *W);
+            printf("\n");
+            exit(0);
+        }
+    }
+    */
   *dW_dx = dw_dx * kernel_constant * kernel_gamma_inv_dim_plus_one;
 }
 
