885e593935e36d0ec90ac4d755ab2e850dd8da51
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2023-11-08 14:44:29 +0100

    Do not change the error function

--- src/error.h
+++ src/error.h
@@ -65,7 +65,7 @@ extern int engine_rank;
             ##__VA_ARGS__);                                                \
     memdump(engine_rank);                                                  \
     fflush(stderr);                                                        \
-    abort();                                                               \
+    MPI_Abort(MPI_COMM_WORLD, -1);                                         \
   })
 #else
 extern int engine_rank;
