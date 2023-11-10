43dd6b03cfbe5c9c4023f5794f6920f5ec7d6acc
Federico Andrés Stasyszyn <fstasyszyn@unc.edu.ar>
2023-11-10 21:23:47 +0000

    Fstasys/clean blast now with vp

---
+++ examples/MHDTests/FastRotor/ODI/run.sh
@@ -0,0 +1,5 @@
+# Run SWIFT
+../../../../sw_ODI --hydro --threads=16 ../FR_schemes.yml 2>&1 > out.log 
+
+# Plot the temperature evolution
+python3 ../plot_all.py 0 60
---
+++ examples/MHDTests/MagneticBlastWave/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Generate the initial conditions if they are not present.
+if [ ! -e MagneticBlastWave_LR.hdf5 ]
+then
+    echo "Fetching Glass Files..."
+    ./getGlass.sh
+    echo "Generating the ICs"
+    python ./makeIC.py 
+fi
+
+# Run SWIFT
+../../../swift --hydro --threads=16 ../FastRotor.yml 2>&1 > out.log 
+
+# Plot the temperature evolution
+# python3 ./plot_all.py 0 60
