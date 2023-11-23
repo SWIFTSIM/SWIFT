8af9795490f35bfd11196b114e57e91bccfd0369
Stan-Verhoeve <verhoeve@strw.leidenuniv.nl>
2023-11-23 21:04:16 +0100

    Example of advection in cosmo (Not working yet)

---
+++ examples/RadiativeTransferTests/Cosmo_Advection_1D/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# make run.sh fail if a subcommand fails
+set -e
+set -o pipefail
+
+if [ ! -f advection_1D.hdf5 ]; then
+    echo "Generating ICs"
+    python3 makeIC.py
+fi
+
+# Run SWIFT with RT
+../../../swift \
+    --hydro \
+    --cosmology \
+    --threads=4 \
+    --verbose=0  \
+    --radiation \
+    --stars \
+    --feedback \
+    --external-gravity \
+    --fpe \
+    ./rt_advection1D.yml 2>&1 | tee output.log
+
+python3 ./plotSolution.py
