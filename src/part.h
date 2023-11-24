c08a93687c6ac05c692eac08270274b38e91728e
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-11-24 16:22:16 +0100

    put metal mass fluxes behind #ifdef in `chemistry_part_data` and move corresponding #define to configure.ac

--- src/part.h
+++ src/part.h
@@ -73,9 +73,6 @@ struct threadpool;
 #define hydro_need_extra_init_loop 0
 #define EXTRA_HYDRO_LOOP
 #define MPI_SYMMETRIC_FORCE_INTERACTION
-#ifdef GIZMO_MFV_SPH
-#define HYDRO_DOES_MASS_FLUX
-#endif
 #elif defined(SHADOWFAX_SPH)
 #include "./hydro/Shadowswift/hydro_part.h"
 #define hydro_need_extra_init_loop 0
