fa3d67dd1af278c65f2a6e6d1aa2220c21193eef
Federico Stasyszyn <fstasyszyn@unc.edu.ar>
2023-11-29 15:32:27 +0100

    Added comments to output in scree

--- examples/MHDTests/CosmoVolume/makeIC.py
+++ examples/MHDTests/CosmoVolume/makeIC.py
@@ -42,7 +42,7 @@ wavelen = BoxSize / 10.0
 wavenum = 2.0 * np.pi / wavelen
 Aini = Bini / wavenum * afact;
 
-print(wavelen,wavenum)
+#print(wavelen,wavenum)
 #####################################################
 #### Positions in Comoving #### 
 #### Other quatities in Physical in the IC files ####
@@ -53,8 +53,8 @@ B[:,0] = Bini * (np.sin(pos_in[:,2]*wavenum) + np.cos(pos_in[:,1]*wavenum))
 B[:,1] = Bini * (np.sin(pos_in[:,0]*wavenum) + np.cos(pos_in[:,2]*wavenum))
 B[:,2] = Bini * (np.sin(pos_in[:,1]*wavenum) + np.cos(pos_in[:,0]*wavenum))
 
-print(min(A[:,0]),max(A[:,0]))
-print(min(B[:,0]),max(B[:,0]))
+print("VPotencial min/max: ",min(A[:,0]),max(A[:,0]))
+print("Bfield min/max",min(B[:,0]),max(B[:,0]))
 
 #### Constant field in X direction ####
 #B[:,0] = np.where(pos_in[:,1] < BoxSize * 0.5, Bini, -Bini)
@@ -75,12 +75,11 @@ fileOutput = h5py.File(fileOutputName, "a")
 #print(list(fileOutput.keys()))
 #grpu = fileOutput.require_group(["/Units"])
 grpu = fileOutput["/Units"]
-print(grpu.attrs["Unit length in cgs (U_L)"])
-print(grpu.attrs["Unit mass in cgs (U_M)"])
-print(grpu.attrs["Unit time in cgs (U_t)"]) 
-print(grpu.attrs["Unit current in cgs (U_I)"])
-print(grpu.attrs["Unit temperature in cgs (U_T)"])
-
+#print(grpu.attrs["Unit length in cgs (U_L)"])
+#print(grpu.attrs["Unit mass in cgs (U_M)"])
+#print(grpu.attrs["Unit time in cgs (U_t)"]) 
+#print(grpu.attrs["Unit current in cgs (U_I)"])
+#print(grpu.attrs["Unit temperature in cgs (U_T)"])
 grpu.attrs["Unit current in cgs (U_I)"] = 4.788e6  # amperes to be Gauss
 
 # Particle group
