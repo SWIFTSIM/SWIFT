2e3ce961a487700d726231016d5de7782c2696d0
Darwin Roduit <darwin.roduit@epfl.ch>
2024-01-10 09:36:59 +0100

    Added missing function arguments in default sink

--- src/sink/Default/sink_iact.h
+++ src/sink/Default/sink_iact.h
@@ -69,7 +69,10 @@ __attribute__((always_inline)) INLINE static void
 runner_iact_nonsym_sinks_sink_swallow(const float r2, const float dx[3],
                                       const float ri, const float rj,
                                       struct sink *restrict si,
-                                      struct sink *restrict sj) {}
+                                      struct sink *restrict sj,
+				      const int with_cosmology,
+				      const struct cosmology *cosmo,
+				      const struct gravity_props *grav_props) {}
 
 /**
  * @brief Compute sink-gas swallow interaction (non-symmetric).
@@ -86,6 +89,9 @@ runner_iact_nonsym_sinks_gas_swallow(const float r2, const float dx[3],
                                      const float ri, const float hj,
                                      struct sink *restrict si,
                                      struct part *restrict pj,
+				     const int with_cosmology,
+				     const struct cosmology *cosmo,
+				     const struct gravity_props *grav_props,
 				     const struct sink_props* sink_properties) {}
 
 #endif
