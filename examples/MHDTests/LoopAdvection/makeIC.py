6d488ba9c79846382e7f77c639e2629cb8328803
Orestis Karapiperis <karapiperis@strw.leidenuniv.nl>
2024-01-15 00:38:13 +0100

    Added loop advection test, should run out of the box

---
+++ examples/MHDTests/LoopAdvection/makeIC.py
@@ -0,0 +1,143 @@
+################################################################################
+# This file is part of SWIFT.
+# Copyright (c) 2016 Matthieu Schaller (matthieu.schaller@durham.ac.uk)
+#               2017 Bert Vandenbroucke (bert.vandenbroucke@gmail.com)
+#               2021 Federico Stasyszyn (fstasyszyn@unc.edu.ar)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+import h5py
+import argparse
+import numpy as np
+
+# Generates a swift IC file for the Current Loop Advection test in a periodic box
+
+# Read in number of BCC cells to stack along x and y
+argparser = argparse.ArgumentParser()
+argparser.add_argument(
+    "-r",
+    "--replications",
+    help="Number of BCC unit cells to stack along the x and y directions. Default: 4",
+    default=2,
+    type=int,
+)
+args = argparser.parse_args()
+
+# Parameters
+gamma = 5.0 / 3.0
+R_0 = 0.3
+rho_0 = 1.0
+P_0 = 1.0
+A_0 = 1e-3
+
+fileOutputName = "LoopAdvection.hdf5"
+
+# Stack unit cells
+glass = h5py.File("glassCube_32.hdf5", "r")
+pos_unit_cell = glass["/PartType0/Coordinates"][:, :]
+h_unit_cell = glass["/PartType0/SmoothingLength"][:]
+
+N_unit_cell = len(h_unit_cell)
+
+times = args.replications
+
+cx, cy, cz = 2*times, times, 1
+
+lx, ly, lz = 2.0, 1.0, 1.0 / times
+
+N = int(N_unit_cell * cx * cy * cz)
+pos = np.zeros((N, 3))
+h = np.zeros(N)
+
+c0 = 0
+c1 = N_unit_cell
+for i in range(cx):
+    for j in range(cy):
+        for k in range(cz):
+            pos[c0:c1, 0] = pos_unit_cell[:, 0] + i
+            pos[c0:c1, 1] = pos_unit_cell[:, 1] + j
+            pos[c0:c1, 2] = pos_unit_cell[:, 2] + k
+            h[c0:c1] = h_unit_cell[:]
+            c0 += N_unit_cell
+            c1 += N_unit_cell
+
+# Rescale to desired length
+pos *= lx / cx
+h *= lx / cx
+vol = lx * ly * lz
+
+# Generate extra arrays
+v = np.zeros((N, 3))
+B = np.zeros((N, 3))
+ids = np.linspace(1, N, N)
+m = np.ones(N) * rho_0 * vol / N
+u = np.ones(N) * P_0 / (rho_0 * (gamma - 1))
+
+# Instantiate extra arrays
+
+v[:,0] = 2.0
+v[:,1] = 1.0
+v[:,2] = 0.1 / np.sqrt(5)
+
+rot = np.sqrt((pos[:, 0] - 0.5*lx) ** 2 + (pos[:, 1] - 0.5*ly) ** 2)
+mask = rot < R_0
+
+B[:,0][mask] = -A_0 * (pos[:,1][mask] - 0.5*ly) / rot[mask]
+B[:,1][mask] = A_0 * (pos[:,0][mask] - 0.5*lx) / rot[mask]
+
+
+B[:,0][mask][rot[mask] == 0] = A_0 / np.sqrt(2)
+B[:,1][mask][rot[mask] == 0] = A_0 / np.sqrt(2)
+
+'''
+B[:,0][mask][rot[mask] != 0] /= rot[mask][rot[mask] != 0] 
+B[:,1][mask][rot[mask] != 0] /= rot[mask][rot[mask] != 0] 
+'''
+
+# File
+fileOutput = h5py.File(fileOutputName, "w")
+
+# Header
+grp = fileOutput.create_group("/Header")
+grp.attrs["BoxSize"] = [lx, ly, lz]
+grp.attrs["NumPart_Total"] = [N, 0, 0, 0, 0, 0]
+grp.attrs["NumPart_Total_HighWord"] = [0, 0, 0, 0, 0, 0]
+grp.attrs["NumPart_ThisFile"] = [N, 0, 0, 0, 0, 0]
+grp.attrs["Time"] = 0.0
+grp.attrs["NumFileOutputsPerSnapshot"] = 1
+grp.attrs["MassTable"] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+grp.attrs["Flag_Entropy_ICs"] = [0, 0, 0, 0, 0, 0]
+grp.attrs["Dimension"] = 3
+
+# Units
+grp = fileOutput.create_group("/Units")
+grp.attrs["Unit length in cgs (U_L)"] = 1.0
+grp.attrs["Unit mass in cgs (U_M)"] = 1.0
+grp.attrs["Unit time in cgs (U_t)"] = 1.0
+grp.attrs["Unit current in cgs (U_I)"] = 1.0
+grp.attrs["Unit temperature in cgs (U_T)"] = 1.0
+
+# Particle group
+grp = fileOutput.create_group("/PartType0")
+grp.create_dataset("Coordinates", data=pos, dtype="d")
+grp.create_dataset("Velocities", data=v, dtype="f")
+grp.create_dataset("Masses", data=m, dtype="f")
+grp.create_dataset("SmoothingLength", data=h, dtype="f")
+grp.create_dataset("InternalEnergy", data=u, dtype="f")
+grp.create_dataset("ParticleIDs", data=ids, dtype="L")
+grp.create_dataset("MagneticFluxDensities", data=B, dtype="f")
+
+fileOutput.close()
---
+++ examples/MHDTests/LoopAdvection/plotSolution.py
@@ -0,0 +1,126 @@
+import numpy as np
+import h5py
+import argparse
+import unyt
+import matplotlib.pyplot as plt
+
+from unyt import cm
+
+from swiftsimio import load
+from swiftsimio.visualisation.projection import project_gas
+
+# Parse command line arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument("input")
+argparser.add_argument("output")
+args = argparser.parse_args()
+
+# Load snapshot
+filename = args.input
+data = load(filename)
+
+# Retrieve some information about the simulation run
+artDiffusion = data.metadata.hydro_scheme["Artificial Diffusion Constant"]
+dedHyp = data.metadata.hydro_scheme["Dedner Hyperbolic Constant"]
+dedHypDivv = data.metadata.hydro_scheme["Dedner Hyperbolic div(v) Constant"]
+dedPar = data.metadata.hydro_scheme["Dedner Parabolic Constant"]
+eta = data.metadata.hydro_scheme["Resistive Eta"]
+git = data.metadata.code["Git Revision"]
+gitBranch = data.metadata.code["Git Branch"]
+hydroScheme = data.metadata.hydro_scheme["Scheme"]
+kernel = data.metadata.hydro_scheme["Kernel function"]
+neighbours = data.metadata.hydro_scheme["Kernel target N_ngb"]
+
+# Retrieve particle attributes of interest
+R0 = 0.3
+B0 = 1e-3
+B = data.gas.magnetic_flux_densities
+normB = np.sqrt(B[:, 0] ** 2 + B[:, 1] ** 2 + B[:, 2] ** 2) / B0
+
+curlB = data.gas.magnetic_flux_curl * R0 / B0
+normCurlB = np.sqrt(curlB[:, 0] ** 2 + curlB[:, 1] ** 2 + curlB[:, 2] ** 2)
+
+divB = data.gas.magnetic_divergences
+h = data.gas.smoothing_lengths
+
+# Generate mass weighted maps of quantities of interest
+data.gas.mass_weighted_normB = data.gas.masses * normB
+
+data.gas.mass_weighted_normCurlB = data.gas.masses * normCurlB
+
+data.gas.mass_weighted_error = data.gas.masses * np.log10(
+    np.maximum(h * abs(divB) / (normB + 0.01 * np.max(normB)), 1e-6)
+)
+
+common_arguments = dict(
+    data=data,
+    resolution=512,
+    parallel=True,
+    region=[0.5*cm,1.5*cm,0.0*cm,1.0*cm]
+)
+
+mass_map = project_gas(**common_arguments, project="masses")
+
+mass_weighted_normB_map = project_gas(**common_arguments, project="mass_weighted_normB")
+
+mass_weighted_normCurlB_map = project_gas(**common_arguments, project="mass_weighted_normCurlB")
+
+mass_weighted_error_map = project_gas(**common_arguments, project="mass_weighted_error")
+
+# Take out mass dependence
+normB_map = mass_weighted_normB_map / mass_map
+normCurlB_map = mass_weighted_normCurlB_map / mass_map
+error_map = mass_weighted_error_map / mass_map
+
+# Plot maps
+plt.rcParams.update({'font.size': 16})
+fig, ax = plt.subplots(2,2,figsize=(12.25,11.333))
+
+a00 = ax[0,0].contourf(
+    normB_map.value.T, cmap="gist_heat", levels=np.linspace(0.0, 1.0, 100)
+)
+a01 = ax[0,1].contourf(
+    normCurlB_map.value.T, cmap="gist_heat", levels=np.linspace(0.0, 15.0, 100)
+)
+a10 = ax[1,0].contourf(
+    error_map.value.T, cmap="jet", levels=np.linspace(-5.0, 0.0, 6)
+)
+
+# Add panel with infromation about the run 
+text_common_args = dict(fontsize=10, ha='center', va='center', transform=ax[1,1].transAxes)
+
+ax[1,1].text(
+    0.5,
+    0.8,
+    "Current Loop Advection at time $t=%.2f$" % data.metadata.time,
+    **text_common_args
+)
+ax[1,1].text(0.5, 0.7, "SWIFT %s" % git.decode("utf-8"), **text_common_args)
+ax[1,1].text(0.5, 0.6, "Branch %s" % gitBranch.decode("utf-8"), **text_common_args)
+ax[1,1].text(0.5, 0.5, hydroScheme.decode("utf-8"), **text_common_args)
+ax[1,1].text(0.5, 0.4, kernel.decode("utf-8") + " with $%.2f$ neighbours" % (neighbours), **text_common_args)
+ax[1,1].text(0.5, 0.3, "Artificial diffusion: $%.2f$ " % (artDiffusion), **text_common_args)
+ax[1,1].text(0.5, 0.2, "Dedner Hyp, Hyp_div(v), Par: $%.2f,%.2f,%.2f$ " % (dedHyp,dedHypDivv,dedPar), **text_common_args)
+ax[1,1].text(0.5, 0.1, "Physical resistivity $\eta$: $%.2f$ " % (eta), **text_common_args)
+ax[1,1].axis('off')
+
+for axi in ax:
+    for axii in axi:
+        axii.set_xticks([])
+        axii.set_yticks([])
+        axii.set_aspect("equal")
+
+# Set appropriate colourbars
+cbar1 = fig.colorbar(
+    a00, ax=ax[0,0], label=r"$|\mathbf{B}| / |\mathbf{B}_0|$", fraction=0.042, pad=0.04, location='left', ticks=np.linspace(0.0,1.0,6)
+)
+
+cbar2 = fig.colorbar(
+    a01, ax=ax[0,1], label=r"$R_0 \: |\nabla \times \mathbf{B}| / |\mathbf{B}_0|$", fraction=0.042, pad=0.04, ticks=np.linspace(0.0,15.0,6)
+)
+
+cbar3 = fig.colorbar(a10, ax=ax[1,0], label=r"$\mathrm{log}_{10} \left( h \: |\nabla \cdot \mathbf{B}| / |\mathbf{B}| \right)$", fraction=0.042, pad=0.04, location='left')
+
+plt.subplots_adjust(wspace=0, hspace=0)
+
+plt.savefig(args.output)
