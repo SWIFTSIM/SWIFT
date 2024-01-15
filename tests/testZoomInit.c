925e41416f2d06a6eb54b9dbd44d7e08573c6ad1
wjr21 <wjr21@sussex.ac.uk>
2024-01-15 14:16:45 +0000

    Removing incorrect const

--- tests/testZoomInit.c
+++ tests/testZoomInit.c
@@ -97,8 +97,7 @@ int main(int argc, char *argv[]) {
   create_dummy_gravity_props(grav_props);
 
   /* Run the zoom_init function. */
-  zoom_region_init((const struct swift_params *)&param_file, s,
-                   (const struct gravity_props *)grav_props, 1);
+  zoom_region_init(&param_file, s, (const struct gravity_props *)grav_props, 1);
 
   if (test_type) {
   }
