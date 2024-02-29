ad4a3ec6b3501827edbae4d1592c44c6549e9d04
Darwin Roduit <darwin.roduit@epfl.ch>
2024-02-29 15:50:54 +0100

    Improved output description

--- src/star_formation/GEAR/star_formation_io.h
+++ src/star_formation/GEAR/star_formation_io.h
@@ -97,8 +97,8 @@ star_formation_write_sparticles(const struct spart* sparts,
   list[4] =
       io_make_output_field("StellarParticleType", CHAR, 1, UNIT_CONV_NO_UNITS,
                            0.f, sparts, feedback_data.star_type,
-                           "Type of stellar particle: 0=stellar part. without "
-                           "SNII 1=single star 2=normal");
+                           "Type of stellar particle: 0=single star ; 1=stellar"
+                           " part. without SNII ; 2=normal");
 
   return 5;
 }
