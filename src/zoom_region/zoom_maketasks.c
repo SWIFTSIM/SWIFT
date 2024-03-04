96a091a699680ce222905398e597fd6132ac3864
wjr21 <wjr21@sussex.ac.uk>
2024-03-04 12:37:10 +0000

    Removed const from tics

--- src/zoom_region/zoom_maketasks.c
+++ src/zoom_region/zoom_maketasks.c
@@ -921,7 +921,7 @@ void engine_make_self_gravity_tasks_mapper_buffer_bkg(void *map_data,
  */
 void zoom_engine_make_self_gravity_tasks(struct space *s, struct engine *e) {
 
-  const ticks tic = getticks();
+  ticks tic = getticks();
 
   threadpool_map(
       &e->threadpool, engine_make_self_gravity_tasks_mapper_zoom_cells, NULL,
