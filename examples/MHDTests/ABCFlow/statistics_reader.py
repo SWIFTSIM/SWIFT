d1bd45a1a4ee8bafb5514082f68129e1052fd107
nickishch <nickishch@gmail.com>
2024-03-12 16:25:45 +0100

    added growth rate calculation

--- examples/MHDTests/ABCFlow/statistics_reader.py
+++ examples/MHDTests/ABCFlow/statistics_reader.py
@@ -4,12 +4,22 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+def find_growth_rate(the_time, B_field, nlast=take_last):
+    l = len(B_field)
+    B_field_cut = B_field[l - 1 - nlast : -1]
+    time_cut = the_time[l - 1 - nlast : -1]
+    # print(time_cut,B_field_cut)
+    res = np.polyfit(time_cut, B_field_cut, 1)[0]
+    return np.round(res, 4)
+
 the_statistics=np.transpose(np.loadtxt("statistics.txt"))
 
 Time = the_statistics[1]
 B = np.array(the_statistics[34])
 B = B/B[0]
 divB = np.abs(np.array(the_statistics[35]))
+
+print('The growth rate is: '+str(find_growth_rate(the_time, B, nlast=10/5e-3)))
 
 fig, ax = plt.subplots(1, 2, sharex=True, figsize=(10, 5))
 ax[0].plot(Time, B)
