0829535be97ffab5d926296865c118e1c6b0adf0
wjr21 <wjr21@sussex.ac.uk>
2024-03-13 17:01:26 +0000

    Debugging

--- src/zoom_region/zoom_cell.c
+++ src/zoom_region/zoom_cell.c
@@ -732,9 +732,9 @@ void link_zoom_to_void(struct space *s, struct cell *c) {
   for (int k = 0; k < 8; k++) {
 
     /* Establish the location of the fake progeny cell. */
-    double zoom_loc[3] = {c->loc[0] + (s->zoom_props->width[0] / 2),
-                          c->loc[1] + (s->zoom_props->width[1] / 2),
-                          c->loc[2] + (s->zoom_props->width[2] / 2)};
+    double zoom_loc[3] = {c->loc[0] + (s->zoom_props->width[0]),
+                          c->loc[1] + (s->zoom_props->width[1]),
+                          c->loc[2] + (s->zoom_props->width[2])};
     if (k & 4) zoom_loc[0] += s->zoom_props->width[0];
     if (k & 2) zoom_loc[1] += s->zoom_props->width[1];
     if (k & 1) zoom_loc[2] += s->zoom_props->width[2];
@@ -750,9 +750,10 @@ void link_zoom_to_void(struct space *s, struct cell *c) {
     if (zoom_cell->type != cell_type_zoom)
       error(
           "Zoom cell isn't a zoom cell! (zoom_loc=[%f %f %f] -> "
-          "c->type=%s, c->subtype=%s)",
+          "c->type=%s, c->subtype=%s, c->loc=[%f %f %f])",
           zoom_loc[0], zoom_loc[1], zoom_loc[2], cellID_names[zoom_cell->type],
-          subcellID_names[zoom_cell->subtype]);
+          subcellID_names[zoom_cell->subtype], zoom_cell->loc[0],
+          zoom_cell->loc[1], zoom_cell->loc[2]);
 #endif
 
     /* Link this zoom cell into the void cell hierarchy. */
