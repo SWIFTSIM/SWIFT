bcf4c1382f0b80d023794c863fea9147f6d4c15d
wjr21 <wjr21@sussex.ac.uk>
2024-03-13 13:43:59 +0000

    Doh, you can't define an array in an argument

--- swift.c
+++ swift.c
@@ -1399,12 +1399,14 @@ int main(int argc, char *argv[]) {
 
     /* Initialise the gravity properties */
     bzero(&gravity_properties, sizeof(struct gravity_props));
-    if (with_self_gravity)
+    if (with_self_gravity) {
+      double non_zoom_width[3] = {0.0, 0.0, 0.0};
       gravity_props_init(
           &gravity_properties, params, &prog_const, &cosmo, with_cosmology,
           with_external_gravity, with_baryon_particles, with_DM_particles,
           with_neutrinos, with_DM_background_particles, periodic, s.dim, s.cdim,
-          with_zoom_region ? s.zoom_props->width : {0.0, 0.0, 0.0});
+          with_zoom_region ? s.zoom_props->width : non_zoom_width);
+    }
 
     /* Initialize the neutrino response if used */
     bzero(&neutrino_response, sizeof(struct neutrino_response));
