50611449e022de83319f735f66120a145ed54d94
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2024-04-22 20:38:25 +0200

    Merge the ABC-flow and Roberts flow forcing terms updates from the MHD branch

--- src/forcing/roberts_flow/forcing.h
+++ src/forcing/roberts_flow/forcing.h
@@ -35,11 +35,10 @@
 
 /* Type of flow */
 enum flow {
-  Brandenburg_flow,
-  Roberts_flow_1,
-  Roberts_flow_2,
-  Roberts_flow_3,
-  Roberts_flow_4
+  Roberts_flow_1 = 1,
+  Roberts_flow_2 = 2,
+  Roberts_flow_3 = 3,
+  Roberts_flow_4 = 4
 };
 
 /**
@@ -63,7 +62,10 @@ struct forcing_terms {
 /**
  * @brief Computes the forcing terms.
  *
- * Based on Tilgner & Brandenburg, 2008, MNRAS, 391, 1477.
+ * Based on G.O.Roberts, Dynamo action of fluid motions with two-dimensional
+ * periodicity, Royal Society, Feb. 3, 1972;
+ * Tilgner & Brandenburg, 2008, MNRAS, 391, 1477;
+ * Brandenburg & Ntormousi, arxiv 2211.03476.
  * This version differs from the paper by imposing the velocity directly rather
  * than by giving the particles an acceleration.
  *
@@ -85,67 +87,50 @@ __attribute__((always_inline)) INLINE static void forcing_terms_apply(
   const double k0 = (2. * M_PI / L) * terms->kv;
   const double kf = M_SQRT2 * k0;
   double v_Rob[3];
-  double Psi;
 
   /* Switching between different kinds of flows */
-  /* Note: Roberts worked in yz plane, we work in xy plane just for convenience
-   * and also because A.Brandenburg has flows in xy plane, so our formulas
-   * differ from Robets article by several rotations. Theese rotations are
-   * equivalent to yzx -> xyz permutation*/
+  /* Here we use definitions based on Brandenburg & Ntormousi, arxiv 2211.03476,
+   * SB3 and SB4 */
+  /* The difference between Roberts definitions and the definitions used here
+   * are the following: Roberts worked in yz plane, we work in xy plane just for
+   * convenience, so our formulas differ from Robets article by several
+   * rotations. Theese rotations are equivalent to yzx -> xyz permutation. Also
+   * Roberts cells are tilted 45 degrees and are sqrt(2) times larger We
+   * normalize flow in such way that the rms velocity is u0 (when Vzfactor=1)*/
 
   switch (Flow_kind) {
 
-    case Brandenburg_flow:
+    case Roberts_flow_1:
+      /* Based on Brandenburg & Ntormousi, arxiv 2211.03476, SB4, flow 1 */
 
-      /* Eq. 8 of Tilgner & Brandenburg, 2008, MNRAS, 391, 1477 */
-      // Psi = (u0 / k0) * cos(k0 * p->x[0]) * cos(k0 * p->x[1]);
-
-      /* Eq. 7 of Tilgner & Brandenburg, 2008, MNRAS, 391, 1477 */
-      // v_Rob[0] = u0 * cos(k0 * p->x[0]) * sin(k0 * p->x[1]);
-      // v_Rob[1] = -u0 * sin(k0 * p->x[0]) * cos(k0 * p->x[1]);
-      // v_Rob[2] = kf * Psi;
-
-      // Velocity used to compare with A.B. runs (from overleaf)
-      Psi = (u0 / k0) * sin(k0 * p->x[0]) * sin(k0 * p->x[1]);
       v_Rob[0] = u0 * sin(k0 * p->x[0]) * cos(k0 * p->x[1]);
       v_Rob[1] = -u0 * cos(k0 * p->x[0]) * sin(k0 * p->x[1]);
-      v_Rob[2] = kf * Psi;
-
-      break;
-
-    case Roberts_flow_1:
-      /* Eq. 5.1 of Roberts, Feb. 3, 1972, Vol. 271, No. 1216 (Feb. 3, 1972),
-       * pp. 411-454.*/
-      v_Rob[0] = u0 * sin(k0 * p->x[0]);
-      v_Rob[1] = u0 * sin(k0 * p->x[1]);
-      v_Rob[2] = u0 * (cos(k0 * p->x[0]) - cos(k0 * p->x[1]));
+      v_Rob[2] = u0 * M_SQRT2 * sin(k0 * p->x[0]) * sin(k0 * p->x[1]);
       break;
 
     case Roberts_flow_2:
+      /* Based on Brandenburg & Ntormousi, arxiv 2211.03476, SB4, flow 2 */
 
-      /* Eq. 6.1 of Roberts, Feb. 3, 1972, Vol. 271, No. 1216 (Feb. 3, 1972),
-    pp. 411-454.*/
-      v_Rob[0] = u0 * sin(k0 * p->x[0]);
-      v_Rob[1] = u0 * sin(k0 * p->x[1]);
-      v_Rob[2] = u0 * (cos(k0 * p->x[0]) + cos(k0 * p->x[1]));
+      v_Rob[0] = u0 * sin(k0 * p->x[0]) * cos(k0 * p->x[1]);
+      v_Rob[1] = -u0 * cos(k0 * p->x[0]) * sin(k0 * p->x[1]);
+      v_Rob[2] = u0 * M_SQRT2 * cos(k0 * p->x[0]) * cos(k0 * p->x[1]);
       break;
 
     case Roberts_flow_3:
+      /* Based on Brandenburg & Ntormousi, arxiv 2211.03476, SB4, flow 3 */
 
-      /* Eq. 6.2 of Roberts, Feb. 3, 1972, Vol. 271, No. 1216 (Feb. 3, 1972),
-    pp. 411-454.*/
-      v_Rob[0] = u0 * sin(k0 * p->x[0]);
-      v_Rob[1] = u0 * sin(k0 * p->x[1]);
-      v_Rob[2] = u0 * 2 * cos(k0 * p->x[0]) * cos(k0 * p->x[1]);
+      v_Rob[0] = u0 * sin(k0 * p->x[0]) * cos(k0 * p->x[1]);
+      v_Rob[1] = -u0 * cos(k0 * p->x[0]) * sin(k0 * p->x[1]);
+      v_Rob[2] =
+          u0 * M_SQRT1_2 * (cos(2 * k0 * p->x[0]) + cos(2 * k0 * p->x[1]));
       break;
 
     case Roberts_flow_4:
+      /* Based on Brandenburg & Ntormousi, arxiv 2211.03476, SB4, flow 4 */
 
-      /* Eq. 6.3 of Roberts, Feb. 3, 1972, Vol. 271, No. 1216 (Feb. 3, 1972),
-    pp. 411-454.*/
-      v_Rob[0] = u0 * sin(k0 * p->x[0]);
-      v_Rob[1] = u0 * sin(k0 * p->x[1]);
-      v_Rob[2] = u0 * sin(k0 * (p->x[0] + p->x[1]));
+      v_Rob[0] = u0 * sin(k0 * p->x[0]) * cos(k0 * p->x[1]);
+      v_Rob[1] = -u0 * cos(k0 * p->x[0]) * sin(k0 * p->x[1]);
+      v_Rob[2] = u0 * sin(k0 * p->x[0]);
       break;
 
     default:
@@ -214,9 +199,9 @@ static INLINE void forcing_terms_init(struct swift_params* parameter_file,
       parser_get_param_int(parameter_file, "RobertsFlowForcing:Flow_kind");
   terms->kv = parser_get_param_double(parameter_file, "RobertsFlowForcing:kv");
 
-  if (terms->Flow_kind > 4 || terms->Flow_kind < 0)
+  if (terms->Flow_kind > 4 || terms->Flow_kind < 1)
     error(
-        "Error: Flow_kind variable can take integer values from [0,4] "
+        "Error: Flow_kind variable can take integer values from [1,4] "
         "interval.");
 }
 
