afdd2441aac8f3b52a2bb0d4bea9e8ef1f0343a5
wjr21 <wjr21@sussex.ac.uk>
2024-04-26 11:30:48 +0200

    Splitting neighbour to the zoom depth

--- src/space_split.c
+++ src/space_split.c
@@ -662,6 +662,8 @@ void space_split_recursive(struct space *s, struct cell *c,
   struct engine *e = s->e;
   const integertime_t ti_current = e->ti_current;
   const int with_rt = e->policy & engine_policy_rt;
+  const int zoom_cell_depth =
+      s->with_zoom_region ? s->zoom_props->zoom_cell_depth : 0;
 
   /* If the depth is too large, we have a problem and should stop. */
   if (depth > space_cell_maxdepth) {
@@ -674,10 +676,12 @@ void space_split_recursive(struct space *s, struct cell *c,
         c->grav.parts[1].x[0]);
   }
 
-  /* Split or let it be? */
+  /* Split or let it be? (When running a zoom we split neighbour cells to better
+   * divide interactions between zoom and neighbour cells.)*/
   if ((with_self_gravity && gcount > space_splitsize) ||
       (!with_self_gravity &&
-       (count > space_splitsize || scount > space_splitsize))) {
+       (count > space_splitsize || scount > space_splitsize)) ||
+      (c->subtype == cell_subtype_neighbour && depth < zoom_cell_depth)) {
 
     /* Construct the progeny ready to populate with particles and multipoles
      * (if doing gravity). */
