d499f35f892ede1968ff16a8319f2f9f3708836d
wjr21 <wjr21@sussex.ac.uk>
2024-05-10 15:04:55 +0100

    Ensuring void doesnt enter hierarchical tasks

--- src/engine_maketasks.c
+++ src/engine_maketasks.c
@@ -1846,6 +1846,13 @@ void engine_make_hierarchical_tasks_mapper(void *map_data, int num_elements,
 
   for (int ind = 0; ind < num_elements; ind++) {
     struct cell *c = &((struct cell *)map_data)[ind];
+
+    /* Skip void cells, we make the subset of tasks these need ourselves
+     * elsewhere (only applicable while running a zoom). */
+    if (c->subtype == cell_subtype_void) {
+      continue;
+    }
+
     /* Make the common tasks (time integration) */
     engine_make_hierarchical_tasks_common(e, c);
     /* Add the hydro stuff */
