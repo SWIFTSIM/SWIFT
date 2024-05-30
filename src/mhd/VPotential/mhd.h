4e94ecd11967ae07cdf2f9134fd528ae467ebc58
nickishch <nickishch@gmail.com>
2024-05-30 16:40:10 +0200

    added trigger to VP

--- src/mhd/VPotential/mhd.h
+++ src/mhd/VPotential/mhd.h
@@ -479,6 +479,13 @@ __attribute__((always_inline)) INLINE static void mhd_reset_acceleration(
   for (int k = 0; k < 3; k++) {
     p->mhd_data.tot_mag_F[k] = 0.0f;
   }
+  /* Save induction sources*/
+  for (int k = 0; k < 3; k++) {
+    p->mhd_data.Adv_A_source[k] = 0.0f;
+    p->mhd_data.Diff_A_source[k] = 0.0f;
+    p->mhd_data.Delta_A[k] = 0.0f;
+  }
+
 }
 
 /**
