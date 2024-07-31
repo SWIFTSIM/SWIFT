f06515adebac76c5986899ddeb506b2ec6f0295f
Darwin Roduit <darwin.roduit@epfl.ch>
2024-07-31 13:04:24 +0200

    Add cell_link_sinks() function

--- src/cell.h
+++ src/cell.h
@@ -548,6 +548,7 @@ int cell_link_parts(struct cell *c, struct part *parts);
 int cell_link_gparts(struct cell *c, struct gpart *gparts);
 int cell_link_sparts(struct cell *c, struct spart *sparts);
 int cell_link_bparts(struct cell *c, struct bpart *bparts);
+int cell_link_sinks(struct cell *c, struct sink *sinks);
 int cell_link_foreign_parts(struct cell *c, struct part *parts);
 int cell_link_foreign_gparts(struct cell *c, struct gpart *gparts);
 void cell_unlink_foreign_particles(struct cell *c);
