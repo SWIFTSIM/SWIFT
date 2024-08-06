f82859403d6914387d653b848a50d6672267a9d2
wjr21 <wjr21@sussex.ac.uk>
2024-08-06 13:47:55 +0100

    Debugging

--- src/engine_unskip.c
+++ src/engine_unskip.c
@@ -316,6 +316,8 @@ void engine_do_unskip_mapper(void *map_data, int num_elements,
     /* Handle on the cell */
     struct cell *const c = &cells_top[local_cells[ind]];
 
+    if (c->subtype == cell_subtype_void) message("Got void cell");
+
     /* In what copy of the global list are we?
      * This gives us the broad type of task we are working on. */
     const ptrdiff_t delta = &local_cells[ind] - list_base;
