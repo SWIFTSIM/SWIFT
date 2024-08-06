d9b04a495e74e2c55473642ba72a891e9504dd28
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2024-08-06 15:48:52 +0100

    Remove unnecessary scripts

--- examples/Planetary/DemoImpactInitCond/clean_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove all output files from a previous run
-rm -fv statistics.txt task_level*.txt *used_parameters.yml *.csv *.xmf
-rm -fv timesteps.txt output*.txt
-rm -fv *_0???.hdf5 snapshots/* restart/*
-rm -fv *.png
