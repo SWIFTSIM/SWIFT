8602c1ee805e4696f0adce79b29da1eade5e5769
Darwin Roduit <darwin.roduit@epfl.ch>
2024-08-07 19:40:01 +0200

    Run format.sh

--- src/chemistry/GEAR_DIFFUSION/chemistry.h
+++ src/chemistry/GEAR_DIFFUSION/chemistry.h
@@ -418,10 +418,11 @@ __attribute__((always_inline)) INLINE static void chemistry_first_init_spart(
        have a negative mass fraction) */
     if (data->initial_metallicities[i] >= 0) {
       /* Use the value from the parameter file */
-      sp->chemistry_data.metal_mass_fraction[i] = data->initial_metallicities[i];
+      sp->chemistry_data.metal_mass_fraction[i] =
+          data->initial_metallicities[i];
     }
-     /* else : Use the value from the IC. We are reading the metal mass
-      fraction. So do not overwrite the metallicities */
+    /* else : Use the value from the IC. We are reading the metal mass
+     fraction. So do not overwrite the metallicities */
   }
 }
 
