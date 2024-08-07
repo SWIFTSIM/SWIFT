09d351b89c75b8848716b1159adb53280136e4a5
wjr21 <wjr21@sussex.ac.uk>
2024-08-07 13:59:10 +0100

    Always label subtypes

--- tools/task_plots/analyse_zoom_tasks.py
+++ tools/task_plots/analyse_zoom_tasks.py
@@ -164,12 +164,12 @@ def make_task_hist_split(runs, output=""):
                     run.tasks[i].cj_subtype,
                 )
             labels_dict[name][i] = f"{task}:{run.tasks[i].ci_type}"
-            if run.tasks[i].ci_subtype != "Regular":
-                labels_dict[name][i] += f"({run.tasks[i].ci_subtype})"
+            # if run.tasks[i].ci_subtype != "Regular":
+            labels_dict[name][i] += f"({run.tasks[i].ci_subtype})"
             if "pair" in task:
                 labels_dict[name][i] += f"->{run.tasks[i].cj_type}"
-                if run.tasks[i].cj_subtype != "Regular":
-                    labels_dict[name][i] += f"({run.tasks[i].cj_subtype})"
+                # if run.tasks[i].cj_subtype != "Regular":
+                labels_dict[name][i] += f"({run.tasks[i].cj_subtype})"
             labels_dict[name][i] += f"@{run.tasks[i].ci_depth}"
 
     for i, (name, run) in enumerate(runs.items()):
