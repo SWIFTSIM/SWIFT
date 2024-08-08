eab79aaf0411e0c2d284d7eeb9801a82fa9514e1
Darwin <darwin.roduit@epfl.ch>
2024-08-08 13:56:05 +0000

    Add missing commas to GEAR SF IO

--- src/star_formation/GEAR/star_formation_io.h
+++ src/star_formation/GEAR/star_formation_io.h
@@ -74,7 +74,7 @@ star_formation_write_sparticles(const struct spart* sparts,
 
   list[0] = io_make_physical_output_field(
       "BirthDensities", FLOAT, 1, UNIT_CONV_DENSITY, 0.f, sparts,
-      sf_data.birth_density, /*can convert to comoving=*/0
+      sf_data.birth_density, /*can convert to comoving=*/0,
       "Physical densities at the time of birth of the gas particles that "
       "turned into stars (note that "
       "we store the physical density at the birth redshift, no conversion is "
@@ -82,7 +82,7 @@ star_formation_write_sparticles(const struct spart* sparts,
 
   list[1] =
     io_make_physical_output_field("BirthTemperatures", FLOAT, 1, UNIT_CONV_TEMPERATURE,
-                           0.f, sparts, sf_data.birth_temperature, /*can convert to comoving=*/0
+				  0.f, sparts, sf_data.birth_temperature, /*can convert to comoving=*/0,
                            "Temperatures at the time of birth of the gas "
                            "particles that turned into stars");
 
