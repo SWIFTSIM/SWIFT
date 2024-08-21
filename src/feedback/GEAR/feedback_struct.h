ec6b3f386b6678cf314dbcf8772237c7905d7df8
Darwin Roduit <darwin.roduit@epfl.ch>
2024-08-21 22:06:27 +0200

    Move star_type from feedback_struct to spart

--- src/feedback/GEAR/feedback_struct.h
+++ src/feedback/GEAR/feedback_struct.h
@@ -22,19 +22,6 @@
 #include "chemistry_struct.h"
 
 /**
- * @brief The stellar feedback type for each star type.
- *
- * Now, star particles can represent a single star ("single_star"), a stellar
- * population from a continuous IMF or a stellar population from a whole IMF.
- */
-enum star_feedback_type {
-  single_star,                    /* particle representing a single star */
-  star_population_continuous_IMF, /* particle representing a population of the
-                                     continuous part of the IMF */
-  star_population /* particle representing a population with the whole IMF */
-};
-
-/**
  * @brief Feedback fields carried by each hydro particles
  */
 struct feedback_part_data {};
@@ -78,14 +65,6 @@ struct feedback_spart_data {
 
   /*! Does the particle needs the feedback loop? */
   char will_do_feedback;
-
-  /* Feedback type in function of the star particle type */
-  /* Note for Darwin (July 2024): Ideally, I'd like to move this to the star
-  module and not the feedback. The type of the stellar particle is more related
-  to the star than the feedback. Furthermore, this type may be used in other
-  modules, e.g. RT, star formation, etc. This type is already used by the sink
-  module. */
-  enum star_feedback_type star_type;
 };
 
 #endif /* SWIFT_FEEDBACK_STRUCT_GEAR_H */
