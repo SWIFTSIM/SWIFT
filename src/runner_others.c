e2b131ad93dc73e9f7e2ffade3b51552ffd3a46d
wjr21 <wjr21@sussex.ac.uk>
2024-08-27 12:02:54 +0100

    More readable error

--- src/runner_others.c
+++ src/runner_others.c
@@ -868,7 +868,7 @@ void runner_do_end_grav_force(struct runner *r, struct cell *c, int timer) {
                 "gravitationally with all other gparts "
                 "gp->num_interacted=%lld, total_gparts=%lld (local "
                 "num_gparts=%zd inhibited_gparts=%lld, "
-                "total-gp->num_interacted=%lld, c->type=%s, c->subtype=%s, "
+                "(total-gp->num_interacted)=%lld, c->type=%s, c->subtype=%s, "
                 "c->depth=%d)",
                 my_id, part_type_names[gp->type], gp->num_interacted,
                 e->total_nr_gparts, e->s->nr_gparts, e->count_inhibited_gparts,
