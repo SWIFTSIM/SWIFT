94d9250db852802e2b557836d9d100947cd03401
Darwin Roduit <darwin.roduit@epfl.ch>
2024-09-03 17:54:15 +0200

    Improve comments

--- src/chemistry/GEAR/chemistry_getters.h
+++ src/chemistry/GEAR/chemistry_getters.h
@@ -22,7 +22,6 @@
 #include "const.h"
 #include "part.h"
 
-
 /**
  * @brief Get a 1-element state vector U containing the metal mass density
  * a specific metal group.
@@ -40,24 +39,26 @@ chemistry_part_get_diffusion_state_vector(const struct part *restrict p, int gro
 }
 
 /**
- * @brief Get the diffusion flux of given metal group.
+ * @brief Compute the diffusion flux of given metal group.
+ *
+ * F_diss = - K * \nabla \otimes q
  *
  * @param p Particle.
  * @param metal Index of metal specie
  * @param F_diff (return) Array to write diffusion flux component into
  */
-__attribute__((always_inline)) INLINE static void chemistry_part_get_diffusion_flux(
+__attribute__((always_inline)) INLINE static void chemistry_part_compute_diffusion_flux(
     const struct part *restrict p, int metal, float F_diff[3]) {
 
   const double kappa = p->chemistry_data.kappa;
 
-  /* For isotropic diffusion, \grad U = \nabla \otimes q = \grad n_Z */
-  /* TODO: We should slope-limit nabla_otimes_1. Is the slope-limited value
-     stored inside this variable already? */
-  F_diff[0] = kappa * p->chemistry_data.gradients[metal].nabla_otimes_q[0];
-  F_diff[1] = kappa * p->chemistry_data.gradients[metal].nabla_otimes_q[1];
-  F_diff[2] = kappa * p->chemistry_data.gradients[metal].nabla_otimes_q[2];
+  /* For isotropic diffusion, \grad U = \nabla \otimes q = \grad n_Z.
+     Note: K = kappa * I_3 for isotropic diffusion. */
+  F_diff[0] = - kappa * p->chemistry_data.gradients[metal].nabla_otimes_q[0];
+  F_diff[1] = - kappa * p->chemistry_data.gradients[metal].nabla_otimes_q[1];
+  F_diff[2] = - kappa * p->chemistry_data.gradients[metal].nabla_otimes_q[2];
 }
+
 
 /**
  * @brief Get the gradients of metal mass density a given metal group.
