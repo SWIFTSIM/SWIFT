893a604ecdd44a5c7fbf082dbd1161eafde8c019
Darwin Roduit <darwin.roduit@epfl.ch>
2024-09-03 17:54:38 +0200

    Start implementing HLL riemann solver

--- src/chemistry/GEAR/chemistry_riemann_HLL.h
+++ src/chemistry/GEAR/chemistry_riemann_HLL.h
@@ -19,6 +19,27 @@
 #ifndef SWIFT_GEAR_CHEMISTRY_RIEMANN_HLL_H
 #define SWIFT_GEAR_CHEMISTRY_RIEMANN_HLL_H
 
+#include "hydro.h"
+
+/**
+ * @brief Check if the given input states are vacuum or will generate vacuum
+ */
+__attribute__((always_inline)) INLINE static int chemistry_riemann_is_vacuum(
+    const float rhoL, const float rhoR, float vL, float vR, float aL, float aR) {
+
+  /* vacuum */
+  if (!rhoL || !rhoR) return 1;
+
+  /* vacuum generation */
+  else if (hydro_two_over_gamma_minus_one * aL +
+               hydro_two_over_gamma_minus_one * aR <=
+           vR - vL)
+    return 1;
+
+  /* no vacuum */
+  else
+    return 0;
+}
 
 /**
  * @brief Solve the Riemann problem for the RT equations and return the
@@ -37,31 +58,103 @@
  */
 __attribute__((always_inline)) INLINE static void chemistry_riemann_solve_for_flux(
     const struct part* restrict pi, const struct part* restrict pj, const float UL,
-    const float UR, const float n_unit[3],
-    const float Anorm, const float F_diff_i[3], const float F_diff_j[3], float metal_flux) {
+    const float UR, const float n_unit[3], int g,
+    const float Anorm, const float F_diff_L[3], const float F_diff_R[3], float* metal_flux) {
 										   
-    /* const float UL[4], const float UR[4], const float FLnorm, */
-    /* const float FRnorm, float hyperFluxL[4][3], float hyperFluxR[4][3], */
-    /* const float n_unit[3], float flux_half[4]) { */
+  /* Handle pure vacuum */
+  if (!UL && !UR) {
+    *metal_flux = 0.0f;
+    return;
+  }
 
-  /* /\* Compute lambda_+/- *\/ */
-  /* const float lminus = min3(lambdaLmin, lambdaRmin, 0.f); */
-  /* const float lplus = max3(lambdaLmax, lambdaRmax, 0.f); */
+  const float rhoL = pi->rho;
+  const float rhoR = pj->rho;
+  const float vL[3] = {pi->v[0], pi->v[1], pi->v[2]};
+  const float vR[3] = {pj->v[0], pj->v[1], pj->v[2]};
+  const float PL = hydro_get_comoving_pressure(pi);
+  const float PR = hydro_get_comoving_pressure(pj);
+  
+  /* STEP 0: obtain velocity in interface frame */
+  const float uL = vL[0] * n_unit[0] + vL[1] * n_unit[1] + vL[2] * n_unit[2];
+  const float uR = vR[0] * n_unit[0] + vR[1] * n_unit[1] + vR[2] * n_unit[2];
+  const float aL = sqrtf(hydro_gamma * PR / rhoL);
+  const float aR = sqrtf(hydro_gamma * PR / rhoR);
 
-  /* /\* Sanity check: This should give the same results as GLF solver *\/ */
-  /* if (lminus == 0.f && lplus == 0.f) { */
-  /*   flux_half[0] = 0.f; */
-  /*   flux_half[1] = 0.f; */
-  /*   flux_half[2] = 0.f; */
-  /*   flux_half[3] = 0.f; */
-  /*   return; */
-  /* } */
+  /* Handle vacuum: vacuum does not require iteration and is always exact */
+  if (chemistry_riemann_is_vacuum(rhoL, rhoR, uL, uR, aL, aR)) {
+    *metal_flux = 0.0f;
+    return;
+  }
+
+  /* STEP 1: pressure estimate */
+  const float rhobar = rhoL + rhoR;
+  const float abar = aL + aR;
+  const float pPVRS =
+    0.5f * ((PL + PR) - 0.25f * (uR - uL) * rhobar * abar);
+  const float pstar = max(0.f, pPVRS);
+
+  /* STEP 2: wave speed estimates
+     all these speeds are along the interface normal, since uL and uR are */
+  float qL = 1.0f;
+  if (pstar > PL && PL > 0.0f) {
+    qL = sqrtf(1.0f + 0.5f * hydro_gamma_plus_one * hydro_one_over_gamma *
+	       (pstar / PL - 1.0f));
+  }
+  float qR = 1.0f;
+  if (pstar > PR && PR > 0.0f) {
+    qR = sqrtf(1.0f + 0.5f * hydro_gamma_plus_one * hydro_one_over_gamma *
+	       (pstar / PR - 1.0f));
+  }
+  const float SLmuL = -aL * qL;
+  const float SRmuR = aR * qR;
+  const float Sstar =
+    (PR - PL + rhoL * uL * SLmuL - rhoR * uR * SRmuR) /
+    (rhoL * SLmuL - rhoR * SRmuR);
+
+  const float SL = uL - aL * qL;
+  const float SR = uR - aR * qR;
+
+  /* Get the fastet wavespeed */
+  const float c_fast = max3(SL, SR, Sstar);
+
+  /* Approximate lambda_plus and lambda_minus */
+  const float lambda_plus = max(uL, uR) + c_fast;
+  const float lambda_minus = min(uL, UR) - c_fast;
+
+  /* Compute r */
+  /* const float nabla_o_q_L[3] = {0.0, 0.0, 0.0}; */
+  /* const float nabla_o_q_R[3] = {0, 0, 0};  */
+  /* const float grad_otimes_q_star[3] = {0.5*(nabla_o_q_L[0] + nabla_o_q_R[0]), */
+  /* 				       0.5*(nabla_o_q_L[1] + nabla_o_q_R[1]), */
+  /* 				       0.5*(nabla_o_q_L[2] + nabla_o_q_R[2])}; */
+
+  /* const float K_star_norm = sqrtf(3.0) * (pi->chemistry_data.kappa + pj->chemistry_data.kappa)/2.f; */
+  /* const float dx[3] = {pj->x[0] - pi->x[0], pj->x[1] - pi->x[1], pj->x[1] - pi->x[1]}; */
+  /* const float dx_norm = dx[0]*dx[0] + dx[1]*dx[1] + dx[2]*dx[2]; */
+  /* const float r = dx_norm/K_star_norm * (0.5*labs(vR - vL) + Sstar); */
+  /* const float r_term = (0.2 + r )/ (0.2 + r + r*r); */
+  /* const float norm_term = 0.0; */
+  /* const float alpha = r_term * norm_term ; */
+  const float alpha = 1.0;
+
+  const float dU = UR - UL;
+  const float one_over_dl = 1.f / (lambda_plus - lambda_minus);
+
+  /* Project to reduce to a 1D Problem with 1 quantity */
+  const float Flux_L = F_diff_L[0]*n_unit[0] + F_diff_L[1]*n_unit[1] + F_diff_L[2]*n_unit[2];
+  const float Flux_R = F_diff_R[0]*n_unit[0] + F_diff_R[1]*n_unit[1] + F_diff_R[2]*n_unit[2];
 
   /* Compute F_diff_ij^* */
+  const float flux_hll  = (lambda_plus*Flux_L - lambda_minus*Flux_R + alpha*lambda_plus*lambda_minus*dU)*one_over_dl;
 
-  /* Compute F_diff_ij^* cdot A_ij */
+  if (SL >= 0) {
+    *metal_flux = Flux_L;
+  } else if (SL <= 0 && SR >= 0) {
+    *metal_flux = flux_hll;
+  } else /* SR <= 0 */ {
+    *metal_flux = Flux_R;
+  }
 
-  // Have a look in riemann_hllc.h 
 }
 
 
