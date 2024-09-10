77e691eba75c0cc7b7752bf609f0c751ad8e89e4
wjr21 <wjr21@sussex.ac.uk>
2024-09-10 15:29:14 +0100

    and engine and space

--- src/scheduler.c
+++ src/scheduler.c
@@ -1551,8 +1551,8 @@ static void scheduler_splittask_gravity(struct task *t, struct scheduler *s) {
  */
 static void zoom_scheduler_splittask_gravity_void_pair(struct task *t,
                                                        struct scheduler *s) {
-  /* const struct space *sp = s->space; */
-  /* struct engine *e = sp->e; */
+  const struct space *sp = s->space;
+  struct engine *e = sp->e;
 
   /* Pair interaction? */
   if (t->type == task_type_pair) {
