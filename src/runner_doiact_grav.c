1d86c3b7949ef8fe7d4fe1db44f4b20da76421c5
wjr21 <wjr21@sussex.ac.uk>
2024-09-10 23:03:00 +0100

    Use split downs but with the single function

--- src/runner_doiact_grav.c
+++ src/runner_doiact_grav.c
@@ -67,8 +67,6 @@ void runner_do_grav_down(struct runner *r, struct cell *c, int timer) {
   /* Some constants */
   const struct engine *e = r->e;
 
-  if (c->type == cell_type_zoom && timer) return;
-
   TIMER_TIC;
 
 #ifdef SWIFT_DEBUG_CHECKS
@@ -109,7 +107,9 @@ void runner_do_grav_down(struct runner *r, struct cell *c, int timer) {
         }
 
         /* Recurse */
-        runner_do_grav_down(r, cp, 0);
+        if (cp->grav.super != cp) {
+          runner_do_grav_down(r, cp, 0);
+        }
       }
     }
 
