347f8deb947db471c8556aeeb8a3b2889ebe01d9
wjr21 <wjr21@sussex.ac.uk>
2024-09-10 16:40:21 +0100

    Removing protection that could hide issues

--- src/zoom_region/zoom_maketasks.c
+++ src/zoom_region/zoom_maketasks.c
@@ -374,19 +374,13 @@ static void zoom_engine_make_hierarchical_void_tasks_recursive(struct engine *e,
 
       /* If we're at the super level, unlock the zoom super cell's grav down
        * after the void cell grav down. */
-      if (c->top->void_parent->grav.super != NULL) {
-        message("Unlocking zoom super down with void cell down.");
-        scheduler_addunlock(s, c->top->void_parent->grav.super->grav.down,
-                            c->grav.down);
-      }
+      scheduler_addunlock(s, c->top->void_parent->grav.super->grav.down,
+                          c->grav.down);
 
       /* We also need to unlock the void cell grav down after the zoom cell
        * long range task is complete. */
-      if (c->top->void_parent->grav.super != NULL) {
-        message("Unlocking void cell down with zoom cell long range.");
-        scheduler_addunlock(s, c->grav.long_range,
-                            c->top->void_parent->grav.super->grav.down);
-      }
+      scheduler_addunlock(s, c->grav.long_range,
+                          c->top->void_parent->grav.super->grav.down);
     }
   }
 
