504d79bfca0874253a91a5f91bdc982659839fd0
Darwin Roduit <darwin.roduit@epfl.ch>
2024-09-11 12:20:56 +0200

    Remove comment

--- src/chemistry/GEAR_MFM_DIFFUSION/chemistry.h
+++ src/chemistry/GEAR_MFM_DIFFUSION/chemistry.h
@@ -388,8 +388,6 @@ __attribute__((always_inline)) INLINE static void chemistry_init_part(
   cpd->geometry.matrix_E[2][2] = 0.0f;
 
   /* Update the diffusion coefficient for the new loops */
-  /* TODO: This might be moved to the end of density or gradient, if we want to
-     use a shear tensor using grad velocity */
   p->chemistry_data.kappa = chemistry_part_compute_diffusion_coefficient(p, cd);
 
   /* Init the gradient for the next loops */
