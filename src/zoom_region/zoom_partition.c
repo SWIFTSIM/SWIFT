ff75c23d0041711101574b4dac5996213a42f7a6
wjr21 <wjr21@sussex.ac.uk>
2024-09-15 12:42:34 +0200

    What have I done...

--- src/zoom_region/zoom_partition.c
+++ src/zoom_region/zoom_partition.c
@@ -203,8 +203,10 @@ void partition_zoom_vector(int nr_nodes, struct space *s) {
 void zoom_partition_voids(struct space *s, int nodeID) {
 
   /* All void cells are local. */
-  for (int k = 0; k < s->zoom_props->nr_void_cells; k++) {
-    s->cells_top[s->zoom_props->void_cells_top[k]].nodeID = nodeID;
+  for (int k = 0; k < s->nr_cells; k++) {
+    if (s->cells_top[k].subtype == cell_subtype_void) {
+      s->cells_top[k].nodeID = nodeID;
+    }
   }
 
   /* /\* Run through the zoom cells. *\/ */
