53345a4a87dd41e895beed0369fd36d186927ccf
Darwin Roduit <darwin.roduit@epfl.ch>
2024-09-19 13:43:41 +0200

    Update setter functions with the new velocity gradients

--- src/chemistry/GEAR_MFM_DIFFUSION/chemistry_setters.h
+++ src/chemistry/GEAR_MFM_DIFFUSION/chemistry_setters.h
@@ -33,10 +33,20 @@ chemistry_part_reset_gradients(struct part *restrict p) {
   struct chemistry_part_data *chd = &p->chemistry_data;
 
   for (int i = 0; i < GEAR_CHEMISTRY_ELEMENT_COUNT; i++) {
-    chd->gradients[i].nabla_otimes_q[0] = 0.0f;
-    chd->gradients[i].nabla_otimes_q[1] = 0.0f;
-    chd->gradients[i].nabla_otimes_q[2] = 0.0f;
+    chd->gradients.nabla_otimes_q[i][0] = 0.0f;
+    chd->gradients.nabla_otimes_q[i][1] = 0.0f;
+    chd->gradients.nabla_otimes_q[i][2] = 0.0f;
   }
+
+  chd->gradients.v[0][0] = 0.0f;
+  chd->gradients.v[0][1] = 0.0f;
+  chd->gradients.v[0][2] = 0.0f;
+  chd->gradients.v[1][0] = 0.0f;
+  chd->gradients.v[1][1] = 0.0f;
+  chd->gradients.v[1][2] = 0.0f;
+  chd->gradients.v[2][0] = 0.0f;
+  chd->gradients.v[2][1] = 0.0f;
+  chd->gradients.v[2][2] = 0.0f;
 }
 
 /**
@@ -44,18 +54,19 @@ chemistry_part_reset_gradients(struct part *restrict p) {
  *
  * @param p Particle.
  */
-__attribute__((always_inline)) INLINE static void chemistry_part_set_gradients(
-    struct part *restrict p, int g, const double gradF[3]) {
+__attribute__((always_inline)) INLINE static void
+chemistry_part_set_diffusion_gradients(struct part *restrict p, int metal,
+                                       const double gradF[3]) {
 
   struct chemistry_part_data *chd = &p->chemistry_data;
 
-  chd->gradients[g].nabla_otimes_q[0] = gradF[0];
-  chd->gradients[g].nabla_otimes_q[1] = gradF[1];
-  chd->gradients[g].nabla_otimes_q[2] = gradF[2];
+  chd->gradients.nabla_otimes_q[metal][0] = gradF[0];
+  chd->gradients.nabla_otimes_q[metal][1] = gradF[1];
+  chd->gradients.nabla_otimes_q[metal][2] = gradF[2];
 }
 
 /**
- * @brief Update the gradients for the given particle with the
+ * @brief Update the diffusion gradients for the given particle with the
  * given contributions.
  *
  * @param p Particle
@@ -64,14 +75,40 @@ __attribute__((always_inline)) INLINE static void chemistry_part_set_gradients(
  * @param dF gradient of the diffusion flux
  */
 __attribute__((always_inline)) INLINE static void
-chemistry_part_update_gradients(struct part *restrict p, int metal,
-                                double dF[3]) {
+chemistry_part_update_diffusion_gradients(struct part *restrict p, int metal,
+                                          double dF[3]) {
 
   struct chemistry_part_data *chd = &p->chemistry_data;
 
-  chd->gradients[metal].nabla_otimes_q[0] += dF[0];
-  chd->gradients[metal].nabla_otimes_q[1] += dF[1];
-  chd->gradients[metal].nabla_otimes_q[2] += dF[2];
+  chd->gradients.nabla_otimes_q[metal][0] += dF[0];
+  chd->gradients.nabla_otimes_q[metal][1] += dF[1];
+  chd->gradients.nabla_otimes_q[metal][2] += dF[2];
+}
+
+/**
+ * @brief Update the velocity gradients for the given particle with the
+ * given contributions.
+ *
+ * @param p Particle
+ * @param dvx x velocity gradient contribution.
+ * @param dvy y velocity gradient contribution.
+ * @param dvz z velocity gradient contribution.
+ */
+__attribute__((always_inline)) INLINE static void
+chemistry_part_update_velocity_gradients(struct part *restrict p, float dvx[3],
+                                         float dvy[3], float dvz[3]) {
+
+  struct chemistry_part_data *chd = &p->chemistry_data;
+
+  chd->gradients.v[0][0] += dvx[0];
+  chd->gradients.v[0][1] += dvx[1];
+  chd->gradients.v[0][2] += dvx[2];
+  chd->gradients.v[1][0] += dvy[0];
+  chd->gradients.v[1][1] += dvy[1];
+  chd->gradients.v[1][2] += dvy[2];
+  chd->gradients.v[2][0] += dvz[0];
+  chd->gradients.v[2][1] += dvz[1];
+  chd->gradients.v[2][2] += dvz[2];
 }
 
 /**
@@ -79,19 +116,28 @@ chemistry_part_update_gradients(struct part *restrict p, int metal,
  * normalisation factor.
  *
  * @param p Particle.
- * @param metal metal group index to update (0 <= metal <
- * GEAR_CHEMISTRY_ELEMENT_COUNT)
  * @param norm Normalisation factor.
  */
 __attribute__((always_inline)) INLINE static void
-chemistry_part_normalise_gradients(struct part *restrict p, int metal,
-                                   const float norm) {
+chemistry_part_normalise_gradients(struct part *restrict p, const float norm) {
 
   struct chemistry_part_data *chd = &p->chemistry_data;
 
-  chd->gradients[metal].nabla_otimes_q[0] *= norm;
-  chd->gradients[metal].nabla_otimes_q[1] *= norm;
-  chd->gradients[metal].nabla_otimes_q[2] *= norm;
+  for (int i = 0; i < GEAR_CHEMISTRY_ELEMENT_COUNT; i++) {
+    chd->gradients.nabla_otimes_q[i][0] *= norm;
+    chd->gradients.nabla_otimes_q[i][1] *= norm;
+    chd->gradients.nabla_otimes_q[i][2] *= norm;
+  }
+
+  chd->gradients.v[0][0] *= norm;
+  chd->gradients.v[0][1] *= norm;
+  chd->gradients.v[0][2] *= norm;
+  chd->gradients.v[1][0] *= norm;
+  chd->gradients.v[1][1] *= norm;
+  chd->gradients.v[1][2] *= norm;
+  chd->gradients.v[2][0] *= norm;
+  chd->gradients.v[2][1] *= norm;
+  chd->gradients.v[2][2] *= norm;
 }
 
 /**
