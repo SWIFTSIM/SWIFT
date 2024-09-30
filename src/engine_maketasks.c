13390fd73be9e22eb111e92f72fefb53732071f4
wjr21 <wjr21@sussex.ac.uk>
2024-09-30 12:17:45 +0100

    Only need a long range task if the super doesn't exist

--- src/engine_maketasks.c
+++ src/engine_maketasks.c
@@ -1368,9 +1368,13 @@ void engine_make_hierarchical_tasks_gravity(struct engine *e, struct cell *c) {
         c->grav.init = scheduler_addtask(s, task_type_init_grav,
                                          task_subtype_none, 0, 0, c, NULL);
 
-        /* Gravity non-neighbouring pm calculations */
-        c->grav.long_range = scheduler_addtask(
-            s, task_type_grav_long_range, task_subtype_none, 0, 0, c, NULL);
+        /* Gravity non-neighbouring pm calculations. */
+        if (c->top->type != cell_type_zoom ||
+            (c->top->type == cell_type_zoom &&
+             c->top->void_parent->grav.super == NULL)) {
+          c->grav.long_range = scheduler_addtask(
+              s, task_type_grav_long_range, task_subtype_none, 0, 0, c, NULL);
+        }
 
         /* Gravity recursive down-pass */
         c->grav.down = scheduler_addtask(s, task_type_grav_down,
