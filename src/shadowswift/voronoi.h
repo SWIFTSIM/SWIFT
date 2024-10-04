777565ee74f7201039689e62a026ee7edbd91e7c
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2024-10-04 13:15:04 +0200

    Add placeholder empty voronoi struct and functions

--- src/shadowswift/voronoi.h
+++ src/shadowswift/voronoi.h
@@ -5,6 +5,12 @@
 #ifndef SWIFTSIM_SHADOWSWIFT_VORONOI_H
 #define SWIFTSIM_SHADOWSWIFT_VORONOI_H
 
-stuct voronoi;
+struct voronoi {
+  int pair_count[27];
+};
+
+struct voronoi_pair {};
+
+__attribute__((always_inline)) INLINE static void voronoi_destroy(struct voronoi* v) {}
 
 #endif  // SWIFTSIM_SHADOWSWIFT_VORONOI_H
