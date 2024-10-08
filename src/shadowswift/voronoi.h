142937c68c98dadcefae0ff172a0afcc65ed13e6
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2024-10-08 10:58:08 +0200

    Add license headers

--- src/shadowswift/voronoi.h
+++ src/shadowswift/voronoi.h
@@ -1,6 +1,22 @@
-//
-// Created by yuyttenh on 10/4/24.
-//
+/*******************************************************************************
+* This file is part of SWIFT.
+* Copyright (c) 2024 Matthieu Schaller (schaller@strw.leidenuniv.nl)
+*                             Yolan Uyttenhove (Yolan.Uyttenhove@UGent.be)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
 
 #ifndef SWIFTSIM_SHADOWSWIFT_VORONOI_H
 #define SWIFTSIM_SHADOWSWIFT_VORONOI_H
