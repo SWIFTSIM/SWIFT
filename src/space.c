dddc0ef33260cf555dd8ed4fd870c1af82647e39
wjr21 <wjr21@sussex.ac.uk>
2024-10-14 13:33:18 +0100

    Ensuring ceells are considered local

--- src/space.c
+++ src/space.c
@@ -554,7 +554,7 @@ void space_list_useful_top_level_cells(struct space *s) {
   for (int i = 0; i < s->nr_cells; ++i) {
     struct cell *c = &s->cells_top[i];
 
-    if (cell_has_tasks(c)) {
+    if (cell_has_tasks(c) || c->subtype == cell_subtype_void) {
       s->local_cells_with_tasks_top[s->nr_local_cells_with_tasks] = i;
       s->nr_local_cells_with_tasks++;
     }
