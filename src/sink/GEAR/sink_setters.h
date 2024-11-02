319c83da5532718efb79473d5783236ea25b12a8
Darwin Roduit <darwin.roduit@epfl.ch>
2024-11-02 21:47:45 +0100

    Move function to sink_setters

--- src/sink/GEAR/sink_setters.h
+++ src/sink/GEAR/sink_setters.h
@@ -47,4 +47,62 @@ sink_set_sink_birth_time_or_scale_factor(
     sink->birth_time = birth_time;
   }
 }
+
+/**
+ * @brief Update the target mass of the sink particle.
+ *
+ * @param sink the sink particle.
+ * @param sink_props the sink properties to use.
+ * @param phys_const the physical constants in internal units.
+ * @param e The #engine
+ * @param star_counter The star loop counter.
+ */
+INLINE static void sink_update_target_mass(struct sink* sink,
+                                           const struct sink_props* sink_props,
+                                           const struct engine* e,
+                                           int star_counter) {
+
+  float random_number = random_unit_interval_part_ID_and_index(
+      sink->id, star_counter, e->ti_current, random_number_sink_formation);
+
+  const struct feedback_props* feedback_props = e->feedback_props;
+
+  /* Pick the correct table. (if only one table, threshold is < 0) */
+  const float metal =
+      chemistry_get_sink_total_iron_mass_fraction_for_feedback(sink);
+  const float threshold = feedback_props->metallicity_max_first_stars;
+
+  /* If metal < threshold, then the sink generates first star particles. */
+  const int is_first_star = metal < threshold;
+  const struct stellar_model* model;
+  double minimal_discrete_mass_Msun;
+
+  /* Take the correct values if your are a first star or not. */
+  if (!is_first_star) /* (metal >= threshold)*/ {
+    model = &feedback_props->stellar_model;
+    minimal_discrete_mass_Msun = sink_props->minimal_discrete_mass_Msun;
+  } else {
+    model = &feedback_props->stellar_model_first_stars;
+    minimal_discrete_mass_Msun =
+        sink_props->minimal_discrete_mass_first_stars_Msun;
+  }
+
+  const struct initial_mass_function* imf = &model->imf;
+
+  if (random_number < imf->sink_Pc) {
+    /* We are dealing with the continous part of the IMF. */
+    sink->target_mass_Msun = imf->stellar_particle_mass_Msun;
+    sink->target_type = star_population_continuous_IMF;
+  } else {
+    /* We are dealing with the discrete part of the IMF. */
+    random_number = random_unit_interval_part_ID_and_index(
+        sink->id, star_counter + 1, e->ti_current,
+        random_number_sink_formation);
+    double m = initial_mass_function_sample_power_law(
+        minimal_discrete_mass_Msun, imf->mass_max, imf->exp[imf->n_parts - 1],
+        random_number);
+    sink->target_mass_Msun = m;
+    sink->target_type = single_star;
+  }
+}
 #endif /* SWIFT_GEAR_SINK_SETTERS_H */
