d338b4d16bfc8b04e945dcee1eba1cdcfa60889b
wjr21 <wjr21@sussex.ac.uk>
2024-11-13 20:31:32 +0000

    Handling splitting buffer void cells separately

--- src/zoom_region/zoom_split.c
+++ src/zoom_region/zoom_split.c
@@ -28,6 +28,282 @@
 #include "zoom.h"
 
 /**
+ * @brief Link the top level cells in zoom grid to a void parent cell.
+ *
+ * The leaves of the void cell hierarchy are top level cells in the nested grid.
+ * This function sets the progeny of the highest res void cell to be the top
+ * level cell it contains (either zoom or buffer cells).
+ *
+ * NOTE: The void cells with top level progeny are not treated as split cells
+ * since they are linked into the top level "progeny". We don't want to
+ * accidentally treat them as split cells and recurse from void cells straight
+ * through to the zoom cells unless explictly desired.
+ *
+ * @param s The space.
+ * @param c The void cell progeny to link
+ */
+static void zoom_link_void_zoom_leaves(struct space *s, struct cell *c) {
+
+#ifdef SWIFT_DEBUG_CHECKS
+  /* Ensure we have the right kind of cell. */
+  if (c->subtype != cell_subtype_void) {
+    error(
+        "Trying to split cell which isn't a void cell! (c->type=%s, "
+        "c->subtype=%s)",
+        cellID_names[c->type], subcellID_names[c->subtype]);
+  }
+
+  /* Check that the widths are right. */
+  if (fabs((c->width[0] / 2) - s->zoom_props->width[0]) >
+      (0.001 * s->zoom_props->width[0]))
+    error(
+        "The width of the zoom cell is not half the width of the void "
+        "cell were about to link to! (c->width[0]=%f, "
+        "s->zoom_props->width[0]=%f)",
+        c->width[0] / 2, s->zoom_props->width[0]);
+
+#endif
+
+  /* We need to ensure this bottom level isn't treated like a
+   * normal split cell since it's linked into top level "progeny". */
+  c->split = 0;
+
+  /* Initialise the timestep information we need to collect. */
+  integertime_t ti_hydro_end_min = max_nr_timesteps, ti_hydro_beg_max = 0;
+  integertime_t ti_rt_end_min = max_nr_timesteps, ti_rt_beg_max = 0;
+  integertime_t ti_rt_min_step_size = max_nr_timesteps;
+  integertime_t ti_gravity_end_min = max_nr_timesteps, ti_gravity_beg_max = 0;
+  integertime_t ti_stars_end_min = max_nr_timesteps, ti_stars_beg_max = 0;
+  integertime_t ti_sinks_end_min = max_nr_timesteps, ti_sinks_beg_max = 0;
+  integertime_t ti_black_holes_end_min = max_nr_timesteps,
+                ti_black_holes_beg_max = 0;
+
+  /* Loop over the 8 progeny cells which are now the nested top level cells. */
+  for (int k = 0; k < 8; k++) {
+
+    /* Establish the location of the fake progeny cell. */
+    double loc[3] = {c->loc[0] + (c->width[0] / 4),
+                     c->loc[1] + (c->width[1] / 4),
+                     c->loc[2] + (c->width[2] / 4)};
+    if (k & 4) loc[0] += c->width[0] / 2;
+    if (k & 2) loc[1] += c->width[1] / 2;
+    if (k & 1) loc[2] += c->width[2] / 2;
+
+    /* Which cell are we in? */
+    int cid = cell_getid_from_pos(s, loc[0], loc[1], loc[2]);
+
+    /* Get the zoom cell. */
+    struct cell *zoom_cell = &s->cells_top[cid];
+
+    /* Link this nested cell into the void cell hierarchy. */
+    c->progeny[k] = zoom_cell;
+
+    /* Flag this void cell "progeny" as the cell's void cell parent. */
+    zoom_cell->void_parent = c;
+
+    /* Update the timestep information. */
+    ti_hydro_end_min = min(ti_hydro_end_min, zoom_cell->hydro.ti_end_min);
+    ti_hydro_beg_max = max(ti_hydro_beg_max, zoom_cell->hydro.ti_beg_max);
+    ti_rt_end_min = min(ti_rt_end_min, zoom_cell->rt.ti_rt_end_min);
+    ti_rt_beg_max = max(ti_rt_beg_max, zoom_cell->rt.ti_rt_beg_max);
+    ti_rt_min_step_size =
+        min(ti_rt_min_step_size, zoom_cell->rt.ti_rt_min_step_size);
+    ti_gravity_end_min = min(ti_gravity_end_min, zoom_cell->grav.ti_end_min);
+    ti_gravity_beg_max = max(ti_gravity_beg_max, zoom_cell->grav.ti_beg_max);
+    ti_stars_end_min = min(ti_stars_end_min, zoom_cell->stars.ti_end_min);
+    ti_stars_beg_max = max(ti_stars_beg_max, zoom_cell->stars.ti_beg_max);
+    ti_sinks_end_min = min(ti_sinks_end_min, zoom_cell->sinks.ti_end_min);
+    ti_sinks_beg_max = max(ti_sinks_beg_max, zoom_cell->sinks.ti_beg_max);
+    ti_black_holes_end_min =
+        min(ti_black_holes_end_min, zoom_cell->black_holes.ti_end_min);
+    ti_black_holes_beg_max =
+        max(ti_black_holes_beg_max, zoom_cell->black_holes.ti_beg_max);
+  }
+
+  /* Update the timestep information. */
+  c->hydro.ti_end_min = ti_hydro_end_min;
+  c->hydro.ti_beg_max = ti_hydro_beg_max;
+  c->rt.ti_rt_end_min = ti_rt_end_min;
+  c->rt.ti_rt_beg_max = ti_rt_beg_max;
+  c->rt.ti_rt_min_step_size = ti_rt_min_step_size;
+  c->grav.ti_end_min = ti_gravity_end_min;
+  c->grav.ti_beg_max = ti_gravity_beg_max;
+  c->stars.ti_end_min = ti_stars_end_min;
+  c->stars.ti_beg_max = ti_stars_beg_max;
+  c->sinks.ti_end_min = ti_sinks_end_min;
+  c->sinks.ti_beg_max = ti_sinks_beg_max;
+  c->black_holes.ti_end_min = ti_black_holes_end_min;
+  c->black_holes.ti_beg_max = ti_black_holes_beg_max;
+
+  /* Interact the nested cell's multipoles with this cell. */
+  if (s->with_self_gravity) {
+    space_populate_multipole(zoom_cell);
+  }
+
+#ifdef SWIFT_DEBUG_CHECKS
+  /* Ensure the void multipole agrees with the nested cells. */
+  int nr_gparts_in_nested = 0;
+  int nr_gparts_in_void = c->grav.multipole->m_pole.num_gpart;
+  for (int k = 0; k < 8; k++) {
+    /* All progeny should exist */
+    if (c->progeny[k] == NULL) {
+      error("Zoom cell has no progeny!");
+    }
+    nr_gparts_in_nested += c->progeny[k]->grav.multipole->m_pole.num_gpart;
+  }
+
+  if (nr_gparts_in_nested != nr_gparts_in_void) {
+    error(
+        "Nested cell and void cell multipole don't agree! "
+        "(nr_gparts_in_nested=%d nr_gparts_in_void=%d)",
+        nr_gparts_in_nested, nr_gparts_in_void);
+  }
+#endif
+}
+
+/**
+ * @brief Link the top level cells in buffer grid to a void parent cell.
+ *
+ * The leaves of the void cell hierarchy are top level cells in the nested grid.
+ * This function sets the progeny of the highest res void cell to be the top
+ * level cell it contains (either zoom or buffer cells).
+ *
+ * NOTE: The void cells with top level progeny are not treated as split cells
+ * since they are linked into the top level "progeny". We don't want to
+ * accidentally treat them as split cells and recurse from void cells straight
+ * through to the zoom cells unless explictly desired.
+ *
+ * @param s The space.
+ * @param c The void cell progeny to link
+ */
+void zoom_link_void_buffer_leaves(struct space *s, struct cell *c) {
+
+#ifdef SWIFT_DEBUG_CHECKS
+  /* Ensure we have the right kind of cell. */
+  if (c->subtype != cell_subtype_void) {
+    error(
+        "Trying to split cell which isn't a void cell! (c->type=%s, "
+        "c->subtype=%s)",
+        cellID_names[c->type], subcellID_names[c->subtype]);
+  }
+
+  /* Check that the widths are right. */
+  if (fabs((c->width[0] / 2) - s->zoom_props->buffer_width[0]) >
+      (0.001 * s->zoom_props->buffer_width[0]))
+    error(
+        "The width of the buffer cell is not half the width of the void "
+        "cell were about to link to! (c->width[0]=%f, "
+        "s->zoom_props->width[0]=%f)",
+        c->width[0] / 2, s->zoom_props->width[0]);
+
+#endif
+
+  /* If we are above regular buffer cells we need to ensure this bottom level
+   * isn't treated like a normal split cell since it's linked into top level
+   * "progeny". */
+  if (!zoom_cell_overlaps_zoom_region(c, s)) {
+    c->split = 0;
+  }
+
+  /* Initialise the timestep information we need to collect. */
+  integertime_t ti_hydro_end_min = max_nr_timesteps, ti_hydro_beg_max = 0;
+  integertime_t ti_rt_end_min = max_nr_timesteps, ti_rt_beg_max = 0;
+  integertime_t ti_rt_min_step_size = max_nr_timesteps;
+  integertime_t ti_gravity_end_min = max_nr_timesteps, ti_gravity_beg_max = 0;
+  integertime_t ti_stars_end_min = max_nr_timesteps, ti_stars_beg_max = 0;
+  integertime_t ti_sinks_end_min = max_nr_timesteps, ti_sinks_beg_max = 0;
+  integertime_t ti_black_holes_end_min = max_nr_timesteps,
+                ti_black_holes_beg_max = 0;
+
+  /* Loop over the 8 progeny cells which are now the nested top level cells. */
+  for (int k = 0; k < 8; k++) {
+
+    /* Establish the location of the fake progeny cell. */
+    double loc[3] = {c->loc[0] + (c->width[0] / 4),
+                     c->loc[1] + (c->width[1] / 4),
+                     c->loc[2] + (c->width[2] / 4)};
+    if (k & 4) loc[0] += c->width[0] / 2;
+    if (k & 2) loc[1] += c->width[1] / 2;
+    if (k & 1) loc[2] += c->width[2] / 2;
+
+    /* Which cell are we in? */
+    int cid = cell_getid_from_pos(s, loc[0], loc[1], loc[2]);
+
+    /* Get the zoom cell. */
+    struct cell *buffer_cell = &s->cells_top[cid];
+
+    /* Link this nested cell into the void cell hierarchy. */
+    c->progeny[k] = buffer_cell;
+
+    /* Flag this void cell "progeny" as the cell's void cell parent. */
+    buffer_cell->void_parent = c;
+
+    /* If we are in a void buffer cell we need to continue recursing. */
+    if (buffer_cell->subtype == cell_subtype_void) {
+      zoom_void_split_recursive(s, buffer_cell, c->tpid);
+    }
+
+    /* Update the timestep information. */
+    ti_hydro_end_min = min(ti_hydro_end_min, buffer_cell->hydro.ti_end_min);
+    ti_hydro_beg_max = max(ti_hydro_beg_max, buffer_cell->hydro.ti_beg_max);
+    ti_rt_end_min = min(ti_rt_end_min, buffer_cell->rt.ti_rt_end_min);
+    ti_rt_beg_max = max(ti_rt_beg_max, buffer_cell->rt.ti_rt_beg_max);
+    ti_rt_min_step_size =
+        min(ti_rt_min_step_size, buffer_cell->rt.ti_rt_min_step_size);
+    ti_gravity_end_min = min(ti_gravity_end_min, buffer_cell->grav.ti_end_min);
+    ti_gravity_beg_max = max(ti_gravity_beg_max, buffer_cell->grav.ti_beg_max);
+    ti_stars_end_min = min(ti_stars_end_min, buffer_cell->stars.ti_end_min);
+    ti_stars_beg_max = max(ti_stars_beg_max, buffer_cell->stars.ti_beg_max);
+    ti_sinks_end_min = min(ti_sinks_end_min, buffer_cell->sinks.ti_end_min);
+    ti_sinks_beg_max = max(ti_sinks_beg_max, buffer_cell->sinks.ti_beg_max);
+    ti_black_holes_end_min =
+        min(ti_black_holes_end_min, buffer_cell->black_holes.ti_end_min);
+    ti_black_holes_beg_max =
+        max(ti_black_holes_beg_max, buffer_cell->black_holes.ti_beg_max);
+  }
+
+  /* Update the timestep information. */
+  c->hydro.ti_end_min = ti_hydro_end_min;
+  c->hydro.ti_beg_max = ti_hydro_beg_max;
+  c->rt.ti_rt_end_min = ti_rt_end_min;
+  c->rt.ti_rt_beg_max = ti_rt_beg_max;
+  c->rt.ti_rt_min_step_size = ti_rt_min_step_size;
+  c->grav.ti_end_min = ti_gravity_end_min;
+  c->grav.ti_beg_max = ti_gravity_beg_max;
+  c->stars.ti_end_min = ti_stars_end_min;
+  c->stars.ti_beg_max = ti_stars_beg_max;
+  c->sinks.ti_end_min = ti_sinks_end_min;
+  c->sinks.ti_beg_max = ti_sinks_beg_max;
+  c->black_holes.ti_end_min = ti_black_holes_end_min;
+  c->black_holes.ti_beg_max = ti_black_holes_beg_max;
+
+  /* Interact the nested cell's multipoles with this cell. */
+  if (s->with_self_gravity) {
+    space_populate_multipole(buffer_cell);
+  }
+
+#ifdef SWIFT_DEBUG_CHECKS
+  /* Ensure the void multipole agrees with the nested cells. */
+  int nr_gparts_in_nested = 0;
+  int nr_gparts_in_void = c->grav.multipole->m_pole.num_gpart;
+  for (int k = 0; k < 8; k++) {
+    /* All progeny should exist */
+    if (c->progeny[k] == NULL) {
+      error("Zoom cell has no progeny!");
+    }
+    nr_gparts_in_nested += c->progeny[k]->grav.multipole->m_pole.num_gpart;
+  }
+
+  if (nr_gparts_in_nested != nr_gparts_in_void) {
+    error(
+        "Nested cell and void cell multipole don't agree! "
+        "(nr_gparts_in_nested=%d nr_gparts_in_void=%d)",
+        nr_gparts_in_nested, nr_gparts_in_void);
+  }
+#endif
+}
+
+/**
  * @brief Recursively split a cell.
  *
  * @param s The #space in which the cell lives.
@@ -61,6 +337,17 @@ void zoom_void_split_recursive(struct space *s, struct cell *c,
         "Trying to split a Void with particles! "
         "(c->type=%s, c->subtype=%s)",
         cellID_names[c->type], subcellID_names[c->subtype]);
+
+  /* Ensure we haven't missed the zoom cells and gone too deep in the tree. */
+  if (c->subtype != cell_subtype_void || c->width[0] <= s->zoom_props->width[0])
+    error(
+        "We have a zoom cell labelled as a void cell! We have gone too "
+        "deep in the zoom cell tree, this could be because background "
+        "cells are comprable in size to the zoom cells."
+        " (cp->type=%s, cp->subtype=%s, cp->width[0]=%f, cp->depth=%d,"
+        " s->zoom_props->width[0]=%f, zoom_props->zoom_cell_depth=%d)",
+        cellID_names[cp->type], subcellID_names[cp->subtype], cp->width[0],
+        cp->depth, s->zoom_props->width[0], s->zoom_props->zoom_cell_depth);
 #endif
 
   /* If the depth is too large, we have a problem and should stop. */
@@ -83,47 +370,17 @@ void zoom_void_split_recursive(struct space *s, struct cell *c,
     /* Get the progenitor */
     struct cell *cp = c->progeny[k];
 
-#ifdef SWIFT_DEBUG_CHECKS
-    if (cp->subtype == cell_subtype_void &&
-        cp->width[0] <= s->zoom_props->width[0])
-      error(
-          "We have a zoom cell labelled as a void cell! We have gone too "
-          "deep in the zoom cell tree, this could be because background "
-          "cells are comprable in size to the zoom cells."
-          " (cp->type=%s, cp->subtype=%s, cp->width[0]=%f, cp->depth=%d,"
-          " s->zoom_props->width[0]=%f, zoom_props->zoom_cell_depth=%d)",
-          cellID_names[cp->type], subcellID_names[cp->subtype], cp->width[0],
-          cp->depth, s->zoom_props->width[0], s->zoom_props->zoom_cell_depth);
-#endif
-
     /* If the next level progeny is at the zoom level then we need to
      * link the zoom cells in as the progeny of the void sub-cell. */
     if (cp->depth == s->zoom_props->zoom_cell_depth - 1) {
 
-#ifdef SWIFT_DEBUG_CHECKS
-      /* Check that the widths are right. */
-      if (fabs((cp->width[0] / 2) - s->zoom_props->width[0]) >
-          (0.001 * s->zoom_props->width[0]))
-        error(
-            "The width of the zoom cell is not half the width of the void "
-            "cell were about to link to! (cp->width[0]=%f, "
-            "s->zoom_props->width[0]=%f)",
-            cp->width[0] / 2, s->zoom_props->width[0]);
-#endif
-
-      zoom_link_void_leaves(s, cp);
+      zoom_link_void_zoom_leaves(s, cp);
 
     } else if (s->zoom_props->with_buffer_cells &&
-               cp->depth < s->zoom_props->buffer_cell_depth - 1) {
+               cp->depth == s->zoom_props->buffer_cell_depth - 1) {
 
       /* We're at the buffer level so we need to hook in the buffer cells. */
-      zoom_link_void_leaves(s, cp);
-
-      /* Recurse */
-      zoom_void_split_recursive(s, cp, tpid);
-
-      /* Increase the depth */
-      maxdepth = max(maxdepth, cp->maxdepth);
+      zoom_link_void_buffer_leaves(s, cp);
 
     } else {
       /* Recurse */
