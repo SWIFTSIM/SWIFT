e833cdf0292af69658041d4df999f36fe926a92d
wjr21 <wjr21@sussex.ac.uk>
2024-11-13 21:11:42 +0000

    Typo

--- src/zoom_region/zoom_split.c
+++ src/zoom_region/zoom_split.c
@@ -234,7 +234,7 @@ void zoom_link_void_buffer_leaves(struct space *s, struct cell *c) {
     int cid = cell_getid_below_bkg(s->zoom_props->buffer_cdim,
                                    s->zoom_props->buffer_lower_bounds, loc[0],
                                    loc[1], loc[2], s->zoom_props->buffer_iwidth,
-                                   z->zoom_props->buffer_cell_offset);
+                                   s->zoom_props->buffer_cell_offset);
 
     /* Get the zoom cell. */
     struct cell *buffer_cell = &s->cells_top[cid];
