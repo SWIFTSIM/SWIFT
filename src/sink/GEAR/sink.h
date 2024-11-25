1fc8bab23f6406a00516b3daaee05d531e405400
Darwin Roduit <darwin.roduit@epfl.ch>
2024-11-25 21:55:51 +0100

    Do not spawn stars if the sink is has mass < minimal sink mass

--- src/sink/GEAR/sink.h
+++ src/sink/GEAR/sink.h
@@ -775,7 +775,16 @@ INLINE static int sink_spawn_star(struct sink* sink, const struct engine* e,
                                   const struct phys_const* phys_const,
                                   const struct unit_system* restrict us) {
 
-  if (sink->mass > sink->target_mass_Msun * phys_const->const_solar_mass)
+  /* To spawn a star, the sink must:
+     1) have a mass exceeding the star's mass,
+     2) and have a mass exceeding the minimal mass.
+     The second condition is relevant for low resolution simulations, where a
+     new born sink can already spawn stars. After spawning the stars, the sink
+     has a mass << gas mass and can get kicked off by gravitational
+     interactions. Also, if the sink's mass << gas' mass, the gas is never bound
+     to the sink and is thus never accreted. */
+  if (sink->mass > sink->target_mass_Msun * phys_const->const_solar_mass
+      && sink->mass > sink_props->sink_minimal_mass_Msun)
     return 1;
   else
     return 0;
