9771d07a0125ca42bec026e86aab2be01b95717f
Darwin Roduit <darwin.roduit@unige.ch>
2025-01-14 10:18:29 +0100

    Add e->chemistry to kick_part and propagate the changes

--- src/chemistry_additions.h
+++ src/chemistry_additions.h
@@ -64,7 +64,8 @@
 __attribute__((always_inline)) INLINE static void chemistry_kick_extra(
     struct part* p, float dt_therm, float dt_grav, float dt_hydro,
     float dt_kick_corr, const struct cosmology* cosmo,
-    const struct hydro_props* hydro_props) {}
+    const struct hydro_props* hydro_props,
+    const struct chemistry_global_data* chem_dat) {}
 
 /**
  * @brief update metal mass fluxes between two interacting particles during
