fix a bug + unit conversion improve the exampe

examples/GravityTests/MWPotential2014_df/makeIC.py
@@ -24,9 +24,9 @@ N_PARTICLES = 1
 print("Initial conditions written to 'IC.hdf5'")
 
 pos = np.zeros((1, 3))
-pos[0, 0] = 300
-pos[0, 1] = 0
-pos[0, 2] = 0
+pos[0, 0] = 0.1
+pos[0, 1] = 0.1
+pos[0, 2] = 100
 
 
 # pos += np.array(
@@ -34,9 +34,9 @@ pos[0, 2] = 0
 # )  # shifts the particles to the center of the box
 
 vel = np.zeros((1, 3))
-vel[0, 0] = 0
-vel[0, 1] = 100
-vel[0, 2] = 10
+vel[0, 0] = 80
+vel[0, 1] = 0
+vel[0, 2] = 0
 
 
 ids = np.array([1.0])