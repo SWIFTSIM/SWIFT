5c47d5a0eff27c8064698d4af29dc8c589faba33
Darwin Roduit <darwin.roduit@unige.ch>
2025-03-02 15:39:14 +0100

    Add hyperbolic diffusion for the sparts.sf_data output

--- src/star_formation/GEAR/star_formation_struct.h
+++ src/star_formation/GEAR/star_formation_struct.h
@@ -57,6 +57,11 @@ struct star_formation_spart_data {
 
   /*! The progenitor ID */
   long long progenitor_id;
+
+#if defined(CHEMISTRY_GEAR_MF_DIFFUSION) || defined(CHEMISTRY_GEAR_MF_HYPERBOLIC_DIFFUSION)
+  float norm_matrix_K;
+#endif
+
 };
 
 /**
