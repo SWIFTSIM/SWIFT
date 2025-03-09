be894c241adfa243387bf16c7d7646ddaa7fadaa
Darwin Roduit <darwin.roduit@unige.ch>
2025-03-09 13:28:51 +0100

    Add cosmology example for the HomogeneousBox chemistry test

--- examples/ChemistryTests/HomogeneousBox/run.sh
+++ examples/ChemistryTests/HomogeneousBox/run.sh
@@ -5,13 +5,19 @@ set -e
 
 n_threads=8 #Number of threads to use
 level=5 #Number of particles = 2^(3*level)
-jeans_length=0.250 #Jeans wavelenght in unit of the boxsize
+jeans_length=${jeans_length:=0.250}  #Jeans wavelenght in unit of the boxsize
+rho=${rho:=0.1} # Gas density
+run_name=${run_name:=""}
+with_cosmo=${with_cosmo:=0}
 
 #Create the ICs if they do not exist
 if [ ! -e ICs_homogeneous_box.hdf5 ]
 then
     echo "Generating initial conditions to run the example..."
-    python3 makeIC.py --level $level -o ICs_homogeneous_box.hdf5 --lJ $jeans_length
+    python3 makeIC.py --level $level -o ICs_homogeneous_box.hdf5 \
+	    --lJ $jeans_length --rho $rho
+else
+    rm ICs_homogeneous_box.hdf5
 fi
 
 # Get the Grackle cooling table
@@ -42,16 +48,35 @@ fi
 
 printf "Running simulation..."
 
+if [ "$with_cosmo" -eq 1 ]; then
+../../../swift --hydro --star-formation --stars --self-gravity --feedback \
+	       --cooling --sync --limiter --cosmology \
+	       --threads=$n_threads params_cosmology.yml 2>&1 | tee output.log
+else
 ../../../swift --hydro --star-formation --stars --self-gravity --feedback \
 	       --cooling --sync --limiter \
 	       --threads=$n_threads params.yml 2>&1 | tee output.log
-
-# ../../../swift --hydro --stars --self-gravity --cooling --feedback \
-	       # --sync --limiter \
-	       # --threads=$n_threads params.yml 2>&1 | tee output.log
+fi
 
 #Do some data analysis to show what's in this box
 python3 plot_metal_mass_conservation_in_time.py snap/*.hdf5 --symlog
 python3 metal_projection.py snap/snapshot_*0.hdf5 --vmin=-20 --vmax=-3 --log
 python3 plot_projected_qty.py --qty "internal_energy" snap/snapshot_*0.hdf5 --log
 python3 plot_projected_qty.py --qty "pressure" snap/snapshot_*0.hdf5 --log
+
+if [ -z "$run_name" ]; then
+    echo "run_name is empty."
+else
+    if [ -d "$run_name" ]; then
+	echo "$run_name directory exists. Nothing will be moved."
+    else
+	echo "$run_name directory does not exists. It will be created."
+	mkdir $run_name
+	mv timesteps.txt $run_name
+	mv snap $run_name
+	mv unused_parameters.yml $run_name
+	mv used_parameters.yml $run_name
+	mv *.png $run_name
+	mv output.log $run_name
+    fi
+fi
