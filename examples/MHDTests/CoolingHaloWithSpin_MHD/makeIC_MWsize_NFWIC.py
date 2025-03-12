b5382e6f98bf398478bc4f7e35557c2956f285f0
nickishch <nickishch@gmail.com>
2025-03-12 02:05:25 +0100

    first attepmt for NFW initial halo MW size

---
+++ examples/MHDTests/CoolingHaloWithSpin_MHD/makeIC_MWsize_NFWIC.py
@@ -0,0 +1,298 @@
+###############################################################################
+# This file is part of SWIFT.
+# Copyright (c) 2016 Stefan Arridge (stefan.arridge@durham.ac.uk)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+# Halo parameters similar to R. Pakmor, V. Springel, 1212.1452
+
+import h5py
+import sys
+import numpy as np
+import math
+import random
+
+# Generates N particles in a spherically symmetric distribution with density profile ~r^(-2)
+# usage: python3 makeIC.py 1000: generate 1000 particles
+
+# Some constants cgs
+PARSEC_IN_CGS = 3.0856776e18
+KM_PER_SEC_IN_CGS = 1.0e5
+CONST_G_CGS = 6.672e-8
+CONST_MU0_CGS = 4 * np.pi * 1e-2
+MSOL_IN_CGS = 1.9891e33 # Solar mass 
+# First set unit velocity and then the circular velocity parameter for the isothermal potential
+const_unit_velocity_in_cgs = 1.0e5  # kms^-1
+
+# Cosmological parameters
+OMEGA = 0.3  # Cosmological matter fraction at z = 0
+h = 0.67777  # hubble parameter
+# Find H_0, the inverse Hubble time, in cgs
+H_0_cgs = 100.0 * h * KM_PER_SEC_IN_CGS / (1.0e6 * PARSEC_IN_CGS)
+
+# Gas parameters
+gamma = 5.0 / 3.0
+eta = 1.3663 # kernel smoothing
+
+# DM halo parameters
+spin_lambda = 0.05  # spin parameter
+f_b = 0.17  # baryon fraction
+
+# Set the magnitude of the uniform seed magnetic field
+B0_Gaussian_Units = 1e-9 #1e-6  # 1 micro Gauss
+B0_cgs = np.sqrt(CONST_MU0_CGS / (4.0 * np.pi)) * B0_Gaussian_Units
+
+# SPH
+eta = 1.3663 # kernel smoothing
+
+# From this we can find the virial radius, the radius within which the average density of the halo is
+# 200. * the mean matter density
+
+# Set M200 and get R200 and V200
+M_200_cgs = 1e12 * MSOL_IN_CGS 
+rhoc_cgs = 3*H_0_cgs**2/(8*np.pi*CONST_G_CGS)
+r_200_cgs = (3*M_200_cgs/(4*np.pi*rhoc_cgs))**(1/3)
+v_200_cgs = np.sqrt(CONST_G_CGS*M_200_cgs/r_200_cgs)
+v_200 = v_200_cgs / const_unit_velocity_in_cgs 
+
+# Now set the unit length and mass
+const_unit_mass_in_cgs = M_200_cgs
+const_unit_length_in_cgs = r_200_cgs
+print("UnitMass_in_cgs:     ", const_unit_mass_in_cgs)
+print("UnitLength_in_cgs:   ", const_unit_length_in_cgs)
+print("UnitVelocity_in_cgs: ", const_unit_velocity_in_cgs)
+
+# Now set the magnetic field unit
+const_unit_magnetic_field_in_cgs = 1e-7  # 1muG
+
+# Derived quantities
+const_unit_time_in_cgs = const_unit_length_in_cgs / const_unit_velocity_in_cgs
+const_unit_current_in_cgs = const_unit_mass_in_cgs / (
+    const_unit_magnetic_field_in_cgs * const_unit_time_in_cgs ** 2
+)
+print("UnitTime_in_cgs:     ", const_unit_time_in_cgs)
+print("UnitCurrent_in_cgs:  ", const_unit_current_in_cgs)
+const_G = (
+    CONST_G_CGS
+    * const_unit_mass_in_cgs
+    * const_unit_time_in_cgs
+    * const_unit_time_in_cgs
+    / (const_unit_length_in_cgs * const_unit_length_in_cgs * const_unit_length_in_cgs)
+)
+print("G=", const_G)
+
+# Parameters
+periodic = 1  # 1 For periodic box
+boxSize = 4.0
+G = const_G
+N = int(sys.argv[1])  # Number of particles
+
+# Create the file
+filename = "CoolingHalo.hdf5"
+file = h5py.File(filename, "w")
+
+# Units
+grp = file.create_group("/Units")
+grp.attrs["Unit length in cgs (U_L)"] = const_unit_length_in_cgs
+grp.attrs["Unit mass in cgs (U_M)"] = const_unit_mass_in_cgs
+grp.attrs["Unit time in cgs (U_t)"] = (
+    const_unit_length_in_cgs / const_unit_velocity_in_cgs
+)
+grp.attrs["Unit current in cgs (U_I)"] = const_unit_current_in_cgs
+grp.attrs["Unit temperature in cgs (U_T)"] = 1.0
+
+# We define NFW profile in CGS
+rmin = 1e-6 * boxSize
+r = np.linspace(rmin, boxSize*np.sqrt(3)/2, N)
+
+def phi_grav(r_value, c_200=7.2):
+    phi_pref = CONST_G_CGS*M_200_cgs/(np.log(1+c_200)-c_200/(1+c_200))/r_200_cgs
+    return -phi_pref/r_value*np.log(1+r_value*c_200)
+
+# Energy distribution
+u_func = -phi_grav(r)/gamma
+
+# Find density and radial particle distribution
+rho_func_unnorm = u_func**(1/(gamma-1)) 
+f_r_distr_func = rho_func_unnorm * r**2 
+
+# Normalize f(r) to get a probability density function (PDF)
+pdf = f_r_distr_func / np.trapezoid(f_r_distr_func, r)  # Normalize with trapezoidal integration
+# Compute CDF
+cdf = np.cumsum(pdf * np.diff(np.concatenate(([0], r))))  # Cumulative sum approximation
+# set seed for random number
+np.random.seed(1234)
+# Generate N uniform random numbers in [0,1]
+uniform_samples = np.random.rand(N)
+# Use inverse transform sampling (interpolate CDF to find r values)
+sampled_r_values = np.interp(uniform_samples, cdf, r)
+
+# Positions
+# r^(-2) distribution corresponds to uniform distribution in radius
+radius = (
+      sampled_r_values # np.random.rand(N)
+)  # the diagonal extent of the cube
+ctheta = -1.0 + 2 * np.random.rand(N)
+stheta = np.sqrt(1.0 - ctheta ** 2)
+phi = 2 * math.pi * np.random.rand(N)
+coords = np.zeros((N, 3))
+coords[:, 0] = radius * stheta * np.cos(phi)
+coords[:, 1] = radius * stheta * np.sin(phi)
+coords[:, 2] = radius * ctheta
+
+# shift to centre of box
+coords += np.full((N, 3), boxSize / 2.0)
+print("x range = (%f,%f)" % (np.min(coords[:, 0]), np.max(coords[:, 0])))
+print("y range = (%f,%f)" % (np.min(coords[:, 1]), np.max(coords[:, 1])))
+print("z range = (%f,%f)" % (np.min(coords[:, 2]), np.max(coords[:, 2])))
+
+print(np.mean(coords[:, 0]))
+print(np.mean(coords[:, 1]))
+print(np.mean(coords[:, 2]))
+
+# now find the particles which are within the box
+
+x_coords = coords[:, 0]
+y_coords = coords[:, 1]
+z_coords = coords[:, 2]
+
+ind = np.where(x_coords < boxSize)[0]
+x_coords = x_coords[ind]
+y_coords = y_coords[ind]
+z_coords = z_coords[ind]
+
+ind = np.where(x_coords > 0.0)[0]
+x_coords = x_coords[ind]
+y_coords = y_coords[ind]
+z_coords = z_coords[ind]
+
+ind = np.where(y_coords < boxSize)[0]
+x_coords = x_coords[ind]
+y_coords = y_coords[ind]
+z_coords = z_coords[ind]
+
+ind = np.where(y_coords > 0.0)[0]
+x_coords = x_coords[ind]
+y_coords = y_coords[ind]
+z_coords = z_coords[ind]
+
+ind = np.where(z_coords < boxSize)[0]
+x_coords = x_coords[ind]
+y_coords = y_coords[ind]
+z_coords = z_coords[ind]
+
+ind = np.where(z_coords > 0.0)[0]
+x_coords = x_coords[ind]
+y_coords = y_coords[ind]
+z_coords = z_coords[ind]
+
+# count number of particles
+
+N = x_coords.size
+
+print("Number of particles in the box = ", N)
+
+gas_mass = (
+    f_b # np.sqrt(3.0) / 2.0
+)  # virial mass of halo is 1, virial radius is 1, enclosed mass scales with r
+gas_particle_mass = gas_mass / float(N)
+
+# make the coords and radius arrays again
+coords = np.zeros((N, 3))
+coords[:, 0] = x_coords
+coords[:, 1] = y_coords
+coords[:, 2] = z_coords
+
+radius = np.sqrt(
+    (coords[:, 0] - boxSize / 2.0) ** 2
+    + (coords[:, 1] - boxSize / 2.0) ** 2
+    + (coords[:, 2] - boxSize / 2.0) ** 2
+)
+
+# now give particle's velocities and magnetic fields
+v = np.zeros((N, 3))
+B = np.zeros((N, 3))
+
+# first work out total angular momentum of the halo within the virial radius
+# we work in units where r_vir = 1 and M_vir = 1
+Total_E = v_200 ** 2 / 2.0
+J = spin_lambda * const_G / np.sqrt(Total_E)
+print("J =", J)
+# all particles within the virial radius have omega parallel to the z-axis, magnitude
+# is proportional to 1 over the radius
+omega = np.zeros((N, 3))
+for i in range(N):
+    omega[i, 2] = 3.0 * J / radius[i]
+    v[i, :] = np.cross(omega[i, :], (coords[i, :] - boxSize / 2.0))
+    #B[i, 2] = B0_cgs / const_unit_magnetic_field_in_cgs
+    B[i, 0] = B0_cgs / const_unit_magnetic_field_in_cgs
+
+# Header
+grp = file.create_group("/Header")
+grp.attrs["BoxSize"] = boxSize
+grp.attrs["NumPart_Total"] = [N, 0, 0, 0, 0, 0]
+grp.attrs["NumPart_Total_HighWord"] = [0, 0, 0, 0, 0, 0]
+grp.attrs["NumPart_ThisFile"] = [N, 0, 0, 0, 0, 0]
+grp.attrs["Time"] = 0.0
+grp.attrs["NumFilesPerSnapshot"] = 1
+grp.attrs["MassTable"] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+grp.attrs["Flag_Entropy_ICs"] = [0, 0, 0, 0, 0, 0]
+grp.attrs["Dimension"] = 3
+
+# Particle group
+grp = file.create_group("/PartType0")
+
+ds = grp.create_dataset("Coordinates", (N, 3), "d")
+ds[()] = coords
+coords = np.zeros(1)
+
+ds = grp.create_dataset("Velocities", (N, 3), "f")
+ds[()] = v
+v = np.zeros(1)
+
+# Magnetic field
+ds = grp.create_dataset("MagneticFluxDensities", (N, 3), "f")
+ds[()] = B
+B = np.zeros(1)
+
+# All particles of equal mass
+m = np.full((N,), gas_particle_mass)
+ds = grp.create_dataset("Masses", (N,), "f")
+ds[()] = m
+m = np.zeros(1)
+
+# Smoothing lengths
+l = (4.0 * np.pi * radius ** 2 / N) ** (
+    1.0 / 3.0
+)  # local mean inter-particle separation
+h = np.full((N,), eta * l)
+ds = grp.create_dataset("SmoothingLength", (N,), "f")
+ds[()] = h
+h = np.zeros(1)
+
+# Internal energies
+u = -phi_grav(radius)/gamma/const_unit_velocity_in_cgs**2
+u = np.full((N,), u)
+ds = grp.create_dataset("InternalEnergy", (N,), "f")
+ds[()] = u
+u = np.zeros(1)
+
+# Particle IDs
+ids = 1 + np.linspace(0, N, N, endpoint=False)
+ds = grp.create_dataset("ParticleIDs", (N,), "L")
+ds[()] = ids
+
+file.close()
