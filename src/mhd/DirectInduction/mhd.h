610a32816b08e7036a4912fe900b6f78185f6990
nickishch <nickishch@gmail.com>
2025-03-25 18:26:07 +0100

    fixed issue, multiplied with 1/rho^3 before

--- src/mhd/DirectInduction/mhd.h
+++ src/mhd/DirectInduction/mhd.h
@@ -350,8 +350,6 @@ __attribute__((always_inline)) INLINE static void mhd_end_gradient(
   float OW;
   OW = 1.0f;
 
-  const float ker_hdnv = pow_dimension(1.0f/p->rho); /* 1/h^d */
-
   float absB;
   absB = sqrtf(B[0]*B[0]+B[1]*B[1]+B[2]*B[2]);
   float Adv_B_source[3];
@@ -373,7 +371,9 @@ __attribute__((always_inline)) INLINE static void mhd_end_gradient(
 
   Cos_Ind_Diff = (Adv_B_source[0]*Delta_B[0]+Adv_B_source[1]*Delta_B[1]+Adv_B_source[2]*Delta_B[2]);
   
-  p->mhd_data.eta_OWAR += 1.0f/OW * ( 0.5f * p->h * p->h / (absB+FLT_MIN)) * 0.5f * fmaxf(0.0f,1.0f-Cos_Ind_Diff) * Abs_Adv_B_source * (p->mass * kernel_root * ker_hdnv);
+  p->mhd_data.eta_OWAR += 1.0f/OW * ( 0.5f * p->h * p->h / (absB+FLT_MIN)) * 0.5f * fmaxf(0.0f,1.0f-Cos_Ind_Diff) * Abs_Adv_B_source * (p->mass * kernel_root);
+
+  p->mhd_data.eta_OWAR *= pow_dimension(1.0f/p->h);
 
   if (p->mhd_data.eta_OWAR<0.0f){
     error(
