fd29c2312a30cab61ba6480cfc06408103d963dc
nickishch <nickishch@gmail.com>
2025-05-11 20:56:26 +0200

    version with Dedner as diffusion

--- src/mhd/DirectInduction/mhd_iact.h
+++ src/mhd/DirectInduction/mhd_iact.h
@@ -660,8 +660,8 @@ __attribute__((always_inline)) INLINE static void runner_iact_mhd_force(
   for (int i = 0; i < 3; i++) {
     pi->mhd_data.Adv_B_source[i] += mj * dB_dt_pref_i * dB_dt_i[i];
     pj->mhd_data.Adv_B_source[i] += mi * dB_dt_pref_j * dB_dt_j[i];
-    //pi->mhd_data.Adv_B_source[i] -= mj * grad_psi * dx[i];
-    //pj->mhd_data.Adv_B_source[i] += mi * grad_psi * dx[i];
+    pi->mhd_data.Diff_B_source[i] -= mj * grad_psi * dx[i];
+    pj->mhd_data.Diff_B_source[i] += mi * grad_psi * dx[i];
     pi->mhd_data.Diff_B_source[i] += mj * resistive_eta_i * mj * dB_dt_pref_PR * dB[i];
     pj->mhd_data.Diff_B_source[i] -= mi * resistive_eta_j * mi * dB_dt_pref_PR * dB[i];
     pi->mhd_data.Diff_B_source[i] += mj * art_diff_pref_i* dB[i];
@@ -920,7 +920,7 @@ __attribute__((always_inline)) INLINE static void runner_iact_nonsym_mhd_force(
 
   for (int i = 0; i < 3; i++) {
     pi->mhd_data.Adv_B_source[i] += mj * dB_dt_pref_i * dB_dt_i[i];
-   // pi->mhd_data.Adv_B_source[i] -= mj * grad_psi * dx[i];
+    pi->mhd_data.Diff_B_source[i] -= mj * grad_psi * dx[i];
     pi->mhd_data.Diff_B_source[i] += resistive_eta_i * mj * dB_dt_pref_PR * dB[i];
     pi->mhd_data.Diff_B_source[i] += mj * art_diff_pref * dB[i];
     pi->mhd_data.Delta_B[i] += mj * dB_dt_pref_Lap * wi_dr * dB[i];
