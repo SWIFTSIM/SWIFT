3535b09c6f33b347a97702d969906ed0cf403a3c
Yolan Uyttenhove <yolan.uyttenhove@hexagon.com>
2025-05-26 15:32:34 +0200

    Add error at compile time for old gradients implementation (currently no longer supported/working)

--- src/const.h
+++ src/const.h
@@ -86,6 +86,7 @@
 /* If disabled, no gradients are used (first order scheme) */
 #define SHADOWSWIFT_MESHLESS_GRADIENTS
 #ifndef SHADOWSWIFT_MESHLESS_GRADIENTS
+#error "Unimplemented (since switch to fully recursive hydro interactions)"
 #define SHADOWSWIFT_GRADIENTS
 #endif
 /* Always activate the slope limiters if we use gradients (the scheme becomes
