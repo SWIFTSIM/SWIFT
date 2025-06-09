2aa7b59e4b10416818bf4cda4dcbc04908bac56d
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2025-06-09 14:14:19 +0200

    Applied formatting script

--- tests/test125cells.c
+++ tests/test125cells.c
@@ -111,8 +111,8 @@ void set_energy_state(struct part *part, enum pressure_field press, float size,
   part->entropy = pressure / pow_gamma(density);
 #elif defined(PHANTOM_SPH)
   part->u = pressure / (hydro_gamma_minus_one * density);
-#elif defined(MINIMAL_SPH) || defined(HOPKINS_PU_SPH) ||                     \
-    defined(HOPKINS_PU_SPH_MONAGHAN) || defined(ANARCHY_PU_SPH) ||           \
+#elif defined(MINIMAL_SPH) || defined(HOPKINS_PU_SPH) ||           \
+    defined(HOPKINS_PU_SPH_MONAGHAN) || defined(ANARCHY_PU_SPH) || \
     defined(SPHENIX_SPH) || defined(PHANTOM_SPH) || defined(GASOLINE_SPH)
   part->u = pressure / (hydro_gamma_minus_one * density);
 #elif defined(PLANETARY_SPH) || defined(REMIX_SPH)
