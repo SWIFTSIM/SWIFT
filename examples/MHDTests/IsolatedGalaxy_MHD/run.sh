59268cfdc0f5e9a6fd84d660a7fc82a84f6f7cf4
nickishch <nickishch@gmail.com>
2025-06-09 20:58:07 +0200

    fixed run.sh

--- examples/MHDTests/IsolatedGalaxy_MHD/run.sh
+++ examples/MHDTests/IsolatedGalaxy_MHD/run.sh
@@ -12,12 +12,6 @@ then
     ./getEagleCoolingTable.sh
 fi
 
-if [ ! -e UV_dust1_CR1_G1_shield1.hdf5 ]
-then
-    echo "Fetching PS2020 cooling tables for the isolated galaxy example..."
-    ./getPS2020CoolingTables.sh
-fi
-
 if [ ! -e yieldtables ] 
 then     
     echo "Fetching EAGLE stellar yield tables for the isolated galaxy example..."
@@ -27,7 +21,7 @@ fi
 if [ ! -e photometry ]
 then
     echo "Fetching EAGLE photometry tables..."
-    ../getEaglePhotometryTable.sh
+    ./getEaglePhotometryTable.sh
 fi
 
 # Adding magnetic fields.
@@ -35,7 +29,7 @@ echo "Adding magnetic fields..."
 python3 makeIC.py fid.hdf5 fid_B.hdf5 
 
 # Run SWIFT with external potential, cooling, self-gravity and star formation
-#../../../swift --external-gravity --self-gravity --hydro --cooling --stars --star-formation --threads=4 isolated_galaxy.yml 2>&1 | tee output.log
+../../../swift --external-gravity --self-gravity --hydro --cooling --stars --star-formation --threads=4 isolated_galaxy.yml 2>&1 | tee output.log
 
 # Run SWIFT with external potential, cooling, self-gravity, star formation and feedback
-../../../swift --external-gravity --self-gravity --hydro --cooling --stars --star-formation --feedback --sync --threads=4 isolated_galaxy.yml 2>&1 | tee output.log
+#../../../swift --external-gravity --self-gravity --hydro --cooling --stars --star-formation --feedback --sync --threads=4 isolated_galaxy.yml 2>&1 | tee output.log
