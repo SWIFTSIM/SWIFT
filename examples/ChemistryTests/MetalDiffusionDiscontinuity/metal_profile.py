ab24afd7716c7f8217c590bd4c9abd443826642e
Darwin Roduit <darwin.roduit@alumni.epfl.ch>
2025-06-16 12:43:34 +0200

    Remove pNbody dependency

--- examples/ChemistryTests/MetalDiffusionDiscontinuity/metal_profile.py
+++ examples/ChemistryTests/MetalDiffusionDiscontinuity/metal_profile.py
@@ -21,8 +21,6 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from pNbody import *
-from scipy.optimize import curve_fit
 from scipy.special import erf
 import argparse
 from tqdm import tqdm
@@ -30,6 +28,14 @@ import swiftsimio as sw
 
 # %%
 
+def get_fe_metal_mass(data):
+    """Get the Fe metal mass (in code units) from swiftsimio data"""
+    # Get the metal mass
+    if hasattr(data.gas.metal_mass_fractions, 'fe'):
+        m_fe = data.gas.metal_mass_fractions.fe * data.gas.masses
+    else: # This case happens when we run the simulation without --feedback
+        m_fe = data.gas.metal_mass_fractions[:, 0] * data.gas.masses
+    return m_fe
 
 def x_profile(value, x, x_min=None, x_max=None, n_bins=30):
     if x_min is None:
@@ -54,6 +60,7 @@ def x_profile(value, x, x_min=None, x_max=None, n_bins=30):
 
 
 def diffusion_erf_profile(x, t, qR, qL, kappa, x0):
+    """Analytical solution to the discontinuity diffusion problem"""
     return 0.5 * (qR + qL) + 0.5 * (qR - qL) * erf((x - x0) / np.sqrt(4 * kappa * t))
 
 # %%
@@ -91,29 +98,26 @@ log = args.log
 figsize = (6.4, 4.8)
 
 #####################
-# Calcule qR et qL une fois à partir du premier fichier
 print("Reading initial conditions from:", files[0])
-nb_init = Nbody(files[0], ptypes=[0])
-fe_init = nb_init.metals[:, 0] * nb_init.Mass(units="Msol")
-x_init = nb_init.pos[:, 0]
 
-# Domaine total
-x_domain = x_init.max() - x_init.min()
-x_mid = 0.5 * (x_init.min() + x_init.max())
-
-# Get the values. Here we assume the the min is left and max is right.
-qR = np.max(fe_init)
-qL = np.min(fe_init)
-print(f"Initial qL = {qL:.3e}, qR = {qR:.3e}")
-
-# Chargement de kappa depuis le premier fichier également
+# Open the data in the first snapshot to grab some information
 data_init = sw.load(files[0])
+
+# Read kappa from the parameter file
 try:
     kappa = float(
         data_init.metadata.parameters["GEARChemistry:diffusion_coefficient"])
-except:
-    kappa = 2.516846e-03  # Default fallback
-print(f"Using kappa = {kappa:.3e}")
+except KeyError:
+    kappa = args.kappa
+
+# Get the data
+m_fe_init = get_fe_metal_mass(data_init)
+x_init = data_init.gas.coordinates[:, 0]
+
+# Get the values. Here we assume that the the min is left and max is right.
+qR = np.max(m_fe_init)
+qL = np.min(m_fe_init)
+print(f"Initial qL = {qL:.3e}, qR = {qR:.3e}")
 
 # Get the middle of the domain
 L = data_init.metadata.boxsize[0].value
@@ -129,30 +133,25 @@ for filename in tqdm(files):
     if log:
         output_name = "log_" + output_name
 
-    nb = Nbody(filename, ptypes=[0])
-    fe = nb.metals[:, 0] * nb.Mass(units="Msol")
-    x = nb.pos[:, 0]  # Get x positions
+    # Get data
+    m_fe = get_fe_metal_mass(data)
+    x = data.gas.coordinates[:, 0]
+    t = data.metadata.time.value
 
-    x_centers, fe_bin = x_profile(fe, x, x_min, x_max, n_bins=n_bins)
+    # Do the x-axis profile
+    x_centers, fe_bin = x_profile(m_fe, x, x_min, x_max, n_bins=n_bins)
 
-    t = nb.atime  # Time of the snapshot (scale factor)
-
-    # Fit using the error function solution
-    # def fit_func(x, qL, qR):
-    #     return diffusion_erf_profile(x, t, qR, qL, kappa, x_0)
-
-    # popt, pcov = curve_fit(fit_func, x_centers, fe_bin, p0=(qL, qR))
-    # qR, qL = popt
-
+    # Compute the analytical solution
+    # Note: We do not need to fit since we have all parameters from the ICs.
     x_sol = np.linspace(x_min, x_max, 1000)
-    fe_sol = diffusion_erf_profile(x_sol, t, qR, qL, kappa, x_0)
+    m_fe_sol = diffusion_erf_profile(x_sol, t, qR, qL, kappa, x_0)
 
     # Plotting
     fig, ax = plt.subplots(figsize=figsize, layout="tight")
     ax.plot(x_centers, fe_bin, label='Fe mass profile')
-    ax.plot(x_sol, fe_sol, label='Diffusion fit (erf)', linestyle='--')
+    ax.plot(x_sol, m_fe_sol, label='Diffusion fit (erf)', linestyle='--')
     ax.set_xlabel("$x$ [kpc]")
-    ax.set_ylabel("$Fe$ [M$_\odot$]")
+    ax.set_ylabel(r"$Fe$ [M$_\odot$]")
     ax.legend()
 
     if log:
