77a15965827595ce3ad2706410f01bdf8ee43049
Katy Proctor <katy.proctor@fysik.su.se>
2025-07-10 09:21:17 +0200

    Item 1: assign SIDM to PartType7

---
+++ src/SIDM/Default/sidm_io.h
@@ -0,0 +1,138 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (c) 2025 Katy Proctor (katy.proctor@fysik.su.se)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#ifndef SWIFT_DEFAULT_SIDM_IO_H
+#define SWIFT_DEFAULT_SIDM_IO_H
+
+#include "io_properties.h"
+#include "sidm_part.h"
+
+/**
+ * @brief Specifies which si-particle fields to read from a dataset
+ *
+ * @param siparts The si-particle array.
+ * @param list The list of i/o properties to read.
+ * @param num_fields The number of i/o fields to read.
+ */
+INLINE static void sidm_read_particles(struct sipart* siparts,
+                                       struct io_props* list, int* num_fields) {
+
+  /* Say how much we want to read */
+  *num_fields = 5;
+
+  /* List what we want to read */
+  list[0] = io_make_input_field("Coordinates", DOUBLE, 3, COMPULSORY,
+                                UNIT_CONV_LENGTH, siparts, x);
+  list[1] = io_make_input_field("Velocities", FLOAT, 3, COMPULSORY,
+                                UNIT_CONV_SPEED, siparts, v);
+  list[2] = io_make_input_field("Masses", FLOAT, 1, COMPULSORY, UNIT_CONV_MASS,
+                                siparts, mass);
+  list[3] = io_make_input_field("ParticleIDs", LONGLONG, 1, COMPULSORY,
+                                UNIT_CONV_NO_UNITS, siparts, id);
+  list[4] = io_make_input_field("SmoothingLength", FLOAT, 1, OPTIONAL,
+                                UNIT_CONV_LENGTH, siparts, h);
+}
+
+INLINE static void convert_sipart_pos(const struct engine* e,
+                                    const struct sipart* sip, double* ret) {
+
+  const struct space* s = e->s;
+  if (s->periodic) {
+    ret[0] = box_wrap(sip->x[0], 0.0, s->dim[0]);
+    ret[1] = box_wrap(sip->x[1], 0.0, s->dim[1]);
+    ret[2] = box_wrap(sip->x[2], 0.0, s->dim[2]);
+  } else {
+    ret[0] = sip->x[0];
+    ret[1] = sip->x[1];
+    ret[2] = sip->x[2];
+  }
+}
+
+INLINE static void convert_sipart_vel(const struct engine* e,
+                                    const struct sipart* sip, float* ret) {
+
+  const int with_cosmology = (e->policy & engine_policy_cosmology);
+  const struct cosmology* cosmo = e->cosmology;
+  const integertime_t ti_current = e->ti_current;
+  const double time_base = e->time_base;
+
+  const integertime_t ti_beg = get_integer_time_begin(ti_current, sip->time_bin);
+  const integertime_t ti_end = get_integer_time_end(ti_current, sip->time_bin);
+
+  /* Get time-step since the last kick */
+  float dt_kick_grav;
+  if (with_cosmology) {
+    dt_kick_grav = cosmology_get_grav_kick_factor(cosmo, ti_beg, ti_current);
+    dt_kick_grav -=
+        cosmology_get_grav_kick_factor(cosmo, ti_beg, (ti_beg + ti_end) / 2);
+  } else {
+    dt_kick_grav = (ti_current - ((ti_beg + ti_end) / 2)) * time_base;
+  }
+
+  /* Extrapolate the velocites to the current time */
+  const struct gpart* gp = sip->gpart;
+  ret[0] = gp->v_full[0] + gp->a_grav[0] * dt_kick_grav;
+  ret[1] = gp->v_full[1] + gp->a_grav[1] * dt_kick_grav;
+  ret[2] = gp->v_full[2] + gp->a_grav[2] * dt_kick_grav;
+
+  /* Conversion from internal units to peculiar velocities */
+  ret[0] *= cosmo->a_inv;
+  ret[1] *= cosmo->a_inv;
+  ret[2] *= cosmo->a_inv;
+}
+
+/**
+ * @brief Specifies which si-particle fields to write to a dataset
+ *
+ * @param siparts The si-particle array.
+ * @param list The list of i/o properties to write.
+ * @param num_fields The number of i/o fields to write.
+ * @param with_cosmology Are we running a cosmological simulation?
+ * @todo Add DEBUG_INTERACTIONS at a later stage
+ */
+INLINE static void sidm_write_particles(const struct sipart* siparts,
+                                        struct io_props* list, int* num_fields,
+                                        int with_cosmology) {
+
+  /* Say how much we want to write */
+  *num_fields = 5;
+
+  /* List what we want to write */
+  list[0] = io_make_output_field_convert_sipart(
+      "Coordinates", DOUBLE, 3, UNIT_CONV_LENGTH, 1.f, siparts, convert_sipart_pos,
+      "Co-moving position of the particles");
+
+  list[1] = io_make_output_field_convert_sipart(
+      "Velocities", FLOAT, 3, UNIT_CONV_SPEED, 0.f, siparts, convert_sipart_vel,
+      "Peculiar velocities of the particles. This is a * dx/dt where x is the "
+      "co-moving position of the particles.");
+
+  list[2] = io_make_output_field("Masses", FLOAT, 1, UNIT_CONV_MASS, 0.f, siparts,
+                                 mass, "Masses of the particles");
+
+  list[3] = io_make_physical_output_field(
+      "ParticleIDs", ULONGLONG, 1, UNIT_CONV_NO_UNITS, 0.f, siparts, id,
+      /*can convert to comoving=*/0, "Unique ID of the particles");
+
+  list[4] = io_make_output_field(
+      "SmoothingLengths", FLOAT, 1, UNIT_CONV_LENGTH, 1.f, siparts, h,
+      "Co-moving smoothing lengths (FWHM of the kernel) of the particles");
+
+}
+
+#endif /* SWIFT_DEFAULT_SIDM_IO_H */
