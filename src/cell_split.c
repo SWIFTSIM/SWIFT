4cc41d03008095a936c9182b79ec60ff02b0c69d
wjr21 <wjr21@sussex.ac.uk>
2025-08-19 13:56:23 +0100

    More debug info

--- src/cell_split.c
+++ src/cell_split.c
@@ -663,10 +663,11 @@ void cell_reorder_extra_gparts(struct cell *c, struct part *parts,
             "Looking for extra particles beyond this cell's range! "
             "(cell_type/cell_subtype=%s/%s, depth=%d, "
             "c->grav.count=%d, first_not_extra=%d, space_extra_gparts=%d, "
-            "part_type=%s, i=%d",
+            "part_type=%s, i=%d, gparts[i].type=%s)",
             cellID_names[c->type], subcellID_names[c->subtype], c->depth,
             count_real, first_not_extra, space_extra_gparts,
-            part_type_names[gparts[first_not_extra].type], i);
+            part_type_names[gparts[first_not_extra].type], i,
+            part_type_names[gparts[i].type]);
 #endif
 
       /* Swap everything (including pointers) */
