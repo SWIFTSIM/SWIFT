29ca5e4ba4285f1a987f94eb207f4baf166dfe4d
Tom Sandnes <tdsandnes@gmail.com>
2025-08-19 11:00:04 +0100

    Add strength_extra.h

--- src/strength.h
+++ src/strength.h
@@ -31,6 +31,7 @@
 // Strength models
 #include "./strength/strength_artificial_stress.h"
 #include "./strength/strength_damage.h"
+#include "./strength/strength_extra.h"
 #include "./strength/strength_yield_stress_weakening.h"
 #include "./strength/strength_yield_stress.h"
 
---
+++ src/strength/strength_extra.h
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * Copyright (c) 2025 Thomas Sandnes (thomas.d.sandnes@durham.ac.uk)
+ *               2025 Jacob Kegerreis (jacob.kegerreis@durham.ac.uk)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#ifndef SWIFT_STRENGTH_EXTRA_H
+#define SWIFT_STRENGTH_EXTRA_H
+
+/**
+ * @file strength/strength_extra.h
+ * @brief Extra strength calculations that are not model-specific, but are only
+ * required with certain models.
+ */
+
+#include "math.h"
+#include "symmetric_matrix.h"
+
+
+#endif /* SWIFT_STRENGTH_EXTRA_H */
