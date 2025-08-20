b5fad631aa45e5b9e10684f7ac9125f90c1de8ef
wjr21 <wjr21@sussex.ac.uk>
2025-08-20 14:13:51 +0100

    Skip void cells in unskip

--- src/engine_unskip.c
+++ src/engine_unskip.c
@@ -322,6 +322,12 @@ void engine_do_unskip_mapper(void *map_data, int num_elements,
     const ptrdiff_t delta = &local_cells[ind] - list_base;
     const int type = delta / num_active_cells;
 
+    /* Void cells only do gravity tasks. */
+    if (c->subtype == cell_subtype_void &&
+        task_types[type] != task_broad_types_gravity) {
+      continue;
+    }
+
 #ifdef SWIFT_DEBUG_CHECKS
     if (type >= data->multiplier) error("Invalid broad task type!");
     if (c == NULL) error("Got an invalid cell index!");
