80a9f51dec9fb541e4c25c99080a1fbd5ba94f2c
wjr21 <wjr21@sussex.ac.uk>
2025-08-20 11:29:23 +0100

    Ooops

--- src/space_rebuild.c
+++ src/space_rebuild.c
@@ -994,9 +994,9 @@ void space_rebuild(struct space *s, int repartitioned, int verbose) {
 
   /* Ensure the g_index array is in ascending order */
   for (size_t k = 0; k < nr_gparts - 1; k++) {
-    if (g_index[k] >= g_index[k + 1]) {
-      error("g_index is not sorted in ascending order at index %zu: %d >= %d",
-            k, g_index[k], g_index[k + 1]);
+    if (g_index[k] > g_index[k + 1]) {
+      error("g_index is not sorted in ascending order at index %zu: %d > %d", k,
+            g_index[k], g_index[k + 1]);
     }
   }
 #endif
