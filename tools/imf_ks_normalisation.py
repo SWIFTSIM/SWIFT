2ba2a2980542878c38b86126714af191113b8d21
christopherlovell <chrisl1991@hotmail.co.uk>
2025-08-21 17:54:50 +0100

    update fsps parameters

--- tools/imf_ks_normalisation.py
+++ tools/imf_ks_normalisation.py
@@ -2,40 +2,40 @@ import numpy as np
 
 import fsps
 
+ages = np.linspace(0, 10, 100)
+
 sp = fsps.StellarPopulation(
     imf_type=0,  # Salpeter+55
-    compute_vega_mags=False,
     zcontinuous=1,
     sfh=3,
     logzsol=0.0,
     add_agb_dust_model=False,
     use_wr_spectra=False,
     imf_upper_limit=100,
-    imf_lower_limit=0.1,
+    # imf_lower_limit=0.1,
 )
 
 sp.set_tabular_sfh(
-    age=np.array([0,1,2]),
-    sfr=np.array([1,1,1]),
+    age=ages,
+    sfr=np.ones(ages.shape),
 )
 
 salpeter_mag = sp.get_mags(tage=0.1, bands=['galex_fuv'])
 
 sp = fsps.StellarPopulation(
     imf_type=1,  # Chabrier+03
-    compute_vega_mags=False,
     zcontinuous=1,
     sfh=3,
     logzsol=0.0,
     add_agb_dust_model=False,
     use_wr_spectra=False,
     imf_upper_limit=100,
-    imf_lower_limit=0.1,
+    # imf_lower_limit=0.1,
 )
 
 sp.set_tabular_sfh(
-    age=np.array([0,1,2]),
-    sfr=np.array([1,1,1]),
+    age=ages,
+    sfr=np.ones(ages.shape),
 )
 
 compare_mag = sp.get_mags(tage=0.1, bands=['galex_fuv'])
