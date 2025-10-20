d412198e1ff5f3d2fb7fb781a9436694e69af019
wjr21 <wjr21@sussex.ac.uk>
2025-10-20 15:43:58 +0200

    add times too...

--- src/cell_unskip.c
+++ src/cell_unskip.c
@@ -2035,10 +2035,13 @@ int cell_unskip_gravity_tasks(struct cell *c, struct scheduler *s) {
             error(
                 "No pack task for cell %s/%s at depth %d on rank %d (ci is "
                 "%s/%s at depth "
-                "%d on rank %d cj->grav.count=%d, ci->grav.count=%d)",
+                "%d on rank %d cj->grav.count=%d, ci->grav.count=%d, "
+                "ci->grav.ti_end_min=%ld, cj->grav.ti_end_min=%ld, "
+                "e->ti_current=%ld)",
                 cellID_names[cj->type], subcellID_names[cj->subtype], cj->depth,
                 cj_nodeID, cellID_names[ci->type], subcellID_names[ci->subtype],
-                ci->depth, ci_nodeID, cj->grav.count, ci->grav.count);
+                ci->depth, ci_nodeID, cj->grav.count, ci->grav.count,
+                ci->grav.ti_end_min, cj->grav.ti_end_min, e->ti_current);
 
           scheduler_activate_pack(s, cj->mpi.pack, task_subtype_gpart,
                                   ci_nodeID);
@@ -2075,10 +2078,13 @@ int cell_unskip_gravity_tasks(struct cell *c, struct scheduler *s) {
             error(
                 "No pack task for cell %s/%s at depth %d on rank %d (cj is "
                 "%s/%s at depth "
-                "%d on rank %d ci->grav.count=%d, cj->grav.count=%d)",
+                "%d on rank %d ci->grav.count=%d, cj->grav.count=%d, "
+                "ci->grav.ti_end_min=%ld, cj->grav.ti_end_min=%ld, "
+                "e->ti_current=%ld)",
                 cellID_names[ci->type], subcellID_names[ci->subtype], ci->depth,
                 ci_nodeID, cellID_names[cj->type], subcellID_names[cj->subtype],
-                cj->depth, cj_nodeID, ci->grav.count, cj->grav.count);
+                cj->depth, cj_nodeID, ci->grav.count, cj->grav.count,
+                ci->grav.ti_end_min, cj->grav.ti_end_min, e->ti_current);
 
           scheduler_activate_pack(s, ci->mpi.pack, task_subtype_gpart,
                                   cj_nodeID);
