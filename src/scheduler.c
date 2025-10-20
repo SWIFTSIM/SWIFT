4fad2a4a8b5cbb66bfde59c767d09fca7a3fe87f
wjr21 <wjr21@sussex.ac.uk>
2025-10-20 12:48:54 +0200

    Remove skips for debugging

--- src/scheduler.c
+++ src/scheduler.c
@@ -1746,10 +1746,10 @@ static void zoom_scheduler_splittask_gravity_void_self(struct task *t,
       /* Skip empty progeny. */
       if (ci->progeny[i] == NULL) continue;
 
-      /* Skip non-local progeny (no such thing as a foreign self task). */
-      if (ci->progeny[i]->type == cell_type_zoom &&
-          ci->progeny[i]->nodeID != engine_rank)
-        continue;
+      // /* Skip non-local progeny (no such thing as a foreign self task). */
+      // if (ci->progeny[i]->type == cell_type_zoom &&
+      //     ci->progeny[i]->nodeID != engine_rank)
+      //   continue;
 
       /* Create the self task. */
       zoom_scheduler_splittask_gravity_void_self(
@@ -1769,12 +1769,12 @@ static void zoom_scheduler_splittask_gravity_void_self(struct task *t,
         /* Skip empty progeny. */
         if (ci->progeny[k] == NULL) continue;
 
-        /* Skip entirely foreign pairs. */
-        if ((ci->progeny[j]->type == cell_type_zoom &&
-             ci->progeny[k]->type == cell_type_zoom) &&
-            ci->progeny[j]->nodeID != engine_rank &&
-            ci->progeny[k]->nodeID != engine_rank)
-          continue;
+        // /* Skip entirely foreign pairs. */
+        // if ((ci->progeny[j]->type == cell_type_zoom &&
+        //      ci->progeny[k]->type == cell_type_zoom) &&
+        //     ci->progeny[j]->nodeID != engine_rank &&
+        //     ci->progeny[k]->nodeID != engine_rank)
+        //   continue;
 
         /* Create the pair task. */
         zoom_scheduler_splittask_gravity_void_pair(
