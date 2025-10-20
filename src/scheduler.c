412719b08e1de11abc3b425c1f1b61330771ea8c
wjr21 <wjr21@sussex.ac.uk>
2025-10-20 14:02:49 +0200

    Logic typo

--- src/scheduler.c
+++ src/scheduler.c
@@ -1603,7 +1603,7 @@ static void zoom_scheduler_splittask_gravity_void_pair(struct task *t,
 #ifdef WITH_MPI
 #ifdef SWIFT_DEBUG_CHECKS
   /* Ensure we have a proxy if we are at the zoom top level. */
-  if (ci->type == cell_type_zoom && ci->subtype == cell_type_zoom) {
+  if (ci->type == cell_type_zoom && cj->type == cell_type_zoom) {
     engine_check_proxy_exists(e, ci, cj, e->nodeID);
 #endif
 #endif
