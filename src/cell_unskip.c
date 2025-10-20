57c1e3e801d8e263fbd09791bb2e6f322a07f28e
wjr21 <wjr21@sussex.ac.uk>
2025-10-21 00:54:48 +0200

    checking super

--- src/cell_unskip.c
+++ src/cell_unskip.c
@@ -2038,12 +2038,13 @@ int cell_unskip_gravity_tasks(struct cell *c, struct scheduler *s) {
                 "%s/%s at depth "
                 "%d on rank %d cj->grav.count=%d, ci->grav.count=%d, "
                 "ci->grav.ti_end_min=%lld, cj->grav.ti_end_min=%lld, "
-                "e->ti_current=%lld, cj->grav.grav=%p, cj->mpi.send=%p)",
+                "e->ti_current=%lld, cj->grav.grav=%p, cj->mpi.send=%p, "
+                "ci->grav.super=%p)",
                 cellID_names[cj->type], subcellID_names[cj->subtype], cj->depth,
                 cj_nodeID, cellID_names[ci->type], subcellID_names[ci->subtype],
                 ci->depth, ci_nodeID, cj->grav.count, ci->grav.count,
                 ci->grav.ti_end_min, cj->grav.ti_end_min, e->ti_current,
-                cj->grav.grav, cj->mpi.send);
+                cj->grav.grav, cj->mpi.send, ci->grav.super);
           }
 
           scheduler_activate_pack(s, cj->mpi.pack, task_subtype_gpart,
@@ -2084,12 +2085,13 @@ int cell_unskip_gravity_tasks(struct cell *c, struct scheduler *s) {
                 "%s/%s at depth "
                 "%d on rank %d ci->grav.count=%d, cj->grav.count=%d, "
                 "ci->grav.ti_end_min=%lld, cj->grav.ti_end_min=%lld, "
-                "e->ti_current=%lld, ci->grav.grav=%p, ci->mpi.send=%p)",
+                "e->ti_current=%lld, ci->grav.grav=%p, ci->mpi.send=%p, "
+                "cj->grav.super=%p)",
                 cellID_names[ci->type], subcellID_names[ci->subtype], ci->depth,
                 ci_nodeID, cellID_names[cj->type], subcellID_names[cj->subtype],
                 cj->depth, cj_nodeID, ci->grav.count, cj->grav.count,
                 ci->grav.ti_end_min, cj->grav.ti_end_min, e->ti_current,
-                ci->grav.grav, ci->mpi.send);
+                ci->grav.grav, ci->mpi.send, cj->grav.super);
           }
 
           scheduler_activate_pack(s, ci->mpi.pack, task_subtype_gpart,
