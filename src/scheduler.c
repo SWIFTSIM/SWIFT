4bc78ac392f3f64f70682ed3a0c41bb5d010d8b9
wjr21 <wjr21@sussex.ac.uk>
2025-10-22 11:36:55 +0200

    Debugging

--- src/scheduler.c
+++ src/scheduler.c
@@ -1986,14 +1986,11 @@ void scheduler_splittasks(struct scheduler *s, const int fof_tasks,
     for (int ind = 0; ind < s->nr_tasks; ind++) {
       struct task *t = &s->tasks[ind];
 
-      /* Skip empty tasks. */
-      if (t->type == task_type_none) continue;
+      message("Examining task %d (%s/%s).", ind, taskID_names[t->type],
+              subtaskID_names[t->subtype]);
 
       /* Skipped tasks. */
       if (t->skip) continue;
-
-      message("Examining task %d (%s/%s).", ind, taskID_names[t->type],
-              subtaskID_names[t->subtype]);
 
       /* We only care about gravity pairs and gravity MM tasks here. */
       if ((t->type != task_type_pair && t->subtype == task_subtype_grav) &&
