fa51f0b4437bf7c9a243e5fdd22ab387c03d02f4
Darwin Roduit <darwin.roduit@epfl.ch>
2025-10-27 14:31:37 +0100

    Unlock sinks and parts drift when creating particles

--- src/cell_unskip.c
+++ src/cell_unskip.c
@@ -2819,11 +2819,24 @@ int cell_unskip_sinks_tasks(struct cell *c, struct scheduler *s) {
       /* Store current values of dx_max and h_max. */
       if (t->type == task_type_self) {
         cell_activate_subcell_sinks_tasks(ci, NULL, s, with_timestep_sync);
+
+	/* Particle creation requires requires particles in thise cell to be drifted.
+	   So drift them all! */
+	cell_activate_drift_sink(ci, s);
+	cell_activate_drift_part(ci, s);
       }
 
       /* Store current values of dx_max and h_max. */
       else if (t->type == task_type_pair) {
         cell_activate_subcell_sinks_tasks(ci, cj, s, with_timestep_sync);
+
+	/* Particle creation requires requires particles in thise cell to be drifted.
+	   So drift them all! */
+        if (ci_nodeID == nodeID) cell_activate_drift_sink(ci, s);
+        if (cj_nodeID == nodeID) cell_activate_drift_part(cj, s);
+
+	if (cj_nodeID == nodeID) cell_activate_drift_sink(cj, s);
+        if (ci_nodeID == nodeID) cell_activate_drift_part(ci, s);
 
         /* Activate sinks_in for each cell that is part of
          * a pair task as to not miss any dependencies */
