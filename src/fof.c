b730b492232778290244fc3c64bc2a82c73fd61b
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2025-10-28 16:08:18 +0100

    Fix code formatting

--- src/fof.c
+++ src/fof.c
@@ -2580,7 +2580,8 @@ void fof_calc_group_mass(struct fof_props *props, const struct space *s,
       const size_t gas_index = -gparts[i].id_or_neg_offset;
       const float rho_com = hydro_get_comoving_density(&parts[gas_index]);
       max_part_density[index] = fmaxf(rho_com, max_part_density[index]);
-      star_formation_rate[index] += star_formation_get_SFR(&parts[gas_index], &xparts[gas_index]);
+      star_formation_rate[index] +=
+          star_formation_get_SFR(&parts[gas_index], &xparts[gas_index]);
       gas_mass[index] += gparts[i].mass;
     }
 
@@ -2604,8 +2605,8 @@ void fof_calc_group_mass(struct fof_props *props, const struct space *s,
                 MPI_MAX, MPI_COMM_WORLD);
   MPI_Allreduce(MPI_IN_PLACE, max_part_density, props->num_groups, MPI_FLOAT,
                 MPI_MAX, MPI_COMM_WORLD);
-  MPI_Allreduce(MPI_IN_PLACE, gas_mass, props->num_groups, MPI_FLOAT,
-                MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(MPI_IN_PLACE, gas_mass, props->num_groups, MPI_FLOAT, MPI_SUM,
+                MPI_COMM_WORLD);
   MPI_Allreduce(MPI_IN_PLACE, stellar_mass, props->num_groups, MPI_FLOAT,
                 MPI_SUM, MPI_COMM_WORLD);
   MPI_Allreduce(MPI_IN_PLACE, star_formation_rate, props->num_groups, MPI_FLOAT,
@@ -2931,7 +2932,9 @@ void fof_dump_group_data(const struct fof_props *props, const int my_rank,
               mode);
 
       if (my_rank == 0) {
-        fprintf(file, "# %8s %12s %12s %12s %24s %24s %12s %12s %12s %12s %12s %24s %24s \n",
+        fprintf(file,
+                "# %8s %12s %12s %12s %24s %24s %12s %12s %12s %12s %12s %24s "
+                "%24s \n",
                 "Group ID", "Group Size", "Group Mass", "Group Radii",
                 "Group Gas Mass", "Group Stellar Mass", "Group SFR", "CoM_x",
                 "CoM_y", "CoM_z", "Max Density", "Max Density Local Index",
@@ -2945,11 +2948,12 @@ void fof_dump_group_data(const struct fof_props *props, const int my_rank,
       for (int i = 0; i < num_groups; i++) {
 
         fprintf(file,
-                "  %8lld %12lld %12e %12e %12e %12e %12e %12e %12e %12e %12e %24lld %24lld\n",
-                group_index[i], final_group_size[i],
-                group_mass[i], group_radii[i],
-                group_gas_mass[i], group_stellar_mass[i], group_star_formation_rate[i],
-                group_centre_of_mass[i * 3 + 0], group_centre_of_mass[i * 3 + 1],
+                "  %8lld %12lld %12e %12e %12e %12e %12e %12e %12e %12e %12e "
+                "%24lld %24lld\n",
+                group_index[i], final_group_size[i], group_mass[i],
+                group_radii[i], group_gas_mass[i], group_stellar_mass[i],
+                group_star_formation_rate[i], group_centre_of_mass[i * 3 + 0],
+                group_centre_of_mass[i * 3 + 1],
                 group_centre_of_mass[i * 3 + 2], 0., -1ll, -1ll);
       }
       fclose(file);
@@ -3936,12 +3940,15 @@ void fof_compute_group_props(struct fof_props *props,
   if (swift_memalign("fof_group_gas_mass", (void **)&props->group_gas_mass, 32,
                      num_groups * sizeof(float)) != 0)
     error("Failed to allocate list of group gas mass for FOF search.");
-  if (swift_memalign("fof_group_stellar_mass", (void **)&props->group_stellar_mass, 32,
+  if (swift_memalign("fof_group_stellar_mass",
+                     (void **)&props->group_stellar_mass, 32,
                      num_groups * sizeof(float)) != 0)
     error("Failed to allocate list of group stellar mass for FOF search.");
-  if (swift_memalign("fof_group_star_formation_rate", (void **)&props->group_star_formation_rate, 32,
+  if (swift_memalign("fof_group_star_formation_rate",
+                     (void **)&props->group_star_formation_rate, 32,
                      num_groups * sizeof(float)) != 0)
-    error("Failed to allocate list of group star formation rate for FOF search.");
+    error(
+        "Failed to allocate list of group star formation rate for FOF search.");
   if (swift_memalign("fof_max_part_density", (void **)&props->max_part_density,
                      32, num_groups * sizeof(float)) != 0)
     error("Failed to allocate list of max group densities for FOF search.");
