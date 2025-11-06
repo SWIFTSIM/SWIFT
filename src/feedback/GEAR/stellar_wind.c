a6fa18fbd6de3eba6182508cdb7fba6b2f0b98e3
Zachary Yann Rey <zrey@helvetios2.helvetios.cluster>
2025-11-06 11:10:46 +0100

    Upgrade comment in stellar_wind.c

--- src/feedback/GEAR/stellar_wind.c
+++ src/feedback/GEAR/stellar_wind.c
@@ -189,7 +189,9 @@ void stellar_wind_init(struct stellar_wind *sw, struct swift_params *params,
   * @brief Restore a stellar_wind struct from the given FILE as a stream of
   * bytes.
   *
-  *
+  * @param sw The #stellar_wind model.
+  * @param params The simulation parameters.
+  * @param sm The #stellar_model.
 */
 void stellar_wind_restore(struct stellar_wind *sw, FILE *stream,
                           const struct stellar_model *sm) {
