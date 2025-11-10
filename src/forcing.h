7f8cda59ee3544a76e6901be1f1581aae6febe30
melion <elion@lorentz.leidenuniv.nl>
2025-11-10 15:30:45 +0100

    Add Balsara-Kim energy injection scheme

--- src/forcing.h
+++ src/forcing.h
@@ -36,6 +36,8 @@
 #include "./forcing/roberts_flow_acceleration/forcing.h"
 #elif defined(FORCING_ABC_FLOW)
 #include "./forcing/ABC_flow/forcing.h"
+#elif defined(FORCING_BALSARAKIM)
+#include "./forcing/BalsaraKim/forcing.h"
 #else
 #error "Invalid choice of forcing terms"
 #endif
