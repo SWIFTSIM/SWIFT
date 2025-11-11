3f05fc8686c7697f8f0ce9fbb1f1124c7841d17e
melion <elion@lorentz.leidenuniv.nl>
2025-11-11 11:30:42 +0100

    add RadiatedEnergies output to EAGLE cooling

--- src/cooling/EAGLE/cooling_io.h
+++ src/cooling/EAGLE/cooling_io.h
@@ -68,7 +68,13 @@ __attribute__((always_inline)) INLINE static int cooling_write_particles(
   list[0] = io_make_output_field_convert_part(
       "Temperatures", FLOAT, 1, UNIT_CONV_TEMPERATURE, 0.f, parts, xparts,
       convert_part_T, "Temperatures of the gas particles");
-  return 1;
+
+  list[1] = io_make_output_field(
+    "RadiatedEnergies", FLOAT, 1, UNIT_CONV_ENERGY, 
+    -3.f * hydro_gamma_minus_one, xparts, cooling_data.radiated_energy,
+    "Co-moving thermal energies radiated by the cooling mechanism");
+
+  return 2;
 }
 
 #endif /* SWIFT_COOLING_EAGLE_IO_H */
