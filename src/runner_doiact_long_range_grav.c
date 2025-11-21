0562eba748dad344ac737cc90d9b268b1afc8e72
wjr21 <wjr21@sussex.ac.uk>
2025-11-21 14:32:59 +0000

    Testing the original super condition

--- src/runner_doiact_long_range_grav.c
+++ src/runner_doiact_long_range_grav.c
@@ -234,7 +234,7 @@ void runner_count_mesh_interactions_recursive(struct cell *ci, struct cell *cpi,
   }
 
   /* Ok, recurse down but don't go further than where the tasks are. */
-  else {
+  else if (ci->grav.super != cpi) {
     for (int i = 0; i < 8; i++) {
       if (cpi->progeny[i] == NULL) continue;
       for (int j = 0; j < 8; j++) {
