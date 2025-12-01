46c33ac6e431f568371ec5fcfa712a9acb7f31f6
Darwin Roduit <darwin.roduit@epfl.ch>
2025-12-01 10:12:06 +0100

    Remove extra line

--- src/chemistry/GEAR_MF_DIFFUSION/hyperbolic/chemistry_riemann_HLL.h
+++ src/chemistry/GEAR_MF_DIFFUSION/hyperbolic/chemistry_riemann_HLL.h
@@ -19,7 +19,6 @@
 #ifndef SWIFT_CHEMISTRY_GEAR_MF_HYPERBOLIC_DIFFUSION_RIEMANN_HLL_H
 #define SWIFT_CHEMISTRY_GEAR_MF_HYPERBOLIC_DIFFUSION_RIEMANN_HLL_H
 
-
 #include "../chemistry_riemann_checks.h"
 #include "../chemistry_riemann_utils.h"
 #include "../chemistry_struct.h"
