191fd1e44e5cc53f3bb470235a43d02599ddd4f0
wjr21 <wjr21@sussex.ac.uk>
2025-12-15 16:39:32 +0000

    Apply counts to the right level

--- src/runner_doiact_long_range_grav.c
+++ src/runner_doiact_long_range_grav.c
@@ -205,7 +205,7 @@ void runner_count_mesh_interactions_recursive(struct cell *ci, struct cell *cpi,
   struct engine *e = s->e;
 
   /* Handle on the pair's gravity business. */
-  struct gravity_tensors *multi_i = ci->grav.multipole;
+  struct gravity_tensors *multi_i = cpi->grav.multipole;
   struct gravity_tensors *multi_j = cpj->grav.multipole;
 
   /* Don't allow self-interactions */
