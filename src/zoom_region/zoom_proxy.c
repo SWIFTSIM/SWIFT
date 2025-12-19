21329174670d83cc6cf5e32b07803fd14f46e95c
wjr21 <wjr21@sussex.ac.uk>
2025-12-19 15:10:37 +0000

    Mirroring the engine_proxy logic

--- src/zoom_region/zoom_proxy.c
+++ src/zoom_region/zoom_proxy.c
@@ -24,6 +24,70 @@
 #include "zoom.h"
 
 /**
+ * @brief Handle proxy creation for zoom cells inside void cells.
+ *
+ * This helper function processes the case where one or both cells are void,
+ * and we need to create proxies for the actual zoom cells nested within them.
+ *
+ * @param e The #engine.
+ * @param ci The first #cell.
+ * @param cj The second #cell.
+ * @param proxy_type The type of proxy needed.
+ * @param nodeID The current node ID.
+ */
+static void zoom_get_void_cell_proxies(struct engine *e, struct cell *ci,
+                                       struct cell *cj, const int proxy_type,
+                                       const int nodeID) {
+  const struct space *s = e->s;
+  const int ci_is_void = (ci->subtype == cell_subtype_void);
+  const int cj_is_void = (cj->subtype == cell_subtype_void);
+
+  /* Both cells are void: need to find zoom cells in both */
+  if (ci_is_void && cj_is_void) {
+    for (int zid = 0; zid < s->zoom_props->nr_zoom_cells; zid++) {
+      struct cell *zi = &s->cells_top[zid];
+      if (!cell_is_inside(zi, ci)) continue;
+
+      for (int zjd = 0; zjd < s->zoom_props->nr_zoom_cells; zjd++) {
+        struct cell *zj = &s->cells_top[zjd];
+        if (!cell_is_inside(zj, cj)) continue;
+
+        /* Early abort (both same node or both foreign) */
+        if ((zi->nodeID == nodeID && zj->nodeID == nodeID) ||
+            (zi->nodeID != nodeID && zj->nodeID != nodeID))
+          continue;
+
+        engine_add_proxy(e, zi, zj, proxy_type);
+      }
+    }
+  }
+  /* Only ci is void: find zoom cells in ci to pair with cj */
+  else if (ci_is_void) {
+    for (int zid = 0; zid < s->zoom_props->nr_zoom_cells; zid++) {
+      struct cell *zi = &s->cells_top[zid];
+      if (!cell_is_inside(zi, ci)) continue;
+
+      /* Only care about foreign zoom cells */
+      if (zi->nodeID == nodeID) continue;
+
+      engine_add_proxy(e, zi, cj, proxy_type);
+    }
+  }
+  /* Only cj is void: find zoom cells in cj to pair with ci */
+  else {
+    for (int zjd = 0; zjd < s->zoom_props->nr_zoom_cells; zjd++) {
+      struct cell *zj = &s->cells_top[zjd];
+      if (!cell_is_inside(zj, cj)) continue;
+
+      /* Only care about foreign zoom cells */
+      if (zj->nodeID == nodeID) continue;
+
+      engine_add_proxy(e, ci, zj, proxy_type);
+    }
+  }
+}
+
+/**
  * @brief Create and fill the proxies.
  *
  * This is the zoom specific form of engine_makeproxies. Unlike the uniform
@@ -55,34 +119,34 @@ void zoom_engine_makeproxies(struct engine *e) {
   for (int k = 0; k < e->nr_nodes; k++) e->proxy_ind[k] = -1;
   e->nr_proxies = 0;
 
-  /* Loop over the cells */
-  for (int cid = 0; cid < s->nr_cells; cid++) {
+  /* Loop over all cell pairs once */
+  for (int cid = s->zoom_props->bkg_cell_offset; cid < s->nr_cells; cid++) {
 
     /* Get the cell */
     struct cell *ci = &cells[cid];
 
-    /* Skip void cells */
-    if (ci->subtype == cell_subtype_void) continue;
-
-    /* Loop over the prospective neighbours. */
+    /* Loop over the prospective neighbours */
     for (int cjd = cid + 1; cjd < s->nr_cells; cjd++) {
 
       /* Get the cell */
       struct cell *cj = &cells[cjd];
 
-      /* Skip void cells */
-      if (cj->subtype == cell_subtype_void) continue;
+      /* Check if we have at least one void cell */
+      const int ci_is_void = (ci->subtype == cell_subtype_void);
+      const int cj_is_void = (cj->subtype == cell_subtype_void);
+      const int has_void = ci_is_void || cj_is_void;
 
-      /* Early abort (both same node) -> Nigel is happy */
-      if (ci->nodeID == nodeID && cj->nodeID == nodeID) continue;
+      /* For non-void pairs, apply standard node checks early */
+      if (!has_void) {
+        /* Early abort (both same node) */
+        if (ci->nodeID == nodeID && cj->nodeID == nodeID) continue;
 
-      /* Early abort (both foreign node) -> Nigel is angry */
-      if (ci->nodeID != nodeID && cj->nodeID != nodeID) continue;
-
-      /* We might need a proxy, one cell is foreign (Like Nigel and his wife).*/
+        /* Early abort (both foreign node) */
+        if (ci->nodeID != nodeID && cj->nodeID != nodeID) continue;
+      }
 
       /* Calculate the maximum distance based on the diagonal distance of the
-       * pair. */
+       * pair */
       const double ir_diag2 = ci->width[0] * ci->width[0] +
                               ci->width[1] * ci->width[1] +
                               ci->width[2] * ci->width[2];
@@ -92,141 +156,26 @@ void zoom_engine_makeproxies(struct engine *e) {
                               cj->width[2] * cj->width[2];
       const double jr_diag = 0.5 * sqrt(jr_diag2);
 
-      /* Calculate the maximum distance between the cells. */
+      /* Calculate the maximum distance between the cells */
       const double r_max = ir_diag + jr_diag;
 
-      /* Get the proxy type. We only need to do the direct check if both
-       * cells are the same type. Note, the cdim is only used if
-       * icdim == jcdim and we're doing a direct check. */
+      /* Get the proxy type */
       int proxy_type = engine_get_proxy_type(e, ci, cj, r_max);
 
       /* Abort if not in range at all */
       if (proxy_type == proxy_cell_type_none) continue;
 
-      /* Ok, we need to add a proxy. */
-      engine_add_proxy(e, ci, cj, proxy_type);
+      /* Handle void cells: delegate to zoom cells inside */
+      if (has_void) {
+        zoom_get_void_cell_proxies(e, ci, cj, proxy_type, nodeID);
+      }
+      /* Both cells are non-void: add proxy directly */
+      else {
+        engine_add_proxy(e, ci, cj, proxy_type);
+      }
     }
   }
 
-  // /* Now loop again and handle the void cell interactions. */
-  // for (int cid = s->zoom_props->bkg_cell_offset; cid < s->nr_cells; cid++) {
-  //
-  //   /* Get the cell */
-  //   struct cell *ci = &cells[cid];
-  //
-  //   /* Loop over the prospective neighbours. */
-  //   for (int cjd = cid + 1; cjd < s->nr_cells; cjd++) {
-  //
-  //     /* Get the cell */
-  //     struct cell *cj = &cells[cjd];
-  //
-  //     /* If no void cells are involved we are done. */
-  //     if (ci->subtype != cell_subtype_void && cj->subtype !=
-  //     cell_subtype_void)
-  //       continue;
-  //
-  //     /* We might need a proxy, one cell is foreign (Like Nigel and his
-  //     wife).*/
-  //
-  //     /* Calculate the maximum distance based on the diagonal distance of the
-  //      * pair. */
-  //     const double ir_diag2 = ci->width[0] * ci->width[0] +
-  //                             ci->width[1] * ci->width[1] +
-  //                             ci->width[2] * ci->width[2];
-  //     const double ir_diag = 0.5 * sqrt(ir_diag2);
-  //     const double jr_diag2 = cj->width[0] * cj->width[0] +
-  //                             cj->width[1] * cj->width[1] +
-  //                             cj->width[2] * cj->width[2];
-  //     const double jr_diag = 0.5 * sqrt(jr_diag2);
-  //
-  //     /* Calculate the maximum distance between the cells. */
-  //     const double r_max = ir_diag + jr_diag;
-  //
-  //     /* Get the proxy type. We only need to do the direct check if both
-  //      * cells are the same type. Note, the cdim is only used if
-  //      * icdim == jcdim and we're doing a direct check. */
-  //     int proxy_type = engine_get_proxy_type(e, ci, cj, r_max);
-  //
-  //     /* Abort if not in range at all */
-  //     if (proxy_type == proxy_cell_type_none) continue;
-  //
-  //     /* If we have a void cell then loop over zoom cells and find those
-  //      * inside the void cells */
-  //     if (ci->subtype == cell_subtype_void &&
-  //         cj->subtype == cell_subtype_void) {
-  //       for (int zid = 0; zid < s->zoom_props->nr_zoom_cells; zid++) {
-  //         struct cell *zi = &s->cells_top[zid];
-  //         /* Is zi inside ci? */
-  //         if (zi->loc[0] >= ci->loc[0] &&
-  //             zi->loc[0] < ci->loc[0] + ci->width[0] &&
-  //             zi->loc[1] >= ci->loc[1] &&
-  //             zi->loc[1] < ci->loc[1] + ci->width[1] &&
-  //             zi->loc[2] >= ci->loc[2] &&
-  //             zi->loc[2] < ci->loc[2] + ci->width[2]) {
-  //           /* We now need to find the zoom cells in cj */
-  //           for (int zjd = 0; zjd < s->zoom_props->nr_zoom_cells; zjd++) {
-  //             struct cell *zj = &s->cells_top[zjd];
-  //             /* Is zj inside cj? */
-  //             if (zj->loc[0] >= cj->loc[0] &&
-  //                 zj->loc[0] < cj->loc[0] + cj->width[0] &&
-  //                 zj->loc[1] >= cj->loc[1] &&
-  //                 zj->loc[1] < cj->loc[1] + cj->width[1] &&
-  //                 zj->loc[2] >= cj->loc[2] &&
-  //                 zj->loc[2] < cj->loc[2] + cj->width[2]) {
-  //
-  //               /* Early abort (both same node) -> Nigel is happy */
-  //               if (zi->nodeID == nodeID && zj->nodeID == nodeID) continue;
-  //
-  //               /* Early abort (both foreign node) -> Nigel is angry */
-  //               if (zi->nodeID != nodeID && zj->nodeID != nodeID) continue;
-  //
-  //               engine_add_proxy(e, zi, zj, proxy_type);
-  //             }
-  //           }
-  //         }
-  //       }
-  //     } else if (ci->subtype == cell_subtype_void) {
-  //       /* Only ci is void, loop over zoom cells in ci */
-  //       for (int zid = 0; zid < s->zoom_props->nr_zoom_cells; zid++) {
-  //         struct cell *zi = &s->cells_top[zid];
-  //         /* If zi inside ci? */
-  //         if (zi->loc[0] >= ci->loc[0] &&
-  //             zi->loc[0] < ci->loc[0] + ci->width[0] &&
-  //             zi->loc[1] >= ci->loc[1] &&
-  //             zi->loc[1] < ci->loc[1] + ci->width[1] &&
-  //             zi->loc[2] >= ci->loc[2] &&
-  //             zi->loc[2] < ci->loc[2] + ci->width[2]) {
-  //           /* We only care about foreign zoom cells (Nigel is disgusted). */
-  //           if (zi->nodeID == nodeID) continue;
-  //
-  //           /* Add the proxy */
-  //           engine_add_proxy(e, zi, cj, proxy_type);
-  //         }
-  //       }
-  //     } else if (cj->subtype == cell_subtype_void) {
-  //       /* Only cj is void, loop over zoom cells in cj */
-  //       for (int zjd = 0; zjd < s->zoom_props->nr_zoom_cells; zjd++) {
-  //         struct cell *zj = &s->cells_top[zjd];
-  //         /* If zj inside cj? */
-  //         if (zj->loc[0] >= cj->loc[0] &&
-  //             zj->loc[0] < cj->loc[0] + cj->width[0] &&
-  //             zj->loc[1] >= cj->loc[1] &&
-  //             zj->loc[1] < cj->loc[1] + cj->width[1] &&
-  //             zj->loc[2] >= cj->loc[2] &&
-  //             zj->loc[2] < cj->loc[2] + cj->width[2]) {
-  //           /* We only care about foreign zoom cells (Nigel is disgusted). */
-  //           if (zj->nodeID == nodeID) continue;
-  //
-  //           /* Add the proxy */
-  //           engine_add_proxy(e, ci, zj, proxy_type);
-  //         }
-  //       }
-  //     } else {
-  //       /* Ok, we need to add a proxy. */
-  //       engine_add_proxy(e, ci, cj, proxy_type);
-  //     }
-  //   }
-  // }
   /* Be clear about the time */
   if (e->verbose)
     message("took %.3f %s.", clocks_from_ticks(getticks() - tic),
