7075e7876b97e066d5ef6c7d97c88c83bbceec93
wjr21 <wjr21@sussex.ac.uk>
2025-12-30 11:40:59 +0000

    Fixing type...

--- src/zoom_region/zoom_proxy.c
+++ src/zoom_region/zoom_proxy.c
@@ -286,10 +286,10 @@ void zoom_engine_makeproxies(struct engine *e) {
     for (int k = 0; k < p->nr_cells_out; k++) {
       if (p->cells_out[k] == NULL)
         error("NULL cell in proxy output list (%d at proxy %d with type %d).",
-              k, pid, p->type);
+              k, pid, p->cells_out_type[k]);
       if (p->cells_in[k] == NULL)
         error("NULL cell in proxy input list (%d at proxy %d with type %d).", k,
-              pid, p->type);
+              pid, p->cells_out_type[k]);
       send_cell_type_pairs[num_send_cells].ci = p->cells_out[k];
       send_cell_type_pairs[num_send_cells].cj = p->cells_in[k];
       send_cell_type_pairs[num_send_cells++].type = p->cells_out_type[k];
