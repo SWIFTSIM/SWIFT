fd010490cbbc5f6af48b6fe8841490cb16effb4d
Peter W. Draper <p.w.draper@durham.ac.uk>
2026-01-20 09:45:36 +0000

    Add ci/ support scripts

---
+++ ci/gcc-modules.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#  GNU/GCC toolchain modules with serial HDF5.
+#  Source this file.
+echo "Loading the GNU/GCC toolchain for SWIFT."
+
+module purge
+module load gnu_comp/14.1.0
+module load openmpi/5.0.3
+module load gsl/2.8
+module load hdf5/1.14.4
+module load parmetis/4.0.3
+module load fftw/3.3.10
+module load utils
+module load llvm/20.1.0
+export CLANG_FORMAT_CMD="clang-format"
---
+++ ci/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#  Common setup and functions for jobs.
+
+# When exiting in error report current configuration options.
+function ONEXIT {
+   if test "$?" != 0; then
+      echo "Current configuration: $(grep "\./configure" config.log)"
+   fi
+}
+trap ONEXIT EXIT
+
+# Wrap calls to make so we can use -j <something> easily.
+# Also suppress output from libtool. Those logs are too big for gitlab.
+function do_make {
+    make -j 4 V=0 $*
+}
+
+# More chat from the scripts. Re-enable if desperate.
+#set -e
+#set -x
