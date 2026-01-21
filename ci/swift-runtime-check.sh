b52622994dd3a1cb5ecd7969031e820dc80f9cf4
Peter W. Draper <p.w.draper@durham.ac.uk>
2026-01-21 09:23:47 +0000

    Add standard license statement

--- ci/swift-runtime-check.sh
+++ ci/swift-runtime-check.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -l
 
+# This file is part of SWIFT.
+# Copyright (C) 2026 p.w.draper@durham.ac.uk.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#+
 #  Runtime checks of SWIFT. So we build the binaries and do some small checks
 #  that this works for some of the examples.
 #
@@ -7,7 +24,7 @@
 #  as as that is no longer reliable with Intel 2024 onwards.
 #
 #  Peter W. Draper 20-JAN-2026.
-
+#-
 #  Build toolchain.
 source ci/intel-modules.sh
 source ci/setup.sh
