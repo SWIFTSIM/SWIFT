9b7a5b4704a560688436ef7677b13c367e554df3
Peter W. Draper <p.w.draper@durham.ac.uk>
2026-01-26 14:06:47 +0000

    Separate COSMA specifics

---
+++ ci/COSMA/swift-gcc-check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+
+# This file is part of SWIFT.
+# Copyright (C) 2026 p.w.draper@durham.ac.uk.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#+
+#  GCC toolchain compilation and testing.
+#
+#  Peter W. Draper 20-JAN-2026.
+#-
+source ci/COSMA/gcc-modules.sh
+
+#  Checks without parallel HDF5.
+source ci/swift-checks.sh
+
+#  Checks with parallel HDF5.
+source ci/COSMA/gcc-modules-parallel.sh
+source ci/swift-checks-parallel.sh
+
+#  Checks that distribution still works.
+source ci/swift-checks-distribution.sh
+
+exit
