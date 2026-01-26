80b9788e58fa3937bf532da60b8a55a0c4b3bc33
melion <elion@lorentz.leidenuniv.nl>
2026-01-26 15:22:29 +0100

    Change forcing_injected_energy to physical output

--- src/forcing/BalsaraKim/forcing_io.h
+++ src/forcing/BalsaraKim/forcing_io.h
@@ -22,10 +22,11 @@ __attribute__((always_inline)) INLINE static int forcing_write_particles(
     const struct part *parts, const struct xpart *xparts,
     struct io_props *list) {
 
-  list[0] = io_make_output_field(
+  list[0] = io_make_physical_output_field(
       "ForcingInjectedEnergies", FLOAT, 1, UNIT_CONV_ENERGY, 
       -3.f * hydro_gamma_minus_one, xparts, forcing_data.forcing_injected_energy,
-      "Co-moving energies injected by the forcing scheme");
+      /*convertable_to_comoving=*/0,
+      "Physical energies injected by the forcing scheme");
 
   return 1;
 }
