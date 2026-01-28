f22a50812c8b13df0e360952c5a02f5b279fcee0
melion <elion@lorentz.leidenuniv.nl>
2026-01-28 15:12:42 +0100

    Remove unused forcing_part struct

--- src/forcing/BalsaraKim/forcing_struct.h
+++ src/forcing/BalsaraKim/forcing_struct.h
@@ -2,11 +2,6 @@
 #define SWIFT_FORCING_BALSARAKIM_STRUCT_H
 
 /**
- * @brief Properties of the cooling stored in the #part data.
- */
-struct forcing_part_data {};
-
-/**
  * @brief Properties of the cooling stored in the extended particle data.
  */
 struct forcing_xpart_data {
