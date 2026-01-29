651de49063f99ae8be9e003f20189b7a1f2a9aee
wjr21 <wjr21@sussex.ac.uk>
2026-01-29 15:52:25 +0000

    More info in error

--- src/runner_doiact_grav.c
+++ src/runner_doiact_grav.c
@@ -130,7 +130,12 @@ void runner_do_grav_down(struct runner *r, struct cell *c, int timer) {
     /* We can abort early if no interactions via multipole happened */
     if (!c->grav.multipole->pot.interacted) return;
 
-    if (!cell_are_gpart_drifted(c, e)) error("Un-drifted gparts");
+    if (!cell_are_gpart_drifted(c, e))
+      error(
+          "Un-drifted gparts (cell info: type=%s/%s, depth=%d, gcount=%d, "
+          "ti_old_part=%lld, e->ti_current=%lld)",
+          cellID_names[c->type], subcellID_names[c->subtype], c->depth,
+          c->grav.count, c->grav.ti_old_part, e->ti_current);
 
 #ifndef SWIFT_TASKS_WITHOUT_ATOMICS
     /* Lock the cell for the particle updates */
