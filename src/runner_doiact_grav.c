b63cbe07260007e545803965736793226589e564
wjr21 <wjr21@sussex.ac.uk>
2026-02-04 23:02:40 +0000

    More info in error

--- src/runner_doiact_grav.c
+++ src/runner_doiact_grav.c
@@ -139,9 +139,10 @@ void runner_do_grav_down(struct runner *r, struct cell *c, int timer) {
             c->top->void_parent->top->contains_zoom_cells, c->grav.super);
       error(
           "Un-drifted gparts (cell info: type=%s/%s, depth=%d, gcount=%d, "
-          "ti_old_part=%lld, e->ti_current=%lld)",
+          "ti_old_part=%lld, e->ti_current=%lld, c->top=%p, c->grav.super=%p)",
           cellID_names[c->type], subcellID_names[c->subtype], c->depth,
-          c->grav.count, c->grav.ti_old_part, e->ti_current);
+          c->grav.count, c->grav.ti_old_part, e->ti_current, c->top,
+          c->grav.super);
     }
 
 #ifndef SWIFT_TASKS_WITHOUT_ATOMICS
