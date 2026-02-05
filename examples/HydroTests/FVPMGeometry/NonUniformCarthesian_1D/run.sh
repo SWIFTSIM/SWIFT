b8d56e967dc81cdd6940ba562d7dae6400bbc558
Darwin Roduit <darwin.roduit@epfl.ch>
2026-02-05 14:20:47 +0100

    Add a run_name argument to the example run.sh script

--- examples/HydroTests/FVPMGeometry/NonUniformCarthesian_1D/run.sh
+++ examples/HydroTests/FVPMGeometry/NonUniformCarthesian_1D/run.sh
@@ -3,7 +3,8 @@
 n_threads=${n_threads:=2}  # Number of threads to use
 level=${level:=5}  # Number of particles = 2^(dimension*level)
 uniform_mode=${uniform:=0}
-dimension=${dim:=1}
+dimension=${dim:=1} # Number of dimensions of the problem.
+run_name=${run_name:=""}
 
 if [ "$uniform_mode" -eq 1 ]; then
     echo "Mode: Pure Cartesian"
@@ -39,3 +40,22 @@ mkdir "$DIR"
 
 # TODO: Add a python script
 python3 plot_solution.py snap/snapshot_0000.hdf5 snap/snapshot_0050.hdf5
+
+if [ -z "$run_name" ]; then
+    echo "run_name is empty."
+else
+    if [ -d "$run_name" ]; then
+	echo "$run_name directory exists. Nothing will be moved."
+    else
+	echo "$run_name directory does not exists. It will be created."
+	mkdir $run_name
+	mv timesteps.txt $run_name
+	mv snap $run_name
+	mv unused_parameters.yml $run_name
+	mv used_parameters.yml $run_name
+	mv *.png $run_name
+	mv output.log $run_name
+	mv statistics.txt $run_name
+    fi
+fi
+
