2180bfd0d0ed1c0ded602bed838dfa0fd2724f72
Zachary Rey <zachary.rey@epfl.ch>
2026-02-25 13:44:58 +0000

    Stellar Wind examples

--- examples/SubgridTests/StellarWindInjection/getChemistryTable.sh
+++ examples/SubgridTests/StellarWindInjection/getChemistryTable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://obswww.unige.ch/~revaz/DATA/Swift/PreSNeTables/POPII.hdf5
-wget https://obswww.unige.ch/~revaz/DATA/Swift/PreSNeTables/POPIII_PISNe.hdf5
+wget https://obswww.unige.ch/~revazy/DATA/Swift/PreSNeTables/POPII.hdf5
+wget https://obswww.unige.ch/~revazy/DATA/Swift/PreSNeTables/POPIII_PISNe.hdf5
