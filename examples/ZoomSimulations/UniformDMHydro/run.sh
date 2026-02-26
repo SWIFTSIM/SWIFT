3a031045e719aba47bef2f2b795945db0dc897af
wjr21 <wjr21@sussex.ac.uk>
2026-02-26 13:52:00 +0000

    Added hydro and EAGLE version of the toy example

---
+++ examples/ZoomSimulations/UniformDMHydro/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [ ! -e zoom_uniform_dm_hydro.hdf5 ]; then
+	echo "Generating initial conditions for UniformDMHydro..."
+	python3 makeIC.py
+fi
+
+rm -f zoom_uniform_dm_hydro_*.hdf5 statistics.txt output.log
+
+../../../swift --cosmology --hydro --self-gravity --zoom --threads=4 -n 16 zoom_uniform_dm_hydro.yml 2>&1 | tee output.log
