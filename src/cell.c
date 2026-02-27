660120cb77bfe9ebaf9d34240c218bf61442ea07
wjr21 <wjr21@sussex.ac.uk>
2026-02-27 21:22:10 +0000

    Reinstate cell_check_multipole

--- src/cell.c
+++ src/cell.c
@@ -1211,7 +1211,6 @@ void cell_check_foreign_multipole(const struct cell *c) {
  */
 void cell_check_multipole(struct cell *c,
                           const struct gravity_props *const grav_props) {
-  return;
 
 #ifdef SWIFT_DEBUG_CHECKS
   struct gravity_tensors ma;
