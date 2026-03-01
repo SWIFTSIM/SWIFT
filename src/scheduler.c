27dcb02a1d51f3690c42c422aada3831625b22c4
wjr21 <wjr21@sussex.ac.uk>
2026-03-01 20:39:25 +0000

    Adding count to error

--- src/scheduler.c
+++ src/scheduler.c
@@ -1634,9 +1634,10 @@ static void zoom_scheduler_splittask_gravity_void_pair(struct task *t,
     error(
         "zoom_scheduler_splittask_gravity_void_pair called with a non-split "
         "non-void cell: ci->split=%d ci->subtype=%s ci->nodeID=%d "
-        "cj->split=%d cj->subtype=%s cj->nodeID=%d",
-        ci->split, subcellID_names[ci->subtype], ci->nodeID, cj->split,
-        subcellID_names[cj->subtype], cj->nodeID);
+        "ci->grav.count=%lld cj->split=%d cj->subtype=%s cj->nodeID=%d "
+        "cj->grav.count=%lld",
+        ci->split, subcellID_names[ci->subtype], ci->nodeID, ci->grav.count,
+        cj->split, subcellID_names[cj->subtype], cj->nodeID, cj->grav.count);
   }
 
   /* Convert to a grav_mm/progeny task. The flags field encodes which progeny
