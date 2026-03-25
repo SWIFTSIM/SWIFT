795273f9c19bb6a107fa3cfae7450af1bd532670
wjr21 <wjr21@sussex.ac.uk>
2026-03-25 13:22:56 +0000

    Use engine_rank because we don't yet have the engine

--- src/space_regrid.c
+++ src/space_regrid.c
@@ -548,7 +548,7 @@ void space_regrid(struct space *s, int verbose) {
    * the zoom region geometry before moving on. Note that this is always
    * verbose regardless of global verbosity (but only on rank 0 in MPI land). */
   if (s->with_zoom_region && s->cells_top == NULL) {
-    zoom_region_init(s, /*regridding=*/0, /*verbose=*/s->e->nodeID == 0);
+    zoom_region_init(s, /*regridding=*/0, /*verbose=*/engine_rank == 0);
   }
 
   else if (s->with_zoom_region) {
