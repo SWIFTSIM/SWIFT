819402ab0e8ccdc73c96bb787e76ee1aab7357a8
eric-muires <eric.muires@ugent.be>
2026-04-24 14:31:09 +0200

    Another revision of logic and an additional error message to velocities.c

--- src/hydro/Shadowswift/hydro_velocities.h
+++ src/hydro/Shadowswift/hydro_velocities.h
@@ -162,8 +162,7 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
 #ifdef SHADOWSWIFT_STEER_MOTION
 
 #ifdef CENTER_OF_MASS_DENSITY
-    /* Uses the more advanced, yet highly experimental steering option
-     * Largely untested, enter at own risk */
+    /* We getting EXPERIMENTAL with it */
     float ds[3];
     ds[0] = p->geometry.center_of_mass[0];
     ds[1] = p->geometry.center_of_mass[1];
@@ -179,6 +178,10 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
 
     const float d = sqrtf(ds[0] * ds[0] + ds[1] * ds[1] + ds[2] * ds[2]);
     const float soundspeed = hydro_get_comoving_soundspeed(p);
+
+    if (d == 0.0f) {
+      error("Distance to centroid is zero! Abort steering");
+    }
 
 #ifdef SHADOWSWIFT_STEERING_FACEANGLE_FLOWS
     /* Add a correction to the velocity to move generator closer to centre of
@@ -197,77 +200,78 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
     /* Angle Steering Parameters (AREPO Defaults) */
     const float max_angle = p->geometry.max_face_angle;
     const float beta = 2.25f; // Controls how steep angle is before steering. Lower -> more cells steered. AREPO = 2.25 or 2.0
-    float f_shaping_speed = 0.5; // Steering aggressiveness factor. AREPO 0.5
+    const float f_shaping_speed = 0.5; // Steering aggressiveness factor. AREPO 0.5
     const float vchar_dt = dt > 0. ? d / dt : 0.;// Timestep based correction
     float vchar = soundspeed; // Determines cold steering corrective velocity. Default is soundspeed.
-    float prefac = 1.; // Needed for options later
+    float fac = f_shaping_speed / d;
 
-
-    /* Additionally impose that we use the timestep based steering if it is less aggressive than soundspeed steering,
-     * and if the cell is very deformed
-     * Additionally, if d = 0 or dt = 0, we enter here and prevent 1/d = NaN or inf errors
-     */
-    if ((vchar_dt < vchar) || (max_angle > 2 * beta)){
+    /* Additionally impose that we use the timestep based steering if it is less
+     * aggressive than soundspeed steering, and if the cell is very deformed */
+    if ((vchar_dt < vchar) || (max_angle > 2 * beta)) {
       vchar = vchar_dt;
-    } else {
-      if (max_angle > 0.75 * beta) {
-        /* Enter bottom two options of Weinberger 2020 Eq. 39
-         * and apply default factor f_shaping */
+    }
 
+    if (max_angle > 0.75 * beta) {
+      /* Enter bottom two options of Weinberger 2020 Eq. 39
+       * and apply default factor f_shaping */
+
+      /* Fancy settings for vchar */
 #ifdef SHADOWSWIFT_STEERING_COLD_FLOWS
-        /* Enables much more aggressive cold steering if the cell
-         * has high Mach (assumed cold) */
-        if (soundspeed <= 0.f) {
-          vchar = 0;
-        } else {
-          const float Mach_low = 5.f;
-          const float Mach_part = sqrtf(fluid_v[0] * fluid_v[0] +
-                                    fluid_v[1] * fluid_v[1] +
-                                    fluid_v[2] * fluid_v[2]) / soundspeed;
-          if (Mach_part > Mach_low) {
+      /* Enables much more aggressive cold steering if the cell
+       * has high Mach (assumed cold) */
+      if (soundspeed <= 0.f) {
+        vchar = 0;
+      } else {
+        const float Mach_low = 5.f;
+        const float Mach_part =
+            sqrtf(fluid_v[0] * fluid_v[0] + fluid_v[1] * fluid_v[1] +
+                  fluid_v[2] * fluid_v[2]) /
+            soundspeed;
+        if (Mach_part > Mach_low) {
 
-#ifdef SHADOWSWIFT_STEERING_COLD_FLOWS_GRADIENT // Apply cold steering
-            /* Controls for maximum values for steering gradient */
-            const float Mach_high = 10.f;
+#ifdef SHADOWSWIFT_STEERING_COLD_FLOWS_GRADIENT  // Apply cold steering
+          /* Controls for maximum values for steering gradient */
+          const float Mach_high = 10.f;
 
-            /* Apply a smoother steering gradient if between Mach low and high */
-            if (p->rho < 12.f) {
-              // Activate the donkey steering
-              if (Mach_part < Mach_high) {
-                vchar = hydro_velocities_steering_gradient(soundspeed, vchar_dt,
-                  Mach_low, Mach_high, Mach_part);
-              } else {
-                /* Mach is higher than max, just set to max steering */
-                vchar = vchar_dt;
-              }
+          /* Apply a smoother steering gradient if between Mach low and high */
+          if (p->rho < 12.f) {
+            // Activate the donkey steering
+            if (Mach_part < Mach_high) {
+              vchar = hydro_velocities_steering_gradient(
+                  soundspeed, vchar_dt, Mach_low, Mach_high, Mach_part);
+            } else {
+              /* Mach is higher than max, just set to max steering */
+              vchar = vchar_dt;
             }
+          }
 
 #else
-            /* Default cold steering */
-            vchar = vchar_dt;
+          /* Default cold steering */
+          vchar = vchar_dt;
 #endif
-            }
-      }
-        }
-#endif
-
-        if (max_angle < beta) {
-          /* Enter second option of Weinberger 2020 Eq. 39
-           * and apply correction*/
-          prefac = (max_angle - 0.75 * beta) / (0.25 * beta);
-
         }
       }
-    }// Else, no further factors needed to apply third option
+#endif
 
-        float fac = prefac * f_shaping_speed * vchar / d;
+      if (max_angle < beta) {
+        /* Enter second option of Weinberger 2020 Eq. 39
+         * and apply correction
+         * This can only be < 1, therefore if we do not enter this criteria
+         * it automatically applies maximum correction */
+        fac *= (max_angle - 0.75 * beta) / (0.25 * beta);
 
-        /* Apply velocity corrections */
-        v[0] += ds[0] * fac;
-        v[1] += ds[1] * fac;
-        v[2] += ds[2] * fac;
+      }  // If not, then hits third criteria.
+    } // End Angle Criteria
 
-/* Enter else correspondning strictly to SHADOWSWIFT_STEERING_FACEANGLE_FLOWS
+    /* Set characteristic speed into factor */
+    fac *= vchar;
+
+    /* Apply velocity corrections */
+    v[0] += ds[0] * fac;
+    v[1] += ds[1] * fac;
+    v[2] += ds[2] * fac;
+
+/* Enter else correspondning to SHADOWSWIFT_STEERING_FACEANGLE_FLOWS
  * being off, but SHADOWSWIFT_STEER_MOTION being on*/
 #else
 
@@ -275,9 +279,11 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
        to the centroid of their voronoi cell. */
     /* The correction term below is based on the one from Springel (2010). */
     const float R = get_radius_dimension_sphere(p->geometry.volume);
-    const float eta = 0.25f; // Roundness criterion - lower = rounder. Default 0.25
+    const float eta =
+        0.25f;  // Roundness criterion - lower = rounder. Default 0.25
     const float etaR = eta * R;
-    const float xi = 1.0f; // Corrective velocity strength - should prevent shells. Default 1.0
+    const float xi = 1.0f;  // Corrective velocity strength - should prevent
+                            // shells. Default 1.0
     /* We only apply the correction if the offset between centroid and position
        is too large, or if the distance to the nearest face is smaller than the
        distance to the centroid. */
@@ -295,7 +301,8 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
         // /* Gather all variables for cold steering criteria */
         // const float *g = xp->a_grav;
         //
-        // float Egrav = p->conserved.mass * sqrtf(g[0] * g[0] + g[1] * g[1] + g[2] * g[2]) *
+        // float Egrav = p->conserved.mass * sqrtf(g[0] * g[0] + g[1] * g[1] +
+        // g[2] * g[2]) *
         //               hydro_get_comoving_psize(p);
         //
         // const float thermal_energy = xp->u_full * p->conserved.mass;
@@ -303,16 +310,20 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
         // /* NOT SAFE, CAN have 0 soundspeed */
         // const float mach_cold = sqrtf(fluid_v[0] * fluid_v[0] +
         //                                  fluid_v[1] * fluid_v[1] +
-        //                                  fluid_v[2] * fluid_v[2]) / soundspeed;
+        //                                  fluid_v[2] * fluid_v[2]) /
+        //                                  soundspeed;
         //
-        // const float Ekin_cold = 0.5f * p->conserved.mass * (v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+        // const float Ekin_cold = 0.5f * p->conserved.mass * (v[0] * v[0] +
+        // v[1] * v[1] + v[2] * v[2]);
 
         // /* Apply cold steering if needed */
         // /* Demand cold and high mach, if very high mach always */
         // if (((thermal_energy < 1e-1 * Egrav) &&
         //           (mach_cold > 10)) || mach_cold > 50) {
-        //   // message("here! Thermal = %g, Ekin + thermal = %g, Egrav = %g, mach_cold = %g, dens = %g",
-        //   //   thermal_energy, p->timestepvars.Ekin + thermal_energy, Egrav, mach_cold, p->rho);
+        //   // message("here! Thermal = %g, Ekin + thermal = %g, Egrav = %g,
+        //   mach_cold = %g, dens = %g",
+        //   //   thermal_energy, p->timestepvars.Ekin + thermal_energy, Egrav,
+        //   mach_cold, p->rho);
         //
         //   float div_v = p->gradients.v[0][0] +
         //                p->gradients.v[1][1] +
@@ -333,28 +344,30 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
         // if ((thermal_energy < 1e-2 * (Ekin_cold + thermal_energy) &&
         //      thermal_energy > 1e-2 * Egrav) && mach_cold > 5) {
         //
-        //   message("here! Thermal = %g, Ekinmax + thermal = %g, Ekin_cold = %g, Egrav = %g, mach_cold = %g, dens = %g, mass = %g, massflux = %g",
-        //     thermal_energy, p->timestepvars.Ekin + thermal_energy, Ekin_cold, Egrav, mach_cold, p->rho, p->conserved.mass, p->flux.mass);
+        //   message("here! Thermal = %g, Ekinmax + thermal = %g, Ekin_cold =
+        //   %g, Egrav = %g, mach_cold = %g, dens = %g, mass = %g, massflux =
+        //   %g",
+        //     thermal_energy, p->timestepvars.Ekin + thermal_energy, Ekin_cold,
+        //     Egrav, mach_cold, p->rho, p->conserved.mass, p->flux.mass);
         //
         //
         //
         //   fac = fac_dt;
-        // } // Not so sure this works... Egrav term still isnt enough. Plenty of cold unperturbed gas at centres
-
+        // } // Not so sure this works... Egrav term still isnt enough. Plenty
+        // of cold unperturbed gas at centres
 
         /* Physical constants
          * Can't access properly but if we assume neutral minimum internal
          * energy initialisation, this works to get m_h * (gamma-1) / kB
          */
         const double mp_gammaminusone_over_kb =
-          hydro_properties->minimal_temperature /
+            hydro_properties->minimal_temperature /
             (hydro_properties->minimal_internal_energy *
-              hydro_properties->mu_neutral);
-
-
+             hydro_properties->mu_neutral);
 
         /* Gas properties */
-        const double T_transition = hydro_properties->hydrogen_ionization_temperature;
+        const double T_transition =
+            hydro_properties->hydrogen_ionization_temperature;
         const double mu_neutral = hydro_properties->mu_neutral;
         const double mu_ionised = hydro_properties->mu_ionised;
 
@@ -368,11 +381,9 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
         /* Are we above or below the HII -> HI transition? */
         if (T_over_mu > (T_transition + 1.) / mu_ionised) {
           temperature = T_over_mu * mu_ionised;
-        }
-        else if (T_over_mu < (T_transition - 1.) / mu_neutral) {
+        } else if (T_over_mu < (T_transition - 1.) / mu_neutral) {
           temperature = T_over_mu * mu_neutral;
-        }
-        else {
+        } else {
           temperature = T_transition;
         }
 
@@ -381,13 +392,19 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
         //   (2500.f * soundspeed * soundspeed < fluid_v[0] * fluid_v[0] +
         //                                       fluid_v[1] * fluid_v[1] +
         //                                       fluid_v[2] * fluid_v[2])) ||
-        //   ((temperature > 10 * hydro_properties->hydrogen_ionization_temperature)
+        //   ((temperature > 10 *
+        //   hydro_properties->hydrogen_ionization_temperature)
         //     && (25.f * soundspeed * soundspeed < fluid_v[0] * fluid_v[0] +
         //                                           fluid_v[1] * fluid_v[1] +
-        //                                           fluid_v[2] * fluid_v[2]))) {
+        //                                           fluid_v[2] * fluid_v[2])))
+        //                                           {
         //
-        //   // message("here! Thermal = %g, Ekinmax + thermal = %g, Ekin_cold = %g, Egrav = %g, mach_cold = %g, dens = %g, mass = %g, massflux = %g",
-        //   //   thermal_energy, p->timestepvars.Ekin + thermal_energy, Ekin_cold, Egrav, mach_cold, p->rho, p->conserved.mass, p->flux.mass);
+        //   // message("here! Thermal = %g, Ekinmax + thermal = %g, Ekin_cold =
+        //   %g, Egrav = %g, mach_cold = %g, dens = %g, mass = %g, massflux =
+        //   %g",
+        //   //   thermal_energy, p->timestepvars.Ekin + thermal_energy,
+        //   Ekin_cold, Egrav, mach_cold, p->rho, p->conserved.mass,
+        //   p->flux.mass);
         //
         //
         //
@@ -398,46 +415,52 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
         // if (((2500.f * soundspeed * soundspeed < fluid_v[0] * fluid_v[0] +
         //                                       fluid_v[1] * fluid_v[1] +
         //                                       fluid_v[2] * fluid_v[2])) ||
-        //   ((temperature > 10 * hydro_properties->hydrogen_ionization_temperature)
+        //   ((temperature > 10 *
+        //   hydro_properties->hydrogen_ionization_temperature)
         //     && (25.f * soundspeed * soundspeed < fluid_v[0] * fluid_v[0] +
         //                                           fluid_v[1] * fluid_v[1] +
-        //                                           fluid_v[2] * fluid_v[2]))) {
+        //                                           fluid_v[2] * fluid_v[2])))
+        //                                           {
         //
         //   fac = fac_dt;
         //                                           }
 
-
-
         // /* If mflux/m > 1e-2 (and low M) -> No Steering
         //  * If mflux/m < 1e-2 (and high M) -> Steering
         //  * Basically, little growth + high mach -> steer. The rest is fine */
-        // if ((( (p->flux.mass / p->conserved.mass) < 1e-3) && mach_cold > 50) ) {
+        // if ((( (p->flux.mass / p->conserved.mass) < 1e-3) && mach_cold > 50)
+        // ) {
         //
-        //   // message("here! Thermal = %g, Ekin_cold = %g, Egrav = %g, mach_cold = %g, dens = %g, mass = %g, massflux = %g, potential E = %g",
-        //   //   thermal_energy, Ekin_cold, Egrav, mach_cold, p->rho, p->conserved.mass, p->flux.mass, p->gpart->potential * p->conserved.mass);
+        //   // message("here! Thermal = %g, Ekin_cold = %g, Egrav = %g,
+        //   mach_cold = %g, dens = %g, mass = %g, massflux = %g, potential E =
+        //   %g",
+        //   //   thermal_energy, Ekin_cold, Egrav, mach_cold, p->rho,
+        //   p->conserved.mass, p->flux.mass, p->gpart->potential *
+        //   p->conserved.mass);
         //
-        //   // if (thermal_energy > 1e-5 * (p->timestepvars.Ekin + thermal_energy)) {
-        //   //   message("Ekin_max = %g, p.mach = %g,", p->timestepvars.Ekin, p->timestepvars.mach_number);
+        //   // if (thermal_energy > 1e-5 * (p->timestepvars.Ekin +
+        //   thermal_energy)) {
+        //   //   message("Ekin_max = %g, p.mach = %g,", p->timestepvars.Ekin,
+        //   p->timestepvars.mach_number);
         //   // }
         //
         //   fac = fac_dt;
         //
-        // } /* SEEMINGLY LOVES TO APPLY TO EXACT WRONG PARTICLES!, actually in tests it seems fine? IDK why it does that. In restarts from Mach50 it was good? Redo. Mach50 AND
-        // bad idea sadly SADLY THIS LEADS TO NaNs and CRASHES WITH EXPLODING SHELLS!*/
-
-
-
-
-
+        // } /* SEEMINGLY LOVES TO APPLY TO EXACT WRONG PARTICLES!, actually in
+        // tests it seems fine? IDK why it does that. In restarts from Mach50 it
+        // was good? Redo. Mach50 AND bad idea sadly SADLY THIS LEADS TO NaNs
+        // and CRASHES WITH EXPLODING SHELLS!*/
 
         // /* Apply cold steering if needed */
         // /* Demand cold and high mach, if very high mach always */
-        // if (((thermal_energy < 1e-3 * (p->timestepvars.Ekin + thermal_energy) ||
+        // if (((thermal_energy < 1e-3 * (p->timestepvars.Ekin + thermal_energy)
+        // ||
         //           thermal_energy < 1e-3 * Egrav) &&
         //           mach_cold < 8) || mach_cold > 50) {
-        //   // message("here! Thermal = %g, Ekin + thermal = %g, Egrav = %g, mach_cold = %g",
-        //   //   thermal_energy, p->timestepvars.Ekin + thermal_energy, Egrav, mach_cold);
-        //   fac = fac_dt;
+        //   // message("here! Thermal = %g, Ekin + thermal = %g, Egrav = %g,
+        //   mach_cold = %g",
+        //   //   thermal_energy, p->timestepvars.Ekin + thermal_energy, Egrav,
+        //   mach_cold); fac = fac_dt;
         //
         //           }
 
@@ -449,18 +472,15 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
         // if ((100.f * soundspeed * soundspeed < fluid_v[0] * fluid_v[0] +
         //                                          fluid_v[1] * fluid_v[1] +
         //                                          fluid_v[2] * fluid_v[2])
-        //                                          && p->gpart->potential > 0.f) {
+        //                                          && p->gpart->potential >
+        //                                          0.f) {
         //
         //   fac = fac_dt;
         // }
 
-
-
-
-
         if (2500.f * soundspeed * soundspeed < fluid_v[0] * fluid_v[0] +
-                                                 fluid_v[1] * fluid_v[1] +
-                                                 fluid_v[2] * fluid_v[2]) {
+                                                   fluid_v[1] * fluid_v[1] +
+                                                   fluid_v[2] * fluid_v[2]) {
           fac = fac_dt;
         }
 #endif
@@ -472,9 +492,8 @@ __attribute__((always_inline)) INLINE static void hydro_velocities_set(
       v[1] += ds[1] * fac;
       v[2] += ds[2] * fac;
     }
-#endif // Face Angle Flows
-#endif // Steering Motion
-
+#endif  // Face Angle Flows
+#endif  // Steering Motion
   }
 
   if (p->rho < hydro_properties->epsilon_rho) {
