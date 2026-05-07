fd75e539b83200547199e985417745c5fcea898d
wjr21 <wjr21@sussex.ac.uk>
2026-05-07 17:17:20 +0100

    Don't execute the sort loop if needless...

--- src/space.c
+++ src/space.c
@@ -549,6 +549,10 @@ void space_getcells(struct space *s, int nr_cells, struct cell **cells,
  * @param s The #space.
  */
 void space_free_buff_sort_indices(struct space *s) {
+  /* Early exit if we have no hydro or star particles, as we won't have
+   * allocated any sort arrays. */
+  if (s->nr_parts == 0 && s->nr_sparts == 0) return;
+
   for (short int tpid = 0; tpid < s->e->nr_pool_threads; ++tpid) {
     for (struct cell *finger = s->cells_sub[tpid]; finger != NULL;
          finger = finger->next) {
