3b935afedf1aed636e6a771b82b17693c01ca91c
Katy Proctor <katy.proctor@fysik.su.se>
2026-05-12 11:03:44 +0200

    move DM kernel file

--- src/kernel_dark_matter.h
+++ src/kernel_dark_matter.h
@@ -16,8 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-#ifndef SWIFT_NONE_SIDM_KERNEL_H
-#define SWIFT_NONE_SIDM_KERNEL_H
+#ifndef SWIFT_DM_KERNEL_H
+#define SWIFT_DM_KERNEL_H
 
 /**
  * @brief Kernel overlap assuming top-hat kernel.
@@ -27,6 +27,31 @@
  * @param hj Comoving smoothing-length of part*icle j.
  */
 INLINE static float sidm_kernel_overlap_tophat(const float r, const float hi,
-                                               const float hj) {}
+                                               const float hj) {
 
-#endif /* SWIFT_NONE_SIDM_KERNEL_H */
+  const float Vconst = (4.f / 3.f) * M_PI;
+  const float Vi = Vconst * hi * hi * hi;
+  const float Vj = Vconst * hj * hj * hj;
+
+  float V_overlap;
+
+  if (r >= hi + hj) {
+    return 0.f;
+  }
+
+  // sphere within sphere case
+  if (r <= fabsf(hi - hj)) {
+    const float hmin = hi < hj ? hi : hj;
+    V_overlap = Vconst * hmin * hmin * hmin;
+  } else {
+    // usual case
+    const float term = hi + hj - r;
+    V_overlap = M_PI * term * term *
+                (r * r + 2 * r * (hi + hj) - 3 * (hi - hj) * (hi - hj)) /
+                (12.f * r);
+  }
+
+  return V_overlap / (Vi * Vj);
+}
+
+#endif /* SWIFT_DM_KERNEL_H */
