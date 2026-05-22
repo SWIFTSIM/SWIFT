136c65f61c510b8ac9d7dc7f49acebcf3b9a788a
Darwin Roduit <darwin.roduit@epfl.ch>
2026-05-22 08:24:08 +0200

    Try to fix CI by adding missing quotes. Only for the quick MR

--- ci/COSMA/swift-intel-check-mr.sh
+++ ci/COSMA/swift-intel-check-mr.sh
@@ -108,7 +108,7 @@ echo
 echo "-----------------------------------------------"
 echo "Prep1 + 2 loop build (similar to EAGLE-kinetic)"
 echo "-----------------------------------------------"
-do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="-DEXTRA_STAR_LOOPS_1 -DEXTRA_STAR_LOOPS_2"
+do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="'-DEXTRA_STAR_LOOPS_1 -DEXTRA_STAR_LOOPS_2'"
 do_make
 do_make clean
 
@@ -116,7 +116,7 @@ echo
 echo "--------------------------------------------------------"
 echo "Prep2 + 3 loop build (similar to GEAR-mechanical mode 1)"
 echo "--------------------------------------------------------"
-do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="-DEXTRA_STAR_LOOPS_2 -DEXTRA_STAR_LOOPS_3"
+do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="'-DEXTRA_STAR_LOOPS_2 -DEXTRA_STAR_LOOPS_3"
 do_make
 do_make clean
 
@@ -124,7 +124,7 @@ echo
 echo "------------------------------------------------------------"
 echo "Prep2 + 3 + 4 loop build (similar to GEAR-mechanical mode 2)"
 echo "------------------------------------------------------------"
-do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="-DEXTRA_STAR_LOOPS_2 -DEXTRA_STAR_LOOPS_3 -DEXTRA_STAR_LOOPS_4"
+do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="'-DEXTRA_STAR_LOOPS_2 -DEXTRA_STAR_LOOPS_3 -DEXTRA_STAR_LOOPS_4'"
 do_make
 do_make clean
 
@@ -132,7 +132,7 @@ echo
 echo "----------------------------"
 echo "Prep1 + 2 + 3 + 4 loop build"
 echo "----------------------------"
-do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="-DEXTRA_STAR_LOOPS_1 -DEXTRA_STAR_LOOPS_2 -DEXTRA_STAR_LOOPS_3 -DEXTRA_STAR_LOOPS_4"
+do_configure --with-hydro=sphenix --disable-hand-vec --with-stars=basic CFLAGS="-DEXTRA_STAR_LOOPS_1 -DEXTRA_STAR_LOOPS_2 -DEXTRA_STAR_LOOPS_3 -DEXTRA_STAR_LOOPS_4'"
 do_make
 do_make clean
 
