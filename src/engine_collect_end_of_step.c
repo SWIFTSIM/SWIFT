SWIFTSIM/SWIFT

commit 16d59e47f90cc148e734798c02983c6c02a08b61

Add star formation logger for sinks

src/engine_collect_end_of_step.c
@@ -129,7 +129,7 @@ void engine_collect_end_of_step_mapper(void *map_data, int num_elements,
       b_updated += c->black_holes.updated;
 
       /* Check if the cell was inactive and in that case reorder the SFH */
-      if (!cell_is_starting_hydro(c, e)) {
+      if (!cell_is_starting_hydro(c, e) || !cell_is_starting_sinks(c, e)) {
         star_formation_logger_log_inactive_cell(&c->stars.sfh);
       }
 

src/engine.c
@@ -2664,7 +2664,12 @@ int engine_step(struct engine *e) {
 #endif
 
     /* Write the star formation information to the file */
-    if (e->policy & engine_policy_star_formation) {
+    const int with_sinks = (e->policy & engine_policy_sinks);
+    const int with_stars = (e->policy & engine_policy_stars);
+    const int with_star_formation = (e->policy & engine_policy_star_formation);
+    const int with_star_formation_sink = with_sinks && with_stars;
+
+    if (with_star_formation || with_star_formation_sink) {
 
       star_formation_logger_write_to_log_file(e->sfh_logger, e->time,
                                               e->cosmology->a, e->cosmology->z,
@@ -3738,7 +3743,12 @@ void engine_init(
   }
 
   /* Initialize the star formation history structure */
-  if (e->policy & engine_policy_star_formation) {
+  const int with_sinks = (e->policy & engine_policy_sinks);
+  const int with_stars = (e->policy & engine_policy_stars);
+  const int with_star_formation = (e->policy & engine_policy_star_formation);
+  const int with_star_formation_sink = with_sinks && with_stars;
+
+  if (with_star_formation || with_star_formation_sink) {
     star_formation_logger_accumulator_init(&e->sfh);
   }
 
@@ -4052,7 +4062,12 @@ void engine_clean(struct engine *e, const int fof, const int restart) {
     fclose(e->file_timesteps);
     fclose(e->file_stats);
 
-    if (e->policy & engine_policy_star_formation) {
+    const int with_sinks = (e->policy & engine_policy_sinks);
+    const int with_stars = (e->policy & engine_policy_stars);
+    const int with_star_formation = (e->policy & engine_policy_star_formation);
+    const int with_star_formation_sink = with_sinks && with_stars;
+
+    if (with_star_formation || with_star_formation_sink) {
       fclose(e->sfh_logger);
     }
 

src/engine_config.c
@@ -583,8 +583,14 @@ void engine_config(int restart, int fof, struct engine *e,
 #endif  // compiled with RT
     }
 
-    /* Initialize the SFH logger if running with star formation */
-    if (e->policy & engine_policy_star_formation) {
+    /* Initialize the SFH logger if running with star formation or star
+       formation sink */
+    const int with_sinks = (e->policy & engine_policy_sinks);
+    const int with_stars = (e->policy & engine_policy_stars);
+    const int with_star_formation = (e->policy & engine_policy_star_formation);
+    const int with_star_formation_sink = with_sinks && with_stars;
+
+    if (with_star_formation || with_star_formation_sink) {
       e->sfh_logger = fopen("SFR.txt", mode);
       if (e->sfh_logger == NULL)
         error("Could not open the file 'SFR.txt' with mode '%s'.", mode);

src/runner_others.c
@@ -218,6 +218,8 @@ void runner_do_star_formation_sink(struct runner *r, struct cell *c,
   const int with_cosmology = (e->policy & engine_policy_cosmology);
   const int with_feedback = (e->policy & engine_policy_feedback);
   const struct unit_system *restrict us = e->internal_units;
+  const double time_base = e->time_base;
+  const integertime_t ti_current = e->ti_current;
   const int current_stars_count = c->stars.count;
 
   TIMER_TIC;
@@ -229,8 +231,12 @@ void runner_do_star_formation_sink(struct runner *r, struct cell *c,
 
   /* Anything to do here? */
   if (count == 0 || !cell_is_active_sinks(c, e)) {
+    star_formation_logger_log_inactive_cell(&c->stars.sfh);
     return;
   }
+
+  /* Reset the SFR */
+  star_formation_logger_init(&c->stars.sfh);
 
   /* Recurse? */
   if (c->split) {
@@ -241,6 +247,9 @@ void runner_do_star_formation_sink(struct runner *r, struct cell *c,
 
         /* Do the recursion */
         runner_do_star_formation_sink(r, cp, 0);
+
+        /* Update current cell using child cells */
+        star_formation_logger_add(&c->stars.sfh, &cp->stars.sfh);
 
         /* Update the h_max */
         c->stars.h_max = max(c->stars.h_max, cp->stars.h_max);
@@ -257,41 +266,44 @@ void runner_do_star_formation_sink(struct runner *r, struct cell *c,
     for (int k = 0; k < count; k++) {
 
       /* Get a handle on the part. */
-      struct sink *restrict s = &sinks[k];
+      struct sink *restrict sink = &sinks[k];
 
       /* Only work on active particles */
-      if (sink_is_active(s, e)) {
+      if (sink_is_active(sink, e)) {
 
 #ifdef WITH_CSDS
         error("TODO");
 #endif
 
         /* Update the sink properties before spwaning stars */
-        sink_update_sink_properties_before_star_formation(s, e, sink_props,
+        sink_update_sink_properties_before_star_formation(sink, e, sink_props,
                                                           phys_const);
 
         /* Spawn as many stars as necessary
            - loop counter for the random seed.
            - Start by 1 as 0 is used at init (sink_copy_properties) */
         for (int star_counter = 1; sink_spawn_star(
-                 s, e, sink_props, cosmo, with_cosmology, phys_const, us);
+                 sink, e, sink_props, cosmo, with_cosmology, phys_const, us);
              star_counter++) {
 
           /* Create a new star with a mass s->target_mass */
-          struct spart *sp = cell_spawn_new_spart_from_sink(e, c, s);
+          struct spart *sp = cell_spawn_new_spart_from_sink(e, c, sink);
           if (sp == NULL)
             error("Run out of available star particles or gparts");
 
           float displacement[3] = {0.0, 0.0, 0.0};
 
           /* Copy the properties to the star particle */
-          sink_copy_properties_to_star(s, sp, e, sink_props, cosmo,
+          sink_copy_properties_to_star(sink, sp, e, sink_props, cosmo,
                                        with_cosmology, phys_const, us,
                                        displacement);
 
           sp->x_diff[0] += displacement[0];
           sp->x_diff[1] += displacement[1];
           sp->x_diff[2] += displacement[2];
+
+          /* Update the Star formation history */
+          star_formation_logger_log_new_spart(sp, &c->stars.sfh);
 
           /* Verify that we do not have too many stars in the leaf for
            * the sort task to be able to act. */
@@ -332,15 +344,38 @@ void runner_do_star_formation_sink(struct runner *r, struct cell *c,
 
           /* Update sink properties */
           sink_update_sink_properties_during_star_formation(
-              s, sp, e, sink_props, phys_const, star_counter);
+              sink, sp, e, sink_props, phys_const, star_counter);
+
         } /* Loop over the stars to spawn */
 
         /* Update the sink after star formation */
         sink_update_sink_properties_after_star_formation(
-            s, with_cosmology, cosmo, sink_props, phys_const, e->ti_current,
+            sink, with_cosmology, cosmo, sink_props, phys_const, e->ti_current,
             e->time, e->time_base);
 
-      } /* if sink_is_active */
+        /* Time-step size for this particle */
+        double dt_star;
+        if (with_cosmology) {
+          const integertime_t ti_step = get_integer_timestep(sink->time_bin);
+          const integertime_t ti_begin =
+              get_integer_time_begin(ti_current - 1, sink->time_bin);
+
+          dt_star =
+              cosmology_get_delta_time(cosmo, ti_begin, ti_begin + ti_step);
+
+        } else {
+          dt_star = get_timestep(sink->time_bin, time_base);
+        }
+
+        /* Add the SFR and SFR*dt to the SFH struct of this cell */
+        star_formation_logger_log_active_sink(sink, &c->stars.sfh, dt_star);
+
+      } else { /* if sink_is_active */
+               /* Check if the particle is not inhibited */
+        if (!sink_is_inhibited(sink, e)) {
+          star_formation_logger_log_inactive_sink(sink, &c->stars.sfh);
+        }
+      }
     } /* Loop over the particles */
   }
 

src/space_split.c
@@ -594,6 +594,9 @@ void space_split_recursive(struct space *s, struct cell *c,
 
       cell_set_sink_h_depth(&sinks[k], c);
 
+      /* Collect SFR from the particles after rebuilt */
+      star_formation_logger_log_inactive_sink(&sinks[k], &c->stars.sfh);
+
       /* Reset x_diff */
       sinks[k].x_diff[0] = 0.f;
       sinks[k].x_diff[1] = 0.f;

src/star_formation/EAGLE/star_formation_logger.h
@@ -257,4 +257,29 @@ INLINE static void star_formation_logger_log_inactive_part(
   sf->SFR_inactive += max(xp->sf_data.SFR, 0.f);
 }
 
+/**
+ * @brief Add the SFR tracer to the total active SFR of this cell
+ *
+ * Nothing to do here
+ *
+ * @param sink the #sink
+ * @param sf the SFH logger struct
+ * @param dt_star The length of the time-step in physical internal units.
+ */
+INLINE static void star_formation_logger_log_active_sink(
+    const struct sink *sink, struct star_formation_history *sf,
+    const double dt_star) {}
+
+/**
+ * @brief Add the SFR tracer to the total inactive SFR of this cell as long as
+ * the SFR tracer is larger than 0
+ *
+ * Nothing to do here
+ *
+ * @param sink the #sink
+ * @param sf the SFH logger struct
+ */
+INLINE static void star_formation_logger_log_inactive_sink(
+    const struct sink *sink, struct star_formation_history *sf) {}
+
 #endif /* SWIFT_EAGLE_STARFORMATION_LOGGER_H */

src/star_formation/GEAR/star_formation_logger.h
@@ -65,17 +65,6 @@ INLINE static void star_formation_logger_log_inactive_cell(
   /* initialize number of stars to zero*/
   sf->number_new_stars = 0;
 }
-/**
- * @brief Initialize the star formation history structure in the #engine
- *
- * @param sfh The pointer to the star formation history structure
- */
-INLINE static void star_formation_logger_init(
-    struct star_formation_history *sfh) {
-  /* Initialize the collecting SFH structure to zero */
-  sfh->new_stellar_mass = 0.f;
-  sfh->number_new_stars = 0;
-}
 
 /**
  * @brief add a star formation history struct to an other star formation history
@@ -92,6 +81,71 @@ INLINE static void star_formation_logger_add(
   /* Update the SFH structure */
   sf_update->number_new_stars += sf_add->number_new_stars;
   sf_update->new_stellar_mass += sf_add->new_stellar_mass;
+}
+
+/**
+ * @brief add a star formation history struct to an other star formation history
+ * struct
+ *
+ * @param sf_add the star formation struct which we want to add to the star
+ * formation history
+ * @param sf_update the star formation structure which we want to update
+ */
+INLINE static void star_formation_logger_add_to_accumulator(
+    struct star_formation_history_accumulator *sf_update,
+    const struct star_formation_history *sf_add) {
+
+  /* Update the SFH structure */
+  sf_update->number_new_stars = sf_add->number_new_stars;
+  sf_update->new_stellar_mass = sf_add->new_stellar_mass;
+  sf_update->total_number_stars += sf_add->number_new_stars;
+  sf_update->total_stellar_mass += sf_add->new_stellar_mass;
+}
+
+/**
+ * @brief Initialize the star formation history structure in the #engine
+ *
+ * @param sfh The pointer to the star formation history structure
+ */
+INLINE static void star_formation_logger_init(
+    struct star_formation_history *sfh) {
+  /* Initialize the collecting SFH structure to zero */
+  sfh->new_stellar_mass = 0.f;
+  sfh->number_new_stars = 0;
+}
+
+/**
+ * @brief Initialize the star formation history struct in the #engine when
+ * starting the simulation.
+ *
+ * @param sfh the star_formation_history struct we want to initialize
+ */
+INLINE static void star_formation_logger_accumulator_init(
+    struct star_formation_history_accumulator *sfh) {
+  /* Initialize all values to zero */
+  sfh->new_stellar_mass = 0.f;
+  sfh->number_new_stars = 0;
+  sfh->total_number_stars = 0;
+  sfh->total_stellar_mass = 0.f;
+}
+
+/**
+ * @brief Write the final SFH to a file
+ *
+ * @param fp The file to write to.
+ * @param time the simulation time (time since Big Bang) in internal units.
+ * @param a the scale factor.
+ * @param z the redshift.
+ * @param sf the #star_formation_history struct.
+ * @param step The time-step of the simulation.
+ */
+INLINE static void star_formation_logger_write_to_log_file(
+    FILE *fp, const double time, const double a, const double z,
+    struct star_formation_history_accumulator sf, const int step) {
+
+  fprintf(fp, "%6d %16e %12.7f %14e %14ld %14e %14ld %14e\n", step, time, a, z,
+          sf.total_number_stars, sf.total_stellar_mass, sf.number_new_stars,
+          sf.new_stellar_mass);
 }
 
 /**
@@ -143,7 +197,6 @@ INLINE static void star_formation_logger_init_log_file(
  *
  * @param p the #part
  * @param xp the #xpart
- *
  * @param sf the SFH logger struct
  * @param dt_star The length of the time-step in physical internal units.
  */
@@ -166,56 +219,28 @@ INLINE static void star_formation_logger_log_inactive_part(
     struct star_formation_history *sf) {}
 
 /**
- * @brief add a star formation history struct to an other star formation history
- * struct
+ * @brief Add the SFR tracer to the total active SFR of this cell
  *
- * @param sf_add the star formation struct which we want to add to the star
- * formation history
- * @param sf_update the star formation structure which we want to update
+ * Nothing to do here
+ *
+ * @param sink the #sink
+ * @param sf the SFH logger struct
+ * @param dt_star The length of the time-step in physical internal units.
  */
-INLINE static void star_formation_logger_add_to_accumulator(
-    struct star_formation_history_accumulator *sf_update,
-    const struct star_formation_history *sf_add) {
-
-  /* Update the SFH structure */
-  sf_update->number_new_stars = sf_add->number_new_stars;
-  sf_update->new_stellar_mass = sf_add->new_stellar_mass;
-  sf_update->total_number_stars += sf_add->number_new_stars;
-  sf_update->total_stellar_mass += sf_add->new_stellar_mass;
-}
+INLINE static void star_formation_logger_log_active_sink(
+    const struct sink *sink, struct star_formation_history *sf,
+    const double dt_star) {}
 
 /**
- * @brief Write the final SFH to a file
+ * @brief Add the SFR tracer to the total inactive SFR of this cell as long as
+ * the SFR tracer is larger than 0
  *
- * @param fp The file to write to.
- * @param time the simulation time (time since Big Bang) in internal units.
- * @param a the scale factor.
- * @param z the redshift.
- * @param sf the #star_formation_history struct.
- * @param step The time-step of the simulation.
- */
-INLINE static void star_formation_logger_write_to_log_file(
-    FILE *fp, const double time, const double a, const double z,
-    struct star_formation_history_accumulator sf, const int step) {
-
-  fprintf(fp, "%6d %16e %12.7f %14e %14ld %14e %14ld %14e\n", step, time, a, z,
-          sf.total_number_stars, sf.total_stellar_mass, sf.number_new_stars,
-          sf.new_stellar_mass);
-}
-
-/**
- * @brief Initialize the star formation history struct in the #engine when
- * starting the simulation.
+ * Nothing to do here
  *
- * @param sfh the star_formation_history struct we want to initialize
+ * @param sink the #sink
+ * @param sf the SFH logger struct
  */
-INLINE static void star_formation_logger_accumulator_init(
-    struct star_formation_history_accumulator *sfh) {
-  /* Initialize all values to zero */
-  sfh->new_stellar_mass = 0.f;
-  sfh->number_new_stars = 0;
-  sfh->total_number_stars = 0;
-  sfh->total_stellar_mass = 0.f;
-}
+INLINE static void star_formation_logger_log_inactive_sink(
+    const struct sink *sink, struct star_formation_history *sf) {}
 
 #endif /* SWIFT_GEAR_STARFORMATION_LOGGER_H */

src/star_formation/none/star_formation_logger.h
@@ -135,4 +135,29 @@ INLINE static void star_formation_logger_log_inactive_part(
     const struct part *p, const struct xpart *xp,
     struct star_formation_history *sf) {}
 
+/**
+ * @brief Add the SFR tracer to the total active SFR of this cell
+ *
+ * Nothing to do here
+ *
+ * @param sink the #sink
+ * @param sf the SFH logger struct
+ * @param dt_star The length of the time-step in physical internal units.
+ */
+INLINE static void star_formation_logger_log_active_sink(
+    const struct sink *sink, struct star_formation_history *sf,
+    const double dt_star) {}
+
+/**
+ * @brief Add the SFR tracer to the total inactive SFR of this cell as long as
+ * the SFR tracer is larger than 0
+ *
+ * Nothing to do here
+ *
+ * @param sink the #sink
+ * @param sf the SFH logger struct
+ */
+INLINE static void star_formation_logger_log_inactive_sink(
+    const struct sink *sink, struct star_formation_history *sf) {}
+
 #endif /* SWIFT_NONE_STARFORMATION_LOGGER_H */

commit 695da38f0c6998d7f1c8c5380361e26ae58e1ce4

Fix SFH wiped every step for gas star formation without sinks
The inactive-cell test used ||, so a cell counted as inactive whenever its
sink time-step was not starting. Without sinks that holds on every step
after the first, so the cell SFH was zeroed before collection and SFR.txt
stayed at zero. Regression from 16d59e47f.

src/engine_collect_end_of_step.c
@@ -129,7 +129,7 @@ void engine_collect_end_of_step_mapper(void *map_data, int num_elements,
       b_updated += c->black_holes.updated;
 
       /* Check if the cell was inactive and in that case reorder the SFH */
-      if (!cell_is_starting_hydro(c, e) || !cell_is_starting_sinks(c, e)) {
+      if (!cell_is_starting_hydro(c, e) && !cell_is_starting_sinks(c, e)) {
         star_formation_logger_log_inactive_cell(&c->stars.sfh);
       }
 