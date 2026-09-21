b9e8f35f66b5dae169641a574cc419ffe69992be
phstoot <stoot@strw.leidenuniv.nl>
2026-09-21 15:43:50 +0200

    bugfix multiple accretion

--- src/runner_ghost.c
+++ src/runner_ghost.c
@@ -998,12 +998,13 @@ void runner_do_black_holes_swallow_ghost(struct runner *r, struct cell *c,
                                      e->physical_constants, e->cosmology,
                                      e->cooling_func, e->entropy_floor, e->time,
                                      with_cosmology, dt, e->ti_current);
+        
+        /* Brute-force stellar accretion (TDE) for all active BHs in this cell. */
+        runner_do_bh_stellar_accretion(r, c, 0);
       }
     }
   }
 
-  /* Brute-force stellar accretion (TDE) for all active BHs in this cell. */
-  runner_do_bh_stellar_accretion(r, c, 0);
 
   if (timer) TIMER_TOC(timer_do_black_holes_ghost);
 }
